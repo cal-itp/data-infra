6737f5755fcb05ac784bb86ec47c37a8f853f3aa
Michael Chow <mc_al_github@fastmail.com>
2021-04-05 16:55:08 -0400

    fix: use KubernetesPodOperator on prod

--- airflow/plugins/operators.py
+++ airflow/plugins/operators.py
@@ -8,25 +8,19 @@ from calitp import is_development
 
 @wraps(KubernetesPodOperator)
 def pod_operator(*args, **kwargs):
-    # note that when in_cluster is true, cluster_name is ignored
-    if is_development():
-        in_cluster = False
-    else:
-        in_cluster = True
-
-    project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
-    cluster_name = os.environ["POD_CLUSTER_NAME"]
-    location = os.environ["POD_LOCATION"]
-
     # TODO: tune this, and add resource limits
     namespace = "default"
 
-    return GKEPodOperator(
-        *args,
-        in_cluster=in_cluster,
-        project_id=project_id,
-        location=location,
-        cluster_name=cluster_name,
-        namespace=namespace,
-        **kwargs
-    )
+    if is_development():
+        return GKEPodOperator(
+            *args,
+            in_cluster=False,
+            project_id=os.environ["GOOGLE_CLOUD_PROJECT"],
+            location=os.environ["POD_LOCATION"],
+            cluster_name=os.environ["POD_CLUSTER_NAME"],
+            namespace=namespace,
+            **kwargs
+        )
+
+    else:
+        return KubernetesPodOperator(*args, **kwargs)
