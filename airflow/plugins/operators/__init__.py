2202692ba94a8199ec4a46f50fd8db6fa41c5281
Michael Chow <mc_al_github@fastmail.com>
2021-06-11 11:56:10 -0400

    feat(airflow): operators for loading for csv, sql

--- airflow/plugins/operators/__init__.py
+++ airflow/plugins/operators/__init__.py
@@ -4,3 +4,4 @@ from .once_off_external_task_sensor import OnceOffExternalTaskSensor
 from .pod_operator import PodOperator
 from .python_taskflow_operator import PythonTaskflowOperator
 from .sql_to_warehouse_operator import SqlToWarehouseOperator
+from .csv_to_warehouse_operator import CsvToWarehouseOperator
--- airflow/plugins/operators/sql_to_warehouse_operator.py
+++ airflow/plugins/operators/sql_to_warehouse_operator.py
@@ -1,9 +1,34 @@
+from google.cloud import bigquery
+
 from airflow.models import BaseOperator
 from airflow.contrib.hooks.bigquery_hook import BigQueryHook
 from googleapiclient.errors import HttpError
 from airflow import AirflowException
 
 from calitp import format_table_name
+
+
+def sql_patch_comments(bq_client, table_name, field_comments):
+    """Patch an existing table with new column descriptions."""
+
+    tbl = bq_client.get_table(table_name)
+    old_schema = tbl.schema
+
+    # make a copy of old schema, then
+    new_schema = []
+    for col_entry in old_schema:
+        d_entry = col_entry.to_api_repr()
+        comment = field_comments.get(d_entry["name"])
+
+        if comment:
+            # convert entry to dict, change description field, then recreate
+            d_entry["description"] = comment
+
+        # fine to keep as dict, since updating table.schema can handle
+        new_schema.append(d_entry)
+
+    tbl.schema = new_schema
+    bq_client.update_table(tbl, ["schema"])
 
 
 class SqlToWarehouseOperator(BaseOperator):
@@ -15,18 +40,25 @@ class SqlToWarehouseOperator(BaseOperator):
         dst_table_name,
         bigquery_conn_id="bigquery_default",
         create_disposition="CREATE_IF_NEEDED",
+        fields=None,
         **kwargs,
     ):
         self.sql = sql
         self.dst_table_name = dst_table_name
         self.bigquery_conn_id = bigquery_conn_id
         self.create_disposition = create_disposition
+        self.fields = fields if fields is not None else {}
         super().__init__(**kwargs)
 
     def execute(self, context):
+        """Create a table based on a sql query, then patch in column descriptions."""
+
+        # create table from sql query -----------------------------------------
+
         full_table_name = format_table_name(self.dst_table_name)
         print(full_table_name)
 
+        # TODO: replace bq_hook with google.cloud.bigquery (or pybigquery)
         bq_hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id)
         conn = bq_hook.get_conn()
         cursor = conn.cursor()
@@ -54,3 +86,8 @@ class SqlToWarehouseOperator(BaseOperator):
             )
         except HttpError as err:
             raise AirflowException("BigQuery error: %s" % err.content)
+
+        # patch in comments ---------------------------------------------------
+
+        bq_client = bigquery.Client()
+        sql_patch_comments(bq_client, full_table_name, self.fields)
