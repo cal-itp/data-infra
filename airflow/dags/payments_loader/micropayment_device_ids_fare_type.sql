2a24fedf8827175bea5ae9745297ad52ebd1e6eb
Charles Costanzo <cvc5185@gmail.com>
2021-07-20 09:25:05 -0400

    fix: manually ran commit pre-check

--- airflow/dags/payments_loader/micropayment_device_ids_fare_type.sql
+++ airflow/dags/payments_loader/micropayment_device_ids_fare_type.sql
@@ -11,6 +11,6 @@ SELECT t1.charge_type,
        t1.charge_amount,
        t2.littlepay_transaction_id
 FROM `payments.micropayments` as t1
-LEFT JOIN `payments.micropayment_device_transactions` as t2 
-ON 
+LEFT JOIN `payments.micropayment_device_transactions` as t2
+ON
 t1.micropayment_id = t2.micropayment_id
--- airflow/dags/payments_loader/micropayments.sql
+++ airflow/dags/payments_loader/micropayments.sql
@@ -6,4 +6,4 @@ CREATE OR REPLACE EXTERNAL TABLE `payments.micropayments` OPTIONS (
   format = "csv",
   uris = ["gs://littlepay-data-extract-prod/mst/micropayments/*.psv"],
   field_delimiter = "|"
-) 
+)
