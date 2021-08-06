ba66365e880b2bf4c2a95871dbe2d8e53c2113e2
Natalya Diaz <natalyadiaz@Natalyas-MacBook-Pro-2.local>
2021-08-06 16:09:23 -0400

    feat:schedule-type-2-keys: changed sql script to pull from cleaned tables instead of parsing the dates each time

--- airflow/dags/gtfs_views/gtfs_schedule_calendar_key.sql
+++ airflow/dags/gtfs_views/gtfs_schedule_calendar_key.sql
@@ -1,13 +1,10 @@
 ---
 operator: operators.SqlToWarehouseOperator
-dst_table_name: "views.gtfs_schedule_type2_calendar"
+dst_table_name: "views.gtfs_schedule_type2_calendar_cleaned"
 dependencies:
   - warehouse_loaded
 ---
 
-SELECT
-  * EXCEPT(start_date, end_date),
-  PARSE_DATE("%Y%m%d",start_date) AS start_date,
-  PARSE_DATE("%Y%m%d",end_date) AS end_date,
+SELECT *,
 FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS calendar_key
-FROM `cal-itp-data-infra-staging.gtfs_schedule_type2.calendar`
+FROM `cal-itp-data-infra-staging.gtfs_schedule_type2.calendar_cleaned`
--- airflow/dags/gtfs_views/gtfs_schedule_feed_info_key.sql
+++ airflow/dags/gtfs_views/gtfs_schedule_feed_info_key.sql
@@ -1,13 +1,10 @@
 ---
 operator: operators.SqlToWarehouseOperator
-dst_table_name: "views.gtfs_schedule_type2_feed_info"
+dst_table_name: "views.gtfs_schedule_type2_feed_info_cleaned"
 dependencies:
   - warehouse_loaded
 ---
 
-SELECT
-* EXCEPT(feed_start_date, feed_end_date),
-    PARSE_DATE("%Y%m%d",feed_start_date) AS feed_start_date,
-    PARSE_DATE("%Y%m%d",feed_end_date) AS feed_end_date,
+SELECT *,
 FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS feed_info_key
-FROM `cal-itp-data-infra-staging.gtfs_schedule_type2.feed_info`
+FROM `cal-itp-data-infra-staging.gtfs_schedule_type2.feed_info_cleaned`
