283b1de702137b8bd9fca6bbdb4ffd105d19caf3
Natalya Diaz <natalyadiaz@Natalyas-MacBook-Pro-2.local>
2021-08-10 17:22:55 -0400

    feat:scheduled feeds, changes after call with michael, hashes are calculated across row rather than column

--- airflow/dags/gtfs_schedule_history2/calitp_schedule_feeds.sql
+++ airflow/dags/gtfs_schedule_history2/calitp_schedule_feeds.sql
@@ -4,34 +4,36 @@ dst_table_name: "gtfs_schedule_type2.calitp_feeds"
 dependencies:
   - merge_updates
 ---
+With
 
-With gtfs_schedule_feed_snapshot AS
-(
-    SELECT itp_id, url_number, gtfs_schedule_url, fn,
-    REGEXP_SUBSTR(fn, "\\d+[-.\\/]\\d+[-.\\/]\\d+") AS calitp_extracted_at,
-    FROM `gtfs_schedule_history.tmp_calitp_feeds`
+gtfs_schedule_feed AS (
+    SELECT itp_id, url_number, gtfs_schedule_url, agency_name
+    FROM `gtfs_schedule_history.tmp_calitp_feeds` T
 ),
-md5_hash AS (
-    SELECT *,
-    DATE(NULL) as calitp_deleted_at
-    , TO_BASE64(MD5(TO_JSON_STRING(fn))) AS calitp_hash
-     FROM gtfs_schedule_feed_snapshot
+gtfs_schedule_feed_hashed AS (
+    SELECT
+        *
+        , TO_BASE64(MD5(TO_JSON_STRING(T))) AS calitp_hash
+    FROM `gtfs_schedule_history.tmp_calitp_feeds` T
+),
+gtfs_schedule_feed_snapshot AS (
+    SELECT itp_id, url_number, gtfs_schedule_url, fn, calitp_hash,
+    REGEXP_SUBSTR(fn, "\\d+[-.\\/]\\d+[-.\\/]\\d+") AS calitp_extracted_at
+    FROM gtfs_schedule_feed_hashed
 ),
 lag_md5_hash AS (
     SELECT *
+    ,DATE(NULL) as calitp_deleted_at
     , LAG(calitp_hash)
         OVER (PARTITION BY itp_id, url_number ORDER BY calitp_extracted_at) AS prev_calitp_hash
-        #works
-        FROM md5_hash
+        FROM gtfs_schedule_feed_snapshot
 ),
 
 hash_check AS (
     SELECT *
     , coalesce(calitp_hash!=prev_calitp_hash, true) AS is_changed
-    #add something for null
     , calitp_extracted_at = MIN(calitp_extracted_at)
     OVER (PARTITION BY itp_id, url_number) AS is_first_extraction,
-    #name = "validation.json" AS is_validation
     from lag_md5_hash
 )
 
