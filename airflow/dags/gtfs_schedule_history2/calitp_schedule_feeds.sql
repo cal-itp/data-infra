7ab24db364e7652a83b34332b3f0c5c9158b949f
Natalya Diaz <natalyadiaz@Natalyas-MacBook-Pro-2.local>
2021-08-11 09:54:24 -0400

    feat:scheduled feeds, fix for is_first_extraction to populate correctly

--- airflow/dags/gtfs_schedule_history2/calitp_schedule_feeds.sql
+++ airflow/dags/gtfs_schedule_history2/calitp_schedule_feeds.sql
@@ -5,39 +5,38 @@ dependencies:
   - merge_updates
 ---
 With
-
-gtfs_schedule_feed AS (
-    SELECT itp_id, url_number, gtfs_schedule_url, agency_name
-    FROM `gtfs_schedule_history.tmp_calitp_feeds` T
-),
-gtfs_schedule_feed_hashed AS (
-    SELECT
-        *
-        , TO_BASE64(MD5(TO_JSON_STRING(T))) AS calitp_hash
-    FROM `gtfs_schedule_history.tmp_calitp_feeds` T
-),
-gtfs_schedule_feed_snapshot AS (
-    SELECT itp_id, url_number, gtfs_schedule_url, fn, calitp_hash,
-    REGEXP_SUBSTR(fn, "\\d+[-.\\/]\\d+[-.\\/]\\d+") AS calitp_extracted_at
-    FROM gtfs_schedule_feed_hashed
-),
-lag_md5_hash AS (
-    SELECT *
-    ,DATE(NULL) as calitp_deleted_at
-    , LAG(calitp_hash)
-        OVER (PARTITION BY itp_id, url_number ORDER BY calitp_extracted_at) AS prev_calitp_hash
-        FROM gtfs_schedule_feed_snapshot
-),
-
-hash_check AS (
-    SELECT *
-    , coalesce(calitp_hash!=prev_calitp_hash, true) AS is_changed
-    , calitp_extracted_at = MIN(calitp_extracted_at)
-    OVER (PARTITION BY itp_id, url_number) AS is_first_extraction,
-    from lag_md5_hash
-)
-
+    gtfs_schedule_feed AS (
+        SELECT itp_id, url_number, gtfs_schedule_url, agency_name
+        FROM `gtfs_schedule_history.tmp_calitp_feeds` T
+    ),
+    gtfs_schedule_feed_hashed AS (
+        SELECT
+            *
+            , TO_BASE64(MD5(TO_JSON_STRING(T))) AS calitp_hash
+        FROM `gtfs_schedule_history.tmp_calitp_feeds` T
+    ),
+    gtfs_schedule_feed_snapshot AS (
+        SELECT itp_id, url_number, gtfs_schedule_url, fn, calitp_hash,
+        REGEXP_SUBSTR(fn, "\\d+[-.\\/]\\d+[-.\\/]\\d+") AS calitp_extracted_at
+        FROM gtfs_schedule_feed_hashed
+    ),
+    lag_md5_hash AS (
+        SELECT *
+        ,DATE(NULL) as calitp_deleted_at
+        , LAG(calitp_hash)
+            OVER (PARTITION BY itp_id, url_number ORDER BY calitp_extracted_at) AS prev_calitp_hash
+            FROM gtfs_schedule_feed_snapshot
+    ),
+ # Determine whether file at next extraction has changed md5_hash
+ # use coalesce, so that if a file was added, it will be marked as changed
+    hash_check AS (
+        SELECT *
+        , coalesce(calitp_hash!=prev_calitp_hash, true) AS is_changed
+        , calitp_extracted_at = MIN(calitp_extracted_at)
+        OVER (PARTITION BY itp_id, url_number) AS is_first_extraction,
+        from lag_md5_hash
+    )
 SELECT * EXCEPT (calitp_deleted_at),
-LEAD (calitp_extracted_at)
-OVER (PARTITION BY itp_id ORDER BY calitp_extracted_at) AS calitp_deleted_at
-FROM hash_check
+    LEAD (calitp_extracted_at)
+    OVER (PARTITION BY itp_id, url_number ORDER BY calitp_extracted_at) AS calitp_deleted_at
+    FROM hash_check
