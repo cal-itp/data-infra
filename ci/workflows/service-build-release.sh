9100861f1d716a79f2c03f2cbfd9f0629f971d3f
James Lott <james@lottspot.com>
2021-08-24 16:07:34 -0400

    ci: build & release workflow for services

---
+++ ci/workflows/service-build-release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+#
+# Defaults
+#
+
+test "$RELEASE_ENV"             || RELEASE_ENV=local
+test "$REPO_TAG"                || REPO_TAG=$(git describe --always --tags)
+test "$BUILD_APP"               || BUILD_APP=$(awk -F/ '{ print $1 }' <<< "$REPO_TAG")
+test "$BUILD_ID"                || BUILD_ID=$(awk -F/ '{ print $2 }' <<< "$REPO_TAG")
+test "$BUILD_REPO"              || BUILD_REPO=$(git config --default '' "env.$BUILD_APP/$RELEASE_ENV.build-repo")
+test "$RELEASE_GIT_REMOTE_NAME" || RELEASE_GIT_REMOTE_NAME=$(git config --default '' "env.$RELEASE_ENV.git-remote-name")
+test "$RELEASE_GIT_REMOTE_URL"  || RELEASE_GIT_REMOTE_URL=$(git config --default '' "env.$RELEASE_ENV.git-remote-url")
+test "$KUBECONFIG"              || KUBECONFIG=$(git config --default '' "env.$RELEASE_ENV.kubeconfig")
+
+export KUBECONFIG
+
+#
+# Overrides
+#
+
+BUILD_DIR=$(git rev-parse --show-toplevel)/services/$BUILD_APP
+RELEASE_CHANNEL=$RELEASE_ENV
+RELEASE_KUBE_BASE=$(git rev-parse --show-toplevel)/kubernetes/apps/manifests/$BUILD_APP
+RELEASE_KUBE_KUSTOMIZATION=$(git rev-parse --show-toplevel)/kubernetes/apps/overlays/$BUILD_APP-release/kustomization.yaml
+RELEASE_KUBE_OVERLAY=$(git rev-parse --show-toplevel)/kubernetes/apps/overlays/$BUILD_APP-$RELEASE_ENV
+RELEASE_GIT_COMMIT_DIRTY=1
+
+#
+# Steps
+#
+
+CI_STEPS_DIR=$(git rev-parse --show-toplevel)/ci/steps
+
+CI_STEPS=(
+  validate-clean-worktree
+  build-docker-image
+  template-kustomize-image
+  release-git-branch
+  release-kube-overlay
+)
+
+for step in "${CI_STEPS[@]}"; do
+  printf 'BEGIN STEP: %s\n' "$step"
+  source "$CI_STEPS_DIR/$step.sh"
+done
