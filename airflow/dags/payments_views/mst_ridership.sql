f09f1dd70dc642a04daa3ebac63b10431faa59ff
Michael Chow <mc_al_github@fastmail.com>
2021-08-25 15:58:28 -0400

    chore(content): move payments staging table to views

--- airflow/dags/payments_views/mst_ridership.sql
+++ airflow/dags/payments_views/mst_ridership.sql
@@ -1,11 +1,10 @@
 ---
 operator: operators.SqlToWarehouseOperator
 dst_table_name: "views.mst_ridership"
-external_dependencies:
-  - payments_loader: all
 
 dependencies:
   - mst_transactions_gtfs_enhanced
+  - mst_stg_micropayment_device_ids_fare_type
 ---
 
 SELECT t1.participant_id,
@@ -26,7 +25,7 @@ SELECT t1.participant_id,
        t2.charge_type,
        t2.charge_amount
 FROM `views.mst_transactions_gtfs_enhanced` as t1
-LEFT JOIN `payments.micropayment_device_ids_fare_type` as t2
+LEFT JOIN `views.mst_stg_micropayment_device_ids_fare_type` as t2
 ON t1.littlepay_transaction_id = t2.littlepay_transaction_id
 WHERE charge_type = 'complete_variable_fare'
 OR charge_type = 'flat_fare'
