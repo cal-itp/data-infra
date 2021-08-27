ced691f89d8e876b4c0e8d53497764ab01bfa258
James Lott <james@lottspot.com>
2021-08-27 11:55:00 -0400

    chore(ci): conform git tag variable to naming conventions

--- ci/workflows/service-apply-release.sh
+++ ci/workflows/service-apply-release.sh
@@ -6,10 +6,10 @@ set -e
 #
 
 test "$RELEASE_CHANNEL"         || RELEASE_CHANNEL=$(basename "$(git symbolic-ref HEAD)")
-test "$REPO_TAG"                || REPO_TAG=$(git describe --abbrev=0)
-test "$BUILD_APP"               || BUILD_APP=$(git tag -l --format='%(contents)' "$REPO_TAG" | grep '^BUILD_APP=' | cut -d= -f2-)
-test "$BUILD_ID"                || BUILD_ID=$(git tag -l --format='%(contents)' "$REPO_TAG" | grep '^BUILD_ID=' | cut -d= -f2-)
-test "$BUILD_REPO"              || BUILD_REPO=$(git tag -l --format='%(contents)' "$REPO_TAG" | grep '^BUILD_REPO=' | cut -d= -f2-)
+test "$BUILD_GIT_TAG"           || BUILD_GIT_TAG=$(git describe --abbrev=0)
+test "$BUILD_APP"               || BUILD_APP=$(git tag -l --format='%(contents)' "$BUILD_GIT_TAG" | grep '^BUILD_APP=' | cut -d= -f2-)
+test "$BUILD_ID"                || BUILD_ID=$(git tag -l --format='%(contents)' "$BUILD_GIT_TAG" | grep '^BUILD_ID=' | cut -d= -f2-)
+test "$BUILD_REPO"              || BUILD_REPO=$(git tag -l --format='%(contents)' "$BUILD_GIT_TAG" | grep '^BUILD_REPO=' | cut -d= -f2-)
 test "$BUILD_DIR"               || BUILD_DIR=$(git rev-parse --show-toplevel)/services/$BUILD_APP
 test "$RELEASE_KUBE_OVERLAY"    || RELEASE_KUBE_OVERLAY=$(git rev-parse --show-toplevel)/kubernetes/apps/overlays/$BUILD_APP-$RELEASE_CHANNEL
 
