0e24156e2d3bfe53efe3a60ac83a77bfb2e556b7
Michael Chow <mc_al_github@fastmail.com>
2021-09-14 14:04:13 -0400

    fix(content): golden view name and correctly filter latest data

--- airflow/dags/gtfs_views/gtfs_schedule_data_feed_trip_stops_latest.sql
+++ airflow/dags/gtfs_views/gtfs_schedule_data_feed_trip_stops_latest.sql
@@ -1,6 +1,6 @@
 ---
 operator: operators.SqlToWarehouseOperator
-dst_table_name: "views.gtfs_schedule_data_feed_trip_stops"
+dst_table_name: "views.gtfs_schedule_data_feed_trip_stops_latest"
 dependencies:
   - gtfs_schedule_index_feed_trip_stops
 ---
@@ -20,3 +20,6 @@ LEFT JOIN `views.gtfs_schedule_dim_routes` Route USING (route_key)
 LEFT JOIN `views.gtfs_schedule_dim_trips` Trip USING (trip_key)
 LEFT JOIN `views.gtfs_schedule_dim_stop_times` USING (stop_time_key)
 LEFT JOIN `views.gtfs_schedule_dim_stops` Stop USING (stop_key)
+WHERE
+  Indx.calitp_extracted_at <= CURRENT_DATE()
+  AND Indx.calitp_deleted_at > CURRENT_DATE()
