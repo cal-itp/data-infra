3bafbde8a01cd9515940630f55497888b36ac3a0
Michael Chow <mc_al_github@fastmail.com>
2021-09-22 14:37:47 -0400

    refactor(content): update transitstacks.sql to use sql format

--- airflow/dags/transitstacks_views/transitstacks.sql
+++ airflow/dags/transitstacks_views/transitstacks.sql
@@ -1,23 +1,25 @@
+---
 operator: operators.SqlToWarehouseOperator
 dst_table_name: "views.transitstacks"
 external_dependencies:
   - transitstacks_loader: all
-sql: |
-  WITH
+---
 
-  provider_county AS (
+WITH
 
-    -- note that this overview sheet links to other tables, EXCEPT for its county
-    -- column, so we need to get just key columns and county out of it
-    SELECT transit_provider, itp_id, ntd_id, modes, county
-    FROM `transitstacks.overview`
+provider_county AS (
 
-  )
+  -- note that this overview sheet links to other tables, EXCEPT for its county
+  -- column, so we need to get just key columns and county out of it
+  SELECT transit_provider, itp_id, ntd_id, modes, county
+  FROM `transitstacks.overview`
 
-  SELECT *
-  FROM
-    `{{ "transitstacks.provider_info" | table }}`
-    LEFT JOIN provider_county USING(transit_provider, itp_id, ntd_id,	modes)
-    LEFT JOIN `{{ "transitstacks.fares" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
-    LEFT JOIN `{{ "transitstacks.ntd_finances" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
-    LEFT JOIN `{{ "transitstacks.ntd_stats" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
+)
+
+SELECT *
+FROM
+  `{{ "transitstacks.provider_info" | table }}`
+  LEFT JOIN provider_county USING(transit_provider, itp_id, ntd_id,	modes)
+  LEFT JOIN `{{ "transitstacks.fares" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
+  LEFT JOIN `{{ "transitstacks.ntd_finances" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
+  LEFT JOIN `{{ "transitstacks.ntd_stats" | table }}` USING(transit_provider, itp_id, ntd_id,	modes)
