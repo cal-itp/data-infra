aaa1c8141dae48a8f8000c3501f888358f0c7afe
Mjumbe Poe <mjumbewu@gmail.com>
2021-10-01 11:44:19 -0400

    Rename the provider_name arg to aws_user
    In some instance (like cal-itp) more than one provider's information 
    comes through the same AWS user. This rename is just to make that fact 
    more transparent.

--- airflow/dags/payments_loader/preprocess_calitp.py
+++ airflow/dags/payments_loader/preprocess_calitp.py
@@ -10,4 +10,4 @@ from libs.littlepay import preprocess_littlepay_provider_bucket
 
 
 def main(execution_date, **kwargs):
-    preprocess_littlepay_provider_bucket(execution_date, provider_name="cal-itp")
+    preprocess_littlepay_provider_bucket(execution_date, aws_user="cal-itp")
--- airflow/dags/payments_loader/preprocess_mst.py
+++ airflow/dags/payments_loader/preprocess_mst.py
@@ -10,4 +10,4 @@ from libs.littlepay import preprocess_littlepay_provider_bucket
 
 
 def main(execution_date, **kwargs):
-    preprocess_littlepay_provider_bucket(execution_date, provider_name="mst")
+    preprocess_littlepay_provider_bucket(execution_date, aws_user="mst")
