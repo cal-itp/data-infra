a47b75977a3ed385ef9a08f135a7b61f2f5d43d9
Mjumbe Poe <mjumbewu@gmail.com>
2021-10-01 11:39:04 -0400

    Split preprocess step into separate tasks for each source
    Preprocessing one source takes enough time that it makes sense to have 
    all the processes run in parallel, especially for testing.

--- airflow/plugins/libs/littlepay.py
+++ airflow/plugins/libs/littlepay.py
@@ -1,11 +1,3 @@
-# ---
-# python_callable: main
-# provide_context: true
-# dependencies:
-#   - calitp_included_payments_tables
-# ---
-
-
 from calitp.config import get_bucket
 from calitp.storage import get_fs
 from calitp.sql import get_table
@@ -96,8 +88,3 @@ def preprocess_littlepay_provider_bucket(
                 extracted_at=date_string,
                 delimiter="|",
             )
-
-
-def main(execution_date, **kwargs):
-    preprocess_littlepay_provider_bucket(execution_date, provider_name="mst")
-    preprocess_littlepay_provider_bucket(execution_date, provider_name="sbmtd")
