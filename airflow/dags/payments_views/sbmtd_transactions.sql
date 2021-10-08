9092bf9e6651ede70ef2ddeb629fa0f4e5cdefc5
Mjumbe Poe <mjumbewu@gmail.com>
2021-10-08 12:40:50 -0400

    fix: Include join on calitp_url_number as well
    gtfs_schedule data should be grouped by the combination of calitp_itp_id
    and calitp_url_number as a composite key. calitp_itp_id alone may not be
    unique.

--- airflow/dags/payments_views/sbmtd_transactions.sql
+++ airflow/dags/payments_views/sbmtd_transactions.sql
@@ -15,9 +15,10 @@ route_stops as (
         route_id,
         stop_id
     from gtfs_schedule.stop_times as st
-    join gtfs_schedule.trips as t using (calitp_itp_id, trip_id)
-    join gtfs_schedule.routes as r using (calitp_itp_id, route_id)
+    join gtfs_schedule.trips as t using (calitp_itp_id, calitp_url_number, trip_id)
+    join gtfs_schedule.routes as r using (calitp_itp_id, calitp_url_number, route_id)
     where calitp_itp_id = 293
+    and calitp_url_number = 0
 ),
 
 /*
@@ -62,10 +63,8 @@ transactions as (
     from payments.micropayments as m
     join payments.micropayment_device_transactions using (micropayment_id)
     join payments.device_transactions as dt using (littlepay_transaction_id, customer_id, participant_id)
-    join gtfs_schedule.stops as s on (s.stop_id = dt.location_id)
-    join stop_stats as stat using (stop_id)
+    join stop_stats as stat on stat.stop_id = dt.location_id
     where participant_id = 'sbmtd'
-    and s.calitp_itp_id = 293
 )
 
 select
@@ -82,5 +81,7 @@ select
     t.transaction_date_time_utc
 from transactions as t
 left join gtfs_schedule.routes as r
-    on r.calitp_itp_id = 293 and r.route_id = t.likely_route_id
+    on r.calitp_itp_id = 293
+        and r.calitp_url_number = 0
+        and r.route_id = t.likely_route_id
 order by t.likely_route_id nulls last
