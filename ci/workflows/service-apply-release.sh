ff44c678c77adddbede1691ec68d89f408d5842d
James Lott <james.l@jarv.us>
2021-10-14 15:47:31 -0400

    feat(ci/service-apply-release): decouple releases from git metadata of builds
    There are substantial drawbacks of tightly coupling releases to the git
    tags and notes of their associated builds:
    
    1. This method requires that release files be populated with the same
     version information which is to be encoded in the git tag prior to the
     tag's creation. This effectively forces builds to be tagged by a custom
     script rather than allowing the use of the familiar `git tag -a`
     interface by operators.
    2. The release workflow becomes clunky for cluster services which don't
     use custom image builds (e.g., metabase or jupyterhub).

--- ci/workflows/service-apply-release.sh
+++ ci/workflows/service-apply-release.sh
@@ -25,11 +25,16 @@ done
 test "$CI_STEPS_DIR" || CI_STEPS_DIR=$(git rev-parse --show-toplevel)/ci/steps
 
 #
+# Required
+#
+
+test "$BUILD_APP"               || { printf 'BUILD_APP: '; read BUILD_APP; }
+
+#
 # Defaults
 #
 
 test "$RELEASE_CHANNEL"         || RELEASE_CHANNEL=$(basename "$(git symbolic-ref HEAD)")
-test "$BUILD_GIT_TAG"           || BUILD_GIT_TAG=$(git describe --abbrev=0)
 test "$RELEASE_KUBE_OVERLAY"    || RELEASE_KUBE_OVERLAY=$(git rev-parse --show-toplevel)/kubernetes/apps/overlays/$BUILD_APP-$RELEASE_CHANNEL
 
 #
@@ -45,15 +50,6 @@ export KUBECONFIG
 #
 # Steps
 #
-
-printf 'BEGIN STEP: validate-build-git-tag\n'
-source "$CI_STEPS_DIR/validate-build-git-tag.sh"
-
-printf 'BEGIN STEP: configure-git-remote\n'
-source "$CI_STEPS_DIR/configure-git-remote.sh"
-
-printf 'BEGIN STEP: configure-build-git-notes\n'
-source "$CI_STEPS_DIR/configure-build-git-notes.sh"
 
 printf 'BEGIN STEP: release-kube-overlay\n'
 source "$CI_STEPS_DIR/release-kube-overlay.sh"
