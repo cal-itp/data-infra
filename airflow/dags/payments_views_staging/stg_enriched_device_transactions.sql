c72cf1655b09e6f9e97710489bd67458cda152da
Michael Chow <mc_al_github@fastmail.com>
2021-10-26 10:52:01 -0400

    feat(payments): macro for marking duplicates in payments

---
+++ airflow/dags/payments_views_staging/stg_enriched_device_transactions.sql
@@ -0,0 +1,17 @@
+---
+operator: operators.SqlToWarehouseOperator
+dst_table_name: "payments.stg_enriched_device_transactions"
+
+external_dependencies:
+  - payments_loader: all
+---
+
+{{
+
+  sql_enrich_duplicates(
+    "payments.device_transactions",
+    ["littlepay_transaction_id"],
+    ["transaction_date_time_utc"]
+  )
+
+}}
