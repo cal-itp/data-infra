312de43e838c0eeb2cafc2cacaba67c42a0f4331
Michael Chow <mc_al_github@fastmail.com>
2021-11-22 11:15:40 -0600

    fix(reports): reports index was filtering those without feed info

--- airflow/dags/gtfs_views/reports_gtfs_schedule_index.sql
+++ airflow/dags/gtfs_views/reports_gtfs_schedule_index.sql
@@ -62,9 +62,13 @@ agency_feeds_on_end_date AS (
         , S.itp_id AS calitp_itp_id
         , S.url_number AS calitp_url_number
         , S.agency_name
+        , COALESCE(FI.has_feed_info, FALSE)
+            AS has_feed_info
     FROM `gtfs_schedule_history.calitp_status` S
     JOIN publish_dates_crnt PD ON
         S.calitp_extracted_at = PD.date_end
+    LEFT JOIN has_feed_info_end_date FI
+      USING(calitp_itp_id, calitp_url_number, publish_date)      
 )
 
 SELECT
@@ -75,9 +79,7 @@ SELECT
     , PD.date_start
     , PD.date_end
     , FI.has_feed_info
-    , (calitp_url_number = 0 AND FI.has_feed_info) AS use_for_report
+    , (calitp_url_number = 0 AND has_feed_info) AS use_for_report
 FROM agency_feeds_on_end_date AF
-JOIN has_feed_info_end_date FI
-    USING(calitp_itp_id, calitp_url_number, publish_date)
 JOIN publish_dates_crnt PD
   USING (publish_date)
