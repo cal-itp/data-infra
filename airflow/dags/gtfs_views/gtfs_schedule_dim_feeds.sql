9b9050edd852486d9e2c36c55f960cb7ffed7d32
evansiroky <evan.siroky@yahoo.com>
2021-12-17 12:24:52 -0800

    refactor(views): move raw_feeds extraction to feed fact table

--- airflow/dags/gtfs_views/gtfs_schedule_dim_feeds.sql
+++ airflow/dags/gtfs_views/gtfs_schedule_dim_feeds.sql
@@ -14,8 +14,6 @@ dependencies:
 
 WITH
 
--- Get a list of all the downloaded feeds according to when a unique version of the
--- static feed was downloaded.
 feed_feed_info AS (
     SELECT
         T1.calitp_itp_id
@@ -37,73 +35,6 @@ feed_feed_info AS (
             AND T1.calitp_extracted_at < T2.calitp_deleted_at
             AND T2.calitp_extracted_at < T1.calitp_deleted_at
             AND T2.calitp_itp_id != 200
-),
-
--- Get a list of all the the raw feed data (containg info on raw feed URLs for both
--- static and RT feeds) including the extract date of these feeds according to their
--- filename.
-raw_feed_with_extract_date AS (
-  SELECT
-    *,
-    PARSE_DATE(
-      '%Y-%m-%d',
-      REGEXP_EXTRACT(_FILE_NAME, ".*/([0-9]+-[0-9]+-[0-9]+)")
-    ) AS extract_date
-  FROM gtfs_schedule_history.calitp_feeds_raw
-),
-
--- Join the feed info on the raw extract date. This creates many matches since there
--- will be intermediate raw feed extract dates within the feed info records between
--- their extracted_at / deleted_at ranges
-feed_info_raw_feed_join_all AS (
-  SELECT
-    T1.*
-    , T2.gtfs_schedule_url AS raw_gtfs_schedule_url
-    , T2.gtfs_rt_vehicle_positions_url AS raw_gtfs_rt_vehicle_positions_url
-    , T2.gtfs_rt_service_alerts_url AS raw_gtfs_rt_service_alerts_url
-    , T2.gtfs_rt_trip_updates_url AS raw_gtfs_rt_trip_updates_url
-    , T2.extract_date AS raw_feeds_extract_date
-  FROM feed_feed_info T1
-  LEFT JOIN raw_feed_with_extract_date T2
-    ON
-      T1.calitp_itp_id = T2.itp_id
-      AND T1.calitp_url_number = T2.url_number
-      AND T1.calitp_extracted_at <= T2.extract_date
-      -- Exclude raw feed records on the exact deleted_at date since it is assumed that
-      -- the raw feed URL is potentially no longer valid once the valid range has ended.
-      AND T1.calitp_deleted_at > T2.extract_date
-),
-
--- Isolate to only the maximum raw feeds extract date within each valid feed period.
-last_raw_feeds_extract_date AS (
-  SELECT max(raw_feeds_extract_date) AS max_raw_feeds_extract_date,
-    calitp_itp_id,
-    calitp_url_number,
-    calitp_extracted_at,
-    calitp_deleted_at
-  FROM feed_info_raw_feed_join_all
-  GROUP BY calitp_itp_id,
-    calitp_url_number,
-    calitp_extracted_at,
-    calitp_deleted_at
-),
-
--- Pare down the feed_info_raw_feed_join_all query results to only include matches
--- upon the matched maximum raw feeds extract date or records where both the raw feed
--- extract date and max raw feed extract date are null.
-final_feed_info AS (
-  SELECT T1.*
-  FROM feed_info_raw_feed_join_all AS T1,
-    last_raw_feeds_extract_date T2
-  WHERE
-    T1.calitp_itp_id = T2.calitp_itp_id
-    AND T1.calitp_url_number = T2.calitp_url_number
-    AND T1.calitp_extracted_at = T2.calitp_extracted_at
-    AND T1.calitp_deleted_at = T2.calitp_deleted_at
-    AND (
-      (T1.raw_feeds_extract_date IS NULL AND T2.max_raw_feeds_extract_date IS NULL)
-      OR T1.raw_feeds_extract_date = T2.max_raw_feeds_extract_date
-    )
 )
 
 SELECT
@@ -112,4 +43,4 @@ SELECT
         IFNULL(CAST(T.feed_info_key AS STRING), "NULL")
     )) AS feed_key
     , * EXCEPT(feed_key, feed_info_key)
-FROM final_feed_info T
+FROM feed_feed_info T
