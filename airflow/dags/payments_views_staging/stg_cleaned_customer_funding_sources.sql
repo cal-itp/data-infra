3d7f92ded52c5082c8b6b6159e98b0c372863da5
Mjumbe Poe <mjumbewu@gmail.com>
2021-12-16 21:54:31 -0500

    Add cleaned tables for customers and customer funding sources

---
+++ airflow/dags/payments_views_staging/stg_cleaned_customer_funding_sources.sql
@@ -0,0 +1,41 @@
+---
+operator: operators.SqlToWarehouseOperator
+dst_table_name: "payments.stg_cleaned_customer_funding_sources"
+
+dependencies:
+  - stg_enriched_customer_funding_source
+---
+
+SELECT
+    customer_id,
+    funding_source_vault_id,
+    principal_customer_id,
+    bin,
+    masked_pan,
+    card_scheme,
+    issuer,
+    issuer_country,
+    form_factor,
+
+    -- If there is no record leading this one over the specified window, then
+    -- assume that this record has been valid since the beginning of time. If
+    -- there is a previous record, assume this record is valid since the time
+    -- the file was exported.
+    CASE
+        WHEN LEAD(calitp_export_datetime) OVER unique_ids IS NULL
+        THEN DATETIME(TIMESTAMP '0001-01-01 00:00:00+00:00')
+        ELSE calitp_export_datetime END AS calitp_valid_at,
+
+    -- If there is no record lagging this one over the specified window, then
+    -- assume that this record will be valid forever. Otherwise assume it is
+    -- invalid at the time that the next record was exported.
+    COALESCE(
+        LAG(calitp_export_datetime) OVER unique_ids,
+        DATETIME(TIMESTAMP '9999-12-31 23:59:59+00:00')) AS calitp_invalid_at
+
+FROM payments.stg_enriched_customer_funding_source
+WHERE calitp_dupe_number = 1
+WINDOW unique_ids AS (
+    PARTITION BY funding_source_vault_id, customer_id
+    ORDER BY calitp_customer_id_rank)
+ORDER BY customer_id, calitp_valid_at DESC
