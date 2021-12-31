d79a024acb7e9fa9d49cd78642d551d89af28813
evansiroky <evan.siroky@yahoo.com>
2021-12-31 13:23:52 -0800

    docs(airflow): add documentation about AirtableToWarehouseOperator

--- airflow/plugins/operators/airtable_to_warehouse.py
+++ airflow/plugins/operators/airtable_to_warehouse.py
@@ -67,6 +67,30 @@ class AirtableToWarehouseOperator(BaseOperator):
         api_key=None,
         **kwargs,
     ):
+        """An operator that will download data from an airtable and load it into
+            BigQuery and/or save it as a CSV file with the current date in Google Cloud
+            Storage (GCS).
+
+        Args:
+            air_base_id (str): The underlying id of the airtable instance being used.
+            air_table_name (str): The table name that should be extracted from the
+                airtable base
+            table_name (str): The name of the table to save to in Big Query
+            id_name (str, optional): The name to give the ID column. Defaults to "__id".
+            gcs_path (str, optional): A GCS path prefix. If not provided, no uploading
+                to GCS will occur. The resulting item gets saved to GCS at the following
+                path:
+                    `{base_calitp_bucket}/{gcs_path}/{execution_date}/{table_name}.csv`.
+            rename_fields (list, optional): A list of objects where each object consists
+                of a string key and value representing the new desired column name and
+                airtable column name respectively. Defaults to None.
+            column_prefix (str, optional): A string prefix to rename all columns with.
+                This prefix is not applied to columns affected by a rename triggered
+                from providing either `id_name` or `rename_fields`. Defaults to None.
+            api_key (str, optional): The API key to use when downloading from airtable.
+                This can be someone's personal API key. If not provided, the environment
+                variable of `CALITP_AIRTABLE_API_KEY` is used.
+        """
         self.air_base_id = air_base_id
         self.air_table_name = air_table_name
         self.table_name = table_name
