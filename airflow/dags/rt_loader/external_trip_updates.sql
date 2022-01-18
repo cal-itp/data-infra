a1c868e63976a28fb434c4ff4238a03f7cbddb8b
Laurie Merrell <laurie.m@jarv.us>
2022-01-18 13:45:18 -0600

    fix(gtfs-rt): register new operator; make loaders context aware (execution date); change fields

--- airflow/dags/rt_loader/external_trip_updates.sql
+++ airflow/dags/rt_loader/external_trip_updates.sql
@@ -5,5 +5,5 @@ operator: operators.SqlQueryOperator
 CREATE OR REPLACE EXTERNAL TABLE `gtfs_rt.trip_updates`
 OPTIONS (
     format = "PARQUET",
-    uris = ["gs://gtfs-data/rt-processed/trip_updates/*.parquet"]
+    uris = ["{{get_bucket()}}/rt-processed/trip_updates/*.parquet"]
 )
