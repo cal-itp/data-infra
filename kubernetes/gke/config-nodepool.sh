06a70c3421492aa6c33029bb87a97631e2bb2895
James Lott <james.l@jarv.us>
2022-01-25 21:34:18 -0500

    config-nodepool.sh: teardown apps-v1 nodepool
    We found that this node pool had unhealthy nodes which were continuously
    being rebooted in auto repair attempts

--- kubernetes/gke/config-nodepool.sh
+++ kubernetes/gke/config-nodepool.sh
@@ -1,22 +1,18 @@
 GKE_NODEPOOL_NAMES=(
-  'apps-v1'
   'apps-v2'
 )
 
 declare -A GKE_NODEPOOL_NODE_COUNTS
 GKE_NODEPOOL_NODE_COUNTS=(
-  ['apps-v1']=1
   ['apps-v2']=1
 )
 
 declare -A GKE_NODEPOOL_NODE_LOCATIONS
 GKE_NODEPOOL_NODE_LOCATIONS=(
-  ['apps-v1']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
   ['apps-v2']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
 )
 
 declare -A GKE_NODEPOOL_MACHINE_TYPES
 GKE_NODEPOOL_MACHINE_TYPES=(
-  ['apps-v1']=n1-standard-4
   ['apps-v2']=n1-standard-4
 )
