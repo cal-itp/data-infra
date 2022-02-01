dd49005bb8fb1a096555f7a164c010629f9d3606
Laurie Merrell <laurie.m@jarv.us>
2022-02-01 15:17:15 -0600

    fix(gtfs-schedule): trim whitespace -- first pass

--- airflow/dags/gtfs_views_staging/calendar_clean.sql
+++ airflow/dags/gtfs_views_staging/calendar_clean.sql
@@ -4,13 +4,26 @@ dst_table_name: "gtfs_schedule_type2.calendar_clean"
 dependencies:
   - type2_loaded
 ---
--- Trim service_id for 273, 271 specific issue
+
+-- Trim all string fields
+-- Incoming schema explicitly defined in gtfs_schedule_history external table definition
+
 SELECT
-    * EXCEPT(start_date, end_date, calitp_deleted_at, service_id),
-    TRIM(service_id) as service_id,
-    PARSE_DATE("%Y%m%d",start_date) AS start_date,
-    PARSE_DATE("%Y%m%d",end_date) AS end_date,
-    FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING)))
-        AS calendar_key,
-    COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
+    calitp_itp_id
+    , calitp_url_number
+    , TRIM(service_id) as service_id
+    , TRIM(monday) as monday
+    , TRIM(tuesday) as tuesday
+    , TRIM(wednesday) as wednesday
+    , TRIM(thursday) as thursday
+    , TRIM(friday) as friday
+    , TRIM(saturday) as saturday
+    , TRIM(sunday) as sunday
+    , PARSE_DATE("%Y%m%d", TRIM(start_date)) AS start_date
+    , PARSE_DATE("%Y%m%d", TRIM(end_date)) AS end_date
+    , calitp_extracted_at
+    , calitp_hash
+    , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING)))
+        AS calendar_key
+    , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
 FROM `gtfs_schedule_type2.calendar`
--- airflow/dags/gtfs_views_staging/calendar_dates_clean.sql
+++ airflow/dags/gtfs_views_staging/calendar_dates_clean.sql
@@ -4,10 +4,17 @@ dst_table_name: "gtfs_schedule_type2.calendar_dates_clean"
 dependencies:
   - type2_loaded
 ---
--- Trim service_id for 273, 271 specific issue
+
+-- Trim all string fields
+-- Incoming schema explicitly defined in gtfs_schedule_history external table definition
+
 SELECT
-  * EXCEPT(date, calitp_deleted_at, service_id)
+  calitp_itp_id
+  , calitp_url_number
   , TRIM(service_id) as service_id
-  , PARSE_DATE("%Y%m%d",date) AS date
+  , TRIM(exception_type) as exception_type
+  , calitp_extracted_at
+  , calitp_hash
+  , PARSE_DATE("%Y%m%d", TRIM(date)) AS date
   , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
 FROM `gtfs_schedule_type2.calendar_dates`
