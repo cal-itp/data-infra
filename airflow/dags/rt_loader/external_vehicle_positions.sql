4c63d7a365a1cb03a1d2f536b455b71cc3cf0fce
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2022-02-03 20:16:03 -0500

    Revert "MVP of just producing JSON from RT protos"

--- airflow/dags/rt_loader/external_vehicle_positions.sql
+++ airflow/dags/rt_loader/external_vehicle_positions.sql
@@ -1,12 +1,9 @@
 ---
 operator: operators.SqlQueryOperator
-
-dependencies:
-  - parse_rt_vehicle_positions
 ---
 
 CREATE OR REPLACE EXTERNAL TABLE `gtfs_rt.vehicle_positions`
 OPTIONS (
-    format = "JSON",
-    uris = ["{{get_bucket()}}/rt-processed/vehicle_positions/*.jsonl.gz"]
+    format = "PARQUET",
+    uris = ["gs://gtfs-data/rt-processed/vehicle_positions/*.parquet"]
 )
