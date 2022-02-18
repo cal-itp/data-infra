a078b5d09c0cd3b52fe9836c1d288a2dab8c4e37
Laurie Merrell <laurie.m@jarv.us>
2022-02-18 17:20:40 -0600

    chore: shorten lines

--- airflow/dags/rt_loader_files/rt_views_gtfs_rt_fact_files_wide_hourly.sql
+++ airflow/dags/rt_loader_files/rt_views_gtfs_rt_fact_files_wide_hourly.sql
@@ -22,5 +22,18 @@ dependencies:
 
 SELECT *
 FROM
-(SELECT date_extracted, calitp_itp_id, calitp_url_number, name, calitp_extracted_at, hour_extracted FROM `views.gtfs_rt_fact_files`)
-PIVOT(count(calitp_extracted_at) file_count_hr FOR hour_extracted in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23))
+    (SELECT
+        date_extracted,
+        calitp_itp_id,
+        calitp_url_number,
+        name,
+        calitp_extracted_at,
+        hour_extracted
+    FROM `views.gtfs_rt_fact_files`)
+PIVOT(
+    count(calitp_extracted_at) file_count_hr
+    FOR hour_extracted in
+        (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         11, 12, 13, 14, 15, 16, 17, 18, 19,
+         20, 21, 22, 23)
+    )
