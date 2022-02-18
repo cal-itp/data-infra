ac6fa925d7815fad8172448dab6cb8c0dee9eb8e
Natalya Diaz <natalya.d@jarv.us>
2022-02-17 22:31:46 -0500

    feat(rt-archiver) added table vto visualize logger fetch/write messages

---
+++ airflow/dags/rt_views/gtfs_rt_thread_errors.sql
@@ -0,0 +1,96 @@
+---
+operator: operators.SqlToWarehouseOperator
+dst_table_name: "views.gtfs_rt_thread_errors"
+
+description: |
+
+
+fields:
+  file_hash:
+  calitp_itp_id:
+  calitp_url_number:
+  feed_file
+  start_fetch_time:
+  completed_fetch_time:
+  start_write_time:
+  completed_fetch_time:
+  error_time:
+  error_message:
+---
+
+With
+start_fetch AS (
+    SELECT
+        textpayload,
+        timestamp AS start_fetch_time,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS file_hash,
+        REGEXP_EXTRACT(textPayload, "mapper_key=([0-9]+)")AS calitp_itp_id,
+        REGEXP_EXTRACT(textPayload, "mapper_key=[0-9]+/([0-9]+)")AS calitp_url_number,
+        REGEXP_EXTRACT(textpayload,"mapper_key=[0-9]+/[0-9]+/?([a-zA-Z0-9\\.\\_\\-]+)?") AS feed_file_name,
+        REGEXP_EXTRACT(textpayload,"mapper_key=([0-9]+/[0-9]+/?[a-zA-Z0-9\\.\\_\\-]+)?") AS feed_file,
+
+    FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
+    WHERE textPayload like "%start fetch%"
+),
+complete_fetch AS (
+    SELECT
+        timestamp as completed_fetch_time,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_file_hash,
+    FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
+    WHERE textPayload like "%completed fetch%"
+),
+fetch_join_table AS (
+    SELECT * FROM start_fetch t1
+    JOIN complete_fetch t2  ON t1.file_hash = t2.c_file_hash
+),
+start_write AS (
+    SELECT
+        textpayload,
+        timestamp AS start_write_time,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS file_hash,
+    FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
+    WHERE textPayload like "%start write%"
+),
+complete_write AS (
+    SELECT
+        timestamp AS completed_write_time,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_file_hash,
+    FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
+    WHERE textPayload like "%completed write%"
+),
+write_join_table AS (
+    SELECT * FROM start_write t3
+    JOIN complete_write t4  ON t3.file_hash = t4.c_file_hash
+),
+log_table AS (
+    SELECT
+t1. file_hash,
+t1.calitp_itp_id,
+t1.calitp_url_number,
+t1.feed_file,
+t1. start_fetch_time,
+t1.completed_fetch_time,
+t2.start_write_time,
+t2.completed_write_time,
+ FROM fetch_join_table t1 JOIN write_join_table t2 ON t1.file_hash = t2.file_hash
+),
+stderr_table AS (
+    SELECT textPayload, timestamp AS error_time,
+    REGEXP_EXTRACT(textpayload,"fetcher ([0-9]+/[0-9]+/?[a-zA-Z0-9\\.\\_\\-]+)?") AS feed_file,
+    FROM `cal-itp-data-infra.gtfs_rt_logs.stderr`
+    WHERE textPayload LIKE "%fetcher%"
+)
+SELECT
+t1. file_hash,
+t1.calitp_itp_id,
+t1.calitp_url_number,
+t1.feed_file,
+t1. start_fetch_time,
+t1.completed_fetch_time,
+t1. start_write_time,
+t1.completed_write_time,
+t2.error_time,
+t2.textpayload AS error_message
+FROM log_table t1
+JOIN stderr_table t2 ON t1.feed_file = t2.feed_file
+AND t2.error_time >= t1.start_fetch_time
