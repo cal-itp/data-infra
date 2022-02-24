43099f287790327ad5187bfd48af13f2818337ae
machikoyasuda <machikoyasuda@gmail.com>
2022-02-24 20:26:26 +0000

    refactor(operator): move api key/secret lookup; add and update docstrings

--- airflow/plugins/operators/amplitude_to_flattened_json.py
+++ airflow/plugins/operators/amplitude_to_flattened_json.py
@@ -15,12 +15,26 @@ DATE_FORMAT = "%Y%m%dT%H"
 def amplitude_to_df(
     start: str,
     end: str,
-    api_key: str = os.environ.get("CALITP_AMPLITUDE_BENEFITS_API_KEY"),
-    secret_key: str = os.environ.get("CALITP_AMPLITUDE_BENEFITS_SECRET_KEY"),
+    api_key: str = None,
+    secret_key: str = None,
 ):
-    """Export data from Amplitude into a dataframe."""
+    """
+    Export zipped JSON data from Amplitude API and returns as a panda dataframe.
+
+    Args:
+        start: Start date as a string, first hour included in data series, formatted YYYYMMDDTHH (e.g. '20150201T05').
+        end: End date as a string, same format as start.
+        api_key: API key from Amplitude API.
+        secret_key: Secret key from Amplitude API.
+
+    Returns:
+        A panda dataframe which has all the events within the date range.
+    """
+
     url = "https://amplitude.com/api/2/export"
     params = {"start": start, "end": end}
+    api_key = api_key or os.environ.get("CALITP_AMPLITUDE_BENEFITS_API_KEY")
+    secret_key = secret_key or os.environ.get("CALITP_AMPLITUDE_BENEFITS_SECRET_KEY")
 
     response = requests.get(url, params=params, auth=(api_key, secret_key), stream=True)
 
@@ -40,8 +54,7 @@ def amplitude_to_df(
 
 class AmplitudeToFlattenedJSONOperator(BaseOperator):
     """
-    An operator that will download data from Amplitude and load it into
-    the CalITP data warehouse.
+    An operator that will download data from Amplitude and upload the resulting flattened JSON to GCS.
     """
 
     def __init__(self, **kwargs):
