80cfac324339903b5b11dfafd889524d43118e7c
Andrew Vaccaro <atvaccaro@gmail.com>
2022-02-25 12:53:28 -0500

    add column descriptions

--- airflow/dags/rt_loader/external_validation_service_alerts.sql
+++ airflow/dags/rt_loader/external_validation_service_alerts.sql
@@ -1,5 +1,21 @@
 ---
 operator: operators.SqlQueryOperator
+description: |
+  GTFS RT validation errors as returned by the validator. Each row corresponds to
+  the number of occurrences of a given error for a single itp_id/url/tick/entity combination.
+fields:
+  calitp_itp_id: |
+    The ITP ID associated with the service alert.
+  calitp_url_number: |
+    The URL number associated with the service alert.
+  calitp_extracted_at: |
+    When the original file was downloaded.
+  rt_feed_type: |
+    The type of RT feed entity; will always be service alerts.
+  error_id: |
+    An error ID as defined in the GTFS RT validator repo.
+  n_occurrences: |
+    The number of occurrences of this error.
 dependencies:
     - load_rt_validations
 ---
