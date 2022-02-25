2a34f1153e64afb31842db1c6c89688a9e016ae5
Natalya Diaz <natalya.d@jarv.us>
2022-02-25 12:09:23 -0500

    feat(gtfs-archiver):renamed file hash to transation_id because that is what it is

--- airflow/dags/rt_views/gtfs_rt_log_errors.sql
+++ airflow/dags/rt_views/gtfs_rt_log_errors.sql
@@ -2,11 +2,11 @@
 operator: operators.SqlToWarehouseOperator
 dst_table_name: "views.gtfs_rt_log_errors"
 
-description: | Each row is a unique file_hash of a feed that generated an exception within GCP logger
+description: | Each row is a unique transaction_id of a feed that generated an exception within GCP logger
 
 
 fields:
-  file_hash: unique identifier for each feed download attempt.
+  transaction_id: unique identifier for each feed download attempt.
   calitp_itp_id: Feed ITP ID.
   calitp_url_number: Feed URL number.
   feed_file: Combination calitp_itp_id, URL number, and feed URL name.
@@ -27,7 +27,7 @@ start_fetch AS (
     SELECT
         textpayload,
         timestamp AS start_fetch_time,
-        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS file_hash,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS transaction_id,
         REGEXP_EXTRACT(textPayload, "mapper_key=([0-9]+)")AS calitp_itp_id,
         REGEXP_EXTRACT(textPayload, "mapper_key=[0-9]+/([0-9]+)")AS calitp_url_number,
         REGEXP_EXTRACT(textpayload,"mapper_key=[0-9]+/[0-9]+/?([a-zA-Z0-9\\.\\_\\-]+)?") AS feed_file_name,
@@ -39,36 +39,36 @@ start_fetch AS (
 complete_fetch AS (
     SELECT
         timestamp as completed_fetch_time,
-        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_file_hash,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_transaction_id,
     FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
     WHERE textPayload like "%completed fetch%"
 ),
 fetch_join_table AS (
     SELECT * FROM start_fetch t1
-    JOIN complete_fetch t2  ON t1.file_hash = t2.c_file_hash
+    JOIN complete_fetch t2  ON t1.transaction_id = t2.c_transaction_id
 ),
 start_write AS (
     SELECT
         textpayload,
         timestamp AS start_write_time,
-        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS file_hash,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS transaction_id,
     FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
     WHERE textPayload like "%start write%"
 ),
 complete_write AS (
     SELECT
         timestamp AS completed_write_time,
-        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_file_hash,
+        REGEXP_EXTRACT(textPayload, r'\[txn (.*?)\]') AS c_transaction_id
     FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
     WHERE textPayload like "%completed write%"
 ),
 write_join_table AS (
     SELECT * FROM start_write t3
-    JOIN complete_write t4  ON t3.file_hash = t4.c_file_hash
+    JOIN complete_write t4  ON t3.transaction_id = t4.c_transaction_id
 ),
 log_table AS (
     SELECT
-        t1.file_hash,
+        t1.transaction_id,
         t1.calitp_itp_id,
         t1.calitp_url_number,
         t1.feed_file,
@@ -76,7 +76,7 @@ log_table AS (
         t1.completed_fetch_time,
         t2.start_write_time,
         t2.completed_write_time,
- FROM fetch_join_table t1 JOIN write_join_table t2 ON t1.file_hash = t2.file_hash
+ FROM fetch_join_table t1 JOIN write_join_table t2 ON t1.transaction_id = t2.transaction_id
 ),
 stderr_table AS (
     SELECT textPayload, timestamp AS error_time,
@@ -85,7 +85,7 @@ stderr_table AS (
     WHERE textPayload LIKE "%fetcher%"
 )
 SELECT
-    t1.file_hash,
+    t1.transaction_id,
     t1.calitp_itp_id,
     t1.calitp_url_number,
     t1.feed_file,
@@ -96,5 +96,5 @@ SELECT
     t2.error_time,
     t2.textpayload AS error_message
 FROM log_table t1
-JOIN stderr_table t2 ON t1.feed_file = t2.feed_file
+JOIN stderr_table t2 ON t1.transaction_id = t2.transaction_id
 AND t2.error_time >= t1.start_fetch_time AND t2.error_time < t1.completed_write_time
