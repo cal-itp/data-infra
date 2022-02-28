d680421003cb49687ca5f7caceb3ed989c2196ce
Laurie Merrell <laurie.m@jarv.us>
2022-02-28 14:57:43 -0600

    fix transfers_clean

--- airflow/dags/gtfs_views_staging/transfers_clean.sql
+++ airflow/dags/gtfs_views_staging/transfers_clean.sql
@@ -1,6 +1,6 @@
 ---
 operator: operators.SqlToWarehouseOperator
-dst_table_name: "gtfs_views_staging.levels_clean"
+dst_table_name: "gtfs_views_staging.transfers_clean"
 dependencies:
   - type2_loaded
 ---
@@ -11,11 +11,11 @@ dependencies:
 SELECT
     calitp_itp_id
     , calitp_url_number
-    , TRIM(level_id) as level_id
-    , level_index
-    , TRIM(level_name) as level_name
+    , TRIM(from_stop_id) as from_stop_id
+    , TRIM(to_stop_id) as to_stop_id
+    , TRIM(transfer_type) as transfer_type
     , calitp_extracted_at
     , calitp_hash
-    , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS level_key
+    , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS transfer_key
     , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
-FROM `gtfs_schedule_type2.levels`
+FROM `gtfs_schedule_type2.transfers`
