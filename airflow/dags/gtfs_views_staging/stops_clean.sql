1da6948ef9393d37f6fd934c5be8f051f367f0e9
Laurie Merrell <laurie.m@jarv.us>
2022-03-01 12:50:40 -0600

    fix(gtfs schedule): move lat/long data type handling to gtfs_views_staging

--- airflow/dags/gtfs_views_staging/stops_clean.sql
+++ airflow/dags/gtfs_views_staging/stops_clean.sql
@@ -6,7 +6,9 @@ dependencies:
 ---
 
 SELECT
-    * EXCEPT(calitp_deleted_at)
+    * EXCEPT(calitp_deleted_at, stop_lat, stop_lon)
+    , SAFE_CAST(stop_lat AS FLOAT64)
+    , SAFE_CAST(stop_lon AS FLOAT64)
     , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS stop_key
     , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
 FROM `gtfs_schedule_type2.stops`
