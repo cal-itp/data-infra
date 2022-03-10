58a4ef34e20909359c0d2ccfdce84591925db58d
Laurie Merrell <laurie.m@jarv.us>
2022-03-10 09:53:18 -0600

    fix(gtfs-rt): get feed key in validations fact table; also remove hard-coded datatsets

--- airflow/dags/rt_views/gtfs_rt_fact_daily_validation_errors.sql
+++ airflow/dags/rt_views/gtfs_rt_fact_daily_validation_errors.sql
@@ -31,6 +31,7 @@ tests:
 
 external_dependencies:
   - rt_loader: external_validation_service_alerts
+  - gtfs_views: gtfs_schedule_dim_feeds
 ---
 
 -- note that for realtime we do not (yet) have a feed_key-type identifier
@@ -38,21 +39,27 @@ external_dependencies:
 
 with unioned as (
     select *
-    from `cal-itp-data-infra.gtfs_rt.validation_service_alerts`
+    from `gtfs_rt.validation_service_alerts`
     union all
     select *
-    from `cal-itp-data-infra.gtfs_rt.validation_trip_updates`
+    from `gtfs_rt.validation_trip_updates`
     union all
     select *
-    from `cal-itp-data-infra.gtfs_rt.validation_vehicle_positions`
+    from `gtfs_rt.validation_vehicle_positions`
 )
-SELECT
+error_counts as (
+  SELECT
       calitp_itp_id
     , calitp_url_number
     , rt_feed_type
     , error_id
     , DATE(calitp_extracted_at) as date
     , sum(n_occurrences) as occurrences
-
 FROM unioned
 GROUP BY calitp_itp_id, calitp_url_number, rt_feed_type, error_id, date
+)
+SELECT t1.*,
+    t2.feed_key
+FROM error_counts t1
+LEFT JOIN `views.gtfs_schedule_dim_feeds` t2
+    ON t1.date >= t2.calitp_extracted_at and t1.date < t2.calitp_deleted_at and t1.calitp_itp_id = t2.calitp_itp_id and t1.calitp_url_number = t2.calitp_url_number
