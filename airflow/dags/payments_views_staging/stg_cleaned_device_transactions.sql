30bd391ddc996c8488aea90e88856cf4013c1479
Laurie Merrell <laurie.m@jarv.us>
2022-03-18 14:50:09 -0500

    fix(payments): trim route and location (stop) ids at the source

--- airflow/dags/payments_views_staging/stg_cleaned_device_transactions.sql
+++ airflow/dags/payments_views_staging/stg_cleaned_device_transactions.sql
@@ -14,6 +14,10 @@ select distinct * except (
     calitp_file_name,
     calitp_n_dupes,
     calitp_n_dupe_ids,
-    calitp_dupe_number)
+    calitp_dupe_number,
+    route_id,
+    location_id),
+    trim(route_id) as route_id,
+    trim(location_id) as location_id
 from payments.stg_enriched_device_transactions
 where calitp_dupe_number = 1
