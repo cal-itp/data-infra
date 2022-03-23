2f8efc822df4db89a55848a3ce3a74e4f0e01b09
Laurie Merrell <laurie.m@jarv.us>
2022-03-23 15:35:47 -0500

    chore(gtfs schedule): cast shape dist traveled while i'm working on this file anyway

--- airflow/dags/gtfs_views_staging/shapes_clean.sql
+++ airflow/dags/gtfs_views_staging/shapes_clean.sql
@@ -15,7 +15,7 @@ SELECT
     , SAFE_CAST(TRIM(shape_pt_lat) as FLOAT64) as shape_pt_lat
     , SAFE_CAST(TRIM(shape_pt_lon) as FLOAT64) as shape_pt_lon
     , SAFE_CAST(TRIM(shape_pt_sequence) as INT64) as shape_pt_sequence
-    , TRIM(shape_dist_traveled) as shape_dist_traveled
+    , SAFE_CAST(TRIM(shape_dist_traveled) as FLOAT64) as shape_dist_traveled
     , calitp_extracted_at
     , calitp_hash
     , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS shape_key
