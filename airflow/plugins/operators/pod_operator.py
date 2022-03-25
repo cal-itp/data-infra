f5dd3482c77e1cae92c55e9eca6b44ba2137e6a9
Andrew Vaccaro <atvaccaro@gmail.com>
2022-03-25 16:20:39 -0400

    wip scheduling rt jobs on new special node pool

--- airflow/plugins/operators/pod_operator.py
+++ airflow/plugins/operators/pod_operator.py
@@ -1,6 +1,8 @@
 import os
 
 from functools import wraps
+from pprint import pprint
+
 from airflow.contrib.operators.gcp_container_operator import GKEPodOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
@@ -12,13 +14,17 @@ def PodOperator(*args, **kwargs):
     # TODO: tune this, and add resource limits
     namespace = "default"
 
+    if kwargs.get('name') == 'gtfs-rt-validation':
+        pprint(kwargs)
+        # raise RuntimeError
+
     if is_development():
         return GKEPodOperator(
             *args,
             in_cluster=False,
             project_id="cal-itp-data-infra",  # there currently isn't a staging cluster
-            location=os.environ["POD_LOCATION"],
-            cluster_name=os.environ["POD_CLUSTER_NAME"],
+            location='us-west1',#kwargs.get('pod_location', os.environ["POD_LOCATION"]),
+            cluster_name='data-infra-apps',#kwargs.get('cluster_name', os.environ["POD_CLUSTER_NAME"]),
             namespace=namespace,
             **kwargs,
         )
