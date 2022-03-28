c39d9ef55057736419181ff6d4aa250010b382eb
Andrew Vaccaro <atvaccaro@gmail.com>
2022-03-28 14:36:53 -0400

    start getting dbt going

---
+++ warehouse/models/gtfs_rt/staging/stg_gtfs_rt__vehicle_positions.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{ source('gtfs_rt_raw', 'vehicle_positions') }}
+    limit 100
+)
+
+, stg_gtfs_rt__vehicle_positions as (
+    select
+          calitp_itp_id
+        , calitp_url_number
+        , calitp_filepath
+        , id
+        , header.timestamp as header_timestamp
+
+    from source
+)
+
+select * from stg_gtfs_rt__vehicle_positions
