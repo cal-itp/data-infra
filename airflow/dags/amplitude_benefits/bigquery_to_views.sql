73a028cf052c70fcb90517a5a519afcbd4a89ee6
Machiko Yasuda <machiko@compiler.la>
2022-03-28 20:40:11 +0000

    refactor: flatten User Properties and Event Properties

--- airflow/dags/amplitude_benefits/bigquery_to_views.sql
+++ airflow/dags/amplitude_benefits/bigquery_to_views.sql
@@ -9,34 +9,51 @@ description: |
   Benefits application event data from Amplitude, via Google Cloud Services and BigQuery. See more details of data schema here https://developers.amplitude.com/docs/http-api-v2#properties-1
 
 fields:
-  app: App name
-  device_id: A device-specific identifier, such as the Identifier for Vendor on iOS.
-  user_id: A readable ID specified by you.
-  client_event_time: TIMESTAMP
-  event_id: An incrementing counter to distinguish events with the same user_id and timestamp from each other.
-  session_id: The start time of the session in milliseconds since epoch (Unix Timestamp)
-  event_type: The unique identifier for your event.
   amplitude_event_type: Name of Amplitude event.
-  version_name: Version name.
-  device_brand: The device brand that the user is using.
-  device_family: The device family that the user is using.
-  device_type: The device type that the user is using.
-  device_carrier: The carrier that the user is using.
-  location_lat: The current Latitude of the user.
-  location_lng: The current Longitude of the user.
-  language: The language set by the user.
-  city: The current city of the user.
-  event_properties: Event properties with transit provider_name, path, language
-  user_properties: User properties with referring_domain, referrer, user_agent
-  event_time: Event time (e.g. "2021-12-09 00:45:29.430000"),
-  client_upload_time: Client upload time,
-  server_upload_time: Server upload time,
-  server_received_time: Server received time,
   amplitude_id: INT64,
-  user_creation_time: User creation time,
-  uuid: UUID,
+  app: App name,
+  city: The current city of the user,
+  client_event_time: Client event timestamp
+  client_upload_time: Client upload time,
+  device_brand: The device brand that the user is using.
+  device_carrier: The carrier that the user is using.
+  device_family: The device family that the user is using.
+  device_id: A device-specific identifier, such as the Identifier for Vendor on iOS.
+  device_type: The device type that the user is using.
+  event_id: An incrementing counter to distinguish events with the same user_id and timestamp from each other.
+  event_properties_language: Event property language
+  event_properties_path: Event property path,
+  event_properties_provider_name: Event property provider name,
+  event_properties: Event properties with transit provider_name, path, language
+  event_time: Event time (e.g. "2021-12-09 00:45:29.430000"),
+  event_type: The unique identifier for your event.
+  language: The language set by the user,
+  location_lat: The current Latitude of the user,
+  location_lng: The current Longitude of the user,
+  processed_time: Processed time,
   schema: Schema,
-  processed_time: Processed time
+  server_received_time: Server received time,
+  server_upload_time: Server upload time,
+  session_id: The start time of the session in milliseconds since epoch
+  user_creation_time: User creation time,
+  user_id: A readable ID specified by you.
+  user_properties_provider_name: User property provider name,
+  user_properties_referrer: User property preferrer,
+  user_properties_referring_domain: User property referring domain,
+  user_properties_user_agent: User property user agent,
+  uuid: UUID,
+  version_name: Version name
 ---
 
-SELECT * FROM `amplitude.benefits_events`
+SELECT
+  * EXCEPT(user_properties,
+    event_properties),
+  event_properties.provider_name AS event_properties_provider_name,
+  event_properties.path AS event_properties_path,
+  event_properties.language AS event_properties_language
+  user_properties.provider_name AS user_properties_provider_name,
+  user_properties.referring_domain AS user_properties_referring_domain,
+  user_properties.referrer AS user_properties_referrer,
+  user_properties.user_agent AS user_properties_user_agent,
+FROM
+  `amplitude.benefits_events`
