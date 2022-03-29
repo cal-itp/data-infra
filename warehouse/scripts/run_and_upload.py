259e6f5d255b78a7e0dd95224376539e2fbb94d9
Andrew Vaccaro <atvaccaro@gmail.com>
2022-03-28 23:13:17 -0400

    upload latest

--- warehouse/scripts/run_and_upload.py
+++ warehouse/scripts/run_and_upload.py
@@ -9,12 +9,15 @@ import typer
 
 BUCKET = "calitp-dbt-artifacts"
 
-artifacts = []
+artifacts = map(
+    Path, ["index.html", "catalog.json", "manifest.json", "run_results.json"]
+)
 
 
 def run(
     project_dir: Path = os.getcwd(),
     profiles_dir: Path = os.getcwd(),
+    docs: bool = False,
     upload: bool = False,
 ) -> None:
     def get_command(*args) -> List[str]:
@@ -27,11 +30,18 @@ def run(
 
     subprocess.run(get_command("run"))
 
-    if upload:
+    if docs:
+        subprocess.run(get_command("docs", "generate"))
+
         fs = gcsfs.GCSFileSystem(project="cal-itp-data-infra")
-        fs.put()
-    else:
-        print("skipping upload")
+
+        for artifact in artifacts:
+            if upload:
+                _from = str(project_dir / Path("target") / artifact)
+                _to = str(Path(f"gs://{BUCKET}") / Path("latest"))
+                fs.put(lpath=_from, rpath=_to)
+            else:
+                print(f"skipping upload of {artifact}")
 
 
 if __name__ == "__main__":
