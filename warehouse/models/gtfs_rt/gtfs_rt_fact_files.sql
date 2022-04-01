e0b50ccc458e3a497bfc224ac93b08b8b53bd717
Laurie Merrell <laurie.m@jarv.us>
2022-04-01 11:20:05 -0500

    docs(dbt-rt): document rt fact files table

--- warehouse/models/gtfs_rt/gtfs_rt_fact_files.sql
+++ warehouse/models/gtfs_rt/gtfs_rt_fact_files.sql
@@ -1,3 +1,10 @@
+{% docs gtfs_rt_fact_files %}
+
+Each row is a GTFS realtime file that was downloaded.
+  Note that presence of a file is not a guarantee that the downloaded file is complete or valid.
+
+{% enddocs %}
+
 WITH source as (
     select *
     from {{ source('gtfs_rt_raw', 'calitp_files') }}
