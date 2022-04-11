26283d78b00125cd4c9aa2a0192fe9e2461bcb13
Andrew Vaccaro <atvaccaro@gmail.com>
2022-04-11 10:44:50 -0400

    use manifest and rename to test

--- warehouse/scripts/push_to_metabase.sh
+++ warehouse/scripts/push_to_metabase.sh
@@ -2,9 +2,8 @@
 set -e
 
 dbt-metabase models \
-    --dbt_path . \
+    --dbt_manifest_path ./target/manifest.json \
     --dbt_database cal-itp-data-infra \
-    --dbt_schema views \
     --metabase_host dashboards.calitp.org \
     --metabase_user $METABASE_USER \
     --metabase_password $METABASE_PASSWORD \
