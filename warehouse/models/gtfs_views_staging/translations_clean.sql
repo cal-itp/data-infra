fd53105e85aae2467823af4f47298d5d5a3e8a69
Laurie Merrell <laurie.m@jarv.us>
2022-04-11 15:07:15 -0500

    gtfs schedule --> dbt: translations_clean, trips_clean, validation_notices_clean models

---
+++ warehouse/models/gtfs_views_staging/translations_clean.sql
@@ -0,0 +1,31 @@
+{{ config(materialized='table') }}
+
+WITH type2 as (
+    select *
+    from {{ source('gtfs_type2', 'translations') }}
+)
+
+, translations_clean as (
+
+    -- Trim all string fields
+    -- Incoming schema explicitly defined in gtfs_schedule_history external table definition
+
+
+    SELECT
+        calitp_itp_id
+        , calitp_url_number
+        , TRIM(table_name) as table_name
+        , TRIM(field_name) as field_name
+        , TRIM(language) as language
+        , TRIM(translation) as translation
+        , TRIM(record_id) as record_id
+        , TRIM(record_sub_id) as record_sub_id
+        , TRIM(field_value) as field_value
+        , calitp_extracted_at
+        , calitp_hash
+        , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS translation_key
+        , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
+    FROM type2
+)
+
+SELECT * FROM translations_clean
