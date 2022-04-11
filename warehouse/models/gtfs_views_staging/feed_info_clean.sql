741109d49f9a6a85d7ad2b6b8f34b859eaaf87d7
Laurie Merrell <laurie.m@jarv.us>
2022-04-11 14:29:46 -0500

    gtfs schedule --> dbt: feed_info-clean, frequencies_clean models

---
+++ warehouse/models/gtfs_views_staging/feed_info_clean.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='table') }}
+
+WITH type2 as (
+    select *
+    from {{ source('gtfs_type2', 'feed_info') }}
+)
+
+, feed_info_clean as (
+
+    -- Trim all string fields
+    -- Incoming schema explicitly defined in gtfs_schedule_history external table definition
+
+    -- select distinct because of Foothill Transit feed with exact duplicates
+    -- duplicates here result in duplicate feed_keys downstream
+    SELECT DISTINCT
+        calitp_itp_id
+        , calitp_url_number
+        , TRIM(feed_publisher_name) as feed_publisher_name
+        , TRIM(feed_publisher_url) as feed_publisher_url
+        , TRIM(feed_lang) as feed_lang
+        , TRIM(default_lang) as default_lang
+        , TRIM(feed_version) as feed_version
+        , TRIM(feed_contact_email) as feed_contact_email
+        , TRIM(feed_contact_url) as feed_contact_url
+        , calitp_extracted_at
+        , calitp_hash
+        , PARSE_DATE("%Y%m%d", TRIM(feed_start_date)) AS feed_start_date
+        , PARSE_DATE("%Y%m%d", TRIM(feed_end_date)) AS feed_end_date
+        , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING)))
+            AS feed_info_key
+        , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
+    FROM type2
+)
+
+SELECT * FROM feed_info_clean
---
+++ warehouse/models/gtfs_views_staging/frequencies_clean.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+
+WITH type2 as (
+    select *
+    from {{ source('gtfs_type2', 'frequencies') }}
+)
+
+, frequencies_clean as (
+
+    -- Trim all string fields
+    -- Incoming schema explicitly defined in gtfs_schedule_history external table definition
+
+    -- select distinct because of Foothill Transit feed with exact duplicates
+    -- duplicates here result in duplicate feed_keys downstream
+    SELECT
+        calitp_itp_id
+        , calitp_url_number
+        , TRIM(trip_id) as trip_id
+        , TRIM(start_time) as start_time
+        , TRIM(end_time) as end_time
+        , TRIM(headway_secs) as headway_secs
+        , TRIM(exact_times) as exact_times
+        , calitp_extracted_at
+        , calitp_hash
+        , FARM_FINGERPRINT(CONCAT(CAST(calitp_hash AS STRING), "___", CAST(calitp_extracted_at AS STRING))) AS frequency_key
+        , COALESCE(calitp_deleted_at, "2099-01-01") AS calitp_deleted_at
+    FROM type2
+)
+
+SELECT * FROM frequencies_clean
