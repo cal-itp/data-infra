0a4695d78fa889678d34b30fe6aca01862bb988f
Laurie Merrell <laurie.m@jarv.us>
2022-04-14 17:21:09 -0500

    gtfs views --> dbt: gtfs_schedule_stg_daily_service

---
+++ warehouse/models/gtfs_views/gtfs_schedule_stg_daily_service.sql
@@ -0,0 +1,120 @@
+{{ config(materialized='table') }}
+
+WITH calendar_dates_clean AS (
+    SELECT *
+    FROM {{ ref('calendar_dates_clean') }}
+),
+gtfs_schedule_stg_calendar_long AS (
+    SELECT *
+    FROM {{ ref('gtfs_schedule_stg_calendar_long') }}
+),
+dim_date AS (
+    SELECT *
+    FROM {{ ref('dim_date') }}
+),
+-- For a given service_date, find the latest feed describing whether that date
+-- is in service or not. For example, for 2021-04-10, the latest active feed
+-- might be from 2021-04-09 (or even earlier, if it has not been updated recently).
+-- Key features:
+--   * There should be one entry per feed x service_date
+--   * service_date may extend far into the future (depending on service_date_end).
+--     In this case we only extend out to a year from current date.
+--   * is_in_service for service_date(s) past today are not stable. They reflect
+--     what the most recent feed thinks will be in service in e.g. 2050-01-01.
+-- preprocess calendar dates
+cal_dates AS (
+    SELECT
+        * EXCEPT (date)
+        , date AS service_date
+    FROM (
+        -- deduplicate calendar_dates, which does not have a unique id column
+        -- and has identical entries in rare cases
+        SELECT DISTINCT * FROM calendar_dates_clean
+    )
+),
+-- for each day in our date calendar, get service entries that existed
+-- in the feed on that day (for that day).
+cal_dates_daily AS (
+    SELECT
+      t1.*
+    FROM cal_dates t1
+    JOIN dim_date t2
+      ON t1.calitp_extracted_at <= t2.full_date
+        AND t1.calitp_deleted_at > t2.full_date
+        AND t1.service_date = t2.full_date
+),
+-- inclusions from calendar_dates
+date_include AS (
+    SELECT
+      calitp_itp_id
+      , calitp_url_number
+      , service_id
+      , service_date
+      , calitp_extracted_at
+      , calitp_deleted_at
+      , TRUE AS service_inclusion
+    FROM cal_dates_daily
+    WHERE cal_dates_daily.exception_type = "1"
+  ),
+
+  -- exclusions from calendar_dates
+  date_exclude AS (
+    SELECT
+      calitp_itp_id
+      , calitp_url_number
+      , service_id
+      , service_date
+      , calitp_extracted_at
+      , calitp_deleted_at
+      , TRUE AS service_exclusion
+    FROM cal_dates_daily
+    WHERE cal_dates_daily.exception_type = "2"
+  ),
+-- for the active calendar entries on a each date (e.g. from the latest feed
+-- on that date), get the service indicator (0 for out of service, 1 for in).
+calendar_daily AS (
+    SELECT
+      t1.* EXCEPT(start_date, end_date, day_name)
+      , t1.start_date AS service_start_date
+      , t1.end_date AS service_end_date
+      , t2.full_date AS service_date
+    FROM  gtfs_schedule_stg_calendar_long t1
+    JOIN dim_date t2
+      ON
+        -- use full_date to get active schedule on that date, and ensure
+        -- that entries have same day_name
+        t1.calitp_extracted_at <= t2.full_date
+        AND COALESCE(t1.calitp_deleted_at, DATE("2099-01-01")) > t2.full_date
+        AND t1.day_name = t2.day_name
+    WHERE
+        -- Service date (full_date) must be between service start and end dates
+        t1.start_date <= t2.full_date
+        AND COALESCE(t1.end_date, DATE("2099-01-01")) >= t2.full_date
+),
+gtfs_schedule_stg_daily_service AS (
+    SELECT
+    * EXCEPT(calitp_extracted_at, calitp_deleted_at)
+    , (service_indicator="1" AND NOT COALESCE(service_exclusion, FALSE))
+        OR COALESCE(service_inclusion, FALSE)
+        AS is_in_service
+        -- Need to coalesce by each individual column of calendar_daily, date_include, and date_exclude tables to account for null values,
+        -- then select the greatest/least to populate extracted/deleted at. Cannot have it as one coalesce because it selects sequentially.
+        -- Chose to use latest extracted date/ earliest deleted date for the most conservative option.
+        , GREATEST(
+                COALESCE(t1.calitp_extracted_at, DATE("1900-01-01"))
+                , COALESCE(t2.calitp_extracted_at, DATE("1900-01-01"))
+                , COALESCE(t3.calitp_extracted_at, DATE("1900-01-01")))
+                AS calitp_extracted_at
+        , LEAST(
+                COALESCE(t1.calitp_deleted_at, DATE("2100-01-01"))
+                , COALESCE(t2.calitp_deleted_at, DATE("2100-01-01"))
+                , COALESCE(t3.calitp_deleted_at, DATE("2100-01-01")))
+                AS calitp_deleted_at
+    FROM calendar_daily t1
+    FULL JOIN date_include t2 USING(calitp_itp_id, calitp_url_number, service_id, service_date)
+    FULL JOIN date_exclude t3 USING(calitp_itp_id, calitp_url_number, service_id, service_date)
+    -- TODO: remove hardcoding--set this to be 1 month in the future, etc..
+    WHERE service_date < DATE_ADD(CURRENT_DATE(), INTERVAL 1 YEAR)
+)
+
+SELECT * FROM gtfs_schedule_stg_daily_service
