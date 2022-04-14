880a3d49727be92674b7a767eca1752555a01d9e
Laurie Merrell <laurie.m@jarv.us>
2022-04-14 09:10:00 -0500

    gffs views --> dbt: gtfs_schedule_dim_routes & pathways comment fix

--- warehouse/models/gtfs_views/gtfs_schedule_dim_pathways.sql
+++ warehouse/models/gtfs_views/gtfs_schedule_dim_pathways.sql
@@ -1,7 +1,5 @@
 {{ config(materialized='table') }}
 
--- from https://gist.github.com/ewhauser/d7dd635ad2d4b20331c7f18038f04817
-
 WITH pathways_clean AS (
     SELECT *
     FROM {{ ref('pathways_clean') }}
