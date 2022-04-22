ad6cdcfa86e3c54e9fbe156ecf8aa7dd34dc965f
Laurie Merrell <laurie.m@jarv.us>
2022-04-22 17:32:19 -0500

    rt views --> dbt: wip -- refactor errors table based on log structure changes

--- warehouse/models/rt_views/gtfs_rt_extraction_errors.sql
+++ warehouse/models/rt_views/gtfs_rt_extraction_errors.sql
@@ -1,19 +1,45 @@
 {{ config(materialized='table') }}
 
-WITH stdout AS (
+--TODO:
+-- join to get itp id, url number, date
+-- join to get safe url based on the above
+-- add safe url, itp id, url number to final output
+
+WITH error_stdout AS (
     SELECT *
     FROM {{ source('gtfs_rt_logs', 'gtfs_schedule_fact_daily_feeds') }}
+    WHERE textPayload LIKE "%error fetching url%"
+),
+
+calitp_feeds AS (
+    SELECT *, _FILE_NAME
+    FROM {{ source('gtfs_rt_schedule_history',
+    'calitp_feeds') }}
+),
+
+calitp_feeds_raw AS (
+    SELECT *, _FILE_NAME
+    FROM {{ source('gtfs_rt_schedule_history',
+    'calitp_feeds') }}
 ),
 
 download_issues AS (
     SELECT
-        textPayload,
         timestamp,
+        -- this URL contains API keys
         REGEXP_EXTRACT(
             textPayload,
-            "error fetching url ([\S+]): ")
-            AS url,
+            -- extract the full URL
+            -- it's a string that doesn't contain spaces hence \S
+            r"error fetching url (\S+): ")
+            AS unsafe_url,
+        REGEXP_EXTRACT(
+            textPayload,
+            -- extract the error itself
+            -- we know it comes right after the URL
+            -- plus colon and single space
+            r"error fetching url [\S]+: (.+)")
+        AS error
     -- note that we've moved the logs to gtfs_rt_logs.stdout, since the table name can't be changed
-    FROM `cal-itp-data-infra.gtfs_rt_logs.stdout`
-    WHERE textPayload LIKE "%error fetching url%"
+    FROM error_stdout
 )
