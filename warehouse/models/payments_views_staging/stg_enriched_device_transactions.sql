371aafccc5193b3ef22f52b7d8ad258c7e11df86
Mjumbe Poe <mjumbewu@gmail.com>
2022-05-03 12:41:35 -0400

    Add device_transactions metadata and update enrich SQL

--- warehouse/models/payments_views_staging/stg_enriched_device_transactions.sql
+++ warehouse/models/payments_views_staging/stg_enriched_device_transactions.sql
@@ -1,26 +1,16 @@
----
-description: "This task makes the assumption that, in the case of duplicated littlepay_transaction_id values, the one with the latest transaction_date_time_utc in the latest export file takes precedence."
-operator: operators.SqlToWarehouseOperator
-dst_table_name: "payments.stg_enriched_device_transactions"
+{{ config(materialized='table') }}
 
-external_dependencies:
-  - payments_loader: all
----
-
-WITH
-
-enriched AS (
+WITH stg_enriched_device_transaction_purchases AS (
     {{
-
-      sql_enrich_duplicates(
-        "payments.device_transactions",
-        ["littlepay_transaction_id"],
-        ["calitp_file_name desc", "transaction_date_time_utc desc"]
-      )
-
+        sql_enrich_duplicates(
+            source('payments', 'device_transactions'),
+            ['littlepay_transaction_id'],
+            ['calitp_file_name desc', 'transaction_date_time_utc desc']
+        )
     }}
 )
 
-SELECT *,
-       DATETIME(TIMESTAMP(transaction_date_time_utc), "America/Los_Angeles") AS transaction_date_time_pacific,
-FROM enriched
+SELECT
+    *,
+    DATETIME(TIMESTAMP(transaction_date_time_utc), "America/Los_Angeles") AS transaction_date_time_pacific,
+FROM stg_enriched_device_transaction_purchases
