10671d0fc03d910a414bef1605b348f63ce7afb2
Mjumbe Poe <mjumbewu@gmail.com>
2022-05-03 16:28:04 -0400

    Add micropayment_device_transactions metadata and update enrich SQL

--- warehouse/models/payments_views_staging/stg_enriched_micropayment_device_transactions.sql
+++ warehouse/models/payments_views_staging/stg_enriched_micropayment_device_transactions.sql
@@ -1,17 +1,13 @@
----
-operator: operators.SqlToWarehouseOperator
-dst_table_name: "payments.stg_enriched_micropayment_device_transactions"
+{{ config(materialized='table') }}
 
-external_dependencies:
-  - payments_loader: all
----
+WITH stg_enriched_micropayment_device_transactions AS (
+    {{
+        sql_enrich_duplicates(
+            source('payments', 'micropayment_device_transactions'),
+            ['calitp_hash'],
+            ['calitp_file_name desc']
+        )
+    }}
+)
 
-{{
-
-  sql_enrich_duplicates(
-    "payments.micropayment_device_transactions",
-    ["calitp_hash"],
-    ["calitp_file_name desc"]
-  )
-
-}}
+SELECT * FROM stg_enriched_micropayment_device_transactions
