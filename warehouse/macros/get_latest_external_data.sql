03eae30e8242375c35aa2c29fca9aed683a31347
Laurie Merrell <laurie.m@jarv.us>
2022-05-16 17:18:36 -0500

    airtable: fix macro comma

--- warehouse/macros/get_latest_external_data.sql
+++ warehouse/macros/get_latest_external_data.sql
@@ -4,7 +4,7 @@ WITH ranked AS (
     SELECT *,
         DENSE_RANK() OVER (ORDER BY {{ columns }}) as rank
     FROM {{ external_table_name }}
-),
+)
 SELECT * FROM ranked WHERE rank = 1
 
 {% endmacro %}
