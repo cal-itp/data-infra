f1f8f1dbf730a06cb45cb4d13a7d53daa205fabe
Andrew Vaccaro <atvaccaro@gmail.com>
2022-05-18 14:30:08 -0400

    actually generate maptiles

--- warehouse/scripts/publish.py
+++ warehouse/scripts/publish.py
@@ -1,6 +1,8 @@
 """
 Publishes various dbt models to various sources.
 """
+import subprocess
+
 import enum
 
 import json
@@ -10,9 +12,11 @@ import shapely.wkt
 import tempfile
 
 import gcsfs
+import geopandas as gpd
 import humanize
 import pandas as pd
 import requests
+import swifter  # noqa
 import typer
 from sqlalchemy import create_engine
 
@@ -25,9 +29,13 @@ from scripts.dbt_artifacts import (
 )
 from tqdm import tqdm
 
+tqdm.pandas()
+
 API_KEY = os.environ.get("CALITP_CKAN_GTFS_SCHEDULE_KEY")
 
 app = typer.Typer()
+
+WGS84 = "EPSG:4326"
 
 
 # Taken from the calitp repo which we can't install because of deps issue
@@ -95,6 +103,7 @@ def _publish_exposure(project: str, bucket: str, exposure: Exposure, dry_run: bo
                 for model in exposure.depends_on.nodes:
                     node = Node._instances[model]
 
+                    geojson_fpath = os.path.join(tmpdir, f"{node.name}.geojson")
                     fpath = os.path.join(tmpdir, destination.filename(node.name))
 
                     # from https://gist.github.com/drmalex07/5a54fc4f1db06a66679e
@@ -103,18 +112,36 @@ def _publish_exposure(project: str, bucket: str, exposure: Exposure, dry_run: bo
                         project_id=project,
                         progress_bar_type="tqdm",
                     )
-                    df.pt_array.apply(
-                        lambda arr: [
-                            shapely.geometry.mapping(shapely.wkt.loads(p))
-                            for p in tqdm(arr)
-                        ]
-                    )
-                    df.to_csv(fpath, index=False)
-                    typer.secho(
-                        f"selected {len(df)} rows ({humanize.naturalsize(os.stat(fpath).st_size)}) from {model_name}"
-                    )
+                    # typer.secho(
+                    #     f"selected {len(df)} rows ({humanize.naturalsize(os.stat(fpath).st_size)}) from {model_name}"
+                    # )
 
-                    hive_path = destination.hive_path(destination, model_name, bucket)
+                    def make_linestring(x):
+
+                        # shapely errors if the array contains only one point
+                        if len(x) > 1:
+                            # each point in the array is wkt
+                            # so convert them to shapely points via list comprehension
+                            as_wkt = [shapely.wkt.loads(i) for i in x]
+                            return shapely.geometry.LineString(as_wkt)
+
+                    # apply the function
+                    df["geometry"] = df.pt_array.swifter.apply(make_linestring)
+                    gdf = gpd.GeoDataFrame(df, geometry="geometry", crs=WGS84)
+                    # gdf = gdf.to_crs(WGS84)
+                    gdf[["geometry"]].to_file(geojson_fpath, driver="GeoJSON")
+
+                    subprocess.run(
+                        [
+                            "tippecanoe",
+                            "-zg",
+                            "-o",
+                            fpath,
+                            geojson_fpath,
+                        ]
+                    ).check_returncode()
+
+                    hive_path = destination.hive_path(destination, node.name, bucket)
 
                     if dry_run:
                         typer.secho(
