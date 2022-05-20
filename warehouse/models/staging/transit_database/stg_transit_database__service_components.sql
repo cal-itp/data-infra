2c5b390e45ede0b62ddc47a528f476e9259314b2
Laurie Merrell <laurie.m@jarv.us>
2022-05-20 13:29:15 -0500

    airtable: tts service-components table

---
+++ warehouse/models/staging/transit_database/stg_transit_database__service_components.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+WITH
+stg_transit_database__service_components AS (
+    {{ get_latest_external_data(
+        external_table_name = source('airtable', 'transit_technology_stacks__service_components'),
+        columns = 'dt DESC, time DESC'
+        ) }}
+)
+
+SELECT * FROM stg_transit_database__service_components
