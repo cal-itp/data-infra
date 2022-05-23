fa72b768ce0f7b073170e30956933043b1478880
Laurie Merrell <laurie.m@jarv.us>
2022-05-23 17:00:57 -0500

    airtable: tts properties and features

---
+++ warehouse/models/staging/transit_database/stg_transit_database__properties_features.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+WITH
+stg_transit_database__properties_features AS (
+    {{ get_latest_external_data(
+        external_table_name = source('airtable', 'transit_technology_stacks__properties_and_features'),
+        columns = 'dt DESC, time DESC'
+        ) }}
+)
+
+SELECT * FROM stg_transit_database__properties_features
