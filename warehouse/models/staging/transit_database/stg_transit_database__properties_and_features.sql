dbbe1ecaabe0fe8d0fb9c959ec572e9eea9ad626
Laurie Merrell <laurie.m@jarv.us>
2022-05-26 10:31:38 -0500

    airtable: tts components -- finish mart models

--- warehouse/models/staging/transit_database/stg_transit_database__properties_and_features.sql
+++ warehouse/models/staging/transit_database/stg_transit_database__properties_and_features.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='table') }}
 
 WITH
-stg_transit_database__properties_features AS (
+stg_transit_database__properties_and_features AS (
     {{ get_latest_external_data(
         external_table_name = source('airtable', 'transit_technology_stacks__properties_and_features'),
         columns = 'dt DESC, time DESC'
         ) }}
 )
 
-SELECT * FROM stg_transit_database__properties_features
+SELECT * EXCEPT(name), name as property_feature_name FROM stg_transit_database__properties_and_features
