183851a9a5fc18f1a370aa73a4d63e672c823cd1
Natalya Diaz <natalya.d@jarv.us>
2022-06-01 13:12:59 -0400

    re-added file

--- warehouse/models/gtfs_views/gtfs_schedule_fact_daily_feeds.sql
+++ warehouse/models/gtfs_views/gtfs_schedule_fact_daily_feeds.sql
@@ -27,6 +27,11 @@ gtfs_schedule_dim_feeds AS (
     FROM {{ ref('gtfs_schedule_dim_feeds') }}
 ),
 
+gtfs_schedule_service AS (
+    SELECT *
+    FROM {{ ref('gtfs_schedule_fact_daily_service') }}
+),
+
 dim_date AS (
     SELECT *
     FROM {{ ref('dim_date') }}
@@ -98,6 +103,8 @@ daily_feed_join AS (
     LEFT JOIN daily_service USING (feed_key)
 ),
 
+
+
 -- Extract raw feeds URLs
 
 raw_feed_urls AS (
@@ -110,12 +117,13 @@ raw_feed_urls AS (
     FROM calitp_feeds_raw
 ),
 
+
 -- join in whether or not a feed download succeeded on a given day and also the raw feed
--- URLs
+    -- URLs
 
 feed_status AS (
     SELECT
-        daily_feeds.*,
+        daily_feed_join.*,
         CASE WHEN download_status.status = "success" THEN "success" ELSE "error"
         END
         AS extraction_status,
@@ -124,22 +132,22 @@ feed_status AS (
         raw_feed_urls.gtfs_rt_vehicle_positions_url AS raw_gtfs_rt_vehicle_positions_url,
         raw_feed_urls.gtfs_rt_service_alerts_url AS raw_gtfs_rt_service_alerts_url,
         raw_feed_urls.gtfs_rt_trip_updates_url AS raw_gtfs_rt_trip_updates_url
-    FROM daily_feeds
+    FROM daily_feed_join
     LEFT JOIN calitp_status AS download_status
         ON
-            daily_feeds.calitp_itp_id = download_status.itp_id
-            AND daily_feeds.calitp_url_number = download_status.url_number
-            AND daily_feeds.date = download_status.calitp_extracted_at
+            daily_feed_join.calitp_itp_id = download_status.itp_id
+            AND daily_feed_join.calitp_url_number = download_status.url_number
+            AND daily_feed_join.date = download_status.calitp_extracted_at
     LEFT JOIN calitp_feed_parse_result AS parse_result
         ON
-            daily_feeds.calitp_itp_id = parse_result.calitp_itp_id
-            AND daily_feeds.calitp_url_number = parse_result.calitp_url_number
-            AND daily_feeds.date = parse_result.calitp_extracted_at
+            daily_feed_join.calitp_itp_id = parse_result.calitp_itp_id
+            AND daily_feed_join.calitp_url_number = parse_result.calitp_url_number
+            AND daily_feed_join.date = parse_result.calitp_extracted_at
     LEFT JOIN raw_feed_urls
         ON
-            daily_feeds.calitp_itp_id = raw_feed_urls.itp_id
-            AND daily_feeds.calitp_url_number = raw_feed_urls.url_number
-            AND daily_feeds.date = raw_feed_urls.extract_date
+            daily_feed_join.calitp_itp_id = raw_feed_urls.itp_id
+            AND daily_feed_join.calitp_url_number = raw_feed_urls.url_number
+            AND daily_feed_join.date = raw_feed_urls.extract_date
 ),
 
 -- join in whether or not a feed updated on this day
@@ -148,7 +156,6 @@ feed_updated AS (
 
     SELECT
         T1.*,
-
         -- Calculate days since update, by forward filling extracted at
         DATE_DIFF(
             T1.date,
