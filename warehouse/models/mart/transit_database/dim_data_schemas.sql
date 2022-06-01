c11dc05499dd24d31d06415555ca0fc99eb1f3c0
Laurie Merrell <laurie.m@jarv.us>
2022-06-01 15:00:02 -0500

    airtable: add data schemas mart, staging

---
+++ warehouse/models/mart/transit_database/dim_data_schemas.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+WITH stg_transit_database__data_schemas AS (
+    SELECT * FROM {{ ref('stg_transit_database__data_schemas') }}
+),
+
+dim_data_schemas AS (
+    SELECT
+        data_schema_id,
+        data_schema_name,
+        status,
+        calitp_extracted_at
+    FROM stg_transit_database__data_schemas
+)
+
+SELECT * FROM dim_data_schemas
