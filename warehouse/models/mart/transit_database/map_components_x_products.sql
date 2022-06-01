6c923db386a85f8a7032a6b37db3c15fb8be462b
Laurie Merrell <laurie.m@jarv.us>
2022-06-01 15:44:56 -0500

    airtable: refactor -- name all pks 'id'

--- warehouse/models/mart/transit_database/map_components_x_products.sql
+++ warehouse/models/mart/transit_database/map_components_x_products.sql
@@ -11,11 +11,13 @@ stg_transit_database__products AS (
 map_components_products_x_products_components AS (
  {{ transit_database_many_to_many(
      table_a = 'stg_transit_database__components',
-     table_a_id_col = 'component_id',
+     table_a_id_col = 'id',
+     table_a_id_col_name = 'component_id',
      table_a_name_col = 'component_name',
      table_a_join_col = 'products',
      table_b = 'stg_transit_database__products',
      table_b_id_col = 'product_id',
+     table_b_id_col_name = 'product_id',
      table_b_name_col = 'product_name',
      table_b_join_col = 'components'
  ) }}
