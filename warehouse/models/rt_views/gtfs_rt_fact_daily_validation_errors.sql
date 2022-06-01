e1da75756bc8c70e02f319505cb33bf67c993899
Andrew Vaccaro <atvaccaro@gmail.com>
2022-06-01 17:57:53 -0400

    start using new RT external tables and unnest the data

--- warehouse/models/rt_views/gtfs_rt_fact_daily_validation_errors.sql
+++ warehouse/models/rt_views/gtfs_rt_fact_daily_validation_errors.sql
@@ -1,34 +1,11 @@
 {{ config(materialized='table') }}
 
-WITH validation_service_alerts AS (
-    SELECT *
-    FROM {{ source('gtfs_rt_raw','validation_service_alerts') }}
-),
-
-validation_trip_updates AS (
-    SELECT *
-    FROM {{ source('gtfs_rt_raw','validation_trip_updates') }}
-),
-
-validation_vehicle_positions AS (
-    SELECT *
-    FROM {{ source('gtfs_rt_raw','validation_vehicle_positions') }}
+WITH errors AS (
+    SELECT * FROM {{ ref('stg_rt_validation_errors') }}
 ),
 
 gtfs_schedule_dim_feeds AS (
-    SELECT *
-    FROM {{ ref('gtfs_schedule_dim_feeds') }}
-),
-
-unioned AS (
-    SELECT *
-    FROM validation_service_alerts
-    UNION ALL
-    SELECT *
-    FROM validation_trip_updates
-    UNION ALL
-    SELECT *
-    FROM validation_vehicle_positions
+    SELECT * FROM {{ ref('gtfs_schedule_dim_feeds') }}
 ),
 
 error_counts AS (
@@ -37,20 +14,15 @@ error_counts AS (
         calitp_url_number,
         rt_feed_type,
         error_id,
-        DATE(calitp_extracted_at) AS date,
-        SUM(n_occurrences) AS occurrences
-    FROM unioned
-    GROUP BY
-        calitp_itp_id,
-        calitp_url_number,
-        rt_feed_type,
-        error_id,
-        date
+        date,
+        COUNT(*) AS occurrences
+    FROM errors
+    GROUP BY 1, 2, 3, 4, 5
 ),
+
 -- join with schedule dim feeds to get feed key
 -- note that this matching is imperfect; the schedule that is used for validation
 -- is actually pulled from gtfs_schedule_history.calitp_feed_status
-
 gtfs_rt_fact_daily_validation_errors AS (
     SELECT
         t1.*,
