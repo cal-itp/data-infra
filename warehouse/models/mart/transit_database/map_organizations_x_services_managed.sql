485296d47b6f5a5871ce5a3d0ec90cca138f700e
Laurie Merrell <laurie.m@jarv.us>
2022-06-03 08:52:04 -0500

    airtable: missing key relabels and finish properties & features

--- warehouse/models/mart/transit_database/map_organizations_x_services_managed.sql
+++ warehouse/models/mart/transit_database/map_organizations_x_services_managed.sql
@@ -11,14 +11,14 @@ stg_transit_database__services AS (
 map_organizations_x_services AS (
  {{ transit_database_many_to_many(
      table_a = 'stg_transit_database__organizations',
-     table_a_id_col = 'key',
-     table_a_id_col_name = 'organization_key',
+     table_a_key_col = 'key',
+     table_a_key_col_name = 'organization_key',
      table_a_name_col = 'name',
      table_a_name_col_name = 'organization_name',
      table_a_join_col = 'mobility_services_managed',
      table_b = 'stg_transit_database__services',
-     table_b_id_col = 'key',
-     table_b_id_col_name = 'service_key',
+     table_b_key_col = 'key',
+     table_b_key_col_name = 'service_key',
      table_b_name_col = 'name',
      table_b_name_col_name = 'service_name',
      table_b_join_col = 'provider'
