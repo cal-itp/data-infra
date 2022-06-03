54ffc6319a82e66daa16da5bb1a2afbdf82ae2df
Laurie Merrell <laurie.m@jarv.us>
2022-06-03 08:32:27 -0500

    airtable: rename id to key; start properties and features

--- warehouse/models/staging/transit_database/stg_transit_database__properties_and_features.sql
+++ warehouse/models/staging/transit_database/stg_transit_database__properties_and_features.sql
@@ -1,11 +1,23 @@
 {{ config(materialized='table') }}
 
 WITH
-stg_transit_database__properties_and_features AS (
+latest AS (
     {{ get_latest_external_data(
         external_table_name = source('airtable', 'transit_technology_stacks__properties_and_features'),
         columns = 'dt DESC, time DESC'
         ) }}
+),
+
+stg_transit_database__properties_and_features AS (
+    SELECT
+        property_feature_id AS key,
+        name,
+        recommended_value,
+        considerations,
+        details,
+        available_in_components,
+        dt AS calitp_extracted_at
+    FROM latest
 )
 
 SELECT * FROM stg_transit_database__properties_and_features
