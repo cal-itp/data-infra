23a1a9a712363bb80ae657508de6ee8d96199af4
Laurie Merrell <laurie.m@jarv.us>
2022-06-15 12:50:28 -0500

    airtable: review comments -- update the synced table mapping macro

--- warehouse/macros/transit_database_synced_table_id_mapping.sql
+++ warehouse/macros/transit_database_synced_table_id_mapping.sql
@@ -1,17 +1,32 @@
 {% macro transit_database_synced_table_id_mapping(
-    table_a,
-    base_a,
-    table_a_id_col,
-    table_a_name_col,
-    table_a_date_col,
-    table_b,
-    base_b,
-    table_b_id_col,
-    table_b_name_col,
-    table_b_date_col,
-    shared_id_name,
-    shared_name_name,
-    shared_date_name) %}
+    table_a_dict,
+    table_b_dict,
+    shared_id_name = 'key',
+    shared_name_name = 'name',
+    shared_date_name = 'date') %}
+
+    -- Takes as input two instances of a synced table from Airtable
+    -- and creates a mapping table to associated the internal Airtable IDs between
+    -- the two instances. Join is performed using the column listed in the
+    -- name_col parameter.
+    -- Table dict entries:
+    -- table_name: CTE or table name to be used (one instance of the synced table)
+    -- base: Name or abbreviation for the base that will be used to prefix records;
+    --  ex: "ct" for the California Transit base
+    -- id_col: Name of the internal Airtable ID column in this table
+    -- name_col: Name of the primary field column in this table
+    -- date_col: Name of the column containing the date that the table was extracted
+
+    {% set table_a = table_a_dict.get('table_name') %}
+    {% set base_a = table_a_dict.get('base') %}
+    {% set table_a_id_col = table_a_dict.get('id_col', 'key') %}
+    {% set table_a_name_col = table_a_dict.get('name_col', 'name') %}
+    {% set table_a_date_col = table_a_dict.get('date_col', 'dt') %}
+    {% set table_b = table_b_dict.get('table_name') %}
+    {% set base_b = table_b_dict.get('base') %}
+    {% set table_b_id_col = table_b_dict.get('id_col', 'key') %}
+    {% set table_b_name_col = table_b_dict.get('name_col', 'name') %}
+    {% set table_b_date_col = table_b_dict.get('date_col', 'dt') %}
 
    SELECT DISTINCT
         {{ base_a }}.{{ table_a_id_col }} AS {{ base_a }}_{{ shared_id_name }},
--- warehouse/models/staging/transit_database/int/int_tts_services_ct_services_map.sql
+++ warehouse/models/staging/transit_database/int/int_tts_services_ct_services_map.sql
@@ -14,19 +14,14 @@ tts_services AS (
 
 int_tts_services_ct_services_map AS (
     {{ transit_database_synced_table_id_mapping(
-        table_a = 'ct_services',
-        base_a = 'ct',
-        table_a_id_col = 'service_id',
-        table_a_name_col = 'name',
-        table_a_date_col = 'dt',
-        table_b = 'tts_services',
-        base_b = 'tts',
-        table_b_id_col = 'service_id',
-        table_b_name_col = 'name',
-        table_b_date_col = 'dt',
-        shared_id_name = 'key',
-        shared_name_name = 'name',
-        shared_date_name = 'date') }}
+        table_a_dict = {'table_name': 'ct_services',
+            'base': 'ct',
+            'id_col': 'service_id'
+        },
+        table_b_dict = {'table_name': 'tts_services',
+            'base': 'tts',
+            'id_col': 'service_id'
+        }) }}
 )
 
 SELECT * FROM int_tts_services_ct_services_map
