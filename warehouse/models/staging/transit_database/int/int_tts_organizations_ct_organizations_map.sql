8866b3a421b3a4d50d60a76c186c8192478fc2f5
Laurie Merrell <laurie.m@jarv.us>
2022-06-15 12:50:28 -0500

    airtable: review comments -- update the synced table mapping macro

--- warehouse/models/staging/transit_database/int/int_tts_organizations_ct_organizations_map.sql
+++ warehouse/models/staging/transit_database/int/int_tts_organizations_ct_organizations_map.sql
@@ -14,19 +14,15 @@ tts_organizations AS (
 
 int_tts_organizations_ct_organizations_map AS (
     {{ transit_database_synced_table_id_mapping(
-        table_a = 'ct_organizations',
-        base_a = 'ct',
-        table_a_id_col = 'organization_id',
-        table_a_name_col = 'name',
-        table_a_date_col = 'dt',
-        table_b = 'tts_organizations',
-        base_b = 'tts',
-        table_b_id_col = 'organization_id',
-        table_b_name_col = 'name',
-        table_b_date_col = 'dt',
-        shared_id_name = 'key',
-        shared_name_name = 'name',
-        shared_date_name = 'date') }}
+        table_a_dict = {'table_name': 'ct_organizations',
+            'base': 'ct',
+            'id_col': 'organization_id'
+        },
+        table_b_dict = {'table_name': 'tts_organizations',
+            'base': 'tts',
+            'id_col': 'organization_id'
+        })
+    }}
 )
 
 SELECT * FROM int_tts_organizations_ct_organizations_map
