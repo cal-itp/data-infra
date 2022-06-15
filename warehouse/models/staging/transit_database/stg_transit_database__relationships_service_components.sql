4149b1b5e68a03d934e02447119df7144070440b
Laurie Merrell <laurie.m@jarv.us>
2022-06-15 11:22:46 -0500

    airtable: review comment - rename parameter in macro to be clearer

--- warehouse/models/staging/transit_database/stg_transit_database__relationships_service_components.sql
+++ warehouse/models/staging/transit_database/stg_transit_database__relationships_service_components.sql
@@ -4,7 +4,7 @@ WITH
 stg_transit_database__relationships_service_components AS (
     {{ get_latest_external_data(
         external_table_name = source('airtable', 'transit_technology_stacks__relationships_service_components'),
-        columns = 'dt DESC, time DESC'
+        order_by = 'dt DESC, time DESC'
         ) }}
 )
 
