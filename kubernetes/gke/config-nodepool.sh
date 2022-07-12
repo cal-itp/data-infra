8e5c8072c6f387b47de3f9a6ae3aedf40dc10180
Andrew Vaccaro <atvaccaro@gmail.com>
2022-07-12 11:04:10 -0400

    switch rt node pool to c2 instances

--- kubernetes/gke/config-nodepool.sh
+++ kubernetes/gke/config-nodepool.sh
@@ -1,6 +1,6 @@
 GKE_NODEPOOL_NAMES=(
   'apps-v2'
-  'gtfsrt-v2'
+  'gtfsrt-v3'
   'jupyterhub-users'
   'jobs-v1'
 )
@@ -8,7 +8,7 @@ GKE_NODEPOOL_NAMES=(
 declare -A GKE_NODEPOOL_NODE_COUNTS
 GKE_NODEPOOL_NODE_COUNTS=(
   ['apps-v2']=1
-  ['gtfsrt-v2']=1
+  ['gtfsrt-v3']=1
   ['jupyterhub-users']=1
   ['jobs-v1']=1
 )
@@ -16,7 +16,7 @@ GKE_NODEPOOL_NODE_COUNTS=(
 declare -A GKE_NODEPOOL_NODE_LOCATIONS
 GKE_NODEPOOL_NODE_LOCATIONS=(
   ['apps-v2']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
-  ['gtfsrt-v2']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
+  ['gtfsrt-v3']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
   ['jupyterhub-users']=$GKE_REGION-a,$GKE_REGION-b,$GKE_REGION-c
   ['jobs-v1']=$GKE_REGION-a
 )
@@ -24,21 +24,21 @@ GKE_NODEPOOL_NODE_LOCATIONS=(
 declare -A GKE_NODEPOOL_MACHINE_TYPES
 GKE_NODEPOOL_MACHINE_TYPES=(
   ['apps-v2']=n1-standard-4
-  ['gtfsrt-v2']=n2-highcpu-16
+  ['gtfsrt-v3']=c2-standard-8
   ['jupyterhub-users']=e2-highmem-2
   ['jobs-v1']=c2-standard-4
 )
 
 declare -A GKE_NODEPOOL_TAINTS
 GKE_NODEPOOL_TAINTS=(
-  ['gtfsrt-v2']='resource-domain=gtfsrt:NoSchedule'
+  ['gtfsrt-v3']='resource-domain=gtfsrt:NoSchedule'
   ['jupyterhub-users']='hub.jupyter.org/dedicated=user'
   ['jobs-v1']='pod-role=computetask:NoSchedule'
 )
 
 declare -A GKE_NODEPOOL_LABELS
 GKE_NODEPOOL_LABELS=(
-  ['gtfsrt-v2']='resource-domain=gtfsrt'
+  ['gtfsrt-v3']='resource-domain=gtfsrt'
   ['jupyterhub-users']='hub.jupyter.org/node-purpose=user'
   ['jobs-v1']='pod-role=computetask'
 )
