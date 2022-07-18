a4c1c04d5f60f713bffda6a8e15047bf6686f1ba
Andrew Vaccaro <atvaccaro@gmail.com>
2022-07-18 15:39:40 -0400

    get the keys from environ for now

--- services/gtfs-rt-archiver-v2/gtfs_rt_archiver_v2/tasks.py
+++ services/gtfs-rt-archiver-v2/gtfs_rt_archiver_v2/tasks.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 import google_crc32c
@@ -14,7 +15,8 @@ from .metrics import (
     FEEDS_DOWNLOADED,
     TASK_SIGNALS,
     HANDLE_TICK_PROCESSING_DELAY,
-    HANDLE_TICK_PROCESSED_BYTES, FEED_REQUEST_FAILURES,
+    HANDLE_TICK_PROCESSED_BYTES,
+    FEED_REQUEST_FAILURES,
 )
 
 
@@ -45,27 +47,34 @@ def instrument_signals(signal, task, exc=None):
     TASK_SIGNALS.labels(signal=signal, exc_type=str(exc)).inc()
 
 
-auth_dict = {key: None
-             for key in [
-                 "AC_TRANSIT_API_KEY",
-                 "MTC_511_API_KEY",
-                 "AMTRAK_GTFS_URL",
-                 "SWIFTLY_AUTHORIZATION_KEY_CALITP",
-             ]}
+auth_dict = {
+    key: None
+    for key in [
+        "AC_TRANSIT_API_KEY",
+        "MTC_511_API_KEY",
+        "AMTRAK_GTFS_URL",
+        "SWIFTLY_AUTHORIZATION_KEY_CALITP",
+    ]
+}
 
 # TODO: can this be in an on_startup() hook? seems to block with greenlet
 print("attempting to populate auth_dict")
 secret_client = secretmanager.SecretManagerServiceClient()
 for key in auth_dict:
-    name = f"projects/cal-itp-data-infra/secrets/{key}/versions/latest"
-    response = secret_client.access_secret_version(request={"name": name})
+    try:
+        auth_dict[key] = os.environ[key]
+    except KeyError:
+        # TODO: does not work :( something to do with fork and grpc
+        print(f"fetching {key}")
+        name = f"projects/cal-itp-data-infra/secrets/{key}/versions/latest"
+        response = secret_client.access_secret_version(request={"name": name})
 
-    crc32c = google_crc32c.Checksum()
-    crc32c.update(response.payload.data)
-    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-        raise ValueError(f"Data corruption detected for secret {name}.")
+        crc32c = google_crc32c.Checksum()
+        crc32c.update(response.payload.data)
+        if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+            raise ValueError(f"Data corruption detected for secret {name}.")
 
-    auth_dict[key] = response.payload.data.decode("UTF-8")
+        auth_dict[key] = response.payload.data.decode("UTF-8")
 
 
 @huey.task(expires=5)
@@ -87,5 +96,5 @@ def fetch(tick: datetime, record: AirtableGTFSDataRecord):
             content_type="application/octet-stream",
             client=client,
         )
-        HANDLE_TICK_PROCESSED_BYTES.labels(url).inc(len(content))
-        FEEDS_DOWNLOADED.labels(url=url).inc()
+        HANDLE_TICK_PROCESSED_BYTES.labels(record.uri).inc(len(content))
+        FEEDS_DOWNLOADED.labels(url=record.uri).inc()
