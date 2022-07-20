476a81d2e1ca393a250d751ae314aaab248d0c0a
Andrew Vaccaro <atvaccaro@gmail.com>
2022-07-20 13:47:52 -0400

    wip on validation in new schedule pipeline

---
+++ jobs/gtfs-schedule-validator/gtfs_schedule_validator.py
@@ -0,0 +1,115 @@
+__version__ = '0.1.0'
+
+import json
+import os
+import subprocess
+import tempfile
+from datetime import datetime
+from pathlib import Path
+from typing import Dict
+
+import pendulum
+import typer
+from calitp.storage import fetch_all_in_partition, GTFSFeedExtractInfo, get_fs, GTFSScheduleFeedValidation
+
+JSONL_GZIP_EXTENSION = ".jsonl.gz"
+
+SCHEDULE_VALIDATOR_JAR_LOCATION_ENV_KEY = "GTFS_SCHEDULE_VALIDATOR_JAR"
+JAR_DEFAULT = typer.Option(
+    default=os.environ.get(SCHEDULE_VALIDATOR_JAR_LOCATION_ENV_KEY),
+    help="Path to the GTFS Schedule Validator JAR",
+)
+
+app = typer.Typer()
+
+
+def execute_schedule_validator(
+    fs,
+    zip_path: Path,
+    output_dir: Path,
+    jar_path: Path = os.environ.get(SCHEDULE_VALIDATOR_JAR_LOCATION_ENV_KEY),
+    verbose=False,
+) -> (Dict, Dict):
+    if not isinstance(zip_path, Path):
+        raise TypeError("must provide a path to the zip file")
+
+    args = [
+                "java",
+                "-jar",
+                str(jar_path),
+                "--input",
+                str(zip_path),
+                "--output_base",
+                str(output_dir),
+                "--feed_name",
+                "us-na",
+            ]
+
+    report_path = Path(output_dir) / "report.json"
+    system_errors_path = Path(output_dir) / "system_errors.json"
+
+    typer.secho(f"executing schedule validator: {' '.join(args)}")
+    subprocess.run(
+        args,
+        capture_output=True,
+    ).check_returncode()
+
+    with open(report_path) as f:
+        report = json.load(f)
+
+    with open(system_errors_path) as f:
+        system_errors = json.load(f)
+
+    return report, system_errors
+
+
+@app.command()
+def validate_extract(
+    zip_path: Path,
+    output_dir: Path,
+    jar_path: Path = os.environ.get(SCHEDULE_VALIDATOR_JAR_LOCATION_ENV_KEY),
+    verbose=False,
+) -> None:
+    """"""
+    execute_schedule_validator(
+        fs=get_fs(),
+        zip_path=zip_path,
+        output_dir=output_dir,
+        jar_path=jar_path,
+        verbose=verbose,
+    )
+
+
+@app.command()
+def validate_day(
+    day: datetime = typer.Argument(
+        default=pendulum.today(),
+        help="The date of data to validate.",
+        formats=["%Y-%m-%d"],
+    ),
+) -> None:
+    day = pendulum.instance(day).date()
+    print(day)
+    extracts = fetch_all_in_partition(
+        cls=GTFSFeedExtractInfo,
+        fs=get_fs(),
+        partitions={
+            "dt": day,
+        }
+    )
+
+    fs = get_fs()
+    outcomes = []
+
+    for extract in extracts:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            report, system_errors = execute_schedule_validator(
+                fs=fs,
+                zip_path=,
+                output_dir=tmp_dir,
+            )
+
+
+
+if __name__ == '__main__':
+    app()
