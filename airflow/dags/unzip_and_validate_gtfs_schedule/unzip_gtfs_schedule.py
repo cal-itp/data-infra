e39b9b6b0a42a641f1ade76069634abbac1f1f14
Laurie Merrell <laurie.m@jarv.us>
2022-08-23 17:35:24 -0500

    gtfs unzipper v2: working version with required functionality

--- airflow/dags/unzip_and_validate_gtfs_schedule/unzip_gtfs_schedule.py
+++ airflow/dags/unzip_and_validate_gtfs_schedule/unzip_gtfs_schedule.py
@@ -11,23 +11,20 @@ from pydantic import Field
 
 from calitp.storage import (
     fetch_all_in_partition,
-    GTFSFeedExtractInfo,
+    GTFSScheduleFeedExtract,
     get_fs,
     GTFSFeedType,
     PartitionedGCSArtifact,
     ProcessingOutcome,
 )
 
-# SCHEDULE_UNZIPPED_BUCKET = os.environ["CALITP_BUCKET__GTFS_SCHEDULE_UNZIPPED"]
-# SCHEDULE_RAW_BUCKET = os.environ["CALITP_BUCKET__GTFS_SCHEDULE_RAW"]
-# for testing:
-SCHEDULE_RAW_BUCKET = "test-calitp-gtfs-schedule-raw"
-SCHEDULE_UNZIPPED_BUCKET = "test-calitp-gtfs-schedule-unzipped"
+SCHEDULE_UNZIPPED_BUCKET = os.environ["CALITP_BUCKET__GTFS_SCHEDULE_UNZIPPED"]
+SCHEDULE_RAW_BUCKET = os.environ["CALITP_BUCKET__GTFS_SCHEDULE_RAW"]
 
 
 class GTFSScheduleFeedFile(PartitionedGCSArtifact):
     bucket: ClassVar[str] = SCHEDULE_UNZIPPED_BUCKET
-    partition_names: ClassVar[List[str]] = GTFSFeedExtractInfo.partition_names
+    partition_names: ClassVar[List[str]] = GTFSScheduleFeedExtract.partition_names
     ts: pendulum.DateTime
     base64_url: str
     zipfile_path: str
@@ -47,7 +44,8 @@ class GTFSScheduleFeedFile(PartitionedGCSArtifact):
 
 
 class GTFSScheduleFeedExtractUnzipOutcome(ProcessingOutcome):
-    extract: GTFSFeedExtractInfo = Field(..., exclude={"config"})
+    extract: GTFSScheduleFeedExtract = Field(..., exclude={"config"})
+    extract_md5hash: str
     zipfile_files: Optional[List[str]]
     zipfile_dirs: Optional[List[str]]
     extracted_files: Optional[List[GTFSScheduleFeedFile]]
@@ -87,9 +85,9 @@ def summarize_zip_contents(zip: zipfile.ZipFile, at: str = ""):
     return files, directories
 
 
-def process_feed_files(fs, extract_path, extract, feed_directory: str = ""):
+def process_feed_files(fs, extract, feed_directory: str = ""):
     zipfile_files = []
-    with fs.open(extract_path) as f:
+    with fs.open(extract.path) as f:
         zip = zipfile.ZipFile(f)
         for file in zipfile.Path(zip, at=feed_directory).iterdir():
             with zip.open(file.name) as f:
@@ -97,7 +95,7 @@ def process_feed_files(fs, extract_path, extract, feed_directory: str = ""):
             file_extract = GTFSScheduleFeedFile(
                 ts=extract.ts,
                 base64_url=extract.base64_url,
-                zipfile_path=extract_path,
+                zipfile_path=extract.path,
                 original_filename=file.name,
                 # TODO: do we need to make this safe?
                 filename=file.name,
@@ -108,25 +106,25 @@ def process_feed_files(fs, extract_path, extract, feed_directory: str = ""):
 
 
 def unzip_individual_feed(
-    fs, extract: GTFSFeedExtractInfo
+    fs, extract: GTFSScheduleFeedExtract
 ) -> GTFSScheduleFeedExtractUnzipOutcome:
     """If zip contains a directory and nothing else at top level or files at top level,
     we return the contents of the top level or the unique directory as the feed.
     If there's a directory *and* other files at top level within zip, or if
     there are nested directories, say we can't parse."""
 
-    # TODO: figure out why this isn't working? should be accessible via just extract.path
-    # need to update all refs once it's working again
-    extract_path = os.path.join(SCHEDULE_RAW_BUCKET, extract.name)
     print(f"Processing {extract.name}")
+    zipfile_md5_hash = ""
     try:
-        with fs.open(extract_path) as f:
+        with fs.open(extract.path) as f:
+            zipfile_md5_hash = f.info()["md5Hash"]
             zip = zipfile.ZipFile(f)
         files, directories = summarize_zip_contents(zip)
-        print(f"Found: {files} and {directories}")
+        print(f"Found files: {files} and directories: {directories}")
     except Exception as e:
         return GTFSScheduleFeedExtractUnzipOutcome(
             success=False,
+            extract_md5hash=zipfile_md5_hash,
             extract=extract,
             exception=e,
         )
@@ -134,6 +132,7 @@ def unzip_individual_feed(
     if len(directories) > 1:
         return GTFSScheduleFeedExtractUnzipOutcome(
             success=False,
+            extract_md5hash=zipfile_md5_hash,
             extract=extract,
             zipfile_files=files,
             zipfile_dirs=directories,
@@ -142,18 +141,20 @@ def unzip_individual_feed(
     # we can treat the contents of that directory as if they were the feed
     elif [item.is_dir() for item in zipfile.Path(zip, at="").iterdir()] == [True]:
         feed_directory = directories[0]
-        zipfile_files = process_feed_files(fs, extract_path, extract, feed_directory)
+        zipfile_files = process_feed_files(fs, extract, feed_directory)
         return GTFSScheduleFeedExtractUnzipOutcome(
             success=True,
+            extract_md5hash=zipfile_md5_hash,
             extract=extract,
             zipfile_files=files,
             zipfile_dirs=directories,
             extracted_files=zipfile_files,
         )
     else:
-        zipfile_files = zipfile_files = process_feed_files(fs, extract_path, extract)
+        zipfile_files = process_feed_files(fs, extract)
         return GTFSScheduleFeedExtractUnzipOutcome(
             success=True,
+            extract_md5hash=zipfile_md5_hash,
             extract=extract,
             zipfile_files=files,
             zipfile_dirs=directories,
@@ -166,7 +167,7 @@ def unzip_extracts(day=pendulum.today()):
 
     day = pendulum.instance(day).date()
     extracts = fetch_all_in_partition(
-        cls=GTFSFeedExtractInfo,
+        cls=GTFSScheduleFeedExtract,
         bucket=SCHEDULE_RAW_BUCKET,
         table=GTFSFeedType.schedule,
         fs=get_fs(),
@@ -177,10 +178,16 @@ def unzip_extracts(day=pendulum.today()):
     )
     print(f"Identified {len(extracts)} records for {day}")
     outcomes = []
-    # TODO: extract all not just first one
-    for extract in extracts[0:1]:
+    for extract in extracts:
         outcome = unzip_individual_feed(fs, extract)
-        print(f"Outcome: {outcome}")
+        print(f"Processing success: {outcome.success}")
         outcomes.append(outcome)
 
-        # TODO: actually save out outcomes file
+    print("Saving results")
+    result = ScheduleUnzipResult(filename="results.jsonl", dt=day, outcomes=outcomes)
+
+    result.save(fs)
+
+    assert len(extracts) == len(
+        result.outcomes
+    ), f"ended up with {len(outcomes)} outcomes from {len(extracts)} extracts"
