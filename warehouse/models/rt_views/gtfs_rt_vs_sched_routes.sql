1fcd4a870c406b7c663a24306c0b1c18ce619424
natam1 <natalielamberg@gmail.com>
2022-08-24 13:06:03 -0700

    add operator

--- warehouse/models/rt_views/gtfs_rt_vs_sched_routes.sql
+++ warehouse/models/rt_views/gtfs_rt_vs_sched_routes.sql
@@ -1,27 +1,18 @@
 WITH 
-gtfs_rt_vp_distinct_trips AS (
+-- selecting the distinct trips from GTFS Vehicle Postions for two operators (SamTrans and Big Blue Bus) and two months
+vp_trips AS (
     SELECT DISTINCT
      calitp_itp_id,
      calitp_url_number,
-     date,
-     trip_id,
+     date AS service_date,
+     trip_id AS vp_trip_id,
      trip_route_id,
      vehicle_id
     FROM {{ ref('stg_rt__vehicle_positions') }}
-    WHERE calitp_itp_id = 300
-        AND date between '2022-04-01' AND '2022-06-30'
-),
-
-vp_trips AS(
-SELECT 
-  date AS service_date, 
-  trip_id AS vp_trip_id, 
-  calitp_itp_id, 
-  calitp_url_number
-FROM gtfs_rt_vp_distinct_trips
-WHERE date BETWEEN '2022-04-01' AND '2022-06-30'
-),
-
+    WHERE date between '2022-05-01' AND '2022-06-30' 
+      AND (calitp_itp_id in (300, 290)
+)),
+-- selecting GTFS schedule data for the same two operators and two months
 sched_trips AS(
 SELECT 
   trip_id, 
@@ -30,10 +21,12 @@ SELECT
   calitp_itp_id, 
   calitp_url_number
 FROM {{ref('gtfs_schedule_fact_daily_trips')}}
-WHERE (calitp_itp_id=300
-    AND service_date BETWEEN '2022-04-01' AND '2022-06-30'
+WHERE service_date BETWEEN '2022-05-01' AND '2022-06-30' 
+    AND (calitp_itp_id in (300, 290)
     AND is_in_service = True)
 ),
+
+-- joining Vehicle Position data and Scheduled data on service date, trip id and itp id and url number
 rt_sched_joined AS(
   SELECT
   T1.calitp_itp_id,
@@ -52,6 +45,8 @@ rt_sched_joined AS(
       AND T1.service_date = T2.service_date
   GROUP BY 1, 2, 3, 4
 ),
+
+-- getting the percent of scheduled trips with vehicle position data and adding the common route name
 with_percent AS(
   SELECT
   T1.calitp_itp_id,
