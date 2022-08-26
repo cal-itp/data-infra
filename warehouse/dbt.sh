45777f8737024b356d78fb96e3a1a7b9415b01e4
Andrew Vaccaro <atvaccaro@gmail.com>
2022-08-26 16:06:50 -0400

    handle local profiles dir

--- warehouse/dbt.sh
+++ warehouse/dbt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t local-dbt -f Dockerfile.local .
-docker run --entrypoint dbt -v $(pwd):/app local-dbt "$@"
+docker run --entrypoint dbt -v ~/.dbt:/local_dbt -v $(pwd):/app local-dbt "$@" --profiles-dir /local_dbt
