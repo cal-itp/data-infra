cd0db5a255acbdcb85b9d88574b2105a4413ce48
Charles Costanzo <cvc5185@gmail.com>
2022-09-01 15:33:50 -0400

    revisions based on patterns in laurie's PR 1712

--- warehouse/models/mart/gtfs_guidelines/fact_daily_transitland_url_check.sql
+++ warehouse/models/mart/gtfs_guidelines/fact_daily_transitland_url_check.sql
@@ -1,6 +1,6 @@
 WITH gtfs_fact_daily_transitland_url_check AS (
     SELECT *
-    FROM {{ ref('stg_feed_aggregator_checks__gtfs_fact_daily_transitland_url_check') }}
+    FROM {{ ref('stg_gtfs_guidelines__fact_daily_transitland_url_check') }}
 )
 
 SELECT * FROM gtfs_fact_daily_transitland_url_check
