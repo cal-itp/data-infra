b9585a2685f27a7a1b819187d4e2248e37c65fa2
Scott Owades <jeremyscottowades@gmail.com>
2022-09-02 10:53:11 -0700

    Switch to using gtfs_schedule_index_feed_trip_stops

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__complete_wheelchair_accessibility_data.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__complete_wheelchair_accessibility_data.sql
@@ -3,12 +3,8 @@ WITH feed_guideline_index AS (
     WHERE check = {{ complete_wheelchair_accessibility_data() }}
 ),
 
-gtfs_schedule_fact_daily_trips AS (
-    SELECT * FROM {{ ref('gtfs_schedule_fact_daily_trips') }}
-),
-
-gtfs_schedule_fact_daily_feed_stops AS (
-    SELECT * FROM {{ ref('gtfs_schedule_fact_daily_feed_stops') }}
+gtfs_schedule_index_feed_trip_stops AS (
+    SELECT * FROM {{ ref('gtfs_schedule_index_feed_trip_stops') }}
 ),
 
 gtfs_schedule_dim_feeds AS (
@@ -23,29 +19,45 @@ gtfs_schedule_dim_stops AS (
     SELECT * FROM {{ ref('gtfs_schedule_dim_stops') }}
 ),
 
--- Counts use of wheelchair_accessible field in trips.txt for a given FEED (which spans multiple days)
+-- One row for every trip_key, mapping it to to its feed_key.
 feed_trips AS (
+    SELECT
+        DISTINCT trip_key,
+        feed_key
+    FROM gtfs_schedule_index_feed_trip_stops
+),
+
+-- One row for every stop_key, mapping it to to its feed_key.
+feed_stops AS (
+    SELECT
+        DISTINCT stop_key,
+        feed_key
+    FROM gtfs_schedule_index_feed_trip_stops
+),
+
+-- Counts use of wheelchair_accessible field in trips.txt for a given FEED (which spans multiple days)
+feed_trips_aggregate AS (
     SELECT
         t1.feed_key,
         COUNT(CASE
                 WHEN t2.wheelchair_accessible IS NOT NULL THEN 1
               END) AS accessibility_trips,
         COUNT(*) AS trips
-    FROM gtfs_schedule_fact_daily_trips AS t1
+    FROM feed_trips AS t1
     LEFT JOIN gtfs_schedule_dim_trips AS t2
       ON t1.trip_key = t2.trip_key
     GROUP BY t1.feed_key
 ),
 
 -- Counts use of wheelchair_boarding field in stops.txt for a given FEED (which spans multiple days)
-feed_stops AS (
+feed_stops_aggregate AS (
     SELECT
         t1.feed_key,
         COUNT(CASE
                 WHEN t2.wheelchair_boarding IS NOT NULL THEN 1
               END) AS accessibility_stops,
         COUNT(*) AS stops
-    FROM gtfs_schedule_fact_daily_feed_stops AS t1
+    FROM feed_stops AS t1
     LEFT JOIN gtfs_schedule_dim_stops AS t2
       ON t1.stop_key = t2.stop_key
    GROUP BY t1.feed_key
@@ -57,6 +69,10 @@ feed_wheelchair_check AS (
     SELECT
         t3.calitp_itp_id,
         t3.calitp_url_number,
+        t1.accessibility_trips,
+        t1.trips,
+        t2.accessibility_stops,
+        t2.stops,
         {{ complete_wheelchair_accessibility_data() }} AS check,
         CASE
             WHEN t1.accessibility_trips = t1.trips AND t2.accessibility_stops = t2.stops THEN "PASS"
@@ -64,8 +80,8 @@ feed_wheelchair_check AS (
         END AS status,
         t3.calitp_extracted_at,
         t3.calitp_deleted_at
-    FROM feed_trips AS t1
-    LEFT JOIN feed_stops AS t2
+    FROM feed_trips_aggregate AS t1
+    LEFT JOIN feed_stops_aggregate AS t2
       ON t1.feed_key = t2.feed_key
     LEFT JOIN gtfs_schedule_dim_feeds AS t3
       ON t1.feed_key = t3.feed_key
