4bc0f81a1d798e2aaa839bbaa76ca90334b57f62
Scott Owades <jeremyscottowades@gmail.com>
2022-09-02 16:19:37 -0700

    implement laurie's suggested simplification

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__complete_wheelchair_accessibility_data.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__complete_wheelchair_accessibility_data.sql
@@ -3,107 +3,117 @@ WITH feed_guideline_index AS (
     WHERE check = {{ complete_wheelchair_accessibility_data() }}
 ),
 
-gtfs_schedule_index_feed_trip_stops AS (
-    SELECT * FROM {{ ref('gtfs_schedule_index_feed_trip_stops') }}
-),
-
-gtfs_schedule_dim_feeds AS (
-    SELECT * FROM {{ ref('gtfs_schedule_dim_feeds') }}
-),
-
-gtfs_schedule_dim_trips AS (
+dim_trips AS (
     SELECT * FROM {{ ref('gtfs_schedule_dim_trips') }}
 ),
 
-gtfs_schedule_dim_stops AS (
+dim_stops AS (
     SELECT * FROM {{ ref('gtfs_schedule_dim_stops') }}
 ),
 
--- One row for every trip_key, mapping it to to its feed_key.
-feed_trips AS (
-    SELECT
-        DISTINCT trip_key,
-        feed_key
-    FROM gtfs_schedule_index_feed_trip_stops
+summarize_trips AS (
+   SELECT
+       calitp_itp_id,
+       calitp_url_number,
+       calitp_extracted_at,
+       calitp_deleted_at,
+       COUNTIF(wheelchair_accessible IS NOT NULL) AS ct_accessible_trips,
+       COUNT(*) AS ct_trips
+    FROM dim_trips
+   GROUP BY 1, 2, 3, 4
 ),
 
--- One row for every stop_key, mapping it to to its feed_key.
-feed_stops AS (
-    SELECT
-        DISTINCT stop_key,
-        feed_key
-    FROM gtfs_schedule_index_feed_trip_stops
-),
-
--- Counts use of wheelchair_accessible field in trips.txt for a given FEED (which spans multiple days)
-feed_trips_aggregate AS (
-    SELECT
+daily_trips AS (
+  SELECT
+    t1.date,
+    t1.calitp_itp_id,
+    t1.calitp_url_number,
+    t1.calitp_agency_name,
+    t1.feed_key,
+    t1.check,
+    t1.feature,
+    SUM(t2.ct_accessible_trips) AS tot_accessible_trips,
+    SUM(t2.ct_trips) AS tot_trips
+  FROM feed_guideline_index AS t1
+  LEFT JOIN summarize_trips AS t2
+       ON t1.date >= t2.calitp_extracted_at
+       AND t1.date < t2.calitp_deleted_at
+       AND t1.calitp_itp_id = t2.calitp_itp_id
+       AND t1.calitp_url_number = t2.calitp_url_number
+ GROUP BY
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
         t1.feed_key,
-        COUNT(CASE
-                WHEN t2.wheelchair_accessible IS NOT NULL THEN 1
-              END) AS accessibility_trips,
-        COUNT(*) AS trips
-    FROM feed_trips AS t1
-    LEFT JOIN gtfs_schedule_dim_trips AS t2
-      ON t1.trip_key = t2.trip_key
-    GROUP BY t1.feed_key
+        t1.check,
+        t1.feature
 ),
 
--- Counts use of wheelchair_boarding field in stops.txt for a given FEED (which spans multiple days)
-feed_stops_aggregate AS (
-    SELECT
+summarize_stops AS (
+   SELECT
+       calitp_itp_id,
+       calitp_url_number,
+       calitp_extracted_at,
+       calitp_deleted_at,
+       COUNTIF(wheelchair_boarding IS NOT NULL) AS ct_accessible_stops,
+       COUNT(*) AS ct_stops
+    FROM dim_stops
+   GROUP BY 1, 2, 3, 4
+),
+
+daily_stops AS (
+  SELECT
+    t1.date,
+    t1.calitp_itp_id,
+    t1.calitp_url_number,
+    t1.calitp_agency_name,
+    t1.feed_key,
+    t1.check,
+    t1.feature,
+    SUM(t2.ct_accessible_stops) AS tot_accessible_stops,
+    SUM(t2.ct_stops) AS tot_stops
+  FROM feed_guideline_index AS t1
+  LEFT JOIN summarize_stops AS t2
+       ON t1.date >= t2.calitp_extracted_at
+       AND t1.date < t2.calitp_deleted_at
+       AND t1.calitp_itp_id = t2.calitp_itp_id
+       AND t1.calitp_url_number = t2.calitp_url_number
+ GROUP BY
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
         t1.feed_key,
-        COUNT(CASE
-                WHEN t2.wheelchair_boarding IS NOT NULL THEN 1
-              END) AS accessibility_stops,
-        COUNT(*) AS stops
-    FROM feed_stops AS t1
-    LEFT JOIN gtfs_schedule_dim_stops AS t2
-      ON t1.stop_key = t2.stop_key
-   GROUP BY t1.feed_key
+        t1.check,
+        t1.feature
 ),
 
--- With one row per feed, combines trips and stops wheelchair checks
--- Also joins with dim_feeds, to get ITP-ID and extraction/deletion dates
-feed_wheelchair_check AS (
-    SELECT
-        t3.calitp_itp_id,
-        t3.calitp_url_number,
-        t1.accessibility_trips,
-        t1.trips,
-        t2.accessibility_stops,
-        t2.stops,
-        {{ complete_wheelchair_accessibility_data() }} AS check,
-        CASE
-            WHEN t1.accessibility_trips = t1.trips AND t2.accessibility_stops = t2.stops THEN "PASS"
-        ELSE "FAIL"
-        END AS status,
-        t3.calitp_extracted_at,
-        t3.calitp_deleted_at
-    FROM feed_trips_aggregate AS t1
-    LEFT JOIN feed_stops_aggregate AS t2
-      ON t1.feed_key = t2.feed_key
-    LEFT JOIN gtfs_schedule_dim_feeds AS t3
-      ON t1.feed_key = t3.feed_key
-),
-
--- Joins our feed-level check with feed_guideline_index, where each feed will have a row for every day it is active
-daily_wheelchair_check AS (
+accessibility_check AS (
     SELECT
         t1.date,
         t1.calitp_itp_id,
         t1.calitp_url_number,
         t1.calitp_agency_name,
+        t1.feed_key,
         t1.check,
         t1.feature,
-        t2.status
-    FROM feed_guideline_index AS t1
-    LEFT JOIN feed_wheelchair_check AS t2
-       ON t1.calitp_itp_id = t2.calitp_itp_id
-      AND t1.calitp_url_number = t2.calitp_url_number
-      AND t1.check = t2.check
-      AND t1.date >= t2.calitp_extracted_at
-      AND t1.date < t2.calitp_deleted_at
+        t1.tot_trips,
+        t1.tot_accessible_trips,
+        t2.tot_stops,
+        t2.tot_accessible_stops,
+        CASE
+            WHEN t1.tot_accessible_trips = t1.tot_trips AND t2.tot_accessible_stops = t2.tot_stops THEN "PASS"
+        ELSE "FAIL"
+        END AS status,
+      FROM daily_trips t1
+      LEFT JOIN daily_stops t2
+             ON t1.date = t2.date
+            AND t1.calitp_itp_id = t2.calitp_itp_id
+            AND t1.calitp_url_number = t2.calitp_url_number
+            AND t1.feed_key = t2.feed_key
+            AND t1.check = t2.check
+            AND t1.feature = t2.feature
 )
 
-SELECT * FROM daily_wheelchair_check
+SELECT * FROM accessibility_check
