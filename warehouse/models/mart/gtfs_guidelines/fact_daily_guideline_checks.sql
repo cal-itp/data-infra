5f8b99bb57dd50d810064172375d3151c9948f90
Scott Owades <jeremyscottowades@gmail.com>
2022-09-08 14:57:51 -0700

    5 new draft checks for rt feed

--- warehouse/models/mart/gtfs_guidelines/fact_daily_guideline_checks.sql
+++ warehouse/models/mart/gtfs_guidelines/fact_daily_guideline_checks.sql
@@ -25,6 +25,26 @@ stg_gtfs_guidelines__technical_contact_listed AS (
     SELECT * FROM {{ ref('stg_gtfs_guidelines__technical_contact_listed') }}
 ),
 
+stg_gtfs_guidelines__no_rt_critical_validation_errors AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__no_rt_critical_validation_errors') }}
+),
+
+stg_gtfs_guidelines__trip_id_alignment AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__trip_id_alignment') }}
+),
+
+stg_gtfs_guidelines__vehicle_positions_feed_present AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__vehicle_positions_feed_present') }}
+),
+
+stg_gtfs_guidelines__trip_updates_feed_present AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__trip_updates_feed_present') }}
+),
+
+stg_gtfs_guidelines__service_alerts_feed_present AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__service_alerts_feed_present') }}
+),
+
 fact_daily_guideline_checks AS (
     SELECT
         {{ gtfs_guidelines_columns() }}
@@ -49,6 +69,26 @@ fact_daily_guideline_checks AS (
     SELECT
         {{ gtfs_guidelines_columns() }}
     FROM stg_gtfs_guidelines__technical_contact_listed
+    UNION ALL
+    SELECT
+        {{ gtfs_guidelines_columns() }}
+    FROM no_rt_critical_validation_errors
+    UNION ALL
+    SELECT
+        {{ gtfs_guidelines_columns() }}
+    FROM stg_gtfs_guidelines__trip_id_alignment
+    UNION ALL
+    SELECT
+        {{ gtfs_guidelines_columns() }}
+    FROM stg_gtfs_guidelines__vehicle_positions_feed_present
+    UNION ALL
+    SELECT
+        {{ gtfs_guidelines_columns() }}
+    FROM stg_gtfs_guidelines__trip_updates_feed_present
+    UNION ALL
+    SELECT
+        {{ gtfs_guidelines_columns() }}
+    FROM stg_gtfs_guidelines__service_alerts_feed_present
 )
 
 SELECT * FROM fact_daily_guideline_checks
---
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__service_alerts_feed_present.sql
@@ -0,0 +1,40 @@
+WITH feed_guideline_index AS (
+    SELECT * FROM {{ ref('stg_gtfs_guidelines__feed_guideline_index') }}
+    WHERE check = {{ vehicle_positions_feed_present() }}
+),
+
+-- It's called "hourly" but if you use the "file_count_day" it's a daily table
+gtfs_rt_fact_files_wide_daily AS (
+SELECT * FROM {{ ref('gtfs_rt_fact_files_wide_hourly') }}
+),
+
+rt_files_by_day AS (
+    SELECT
+        calitp_itp_id,
+        calitp_url_number,
+        date_extracted AS date,
+        name
+    FROM gtfs_rt_fact_files_wide_daily
+    WHERE name = "service_alerts"
+),
+
+feed_daily_check AS (
+    SELECT
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
+        t1.check,
+        t1.feature,
+        CASE
+            WHEN t2.name IS NOT null THEN "PASS"
+        ELSE "FAIL"
+        END AS status,
+    FROM feed_guideline_index AS t1
+    LEFT JOIN rt_files_by_day AS t2
+         ON t1.calitp_itp_id = t2.calitp_itp_id
+         AND t1.calitp_url_number = t2.calitp_url_number
+         AND t1.date = t2.date
+)
+
+SELECT * FROM feed_daily_check
