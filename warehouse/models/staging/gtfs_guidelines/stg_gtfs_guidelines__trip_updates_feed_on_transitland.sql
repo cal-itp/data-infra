58dbf16bad7d26b70a24e3b0442e9d0b0a38cd6f
Scott Owades <jeremyscottowades@gmail.com>
2022-09-21 10:49:32 -0700

    correct for url_number not being accurate

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__trip_updates_feed_on_transitland.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__trip_updates_feed_on_transitland.sql
@@ -10,13 +10,13 @@ SELECT * FROM {{ ref('stg_gtfs_guidelines__fact_daily_transitland_url_check') }}
 trip_updates_daily_check AS (
     SELECT
         calitp_itp_id,
-        calitp_url_number,
-        dt AS date,
-        transitland_status
-    FROM fact_daily_transitland_url_check
-    WHERE url_type = "gtfs_rt_trip_updates_url"
+        dt AS date
+   FROM fact_daily_transitland_url_check
+  WHERE url_type = "gtfs_rt_trip_updates_url"
+    AND transitland_status = 'present'
 ),
 
+-- Note: since calitp_url_number is not available in fact_daily_transitland_url_check, this is a bit of a fuzzy join - to be improved after refactor
 feed_daily_check AS (
     SELECT
         t1.date,
@@ -26,14 +26,13 @@ feed_daily_check AS (
         t1.check,
         t1.feature,
         CASE
-            WHEN t2.transitland_status = 'present' THEN "PASS"
+            WHEN t2.calitp_itp_id IS NOT null THEN "PASS"
         ELSE "FAIL"
         END AS status,
     FROM feed_guideline_index AS t1
     LEFT JOIN trip_updates_daily_check AS t2
          ON t1.calitp_itp_id = t2.calitp_itp_id
-         AND t1.calitp_url_number = t2.calitp_url_number
-         AND t1.date = t2.date
+        AND t1.date = t2.date
 )
 
 SELECT * FROM feed_daily_check
