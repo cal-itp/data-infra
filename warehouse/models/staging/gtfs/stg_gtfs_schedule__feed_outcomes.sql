f9db29a2bc27ee8e6bb2332ca9f430872262e670
Laurie Merrell <laurie.m@jarv.us>
2022-09-23 18:33:51 -0500

    attempt at combined feed outcomes... could become fact daily feeds

--- warehouse/models/staging/gtfs/stg_gtfs_schedule__feed_outcomes.sql
+++ warehouse/models/staging/gtfs/stg_gtfs_schedule__feed_outcomes.sql
@@ -3,7 +3,7 @@
 WITH download_outcomes AS (
     SELECT
         *,
-        {{ to_url_safe_base64('`extract`.config.url') }} as base64_url
+        {{ to_url_safe_base64('config.url') }} as base64_url
     FROM {{ source('external_gtfs_schedule', 'download_outcomes') }}
 ),
 
@@ -15,40 +15,28 @@ unzip_outcomes AS (
 
 stg_gtfs_schedule__feed_outcomes AS (
     SELECT
-        download_outcomes.dt AS download_dt,
-        download_outcomes.ts AS download_ts,
+        download_outcomes.dt AS dt,
+        {{ farm_surrogate_key(['download_outcomes.base64_url', 'download_outcomes.ts']) }} as key,
+        download_outcomes.config.name AS name,
+        download_outcomes.config.url AS url,
+        download_outcomes.config.feed_type AS feed_type,
+        download_outcomes.config.extracted_at AS config_extracted_at,
+        download_outcomes.config.schedule_url_for_validation AS schedule_url_for_validation,
         download_outcomes.success AS download_success,
         download_outcomes.exception AS download_exception,
         download_outcomes.extract.response_code AS download_response_code,
-        download_outcomes.extract.config.url AS download_url,
-        download_outcomes.base64_url AS download_base64_url,
-        unzip_outcomes.dt AS unzip_dt,
-        unzip_outcomes.extract.ts AS unzip_ts,
+        download_outcomes.extract.response_headers AS download_response_headers,
         unzip_outcomes.success AS unzip_success,
         unzip_outcomes.exception AS unzip_exception,
-        unzip_outcomes.base64_url AS unzip_base64_url
+        unzip_outcomes.zipfile_extract_md5hash,
+        unzip_outcomes.zipfile_files,
+        unzip_outcomes.zipfile_dirs,
+        download_outcomes.base64_url AS base64_url,
+        download_outcomes.ts AS ts
     FROM download_outcomes
-    FULL OUTER JOIN unzip_outcomes
+    LEFT JOIN unzip_outcomes
         ON download_outcomes.base64_url = unzip_outcomes.base64_url
         AND download_outcomes.ts = unzip_outcomes.extract.ts
 )
-
--- stg_feeds AS (
---     SELECT
---         url,
---         ts,
---         zipfile_extract_md5hash,
---         success,
---         FIRST_VALUE(ts)
---         OVER (PARTITION BY url, zipfile_extract_md5hash, success ORDER BY ts
---             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
---         AS first_extract,
---         LAST_VALUE(ts)
---         OVER (PARTITION BY url, zipfile_extract_md5hash, success ORDER BY ts
---             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
---         AS latest_extract,
---         DENSE_RANK() OVER (ORDER BY ts DESC) = 1 AS in_latest
---     FROM zipfiles
--- )
 
 SELECT * FROM stg_gtfs_schedule__feed_outcomes
