d4c6a869641d3698c7222feb4299d86ec31d9efe
Laurie <55149902+lauriemerrell@users.noreply.github.com>
2022-10-04 09:23:52 -0500

    switch keying for vp messages (#1873)

--- warehouse/models/mart/gtfs/fct_vehicle_positions_messages.sql
+++ warehouse/models/mart/gtfs/fct_vehicle_positions_messages.sql
@@ -22,7 +22,7 @@ fct_vehicle_positions_messages AS (
     SELECT
         -- ideally this would not include vehicle_id / trip_id, but using it for now because
         -- MTC 511 regional feed does not have feed-unique entity ids
-        {{ dbt_utils.surrogate_key(['base64_url', '_extract_ts', 'id', 'vehicle_id', 'trip_id']) }} as key,
+        {{ dbt_utils.surrogate_key(['base64_url', '_extract_ts', 'id', 'position_latitude', 'position_longitude']) }} as key,
         gtfs_dataset_key,
         dt,
         hour,
