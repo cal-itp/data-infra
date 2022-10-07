ac58fe528a1366b8424a064cad2dcb1671f4c6da
Laurie Merrell <laurie.m@jarv.us>
2022-10-07 17:12:53 -0500

    dedupe trips

---
+++ warehouse/models/intermediate/gtfs/int_gtfs_schedule__deduped_trips.sql
@@ -0,0 +1,29 @@
+WITH stg_gtfs_schedule__trips AS (
+    SELECT *
+    FROM {{ ref('stg_gtfs_schedule__trips') }}
+),
+
+int_gtfs_schedule__deduped_trips AS (
+{{ util_dedupe(table_name = 'stg_gtfs_schedule__trips',
+    all_columns = [
+        'base64_url',
+        'ts',
+        'route_id',
+        'service_id',
+        'trip_id',
+        'shape_id',
+        'trip_headsign',
+        'trip_short_name',
+        'direction_id',
+        'block_id',
+        'wheelchair_accessible',
+        'bikes_allowed'
+    ],
+    dedupe_key = [
+        'base64_url',
+        'ts',
+        'trip_id'
+    ]) }}
+)
+
+SELECT * FROM int_gtfs_schedule__deduped_trips
