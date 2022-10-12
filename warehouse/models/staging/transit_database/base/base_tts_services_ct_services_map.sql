ff836324ee49251dca5e630994346028c28f00a8
Andrew Vaccaro <atvaccaro@gmail.com>
2022-10-12 12:45:32 -0400

    de-dupe services by name before building map

--- warehouse/models/staging/transit_database/base/base_tts_services_ct_services_map.sql
+++ warehouse/models/staging/transit_database/base/base_tts_services_ct_services_map.sql
@@ -1,15 +1,27 @@
 WITH
 
 ct_services AS (
-    SELECT *, LEAD({{ make_end_of_valid_range('ts') }}, 1,  CAST("2099-01-01" AS TIMESTAMP)) OVER (PARTITION BY id ORDER BY ts) AS next_ts
+    SELECT *
     FROM {{ source('airtable', 'california_transit__services') }}
     WHERE TRIM(name) != ""
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY name ORDER BY null) = 1
 ),
 
 tts_services AS (
+    SELECT *
+    FROM {{ source ('airtable', 'transit_technology_stacks__services') }}
+    WHERE TRIM (name) != ""
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY name ORDER BY null) = 1
+),
+
+ct_services_with_next_ts AS (
     SELECT *, LEAD({{ make_end_of_valid_range('ts') }}, 1,  CAST("2099-01-01" AS TIMESTAMP)) OVER (PARTITION BY id ORDER BY ts) AS next_ts
-    FROM {{ source('airtable', 'transit_technology_stacks__services') }}
-    WHERE TRIM(name) != ""
+    FROM ct_services
+),
+
+ tts_services_with_next_ts AS (
+    SELECT *, LEAD({{ make_end_of_valid_range('ts') }}, 1,  CAST("2099-01-01" AS TIMESTAMP)) OVER (PARTITION BY id ORDER BY ts) AS next_ts
+    FROM tts_services
 ),
 
 base_tts_services_ct_services_map AS (
@@ -22,8 +34,8 @@ base_tts_services_ct_services_map AS (
         CASE WHEN ct.ts < tts.ts THEN tts.ts ELSE ct.ts END AS ts,
         -- use the earlier one for end date
         CASE WHEN ct.next_ts < tts.next_ts THEN ct.next_ts ELSE tts.next_ts END AS next_ts
-    FROM ct_services AS ct
-    INNER JOIN tts_services AS tts
+    FROM ct_services_with_next_ts AS ct
+    INNER JOIN tts_services_with_next_ts AS tts
         ON ct.name = tts.name
         AND ct.ts < tts.next_ts
         AND ct.next_ts > tts.ts
