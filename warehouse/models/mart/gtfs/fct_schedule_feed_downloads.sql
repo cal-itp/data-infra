cal-itp/data-infra

commit 3bf583d0d099bc46fcb0cc12a99f88d77c9f5a34

Allow historical feeds to map to current gtfs_dataset records (#1937)
* relax dbt version

* trim whitespace before base64 encoding, create mapping from urls to latest airtable record

* use new table for getting gtfs_dataset_key and adjust tests

warehouse/models/mart/gtfs/_mart_gtfs.yml
@@ -23,6 +23,18 @@ models:
         name: base64_url
         description: |
           Base 64 encoded URL from which this data was scraped.
+      - &gtfs_dataset_key
+        name: gtfs_dataset_key
+        tests:
+          - not_null
+          - relationships:
+              to: ref('dim_gtfs_datasets')
+              field: key
+              config:
+                # there are a dozen rows of SMART transit which was deleted from Airtable
+                # this will work without exception once the Airtable dim is historical;
+                # this threshold may need to increase if the backfill occurs prior to that
+                error_if: ">20"
       - &_valid_from
         name: _valid_from
         description: '{{ doc("column_valid_from") }}'
@@ -45,10 +57,7 @@ models:
       - name: feed_key
         description: |
           Foreign key to the `dim_schedule_feeds` table.
-      - &gtfs_dataset_key
-        name: gtfs_dataset_key
-        description: |
-          Foreign key to the `dim_gtfs_datasets` table.
+      - *gtfs_dataset_key
       - name: is_future
         description: |
           Boolean indicating whether this date is in the future relative to the last time that the table
@@ -132,16 +141,18 @@ models:
           entity `id`, `vehicle_id`, and `trip_id`.
         tests:
           - unique:
-              config:
-                # TODO: make an RT test macro that does this date subtraction
-                # test hour = 0 UTC because that's 5pm Pacific = PM peak, good sample of data
-                where: dt >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) AND (EXTRACT(HOUR FROM hour)) = 0
+              # TODO: make an RT test macro that does this date subtraction
+              # test hour = 0 UTC because that's 5pm Pacific = PM peak, good sample of data
+              where: dt >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) AND (EXTRACT(HOUR FROM hour)) = 0
           - not_null:
-              config:
-                where: dt >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) AND (EXTRACT(HOUR FROM hour)) = 0
+              where: dt >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) AND (EXTRACT(HOUR FROM hour)) = 0
       - name: gtfs_dataset_key
         description: |
           The primary key for the record in `dim_gtfs_datasets` associated with this message.
+        columns:
+          - <<: *gtfs_dataset_key
+            config:
+              where: dt >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) AND (EXTRACT(HOUR FROM hour)) = 0
       - name: dt
         description: |
           Date on which we scraped this message.
@@ -156,7 +167,7 @@ models:
       - name: _extract_ts
         description: |
           Time at which this message was scraped.
-      - name: _gtfs_dataset_name
+      - name: _name
         description: |
           String name of the GTFS dataset of which this message is a part.
           This field is provided for human readability and should not be used as a join key.
@@ -973,8 +984,7 @@ models:
           Synthetic primary key constructed from `feed_key` and `trip_id`.
         tests:
           - unique:
-              config:
-                where: "not warning_duplicate_primary_key"
+              where: "not warning_duplicate_primary_key"
           - not_null
       - *base64_url
       - name: route_id
@@ -1159,6 +1169,11 @@ models:
           - not_null
       - name: feed_key
         description: Foreign key to dim_schedule_feeds.
+        tests:
+          - not_null
+          - relationships:
+              to: ref('dim_schedule_feeds')
+              field: key
       - name: service_date
         description: Date on which this trip was active.
       - name: service_id
@@ -1167,8 +1182,16 @@ models:
           has service on this date.
       - name: trip_key
         description: Foreign key to dim_trips.
+        tests:
+          - relationships:
+              to: ref('dim_trips')
+              field: key
       - name: route_key
         description: Foreign key to dim_routes.
+        tests:
+          - relationships:
+              to: ref('dim_routes')
+              field: key
       - name: warning_duplicate_trip_primary_key
         description: |
           Rows with `true` in this column have a duplicate primary key in dim_trips;

warehouse/models/mart/gtfs/fct_schedule_feed_downloads.sql
@@ -5,9 +5,8 @@ WITH joined_feed_outcomes AS (
     FROM {{ ref('int_gtfs_schedule__joined_feed_outcomes') }}
 ),
 
-dim_schedule_feeds AS (
-    SELECT *
-    FROM {{ ref('dim_schedule_feeds') }}
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
 ),
 
 fct_schedule_feeds AS (

warehouse/models/intermediate/gtfs/int_gtfs_schedule__joined_feed_outcomes.sql
@@ -13,10 +13,10 @@ int_gtfs_schedule__grouped_feed_file_parse_outcomes AS (
     FROM {{ ref('int_gtfs_schedule__grouped_feed_file_parse_outcomes') }}
 ),
 
-dim_gtfs_datasets AS (
-    SELECT *
-    FROM {{ ref('dim_gtfs_datasets') }}
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
 ),
+
 
 join_outcomes_only AS (
     SELECT
@@ -42,7 +42,7 @@ join_outcomes_only AS (
 
 int_gtfs_schedule__joined_feed_outcomes AS (
     SELECT
-        gd.key AS gtfs_dataset_key,
+        urls_to_gtfs_datasets.gtfs_dataset_key,
         f.ts,
         f.base64_url,
         f._config_extract_ts,
@@ -55,11 +55,8 @@ int_gtfs_schedule__joined_feed_outcomes AS (
         f.zipfile_dirs,
         f.pct_files_successfully_parsed
     FROM join_outcomes_only AS f
-    -- TODO: this can lead to fanout until we de-dupe on the dim_gtfs_datasets side
-    -- currently no enforcement that URL is unique
-    LEFT JOIN dim_gtfs_datasets AS gd
-        ON f.base64_url = gd.base64_url
-            AND f._config_extract_ts BETWEEN gd._valid_from AND gd._valid_to
+    LEFT JOIN urls_to_gtfs_datasets
+        ON f.base64_url = urls_to_gtfs_datasets.base64_url
 )
 
 SELECT * FROM int_gtfs_schedule__joined_feed_outcomes

warehouse/models/intermediate/transit_database/_int_transit_database.yml
@@ -21,3 +21,25 @@ models:
             - product_key
             - component_key
             - calitp_extracted_at
+
+  - name: int_transit_database__urls_to_gtfs_datasets
+    description: |
+      Creates a 1:N relationship so any given extract/URL in the
+      GTFS models can identify its source gtfs_datasets record.
+      Currently this is unversioned; any historical URLs point
+      to the latest version of their gtfs_datasets record.
+    columns:
+      - name: base64_url
+        tests:
+          - not_null
+          - unique
+      - name: gtfs_dataset_key
+        tests:
+          - relationships:
+              to: ref('dim_gtfs_datasets')
+              field: key
+              config:
+                # four SMART urls and two test URLs that have been removed
+                # will work once the dim is historical
+                error_if: ">10"
+                warn_if: ">0"

warehouse/models/intermediate/transit_database/int_transit_database__urls_to_gtfs_datasets.sql
@@ -1,0 +1,21 @@
+WITH
+
+gtfs_datasets AS (
+    SELECT * FROM {{ ref('stg_transit_database__gtfs_datasets') }}
+),
+
+int_transit_database__urls_to_gtfs_datasets AS (
+    SELECT
+        base64_url,
+        airtable_record_id AS gtfs_dataset_key -- this is defined in {{ ref('dim_gtfs_datasets') }}
+    FROM gtfs_datasets
+    WHERE base64_url IS NOT NULL
+    -- NOTE: there could be more than 1 record per URL per ts if a given
+    -- URL is set in more than one gtfs_dataset row; our general assertion
+    -- is that we really should think of the underlying Airtable data
+    -- as unique at the URL level and merge records if necessary to
+    -- enforce that uniqueness constraint
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY base64_url ORDER BY ts DESC) = 1
+)
+
+SELECT * FROM int_transit_database__urls_to_gtfs_datasets

warehouse/models/mart/gtfs/fct_daily_schedule_feeds.sql
@@ -10,9 +10,9 @@ dim_schedule_feeds AS (
     FROM {{ ref('dim_schedule_feeds') }}
 ),
 
-dim_gtfs_datasets AS (
-    SELECT *
-    FROM {{ ref('dim_gtfs_datasets') }}
+
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
 ),
 
 make_noon_pacific AS (
@@ -27,14 +27,13 @@ fct_daily_schedule_feeds AS (
         {{ dbt_utils.surrogate_key(['t1.date_day', 't2.key']) }} AS key,
         t1.date_day AS date,
         t2.key AS feed_key,
-        t3.key AS gtfs_dataset_key,
+        urls_to_gtfs_datasets.gtfs_dataset_key AS gtfs_dataset_key,
         t1.date_day > CURRENT_DATE() AS is_future
     FROM make_noon_pacific AS t1
     INNER JOIN dim_schedule_feeds AS t2
         ON t1.noon_pacific BETWEEN t2._valid_from AND t2._valid_to
-    LEFT JOIN dim_gtfs_datasets AS t3
-        ON t1.noon_pacific BETWEEN t3._valid_from AND t3._valid_to
-            AND t2.base64_url = t3.base64_url
+    LEFT JOIN urls_to_gtfs_datasets
+        ON t2.base64_url = urls_to_gtfs_datasets.base64_url
 )
 
 SELECT * FROM fct_daily_schedule_feeds

warehouse/models/mart/gtfs/fct_daily_scheduled_trips.sql
@@ -15,6 +15,14 @@ dim_routes AS (
     FROM {{ ref('dim_routes') }}
 ),
 
+dim_schedule_feeds AS (
+    SELECT * FROM {{ ref('dim_schedule_feeds') }}
+),
+
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
+),
+
 fct_daily_scheduled_trips AS (
     SELECT
         {{ dbt_utils.surrogate_key(['service_index.service_date', 'trips.key']) }} as key,
@@ -23,6 +31,7 @@ fct_daily_scheduled_trips AS (
         service_index.service_id,
         trips.key AS trip_key,
         routes.key AS route_key,
+        urls_to_gtfs_datasets.gtfs_dataset_key AS gtfs_dataset_key,
         trips.warning_duplicate_primary_key AS warning_duplicate_trip_primary_key
     FROM int_gtfs_schedule__daily_scheduled_service_index AS service_index
     LEFT JOIN dim_trips AS trips
@@ -31,6 +40,10 @@ fct_daily_scheduled_trips AS (
     LEFT JOIN dim_routes AS routes
         ON service_index.feed_key = routes.feed_key
         AND trips.route_id = routes.route_id
+    LEFT JOIN dim_schedule_feeds AS feeds
+        ON service_index.feed_key = feeds.key
+    LEFT JOIN urls_to_gtfs_datasets
+        ON feeds.base64_url = urls_to_gtfs_datasets.base64_url
 )
 
 SELECT * FROM fct_daily_scheduled_trips

warehouse/models/mart/gtfs/fct_vehicle_positions_messages.sql
@@ -3,6 +3,10 @@ WITH stg_gtfs_rt__vehicle_positions AS (
     FROM {{ ref('stg_gtfs_rt__vehicle_positions') }}
 ),
 
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
+),
+
 dim_gtfs_datasets AS (
     SELECT *
     FROM {{ ref('dim_gtfs_datasets') }}
@@ -10,13 +14,14 @@ dim_gtfs_datasets AS (
 
 keying AS (
     SELECT
-        gd.key as gtfs_dataset_key,
+        urls_to_gtfs_datasets.gtfs_dataset_key,
         gd.name as _gtfs_dataset_name,
         vp.*
     FROM stg_gtfs_rt__vehicle_positions AS vp
+    LEFT JOIN urls_to_gtfs_datasets
+        ON vp.base64_url = urls_to_gtfs_datasets.base64_url
     LEFT JOIN dim_gtfs_datasets AS gd
-        ON vp.base64_url = gd.base64_url
-        AND vp._config_extract_ts BETWEEN gd._valid_from AND gd._valid_to
+        ON urls_to_gtfs_datasets.gtfs_dataset_key = gd.key
 ),
 
 fct_vehicle_positions_messages AS (

commit b95f5e0859c30a0eb65f4c9ae2faeddb7423ee24

hotfixes for issues introduced in PR #1937 -- missing refs

warehouse/models/mart/gtfs/_mart_gtfs.yml
@@ -23,18 +23,6 @@ models:
         name: base64_url
         description: |
           Base 64 encoded URL from which this data was scraped.
-      - &gtfs_dataset_key
-        name: gtfs_dataset_key
-        tests:
-          - not_null
-          - relationships:
-              to: ref('dim_gtfs_datasets')
-              field: key
-              config:
-                # there are a dozen rows of SMART transit which was deleted from Airtable
-                # this will work without exception once the Airtable dim is historical;
-                # this threshold may need to increase if the backfill occurs prior to that
-                error_if: ">20"
       - &_valid_from
         name: _valid_from
         description: '{{ doc("column_valid_from") }}'
@@ -57,7 +45,18 @@ models:
       - name: feed_key
         description: |
           Foreign key to the `dim_schedule_feeds` table.
-      - *gtfs_dataset_key
+      - &gtfs_dataset_key
+        name: gtfs_dataset_key
+        tests:
+          - not_null
+          - relationships:
+              to: ref('dim_gtfs_datasets')
+              field: key
+              config:
+                # there are a dozen rows of SMART transit which was deleted from Airtable
+                # this will work without exception once the Airtable dim is historical;
+                # this threshold may need to increase if the backfill occurs prior to that
+                error_if: ">20"
       - name: is_future
         description: |
           Boolean indicating whether this date is in the future relative to the last time that the table
@@ -77,7 +76,9 @@ models:
         description: |
           Foreign key to the `dim_schedule_feeds` table.
         tests:
-          - not_null
+          - not_null:
+              config:
+                where: download_success
           - relationships:
               to: ref('dim_schedule_feeds')
               field: key

warehouse/models/mart/gtfs/fct_schedule_feed_downloads.sql
@@ -5,6 +5,11 @@ WITH joined_feed_outcomes AS (
     FROM {{ ref('int_gtfs_schedule__joined_feed_outcomes') }}
 ),
 
+dim_schedule_feeds AS (
+    SELECT *
+    FROM {{ ref('dim_schedule_feeds') }}
+),
+
 urls_to_gtfs_datasets AS (
     SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
 ),
@@ -13,7 +18,7 @@ fct_schedule_feeds AS (
     SELECT
         {{ dbt_utils.surrogate_key(['j.base64_url', 'j.ts']) }} as key,
         f.key AS feed_key,
-        j.gtfs_dataset_key,
+        u.gtfs_dataset_key,
         j.ts,
         j.base64_url,
         j.download_success,
@@ -25,6 +30,8 @@ fct_schedule_feeds AS (
         j.zipfile_dirs,
         j.pct_files_successfully_parsed
     FROM joined_feed_outcomes AS j
+    LEFT JOIN urls_to_gtfs_datasets AS u
+        ON j.base64_url = u.base64_url
     LEFT JOIN dim_schedule_feeds AS f
         ON j.base64_url = f.base64_url
         AND j.ts BETWEEN f._valid_from AND f._valid_to
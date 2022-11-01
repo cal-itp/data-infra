b95f5e0859c30a0eb65f4c9ae2faeddb7423ee24
Laurie Merrell <laurie.m@jarv.us>
2022-11-01 08:43:54 -0500

    hotfixes for issues introduced in PR #1937 -- missing refs

--- warehouse/models/mart/gtfs/fct_schedule_feed_downloads.sql
+++ warehouse/models/mart/gtfs/fct_schedule_feed_downloads.sql
@@ -5,6 +5,11 @@ WITH joined_feed_outcomes AS (
     FROM {{ ref('int_gtfs_schedule__joined_feed_outcomes') }}
 ),
 
+dim_schedule_feeds AS (
+    SELECT *
+    FROM {{ ref('dim_schedule_feeds') }}
+),
+
 urls_to_gtfs_datasets AS (
     SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
 ),
@@ -13,7 +18,7 @@ fct_schedule_feeds AS (
     SELECT
         {{ dbt_utils.surrogate_key(['j.base64_url', 'j.ts']) }} as key,
         f.key AS feed_key,
-        j.gtfs_dataset_key,
+        u.gtfs_dataset_key,
         j.ts,
         j.base64_url,
         j.download_success,
@@ -25,6 +30,8 @@ fct_schedule_feeds AS (
         j.zipfile_dirs,
         j.pct_files_successfully_parsed
     FROM joined_feed_outcomes AS j
+    LEFT JOIN urls_to_gtfs_datasets AS u
+        ON j.base64_url = u.base64_url
     LEFT JOIN dim_schedule_feeds AS f
         ON j.base64_url = f.base64_url
         AND j.ts BETWEEN f._valid_from AND f._valid_to
