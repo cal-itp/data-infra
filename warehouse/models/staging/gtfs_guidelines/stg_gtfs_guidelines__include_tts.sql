67175dc146a059f200d6dd4d6df68b8e7d6d480f
Scott Owades <jeremyscottowades@gmail.com>
2022-11-01 17:40:14 -0700

    update docs

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__include_tts.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__include_tts.sql
@@ -14,27 +14,30 @@ tts_issue_stops AS (
        calitp_extracted_at,
        calitp_deleted_at
     FROM dim_stops
+    -- When there is no tts_stop_name field for a given stop_name, or tts_stop_name is identical to stop_name, we proceed to run a few tests
     WHERE (tts_stop_name IS null OR tts_stop_name = stop_name) AND
-    -- "exists" statement checks whether stop name contains any words in a list.
-    -- It performs this by breaking apart stop_name into an array, and then joining that array to an array of "no-no" words
-    -- The alternative to this was a long repetitive query: "stop_name LIKE x OR stop_name LIKE y OR..."
          (
-            -- EXISTS function returns true if the stop_name contains any of the listed strings, and false if not
+            -- Test 1: check for abbreviations that need to be spelled out, including directions (n, sb) and ROW types (st, rd)
+            ---- EXISTS function returns true if the stop_name contains any of the listed "no-no words", and false if not
             EXISTS (
                   SELECT 1
-                    -- Create an array of strings from stop_name
+                    -- Create an array of strings, called stop_name_parts, from stop_name
                     -- ie. 1234 North St becomes (1234,north,st)
                     FROM UNNEST(SPLIT(LOWER(stop_name), ' ')) stop_name_parts
+                    -- Join stop_name_parts to list of "no-no words"
                     JOIN UNNEST([
-                                -- directions, "n" should read "north"
+                                -- Directional abbreviations, ie "n" should read "north"
                                 "n","s","e","w","ne","se","sw","nw","nb","sb","eb","wb",
-                                -- street suffixes, "st" should read "street"
+                                -- ROW abbreviations, ie "st" should read "street"
                                 "st","rd","blvd","hwy"
                     ]) tts_necessary_strings
                          ON stop_name_parts = tts_necessary_strings)
-            -- 2 or more adjacent numbers, "21" should read "twenty one"
+            -- Test 2: Check for >=2 adjacent numerals
+            -- ie "21" should read "twenty one"
             OR REGEXP_CONTAINS(stop_name, '[0-9][0-9]')
-            -- certain symbols, "pine/baker" should read "pine and baker"
+            -- Test 3: Check for symbols that need be spelled out
+            -- ie "pine/baker" should read "pine and baker"
+            -- Note that the brackets are part of regex, and aren't on the "no-no character list"
             OR REGEXP_CONTAINS(stop_name, r'[/()]')
         )
    GROUP BY 1, 2, 3, 4
