dd175777088217d96639c4b5980808e1e05ede9b
Laurie Merrell <laurie.m@jarv.us>
2022-11-02 13:11:23 -0500

    add references to gtfs schedule dataset / feed

--- warehouse/models/mart/gtfs/fct_daily_rt_feed_files.sql
+++ warehouse/models/mart/gtfs/fct_daily_rt_feed_files.sql
@@ -7,7 +7,22 @@
     {% set max_date = dates[0] %}
 {% endif %}
 
-WITH parse_outcomes AS (
+WITH int_transit_database__urls_to_gtfs_datasets AS (
+    SELECT *
+    FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
+),
+
+fct_daily_schedule_feeds AS (
+    SELECT *
+    FROM {{ ref('fct_daily_schedule_feeds') }}
+),
+
+dim_gtfs_datasets AS (
+    SELECT *
+    FROM {{ ref('dim_gtfs_datasets') }}
+),
+
+parse_outcomes AS (
     SELECT *
     FROM {{ ref('int_gtfs_rt__unioned_parse_outcomes') }}
     {% if is_incremental() %}
@@ -35,9 +50,8 @@ grouped_parse_outcomes AS (
         parse_success
 ),
 
-fct_daily_rt_feed_data AS (
+pivoted_parse_outcomes AS (
     SELECT
-        {{ dbt_utils.surrogate_key(['dt', 'base64_url']) }} AS key,
         dt,
         base64_url,
         feed_type,
@@ -45,6 +59,27 @@ fct_daily_rt_feed_data AS (
         parse_failure_file_count
     FROM grouped_parse_outcomes
     PIVOT(SUM(file_count) FOR aggregation_outcome IN ("parse_success_file_count", "parse_failure_file_count"))
+),
+
+fct_daily_rt_feed_files AS (
+    SELECT
+        parse.dt as date,
+        {{ dbt_utils.surrogate_key(['parse.dt', 'parse.base64_url']) }} AS key,
+        parse.base64_url,
+        parse.feed_type,
+        parse.parse_success_file_count,
+        parse.parse_failure_file_count,
+        url_map.gtfs_dataset_key,
+        datasets.schedule_to_use_for_rt_validation_gtfs_dataset_key,
+        schedule.key AS schedule_feed_key
+    FROM pivoted_parse_outcomes AS parse
+    LEFT JOIN int_transit_database__urls_to_gtfs_datasets AS url_map
+        ON parse.base64_url = url_map.base64_url
+    LEFT JOIN dim_gtfs_datasets AS datasets
+        ON url_map.gtfs_dataset_key = datasets.key
+    LEFT JOIN fct_daily_schedule_feeds AS schedule
+        ON datasets.schedule_to_use_for_rt_validation_gtfs_dataset_key = schedule.gtfs_dataset_key
+        AND parse.dt = schedule.date
 )
 
-SELECT * FROM fct_daily_rt_feed_data
+SELECT * FROM fct_daily_rt_feed_files
