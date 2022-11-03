e3d63735c30389c14884354f6669d699d54d20d0
Scott Owades <jeremyscottowades@gmail.com>
2022-11-03 11:31:16 -0700

    include calendar_dates

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__no_expired_services.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__no_expired_services.sql
@@ -3,7 +3,7 @@ WITH feed_guideline_index AS (
     WHERE check = {{ no_expired_services() }}
 ),
 
-stalest_services AS (
+stalest_calendar_services AS (
    SELECT
        calitp_itp_id,
        calitp_url_number,
@@ -11,6 +11,17 @@ stalest_services AS (
        calitp_deleted_at,
        MIN(end_date) AS end_date
     FROM {{ ref('calendar_clean') }}
+   GROUP BY 1, 2, 3, 4
+),
+
+stalest_calendar_dates_services AS (
+   SELECT
+       calitp_itp_id,
+       calitp_url_number,
+       calitp_extracted_at,
+       calitp_deleted_at,
+       MIN(date) AS date
+    FROM {{ ref('calendar_dates_clean') }}
    GROUP BY 1, 2, 3, 4
 ),
 
@@ -23,13 +34,19 @@ daily_stalest_services AS (
     t1.feed_key,
     t1.check,
     t1.feature,
-    MIN(t2.end_date) AS end_date
+    MIN(t2.end_date) AS min_end_date,
+    MIN(t3.date) AS min_date
   FROM feed_guideline_index AS t1
-  LEFT JOIN stalest_services AS t2
+  LEFT JOIN stalest_calendar_services AS t2
        ON t1.date >= t2.calitp_extracted_at
        AND t1.date < t2.calitp_deleted_at
        AND t1.calitp_itp_id = t2.calitp_itp_id
        AND t1.calitp_url_number = t2.calitp_url_number
+  LEFT JOIN stalest_calendar_dates_services AS t3
+       ON t1.date >= t3.calitp_extracted_at
+       AND t1.date < t3.calitp_deleted_at
+       AND t1.calitp_itp_id = t3.calitp_itp_id
+       AND t1.calitp_url_number = t3.calitp_url_number
  GROUP BY 1, 2, 3, 4, 5, 6, 7
 ),
 
@@ -42,13 +59,13 @@ stale_service_check AS (
         feed_key,
         check,
         feature,
-        end_date,
+        min_end_date,
+        min_date,
         CASE
-            WHEN end_date < date THEN "FAIL"
-            WHEN end_date >= date THEN "PASS"
-            -- Else clause captures cases where there is no calendar.txt file, or it is empty. This is OK, as some feeds rely on calendar_dates.txt instead
-            ELSE "PASS"
-        END AS status,
+            WHEN min_end_date < date OR min_date < date THEN "FAIL"
+            WHEN min_end_date >= date OR min_date >= date THEN "PASS"
+            ELSE "N/A"
+        END AS status
       FROM daily_stalest_services
 )
 
