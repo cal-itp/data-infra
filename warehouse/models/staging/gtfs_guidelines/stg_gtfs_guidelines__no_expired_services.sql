ecadf5fc4d9c016f8c2c04fe782334331752fb97
Scott Owades <jeremyscottowades@gmail.com>
2022-11-04 13:35:34 -0700

    join on service id

--- warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__no_expired_services.sql
+++ warehouse/models/staging/gtfs_guidelines/stg_gtfs_guidelines__no_expired_services.sql
@@ -3,15 +3,23 @@ WITH feed_guideline_index AS (
     WHERE check = {{ no_expired_services() }}
 ),
 
-stalest_calendar_services AS (
+daily_calendar_service_expiration AS (
    SELECT
-       calitp_itp_id,
-       calitp_url_number,
-       calitp_extracted_at,
-       calitp_deleted_at,
-       MIN(end_date) AS earliest_end_date
-    FROM {{ ref('calendar_clean') }}
-   GROUP BY 1, 2, 3, 4
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
+        t1.feed_key,
+        t1.check,
+        t1.feature,
+        t2.service_id,
+        t2.end_date
+   FROM feed_guideline_index AS t1
+   LEFT JOIN {{ ref('calendar_clean') }} AS t2
+     ON t1.date >= t2.calitp_extracted_at
+    AND t1.date < t2.calitp_deleted_at
+    AND t1.calitp_itp_id = t2.calitp_itp_id
+    AND t1.calitp_url_number = t2.calitp_url_number
 ),
 
 calendar_dates_service_expiration AS (
@@ -21,45 +29,62 @@ calendar_dates_service_expiration AS (
        calitp_extracted_at,
        calitp_deleted_at,
        service_id,
-       MAX(date) AS service_end_date
+       MAX(date) AS end_date
     FROM {{ ref('calendar_dates_clean') }}
    GROUP BY 1, 2, 3, 4, 5
 ),
 
-stalest_calendar_dates_services AS (
+daily_calendar_dates_service_expiration AS (
    SELECT
-       calitp_itp_id,
-       calitp_url_number,
-       calitp_extracted_at,
-       calitp_deleted_at,
-       MIN(service_end_date) AS earliest_end_date
-    FROM calendar_dates_service_expiration
-   GROUP BY 1, 2, 3, 4
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
+        t1.feed_key,
+        t1.check,
+        t1.feature,
+        t2.service_id,
+        t2.end_date
+   FROM feed_guideline_index AS t1
+   LEFT JOIN calendar_dates_service_expiration AS t2
+     ON t1.date >= t2.calitp_extracted_at
+    AND t1.date < t2.calitp_deleted_at
+    AND t1.calitp_itp_id = t2.calitp_itp_id
+    AND t1.calitp_url_number = t2.calitp_url_number
 ),
 
-daily_stalest_services AS (
-  SELECT
-    t1.date,
-    t1.calitp_itp_id,
-    t1.calitp_url_number,
-    t1.calitp_agency_name,
-    t1.feed_key,
-    t1.check,
-    t1.feature,
-    MIN(t2.earliest_end_date) AS c_min_date,
-    MIN(t3.earliest_end_date) AS cd_min_date
-  FROM feed_guideline_index AS t1
-  LEFT JOIN stalest_calendar_services AS t2
-       ON t1.date >= t2.calitp_extracted_at
-       AND t1.date < t2.calitp_deleted_at
-       AND t1.calitp_itp_id = t2.calitp_itp_id
-       AND t1.calitp_url_number = t2.calitp_url_number
-  LEFT JOIN stalest_calendar_dates_services AS t3
-       ON t1.date >= t3.calitp_extracted_at
-       AND t1.date < t3.calitp_deleted_at
-       AND t1.calitp_itp_id = t3.calitp_itp_id
-       AND t1.calitp_url_number = t3.calitp_url_number
- GROUP BY 1, 2, 3, 4, 5, 6, 7
+daily_service_expiration AS (
+   SELECT
+        t1.date,
+        t1.calitp_itp_id,
+        t1.calitp_url_number,
+        t1.calitp_agency_name,
+        t1.feed_key,
+        t1.check,
+        t1.feature,
+        t1.service_id,
+        MAX(t1.end_date,t2.end_date) AS service_end_date
+   FROM daily_calendar_service_expiration AS t1
+   LEFT JOIN daily_calendar_dates_service_expiration AS t2
+     ON t1.date = t2.date
+    AND t1.calitp_itp_id = t2.calitp_itp_id
+    AND t1.calitp_url_number = t2.calitp_url_number
+    AND t1.service_id = t2.service_id
+  GROUP BY 1,2,3,4,5,6,7,8
+),
+
+daily_earliest_service_expiration AS (
+   SELECT
+        date,
+        calitp_itp_id,
+        calitp_url_number,
+        calitp_agency_name,
+        feed_key,
+        check,
+        feature,
+        MIN(service_end_date) AS earliest_service_end_date
+   FROM daily_service_expiration
+  GROUP BY 1,2,3,4,5,6,7
 ),
 
 stale_service_check AS (
@@ -71,14 +96,13 @@ stale_service_check AS (
         feed_key,
         check,
         feature,
-        c_min_date,
-        cd_min_date,
+        earliest_service_end_date,
         CASE
-            WHEN c_min_date < date OR cd_min_date < date THEN "FAIL"
-            WHEN c_min_date >= date OR cd_min_date >= date THEN "PASS"
+            WHEN earliest_service_end_date < date THEN "FAIL"
+            WHEN earliest_service_end_date >= date THEN "PASS"
             ELSE "N/A"
         END AS status
-      FROM daily_stalest_services
+      FROM daily_earliest_service_expiration
 )
 
 SELECT * FROM stale_service_check
