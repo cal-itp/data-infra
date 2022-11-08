073b29ca6ff0e25cb658e1cbfc2610a7adc8b10e
Andrew Vaccaro <atvaccaro@gmail.com>
2022-11-08 10:19:04 -0500

    fix some bad references

--- warehouse/models/staging/gtfs/stg_gtfs_rt__vehicle_positions_outcomes.sql
+++ warehouse/models/staging/gtfs/stg_gtfs_rt__vehicle_positions_outcomes.sql
@@ -1,5 +1,5 @@
 WITH stg_gtfs_rt__vehicle_positions_outcomes AS (
-    {{ gtfs_rt_stg_parse_outcomes('parse', source('external_gtfs_rt', 'vehicle_positions_outcomes')) }}
+    {{ gtfs_rt_stg_outcomes('parse', source('external_gtfs_rt', 'vehicle_positions_outcomes')) }}
 )
 
 SELECT * FROM stg_gtfs_rt__vehicle_positions_outcomes
