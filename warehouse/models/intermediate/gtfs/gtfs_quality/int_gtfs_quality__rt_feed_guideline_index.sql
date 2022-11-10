c991fb9af655cd560ef39fd723e264c1748cffbf
Andrew Vaccaro <atvaccaro@gmail.com>
2022-11-10 14:33:35 -0500

    create unioned rt intermediate models for checks/quality

---
+++ warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__rt_feed_guideline_index.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='ephemeral') }}
+
+WITH fct_daily_rt_feed_files AS (
+    SELECT * FROM {{ ref('fct_daily_rt_feed_files') }}
+),
+
+-- we never want results from the current date, as data will be incomplete
+int_gtfs_quality__rt_feed_guideline_index AS (
+    SELECT
+        date,
+        base64_url,
+    FROM fct_daily_rt_feed_files
+    WHERE date < CURRENT_DATE
+)
+
+SELECT * FROM int_gtfs_quality__rt_feed_guideline_index
