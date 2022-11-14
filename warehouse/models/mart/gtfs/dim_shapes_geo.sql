6dfeefd06691efffd6abcee13f01f9c2424a9050
Laurie Merrell <laurie.m@jarv.us>
2022-11-14 13:53:04 -0600

    dim shapes geo v2

---
+++ warehouse/models/mart/gtfs/dim_shapes_geo.sql
@@ -0,0 +1,58 @@
+{{ config(materialized='table') }}
+
+WITH dim_shapes AS (
+    SELECT *
+    FROM {{ ref('dim_shapes') }}
+),
+
+-- first, cast lat/long to geography
+lat_long AS (
+    SELECT
+        feed_key,
+        shape_id,
+        shape_pt_sequence,
+        ST_GEOGPOINT(
+            shape_pt_lon,
+            shape_pt_lat
+        ) AS pt_geom,
+        _valid_from,
+        _valid_to
+    FROM dim_shapes
+),
+
+-- collect points into an array
+initial_pt_array AS (
+    SELECT
+        feed_key,
+        shape_id,
+        _valid_from,
+        _valid_to,
+        -- don't try to make LINESTRING because of this issue:
+        -- https://stackoverflow.com/questions/58234223/st-makeline-discarding-duplicate-points-even-if-not-consecutive
+        -- also: https://gis.stackexchange.com/questions/426188/can-i-represent-a-route-that-doubles-back-on-itself-in-bigquery-with-a-linestrin
+        -- so instead this is just an array of WKT points
+        ARRAY_AGG(
+            -- ignore nulls so it doesn't error out if there's a null point
+            pt_geom IGNORE NULLS
+            ORDER BY shape_pt_sequence)
+        AS pt_array,
+        -- count number of rows so we can check for nulls (drops) later
+        COUNT(*) AS ct
+    FROM lat_long
+    GROUP BY feed_key, shape_id, _valid_from, _valid_to
+),
+
+dim_shapes_geo AS (
+    SELECT
+        {{ dbt_utils.surrogate_key(['feed_key', 'shape_id']) }} AS key,
+        feed_key,
+        shape_id,
+        pt_array,
+        _valid_from,
+        _valid_to
+    FROM initial_pt_array
+    -- drop shapes that had nulls
+    WHERE ARRAY_LENGTH(pt_array) = ct
+)
+
+SELECT * FROM dim_shapes_geo
