daeb8e7dd5aca7a6932a6542f2d32ae234359a8b
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2022-11-15 12:50:16 -0500

    make changes per chads email; add retries; log errors (#1971)
    * add retries, fix typo, fix couple other things that chad had done manually
    
    * fix what is supposed to be uppercase, fix epsg checking for just numeric value
    
    * address comments
    
    * missed this one
    
    * missed this one

--- warehouse/scripts/publish.py
+++ warehouse/scripts/publish.py
@@ -1,14 +1,17 @@
 """
 Publishes various dbt models to various sources.
+
+TODO: consider using https://github.com/ckan/ckanapi?
 """
 import functools
 from datetime import timedelta
 
 import csv
 
+import backoff
 import pendulum
 from google.cloud import bigquery
-from typing import Optional, Literal, List, Dict, BinaryIO, Tuple
+from typing import Optional, Literal, List, Dict, BinaryIO, Tuple, Any
 
 from pathlib import Path
 
@@ -28,7 +31,7 @@ import humanize
 import pandas as pd
 import requests
 import typer
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, constr
 from requests import Response
 from requests_toolbelt import MultipartEncoder
 
@@ -89,12 +92,12 @@ class MetadataRow(BaseModel):
     use_constraints: Literal["Creative Commons 4.0 Attribution"]
     data_life_span: None
     caltrans_link: None
-    data_standard: Literal["https://developers.google.com/transit/gtfs"]
+    data_standard: Literal["https://gtfs.org/schedule/"]
     notes: None
     gis_theme: None
     gis_horiz_accuracy: Optional[Literal["4m"]]
     gis_vert_accuracy: Optional[Literal["4m"]]
-    gis_coordinate_system_epsg: Optional[str]
+    gis_coordinate_system_epsg: Optional[constr(regex=r"\d+")]  # noqa: F722
     gis_vert_datum_epsg: None
 
     class Config:
@@ -115,7 +118,7 @@ class DictionaryRow(BaseModel):
     field_name: str
     field_alias: None
     field_description: str
-    field_description_authority: Optional[str]
+    field_description_authority: str
     confidential: Literal["N"]
     sensitive: Literal["N"]
     pii: Literal["N"]
@@ -153,8 +156,18 @@ def make_linestring(x):
     return pts[0]
 
 
+@backoff.on_exception(
+    backoff.constant,
+    requests.exceptions.HTTPError,
+    max_tries=2,
+    interval=10,
+)
 def upload_to_ckan(
-    url: str, fname: str, fsize: int, file: BinaryIO, resource_id: str, api_key: str
+    url: str,
+    fname: str,
+    fsize: int,
+    file: BinaryIO,
+    resource_id: str,
 ):
     def ckan_request(action: str, data: Dict) -> Response:
         encoder = MultipartEncoder(fields=data)
@@ -166,12 +179,17 @@ def upload_to_ckan(
 
     if fsize <= CHUNK_SIZE:
         typer.secho(f"uploading {humanize.naturalsize(fsize)} to {resource_id}")
-        requests.post(
+        response = requests.post(
             f"{url}/api/action/resource_update",
             data={"id": resource_id},
             headers={"Authorization": API_KEY},
             files={"upload": file},
-        ).raise_for_status()
+        )
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            typer.secho(f"response body: {response.text}")
+            raise
     else:
         typer.secho(
             f"uploading {humanize.naturalsize(fsize)} to {resource_id} in {humanize.naturalsize(CHUNK_SIZE)} chunks"
@@ -184,7 +202,11 @@ def upload_to_ckan(
                 "size": str(fsize),
             },
         )
-        initiate_response.raise_for_status()
+        try:
+            initiate_response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            typer.secho(f"response body: {initiate_response.text}")
+            raise
         upload_id = initiate_response.json()["result"]["id"]
 
         # https://stackoverflow.com/a/54989668
@@ -242,7 +264,7 @@ def upload_to_ckan(
 
 def _generate_exposure_documentation(
     exposure: Exposure,
-) -> Tuple[List[MetadataRow], List[DictionaryRow]]:
+) -> Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]:
     try:
         resources = next(
             dest
@@ -254,8 +276,8 @@ def _generate_exposure_documentation(
             "cannot generate documentation for exposure without CKAN destination"
         )
 
-    metadata_rows: List[MetadataRow] = []
-    dictionary_rows: List[DictionaryRow] = []
+    metadata_rows: List[Dict[str, Any]] = []
+    dictionary_rows: List[Dict[str, Any]] = []
 
     for node in exposure.depends_on.resolved_nodes:
         name = node.name
@@ -264,78 +286,81 @@ def _generate_exposure_documentation(
         if name in resources and resources[name].description:
             description = resources[name].description
 
+        row = MetadataRow(
+            dataset_name=name,
+            tags=[
+                "transit",
+                "gtfs",
+                "gtfs-schedule",
+                "bus",
+                "rail",
+                "ferry",
+                "mobility",
+            ],
+            description=description,
+            methodology=exposure.meta.methodology,
+            topic="Transportation",
+            publisher_organization="Caltrans",
+            place="CA",
+            frequency="Monthly",
+            next_update=pendulum.today() + timedelta(days=30),
+            creation_date=pendulum.today(),
+            last_update=None,
+            status="Complete",
+            temporal_coverage_begin=None,
+            temporal_coverage_end=None,
+            data_dictionary="",
+            data_dictionary_type="csv",
+            contact_organization="Caltrans",
+            contact_position="Cal-ITP",
+            contact_name="Hunter Owens",
+            contact_email="hunter.owens@dot.ca.gov",
+            public_access_level="Public",
+            access_constraints=None,
+            use_constraints="Creative Commons 4.0 Attribution",
+            data_life_span=None,
+            caltrans_link=None,
+            data_standard="https://gtfs.org/schedule/",
+            notes=None,
+            gis_theme=None,
+            gis_horiz_accuracy="4m",
+            gis_vert_accuracy="4m",
+            gis_coordinate_system_epsg=exposure.meta.coordinate_system_epsg,
+            gis_vert_datum_epsg=None,
+        )
         metadata_rows.append(
-            json.loads(
-                MetadataRow(
-                    dataset_name=name,
-                    tags=[
-                        "transit",
-                        "gtfs",
-                        "gtfs-schedule",
-                        "bus",
-                        "rail",
-                        "ferry",
-                        "mobility",
-                    ],
-                    description=description,
-                    methodology=exposure.meta.methodology,
-                    topic="Transportation",
-                    publisher_organization="Caltrans",
-                    place="CA",
-                    frequency="Monthly",
-                    next_update=pendulum.today() + timedelta(days=30),
-                    creation_date=pendulum.today(),
-                    last_update=None,
-                    status="Complete",
-                    temporal_coverage_begin=None,
-                    temporal_coverage_end=None,
-                    data_dictionary="",
-                    data_dictionary_type="csv",
-                    contact_organization="Caltrans",
-                    contact_position="Cal-ITP",
-                    contact_name="Hunter Owens",
-                    contact_email="hunter.owens@dot.ca.gov",
-                    public_access_level="Public",
-                    access_constraints=None,
-                    use_constraints="Creative Commons 4.0 Attribution",
-                    data_life_span=None,
-                    caltrans_link=None,
-                    data_standard="https://developers.google.com/transit/gtfs",
-                    notes=None,
-                    gis_theme=None,
-                    gis_horiz_accuracy="4m",
-                    gis_vert_accuracy="4m",
-                    gis_coordinate_system_epsg=exposure.meta.coordinate_system_espg,
-                    gis_vert_datum_epsg=None,
-                ).json(models_as_dict=False)
-            )
+            {
+                k.upper(): v
+                for k, v in json.loads(row.json(models_as_dict=False)).items()
+            }
         )
 
         for name, column in node.columns.items():
             if not column.meta.get("publish.ignore", False):
+                row = DictionaryRow(
+                    system_name="Cal-ITP GTFS-Ingest Pipeline",
+                    table_name=node.name,
+                    field_name=column.name,
+                    field_alias=None,
+                    field_description=column.description,
+                    field_description_authority=column.meta.get(
+                        "ckan.authority", node.meta.get("ckan.authority")
+                    ),
+                    confidential="N",
+                    sensitive="N",
+                    pii="N",
+                    pci="N",
+                    field_type=column.meta.get("ckan.type", "STRING"),
+                    field_length=column.meta.get("ckan.length", 1024),
+                    field_precision=column.meta.get("ckan.precision"),
+                    units=None,
+                    domain_type="Unrepresented",
+                    allowable_min_value=None,
+                    allowable_max_value=None,
+                    usage_notes=None,
+                )
                 dictionary_rows.append(
-                    json.loads(
-                        DictionaryRow(
-                            system_name="Cal-ITP GTFS-Ingest Pipeline",
-                            table_name=node.name,
-                            field_name=column.name,
-                            field_alias=None,
-                            field_description=column.description,
-                            field_description_authority="",
-                            confidential="N",
-                            sensitive="N",
-                            pii="N",
-                            pci="N",
-                            field_type=column.meta.get("ckan.type", "STRING"),
-                            field_length=column.meta.get("ckan.length", 1024),
-                            field_precision=column.meta.get("ckan.precision"),
-                            units=None,
-                            domain_type="Unrepresented",
-                            allowable_min_value=None,
-                            allowable_max_value=None,
-                            usage_notes=None,
-                        ).json()
-                    )
+                    {k.upper(): v for k, v in json.loads(row.json()).items()}
                 )
     return metadata_rows, dictionary_rows
 
@@ -367,7 +392,9 @@ def _publish_exposure(
                         fg=typer.colors.GREEN,
                     )
                     with fs.open(hive_path, "w", newline="") as f:
-                        writer = csv.DictWriter(f, fieldnames=cls.__fields__.keys())
+                        writer = csv.DictWriter(
+                            f, fieldnames=[key.upper() for key in cls.__fields__.keys()]
+                        )
                         writer.writeheader()
                         for row in rows:
                             writer.writerow(row)
@@ -416,7 +443,7 @@ def _publish_exposure(
                         f"writing {len(df)} rows ({humanize.naturalsize(os.stat(fpath).st_size)}) from {node.schema_table} to {hive_path}",
                         fg=typer.colors.GREEN,
                     )
-                    fs.put(fpath, hive_path)
+                    fs.put_file(fpath, hive_path)
 
                     fname = destination.filename(model_name)
                     fsize = os.path.getsize(fpath)
@@ -432,7 +459,6 @@ def _publish_exposure(
                                 fsize=fsize,
                                 file=fp,
                                 resource_id=resource.id,
-                                api_key=API_KEY,
                             )
                     else:
                         typer.secho(
@@ -543,7 +569,9 @@ def document_exposure(
         (dictionary_rows, dictionary_output, DictionaryRow),
     ):
         with opener(file, "w", newline="") as f:
-            writer = csv.DictWriter(f, fieldnames=cls.__fields__.keys())
+            writer = csv.DictWriter(
+                f, fieldnames=[key.upper() for key in cls.__fields__.keys()]
+            )
             writer.writeheader()
             for row in rows:
                 writer.writerow(row)
