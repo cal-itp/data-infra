be3b54fa37cd3519c24dd6939dd5109115820514
Andrew Vaccaro <atvaccaro@gmail.com>
2022-11-22 18:12:40 -0500

    work on versioned schedule validation codes

---
+++ warehouse/models/staging/gtfs/gtfs_quality/stg_gtfs_quality__schedule_validation_code_descriptions.sql
@@ -0,0 +1,29 @@
+WITH
+
+v3 AS (
+    SELECT * FROM {{ ref('gtfs_schedule_validation_code_descriptions_v3_1_1') }}
+),
+
+v4 AS (
+    SELECT * FROPM {{ ref('gtfs_schedule_validation_code_descriptions_v4_0_0') }}
+)
+
+stg_gtfs_quality__schedule_validation_code_descriptions AS (
+    SELECT
+        "type" AS severity,
+        Name AS code,
+        Human_Readable_Description AS description,
+        'v3.1.1' AS gtfs_validator_version, -- this has to match what's in the validator Dockerfile
+    FROM v3
+
+    UNION ALL
+
+    SELECT
+        "type" AS severity,
+        Name AS code,
+        Human_Readable_Description AS description,
+        'v4.0.0' AS gtfs_validator_version, -- this has to match what's in the validator Dockerfile
+    FROM v4
+)
+
+SELECT * FROM stg_gtfs_quality__schedule_validation_code_descriptions
