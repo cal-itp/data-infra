2aa62bdbc86f451236f559e86652801f103d9628
Charles Costanzo <cvc5185@gmail.com>
2022-11-23 13:45:58 -0500

    revisions from another round of Laurie review

--- warehouse/models/mart/gtfs/fct_daily_scheduled_service_summary.sql
+++ warehouse/models/mart/gtfs/fct_daily_scheduled_service_summary.sql
@@ -19,7 +19,10 @@ fct_daily_scheduled_service_summary AS (
         MAX(trip_last_arrival_ts) AS last_arrival_ts,
         SUM(n_stop_times) AS n_stop_times,
         COUNT(DISTINCT route_id) AS n_routes,
-        LOGICAL_OR(contains_warning_duplicate_primary_key) AS contains_warning_duplicate_primary_key,
+        LOGICAL_OR(contains_warning_duplicate_stop_times_primary_key) AS contains_warning_duplicate_stop_times_primary_key,
+        LOGICAL_OR(contains_warning_duplicate_trip_primary_key) AS contains_warning_duplicate_trip_primary_key,
+
+        -- @Laurie, if none of the calculations in this table rely on stop_id, do we need to keep the warning column below?
         LOGICAL_OR(contains_warning_missing_foreign_key_stop_id) AS contains_warning_missing_foreign_key_stop_id
 
     FROM fct_daily_scheduled_trips
--- warehouse/models/mart/gtfs/fct_daily_scheduled_trips.sql
+++ warehouse/models/mart/gtfs/fct_daily_scheduled_trips.sql
@@ -51,14 +51,14 @@ fct_daily_scheduled_trips AS (
 
         shapes.key AS shape_array_key,
         urls_to_gtfs_datasets.gtfs_dataset_key AS gtfs_dataset_key,
-        trips.warning_duplicate_primary_key AS warning_duplicate_trip_primary_key,
+        trips.warning_duplicate_primary_key AS contains_warning_duplicate_trip_primary_key,
 
         stop_times_grouped.n_stops,
         stop_times_grouped.n_stop_times,
         stop_times_grouped.trip_first_departure_ts,
         stop_times_grouped.trip_last_arrival_ts,
         stop_times_grouped.service_hours,
-        stop_times_grouped.contains_warning_duplicate_primary_key,
+        stop_times_grouped.contains_warning_duplicate_primary_key AS contains_warning_duplicate_stop_times_primary_key,
         stop_times_grouped.contains_warning_missing_foreign_key_stop_id
 
     FROM int_gtfs_schedule__daily_scheduled_service_index AS service_index
