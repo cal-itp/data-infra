d5686461b1099c4346f753ebb004d2e8eba6ea47
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2022-12-01 15:05:29 -0500

    RT performance tweaks (#2027)
    * start working on huey optimizations/metrics
    
    * use blocking in prod
    
    * try 3 in test
    
    * do not block in test
    
    * remove unneeded imports
    
    * put 6 replicas for both test and prod
    
    * use new defaults
    
    * bump version to deploy to prod
    
    * put test back on 3 consumers

--- services/gtfs-rt-archiver-v3/gtfs_rt_archiver_v3/tasks.py
+++ services/gtfs-rt-archiver-v3/gtfs_rt_archiver_v3/tasks.py
@@ -5,7 +5,6 @@ from functools import wraps
 from pathlib import Path
 
 import humanize
-import orjson
 import pendulum
 import sentry_sdk
 import structlog
@@ -13,8 +12,6 @@ import typer
 from calitp.storage import download_feed, GTFSDownloadConfig
 from google.cloud import storage
 from huey import RedisHuey
-from huey.registry import Message
-from huey.serializer import Serializer
 from requests import HTTPError, RequestException
 
 from .metrics import (
@@ -25,27 +22,21 @@ from .metrics import (
 )
 
 
-class PydanticSerializer(Serializer):
-    def _serialize(self, data: Message) -> bytes:
-        return orjson.dumps(data._asdict())
-
-    def _deserialize(self, data: bytes) -> Message:
-        # deal with datetimes manually
-        d = orjson.loads(data)
-        d["expires_resolved"] = datetime.fromisoformat(d["expires_resolved"])
-        d["kwargs"]["tick"] = datetime.fromisoformat(d["kwargs"]["tick"])
-        return Message(*d.values())
+class RedisHueyWithMetrics(RedisHuey):
+    pass
 
 
-huey = RedisHuey(
+huey = RedisHueyWithMetrics(
     name=f"gtfs-rt-archiver-v3-{os.environ['AIRFLOW_ENV']}",
-    blocking=False,
+    blocking=os.getenv("CALITP_HUEY_BLOCKING", "true").lower()
+    in ("true", "t", "yes", "y", "1"),
     results=False,
     # serializer=PydanticSerializer(),
     url=os.getenv("CALITP_HUEY_REDIS_URL"),
     host=os.getenv("CALITP_HUEY_REDIS_HOST"),
     port=os.getenv("CALITP_HUEY_REDIS_PORT"),
     password=os.getenv("CALITP_HUEY_REDIS_PASSWORD"),
+    read_timeout=int(os.getenv("CALITP_HUEY_READ_TIMEOUT", 1)),  # default from huey
 )
 
 
