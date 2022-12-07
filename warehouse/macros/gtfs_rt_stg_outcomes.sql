cal-itp/data-infra

commit bd28f798fc9c12044de77e46eb34a9dfc43c4625

consider situations with no RT entity to be successful parses (#2044)
* we should succeed parsing RT if there are no underlying records but the message is valid

* save headers too

* add header as json column in rt outcome external tables

* make header queryable from parse outcomes only

* check against enum

* use SOURCE_DATABASE across the project

warehouse/macros/gtfs_rt_stg_outcomes.sql
@@ -1,6 +1,6 @@
 {% macro gtfs_rt_stg_outcomes(step, source_table) %}
 
-WITH raw_aggregation_outcomes AS (
+WITH raw_outcomes AS (
     SELECT
         *,
         {{ to_url_safe_base64('`extract`.config.url') }} AS base64_url
@@ -23,10 +23,11 @@ stg_gtfs_rt__agg_outcomes AS (
         `extract`.response_headers AS download_response_headers,
         aggregation.step AS step,
         base64_url,
+        {% if step == 'parse' %}header,{% endif %}
         `extract`.ts AS extract_ts
-    FROM raw_aggregation_outcomes
+    FROM raw_outcomes
 )
 
-SELECT * FROM stg_gtfs_rt__agg_outcomes
+SELECT * FROM raw_outcomes
 
 {% endmacro %}

airflow/dags/create_external_tables/gtfs_rt_v2/rt_v2_service_alerts_outcomes.yml
@@ -43,6 +43,8 @@ schema_fields:
         type: INT64
       - name: response_headers
         type: JSON
+  - name: header
+    type: JSON
   - name: aggregation
     type: RECORD
     fields:

airflow/dags/create_external_tables/gtfs_rt_v2/rt_v2_trip_updates_outcomes.yml
@@ -43,6 +43,8 @@ schema_fields:
         type: INT64
       - name: response_headers
         type: JSON
+  - name: header
+    type: JSON
   - name: aggregation
     type: RECORD
     fields:

airflow/dags/create_external_tables/gtfs_rt_v2/rt_v2_vehicle_positions_outcomes.yml
@@ -43,6 +43,8 @@ schema_fields:
         type: INT64
       - name: response_headers
         type: JSON
+  - name: header
+    type: JSON
   - name: aggregation
     type: RECORD
     fields:

jobs/gtfs-rt-parser-v2/gtfs_rt_parser.py
@@ -209,15 +209,17 @@ class RTHourlyAggregation(PartitionedGCSArtifact):
 
 class RTFileProcessingOutcome(ProcessingOutcome):
     # an extract is technically optional if we have a blob missing metadata
+    step: RTProcessingStep
     extract: Optional[GTFSRTFeedExtract]
+    header: Optional[Dict[Any, Any]]
     aggregation: Optional[RTHourlyAggregation]
     blob_path: Optional[str]
 
     @validator("aggregation", allow_reuse=True, always=True)
     def aggregation_exists_if_success(cls, v, values):
-        assert (v is not None) == values[
-            "success"
-        ], "aggregation must exist if and only if the outcome is successful"
+        assert (
+            values["success"] or v is None
+        ), "aggregation cannot exist if there is a failure"
         return v
 
     @validator("blob_path", allow_reuse=True, always=True)
@@ -225,6 +227,12 @@ class RTFileProcessingOutcome(ProcessingOutcome):
         assert (v is None) != (
             values["extract"] is None
         ), "one of blob or extract must be null"
+        return v
+
+    @validator("header", allow_reuse=True)
+    def header_must_exist_for_successful_parses(cls, v, values):
+        if values["success"] and values["step"] == RTProcessingStep.parse:
+            assert v
         return v
 
 
@@ -474,7 +482,7 @@ def parse_and_upload(
                     )
                 outcomes.append(
                     RTFileProcessingOutcome(
-                        step="parse",
+                        step=RTProcessingStep.parse,
                         success=False,
                         exception=e,
                         extract=extract,
@@ -482,7 +490,25 @@ def parse_and_upload(
                 )
                 continue
 
-            if not parsed or "entity" not in parsed:
+            if not parsed:
+                msg = f"WARNING: no parsed dictionary found in {str(extract.path)}"
+                if verbose:
+                    log(
+                        msg,
+                        fg=typer.colors.YELLOW,
+                        pbar=pbar,
+                    )
+                outcomes.append(
+                    RTFileProcessingOutcome(
+                        step=RTProcessingStep.parse,
+                        success=False,
+                        exception=ValueError(msg),
+                        extract=extract,
+                    )
+                )
+                continue
+
+            if "entity" not in parsed:
                 msg = f"WARNING: no parsed entity found in {str(extract.path)}"
                 if verbose:
                     log(
@@ -492,10 +518,10 @@ def parse_and_upload(
                     )
                 outcomes.append(
                     RTFileProcessingOutcome(
-                        step="parse",
-                        success=False,
-                        exception=ValueError(msg),
+                        step=RTProcessingStep.parse,
+                        success=True,
                         extract=extract,
+                        header=parsed["header"],
                     )
                 )
                 continue
@@ -522,10 +548,11 @@ def parse_and_upload(
                 written += 1
             outcomes.append(
                 RTFileProcessingOutcome(
-                    step="parse",
+                    step=RTProcessingStep.parse,
                     success=True,
                     extract=extract,
                     aggregation=hour,
+                    header=parsed["header"],
                 )
             )
             del parsed

warehouse/dbt_project.yml
@@ -23,6 +23,7 @@ clean-targets:         # directories to be removed by `dbt clean`
 
 vars:
   INCREMENTAL_PARTITIONS_LOOKBACK_DAYS: 30
+  SOURCE_DATABASE: cal-itp-data-infra
 
 models:
   calitp_warehouse:

warehouse/models/_source_gtfs_schedule_history.yml
@@ -3,7 +3,7 @@ version: 2
 sources:
   - name: gtfs_schedule_history
     description: Data in the gtfs_schedule_history dataset in BigQuery
-    database: cal-itp-data-infra
+    database: "{{ var('SOURCE_DATABASE') }}"
     schema: gtfs_schedule_history
     tables:
       - name: calitp_feed_parse_result

warehouse/models/gtfs_views_staging/_gtfs_views_staging.yml
@@ -3,7 +3,7 @@ version: 2
 sources:
   - name: gtfs_type2
     description: Data in the gtfs_schedule_type2 dataset in BigQuery
-    database: cal-itp-data-infra
+    database: "{{ var('SOURCE_DATABASE') }}"
     schema: gtfs_schedule_type2
     loaded_at_field: CAST(calitp_extracted_at AS TIMESTAMP)
     freshness:

commit e24b989476f97a85732c3e484667216d8579b699

fix issue introduced in #2044; use correct cte in final select

warehouse/macros/gtfs_rt_stg_outcomes.sql
@@ -28,6 +28,6 @@ stg_gtfs_rt__agg_outcomes AS (
     FROM raw_outcomes
 )
 
-SELECT * FROM raw_outcomes
+SELECT * FROM stg_gtfs_rt__agg_outcomes
 
 {% endmacro %}