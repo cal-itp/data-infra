4c46146f0c231654ec5967ad12fd264562be3f0b
Laurie Merrell <laurie.m@jarv.us>
2022-12-07 13:07:55 -0600

    draft of daily services & documentation

---
+++ warehouse/models/intermediate/transit_database/int_transit_database__service_history.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='table') }}
+
+WITH stg_transit_database__services AS (
+    SELECT *
+    FROM {{ ref('stg_transit_database__services') }}
+),
+
+int_gtfs_quality__service_history AS (
+    SELECT
+        calitp_extracted_at AS date,
+        key,
+        name,
+        assessment_status,
+        currently_operating,
+        service_type
+    FROM stg_transit_database__services
+)
+
+SELECT * FROM int_gtfs_quality__service_history
