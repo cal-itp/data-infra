ccf0c96665c2c6e3afcd332d6edc7150c1d7ce81
Scott Owades <jeremyscottowades@gmail.com>
2022-12-07 17:08:52 -0800

    comments

--- warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__lead_time.sql
+++ warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__lead_time.sql
@@ -5,6 +5,7 @@ WITH feed_guideline_index AS (
 dim_calendar AS (
     SELECT  *,
             -- Single field to represent the calendar for easy comparison
+            -- An example value of this field is "0000011"
             CONCAT(CAST(monday AS string), CAST(tuesday AS string), CAST(wednesday AS string), CAST(thursday AS string),
                 CAST(friday AS string), CAST(saturday AS string), CAST(sunday AS string)) AS day_combo
       FROM {{ ref('dim_calendar') }}
@@ -37,7 +38,7 @@ stop_times_expanded AS (
     SELECT t1.feed_key,
            t1.trip_id,
            t1.stop_sequence,
-           CONCAT(t1.arrival_time,t1.departure_time,t2.stop_lat,t2.stop_lon) AS stop_info_combined
+           CONCAT(t1.arrival_time,t1.departure_time,t2.stop_lat,t2.stop_lon) AS stop_info
       FROM dim_stop_times t1
       LEFT JOIN dim_stops t2
         ON t2.stop_id = t1.stop_id
@@ -49,7 +50,7 @@ stop_times_agg AS (
     SELECT feed_key,
            trip_id,
            -- A single field summarizing all stop_times and stop info for this trip, for easy comparison
-           STRING_AGG(stop_info_combined ORDER BY stop_sequence ASC) AS stop_info_agg
+           STRING_AGG(stop_info ORDER BY stop_sequence ASC) AS stop_info_agg
       FROM stop_times_expanded
      GROUP BY 1,2
 ),
@@ -59,7 +60,7 @@ trips_expanded AS (
     SELECT t1.feed_key,
            t1.trip_id,
            -- Combine stop & schedule summary fields for one summary field to rule them all
-           CONCAT(t2.stop_info_agg,t3.day_combo) AS trip_info_combined,
+           CONCAT(t2.stop_info_agg,t3.day_combo) AS trip_info,
            t3.start_date,
            t3.end_date
       FROM dim_trips t1
@@ -76,14 +77,13 @@ distinct_feed_versions AS (
            key AS feed_key,
            _valid_from
       FROM {{ ref('dim_schedule_feeds') }}
-     WHERE key IS NOT null
 ),
 
 -- Maps each feed_key to the feed_key of the previous version of that feed
 feed_version_history AS (
     SELECT base64_url,
            feed_key,
-           LAG (feed_key) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS previous_feed_key,
+           LAG (feed_key) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS prev_feed_key,
            LEAD (feed_key) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_key,
            LEAD (EXTRACT(date FROM _valid_from)) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_valid_from,
            EXTRACT(date FROM _valid_from) AS valid_from
@@ -94,17 +94,19 @@ feed_version_history AS (
 trips_version_history AS (
   SELECT t1.base64_url,
          t1.feed_key,
-         t1.previous_feed_key,
+         t1.prev_feed_key,
          t1.next_feed_key,
          t1.valid_from,
          t1.next_feed_valid_from,
          trips.trip_id,
          trips.start_date,
          trips.end_date,
-         trips.trip_info_combined AS trip_info_combined,
+         trips.trip_info
     FROM feed_version_history AS t1
     JOIN trips_expanded AS trips
       ON t1.feed_key = trips.feed_key
+    -- Since we are comparing feeds with their previous version, omit the initial version of every feed - no comparison is possible
+   WHERE t1.prev_feed_key IS NOT null
 ),
 
 -- The self-outer-join, with all of the coalescing, allows us to see:
@@ -112,18 +114,24 @@ trips_version_history AS (
 ------ 2 trips that were ADDED since the previous version
 ------ 3 trips that were CHANGED since the previous version
 trips_version_compare AS (
-  SELECT COALESCE(trips.base64_url,prev_trips.base64_url) AS base64_url,
+  SELECT
+         -- base64_url is same between feed versions
+         COALESCE(trips.base64_url,prev_trips.base64_url) AS base64_url,
+         -- one feed's key is the previous feed's next key
          COALESCE(trips.feed_key,prev_trips.next_feed_key) AS feed_key,
+         -- one feed's previous key is the previous feed's key
+         COALESCE(trips.prev_feed_key,prev_trips.feed_key) AS prev_feed_key,
+         -- we need to know the next feed's valid_from date, in cases where a trip is removed since the previous feed
          COALESCE(trips.valid_from,prev_trips.next_feed_valid_from) AS valid_from,
          trips.trip_id,
          prev_trips.trip_id AS prev_trip_id,
-         trips.trip_info_combined,
-         prev_trips.trip_info_combined AS prev_trip_info_combined,
+         trips.trip_info,
+         prev_trips.trip_info AS prev_trip_info,
          DATE_DIFF(COALESCE(trips.valid_from, prev_trips.next_feed_valid_from), COALESCE(trips.start_date,prev_trips.start_date), DAY) AS days_since_start_date,
          DATE_DIFF(COALESCE(trips.end_date, prev_trips.end_date), COALESCE(trips.valid_from,prev_trips.next_feed_valid_from), DAY) AS days_until_end_date
     FROM trips_version_history AS trips
     FULL OUTER JOIN trips_version_history AS prev_trips
-      ON trips.previous_feed_key = prev_trips.feed_key
+      ON trips.prev_feed_key = prev_trips.feed_key
      AND trips.trip_id = prev_trips.trip_id
 ),
 
@@ -133,11 +141,11 @@ daily_improper_trips_updates AS (
          feed_key,
          valid_from AS date,
           -- A new trip is being added
-         COUNT(CASE WHEN prev_trip_info_combined IS null THEN 1 END) AS trip_added,
+         COUNT(CASE WHEN prev_trip_info IS null THEN 1 END) AS trip_added,
           -- An existing trip is being removed
-         COUNT(CASE WHEN trip_info_combined IS null THEN 1 END) AS trip_removed,
+         COUNT(CASE WHEN trip_info IS null THEN 1 END) AS trip_removed,
           -- An existing trip's is being changed
-         COUNT(CASE WHEN trip_info_combined != prev_trip_info_combined THEN 1 END) AS trip_changed
+         COUNT(CASE WHEN trip_info != prev_trip_info THEN 1 END) AS trip_changed
     FROM trips_version_compare
    WHERE
          -- End date is in the future
@@ -147,10 +155,10 @@ daily_improper_trips_updates AS (
    GROUP BY 1,2,3
 ),
 
-calendar_dates_joined AS (
+calendar_dates_version_history AS (
   SELECT t1.base64_url,
          t1.feed_key,
-         t1.previous_feed_key,
+         t1.prev_feed_key,
          t1.next_feed_key,
          t1.valid_from,
          t1.next_feed_valid_from,
@@ -160,22 +168,30 @@ calendar_dates_joined AS (
     FROM feed_version_history AS t1
     JOIN dim_calendar_dates AS cal_dates
       ON t1.feed_key = cal_dates.feed_key
+    -- Since we are comparing feeds with their previous version, omit the initial version of every feed - no comparison is possible
+   WHERE t1.prev_feed_key IS NOT null
 ),
 
 -- The self-outer-join, with all of the coalescing, allows us to see:
 ------ 1 dates that were REMOVED since the previous version
 ------ 2 dates that were ADDED since the previous version
 ------ 3 dates that were CHANGED since the previous version
-calendar_dates_joined_previous AS (
-  SELECT COALESCE(cal_dates.base64_url, prev_cal_dates.base64_url) AS base64_url,
-         COALESCE(cal_dates.feed_key, prev_cal_dates.next_feed_key) AS feed_key,
-         COALESCE(cal_dates.valid_from, prev_cal_dates.next_feed_valid_from) AS valid_from,
+calendar_dates_version_compare AS (
+  SELECT
+         -- base64_url is same between feed versions
+         COALESCE(trips.base64_url,prev_trips.base64_url) AS base64_url,
+         -- one feed's key is the previous feed's next key
+         COALESCE(trips.feed_key,prev_trips.next_feed_key) AS feed_key,
+         -- one feed's previous key is the previous feed's key
+         COALESCE(trips.prev_feed_key,prev_trips.feed_key) AS prev_feed_key,
+         -- we need to know the next feed's valid_from date, in cases where a trip is removed since the previous feed
+         COALESCE(trips.valid_from,prev_trips.next_feed_valid_from) AS valid_from,
          cal_dates.exception_type,
          prev_cal_dates.exception_type AS prev_exception_type,
          DATE_DIFF(COALESCE(cal_dates.date,prev_cal_dates.date), COALESCE(cal_dates.valid_from,prev_cal_dates.next_feed_valid_from), DAY) AS days_until_date
-    FROM calendar_dates_joined AS cal_dates
-    FULL OUTER JOIN calendar_dates_joined AS prev_cal_dates
-      ON cal_dates.previous_feed_key = prev_cal_dates.feed_key
+    FROM calendar_dates_version_history AS cal_dates
+    FULL OUTER JOIN calendar_dates_version_history AS prev_cal_dates
+      ON cal_dates.prev_feed_key = prev_cal_dates.feed_key
      AND cal_dates.service_id_date = prev_cal_dates.service_id_date
 ),
 
@@ -190,7 +206,7 @@ daily_improper_calendar_dates_updates AS (
          COUNT(CASE WHEN exception_type IS null THEN 1 END) AS cal_removed,
           -- An existing calendar_date is changing its exception_type
          COUNT(CASE WHEN exception_type != prev_exception_type THEN 1 END) AS cal_changed
-    FROM calendar_dates_joined_previous
+    FROM calendar_dates_version_compare
    WHERE
         -- Date is between 0 and 7 days from now
         days_until_date >= 0 AND days_until_date <= 7
