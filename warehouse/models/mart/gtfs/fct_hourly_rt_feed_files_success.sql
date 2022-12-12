3848dc87eacc345a7954f5bf2754f413b46268b5
Charles Costanzo <cvc5185@gmail.com>
2022-12-12 12:09:32 -0500

    changed pivot aggregation to MAX()

--- warehouse/models/mart/gtfs/fct_hourly_rt_feed_files_success.sql
+++ warehouse/models/mart/gtfs/fct_hourly_rt_feed_files_success.sql
@@ -70,7 +70,7 @@ pivot_hourly_totals AS (
 
             FROM hourly_totals)
     PIVOT(
-        SUM(pct_success_hour)
+        MAX(pct_success_hour)
         FOR download_hour IN
         (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
             11, 12, 13, 14, 15, 16, 17, 18, 19,
