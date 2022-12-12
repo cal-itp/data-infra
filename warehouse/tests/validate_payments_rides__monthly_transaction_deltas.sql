7540c2be2f377eb19ce5257937268996343c3b7e
Charlie Costanzo <cvc5185@gmail.com>
2022-12-12 11:58:53 -0500

    Payments transaction delta alerts (#2042)
    * added dbt_expectations package, dbt_date config, and 48 hour freshness test to transaction date in payments_rides
    
    * added custom test to test mst transaction deltas
    
    * adding updated poetry.lock file
    
    * adding updated requirements.txt
    
    * added tests for weekly and monthly transaction deltas
    
    * exporting requirements.txt without hashes because action is failing
    
    * revised tests to aggregate by participant_id
    
    * renamed test files to exclude mst

---
+++ warehouse/tests/validate_payments_rides__monthly_transaction_deltas.sql
@@ -0,0 +1,74 @@
+{{ config(store_failures = true) }}
+
+WITH payments_rides AS (
+
+    SELECT *
+    FROM {{ ref('payments_rides') }}
+
+),
+
+extract_count_date AS (
+
+    SELECT
+
+        participant_id,
+        COUNT(*) AS ridership_count,
+        CONCAT(CAST(EXTRACT(YEAR FROM transaction_date_time_pacific) AS string),
+            '-',
+            LPAD(
+                CAST(
+                    EXTRACT(MONTH FROM transaction_date_time_pacific) AS string
+                ),
+                2,
+                '0'
+            )
+        ) AS yearmonth
+
+    FROM payments_rides
+    GROUP BY yearmonth, participant_id
+),
+
+
+calculate_relative_difference AS (
+
+    SELECT
+
+        *,
+        (
+            (
+                ridership_count - LAG(
+                    ridership_count, 1
+                ) OVER (PARTITION BY participant_id ORDER BY yearmonth)
+            ) / LAG(ridership_count, 1) OVER (PARTITION BY participant_id ORDER BY yearmonth)
+        ) * 100
+        AS relative_difference
+
+    FROM extract_count_date
+
+),
+
+test_recent_values AS (
+
+    SELECT
+
+        participant_id,
+        yearmonth,
+        ridership_count,
+        relative_difference
+
+    FROM
+        (SELECT
+            participant_id,
+            yearmonth,
+            ridership_count,
+            relative_difference,
+            RANK() OVER (PARTITION BY participant_id ORDER BY yearmonth DESC) AS rank
+            FROM calculate_relative_difference)
+    WHERE rank != 1
+        AND rank < 5
+        AND ABS(relative_difference) > 25.0
+    ORDER BY yearmonth
+
+)
+
+SELECT * FROM test_recent_values
