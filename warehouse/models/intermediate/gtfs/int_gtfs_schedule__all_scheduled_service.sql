ff7def0ab5d51bdafd244ea376693de65104746b
Laurie Merrell <laurie.m@jarv.us>
2022-12-14 15:05:47 -0600

    working refactor that has discrepancies with prod

--- warehouse/models/intermediate/gtfs/int_gtfs_schedule__all_scheduled_service.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_schedule__all_scheduled_service.sql
@@ -1,12 +1,6 @@
 {{ config(materialized='table') }}
 
-WITH date_spine AS (
-    SELECT *,
-        EXTRACT(DAYOFWEEK FROM date_day) AS day_num
-    FROM {{ ref('util_gtfs_schedule_v2_date_spine') }}
-),
-
-dim_calendar_dates AS (
+WITH dim_calendar_dates AS (
     SELECT *
     FROM {{ ref('dim_calendar_dates') }}
 ),
@@ -18,56 +12,32 @@ int_gtfs_schedule__long_calendar AS (
 
 boolean_calendar_dates AS (
     SELECT
-        date,
+        date AS service_date,
         feed_key,
-        key AS calendar_dates_key,
+        key,
         service_id,
         CASE
             WHEN exception_type = 1 THEN TRUE
             WHEN exception_type = 2 THEN FALSE
-        END AS has_service
+        END AS service_bool
     FROM dim_calendar_dates
 ),
 
--- decide that exception type 2 trumps exception type 1
--- i.e., if same date appears twice with two exception types
--- the cancelation wins and we say no service on that date
--- (this generally shouldn't happen)
-summarize_calendar_dates AS (
-    SELECT
-        date,
-        feed_key,
-        calendar_dates_key,
-        service_id,
-        LOGICAL_AND(has_service) AS has_service
-    FROM boolean_calendar_dates
-    GROUP BY date, feed_key, calendar_dates_key, service_id
-),
-
 daily_services AS (
-
     SELECT
-        date_spine.date_day AS service_date,
+        -- these values will be identical so doesn't matter which is first in coalesce
+        COALESCE(long_cal.service_date, cal_dates.service_date) AS service_date,
         COALESCE(long_cal.feed_key, cal_dates.feed_key) AS feed_key,
-        calendar_key,
-        calendar_dates_key,
-        long_cal.service_id AS calendar_service_id,
-        long_cal.has_service AS calendar_has_service,
-        cal_dates.service_id AS calendar_dates_service_id,
-        cal_dates.has_service AS calendar_dates_has_service,
         COALESCE(long_cal.service_id, cal_dates.service_id) AS service_id,
         -- calendar_dates takes precedence if present: it can modify calendar
         -- if no calendar_dates, use calendar
         -- if neither, no service
-        COALESCE(cal_dates.has_service, long_cal.has_service, FALSE) AS has_service
-    FROM date_spine
-    LEFT JOIN int_gtfs_schedule__long_calendar AS long_cal
-        ON date_spine.day_num = long_cal.day_num
-            AND date_spine.date_day BETWEEN long_cal.start_date AND long_cal.end_date
-    LEFT JOIN summarize_calendar_dates AS cal_dates
-        ON date_spine.date_day = cal_dates.date
-            AND (long_cal.feed_key = cal_dates.feed_key OR long_cal.feed_key IS NULL)
-            AND (long_cal.service_id = cal_dates.service_id OR long_cal.service_id IS NULL)
+        COALESCE(cal_dates.service_bool, long_cal.service_bool) AS service_bool,
+        calendar_key,
+        cal_dates.key AS calendar_dates_key
+    FROM int_gtfs_schedule__long_calendar AS long_cal
+    FULL OUTER JOIN boolean_calendar_dates AS cal_dates
+        USING (key)
 ),
 
 int_gtfs_schedule__all_scheduled_service AS (
@@ -78,8 +48,7 @@ int_gtfs_schedule__all_scheduled_service AS (
         calendar_dates_key,
         service_id
     FROM daily_services
-    WHERE service_id IS NOT NULL
-        AND has_service
+    WHERE service_bool
 )
 
 SELECT * FROM int_gtfs_schedule__all_scheduled_service
