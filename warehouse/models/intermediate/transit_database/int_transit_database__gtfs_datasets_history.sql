2af912f0da403c6920fb5e6a3dbd463e4df702be
Laurie Merrell <laurie.m@jarv.us>
2022-12-15 15:50:59 -0600

    dataset and service data history

---
+++ warehouse/models/intermediate/transit_database/int_transit_database__gtfs_datasets_history.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+WITH stg_transit_database__gtfs_datasets AS (
+    SELECT *
+    FROM {{ ref('stg_transit_database__gtfs_datasets') }}
+),
+
+int_gtfs_quality__gtfs_datasets_history AS (
+    SELECT
+        calitp_extracted_at AS date,
+        key,
+        name,
+        regional_feed_type,
+        base64_url
+    FROM stg_transit_database__gtfs_datasets
+)
+
+SELECT * FROM int_gtfs_quality__gtfs_datasets_history
