6867faedbd4d857ab872cd66ddbec486e29f9322
Laurie Merrell <laurie.m@jarv.us>
2022-12-20 17:06:58 -0600

    sort arrays

--- warehouse/models/mart/gtfs_quality/fct_daily_organization_combined_guideline_checks.sql
+++ warehouse/models/mart/gtfs_quality/fct_daily_organization_combined_guideline_checks.sql
@@ -21,13 +21,13 @@ fct_daily_organization_combined_guideline_checks AS (
             WHEN LOGICAL_AND(status = "N/A") THEN "N/A"
         END as status,
         organization_key,
-        ARRAY_AGG(DISTINCT service_name IGNORE NULLS) AS service_names_included_array,
-        ARRAY_AGG(DISTINCT gtfs_dataset_name IGNORE NULLS) AS gtfs_dataset_names_included_array,
-        ARRAY_AGG(DISTINCT base64_url IGNORE NULLS) AS base64_urls_included_array,
-        ARRAY_AGG(DISTINCT service_key IGNORE NULLS) AS service_keys_included_array,
-        ARRAY_AGG(DISTINCT gtfs_service_data_key IGNORE NULLS) AS gtfs_service_data_keys_included_array,
-        ARRAY_AGG(DISTINCT gtfs_dataset_key IGNORE NULLS) AS gtfs_dataset_keys_included_array,
-        ARRAY_AGG(DISTINCT schedule_feed_key IGNORE NULLS) AS schedule_feed_keys_included_array
+        ARRAY_AGG(DISTINCT service_name IGNORE NULLS ORDER BY service_name) AS service_names_included_array,
+        ARRAY_AGG(DISTINCT gtfs_dataset_name IGNORE NULLS ORDER BY gtfs_dataset_name) AS gtfs_dataset_names_included_array,
+        ARRAY_AGG(DISTINCT base64_url IGNORE NULLS ORDER BY base64_url) AS base64_urls_included_array,
+        ARRAY_AGG(DISTINCT service_key IGNORE NULLS ORDER BY service_key) AS service_keys_included_array,
+        ARRAY_AGG(DISTINCT gtfs_service_data_key IGNORE NULLS ORDER BY gtfs_service_data_key) AS gtfs_service_data_keys_included_array,
+        ARRAY_AGG(DISTINCT gtfs_dataset_key IGNORE NULLS ORDER BY gtfs_dataset_key) AS gtfs_dataset_keys_included_array,
+        ARRAY_AGG(DISTINCT schedule_feed_key IGNORE NULLS ORDER BY schedule_feed_key) AS schedule_feed_keys_included_array
     FROM fct_daily_guideline_checks
     GROUP BY date, organization_key, organization_name, feature, check
 )
--- warehouse/models/mart/gtfs_quality/fct_daily_service_combined_guideline_checks.sql
+++ warehouse/models/mart/gtfs_quality/fct_daily_service_combined_guideline_checks.sql
@@ -21,13 +21,13 @@ fct_daily_service_combined_guideline_checks AS (
             WHEN LOGICAL_AND(status = "N/A") THEN "N/A"
         END as status,
         service_key,
-        ARRAY_AGG(DISTINCT organization_name IGNORE NULLS) AS organization_names_included_array,
-        ARRAY_AGG(DISTINCT gtfs_dataset_name IGNORE NULLS) AS gtfs_dataset_names_included_array,
-        ARRAY_AGG(DISTINCT base64_url IGNORE NULLS) AS base64_urls_included_array,
-        ARRAY_AGG(DISTINCT organization_key IGNORE NULLS) AS organization_keys_included_array,
-        ARRAY_AGG(DISTINCT gtfs_service_data_key IGNORE NULLS) AS gtfs_service_data_keys_included_array,
-        ARRAY_AGG(DISTINCT gtfs_dataset_key IGNORE NULLS) AS gtfs_dataset_keys_included_array,
-        ARRAY_AGG(DISTINCT schedule_feed_key IGNORE NULLS) AS schedule_feed_keys_included_array
+        ARRAY_AGG(DISTINCT organization_name IGNORE NULLS ORDER BY organization_name) AS organization_names_included_array,
+        ARRAY_AGG(DISTINCT gtfs_dataset_name IGNORE NULLS ORDER BY gtfs_dataset_name) AS gtfs_dataset_names_included_array,
+        ARRAY_AGG(DISTINCT base64_url IGNORE NULLS ORDER BY base64_url) AS base64_urls_included_array,
+        ARRAY_AGG(DISTINCT organization_key IGNORE NULLS ORDER BY organization_key) AS organization_keys_included_array,
+        ARRAY_AGG(DISTINCT gtfs_service_data_key IGNORE NULLS ORDER BY gtfs_service_data_key) AS gtfs_service_data_keys_included_array,
+        ARRAY_AGG(DISTINCT gtfs_dataset_key IGNORE NULLS ORDER BY gtfs_dataset_key) AS gtfs_dataset_keys_included_array,
+        ARRAY_AGG(DISTINCT schedule_feed_key IGNORE NULLS ORDER BY schedule_feed_key) AS schedule_feed_keys_included_array
     FROM fct_daily_guideline_checks
     GROUP BY date, service_key, service_name, feature, check
 )
