71be8b6c48358a74cfc35389c51ce902804c228b
Soren Spicknall <soren@spicknall.us>
2023-01-04 10:01:29 -0600

    Enumerate table columns

--- warehouse/models/mart/audit/fct_bigquery_data_access.sql
+++ warehouse/models/mart/audit/fct_bigquery_data_access.sql
@@ -1,7 +1,32 @@
 {{ config(materialized='table') }}
 
 WITH audit_fct_bigquery_data_access AS (
-    SELECT * 
+    SELECT
+        timestamp,
+        date,
+        severity,
+        resource_name,
+        principal_email,
+        job_name,
+        job_type,
+        dbt_invocation_id,
+        create_disposition,
+        destination_table,
+        priority,
+        query,
+        statement_type,
+        write_disposition,
+        duration_in_seconds,
+        referenced_tables,
+        total_billed_bytes,
+        estimated_cost_usd,
+        total_slots_seconds,
+        table_data_read_job_name,
+        dbt_header,
+        dbt_node,
+        payload,
+        metadata,
+        job
     FROM {{ ref('stg_audit__cloudaudit_googleapis_com_data_access') }}
 )
 
