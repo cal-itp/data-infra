9f5ed39fe51e2632bc789bbc1ec4c1289f31205f
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2023-01-05 16:21:59 -0500

    create trip update mart table(s) (#2127)
    * start on trip updates models and use sampled macro in another couple places
    
    * adjust trip update messages keys/tests
    
    * create stop time updates fact
    
    * adjust stop time updates key

---
+++ warehouse/models/mart/gtfs/fct_trip_updates_messages.sql
@@ -0,0 +1,64 @@
+WITH stg_gtfs_rt__trip_updates AS (
+    SELECT *
+    FROM {{ ref('stg_gtfs_rt__trip_updates') }}
+),
+
+urls_to_gtfs_datasets AS (
+    SELECT * FROM {{ ref('int_transit_database__urls_to_gtfs_datasets') }}
+),
+
+dim_gtfs_datasets AS (
+    SELECT *
+    FROM {{ ref('dim_gtfs_datasets') }}
+),
+
+keying AS (
+    SELECT
+        urls_to_gtfs_datasets.gtfs_dataset_key,
+        gd.name as _gtfs_dataset_name,
+        tu.*
+    FROM stg_gtfs_rt__trip_updates AS tu
+    LEFT JOIN urls_to_gtfs_datasets
+        ON tu.base64_url = urls_to_gtfs_datasets.base64_url
+    LEFT JOIN dim_gtfs_datasets AS gd
+        ON urls_to_gtfs_datasets.gtfs_dataset_key = gd.key
+),
+
+fct_trip_updates_messages AS (
+    SELECT
+        -- TODO: this is not unique yet
+        {{ dbt_utils.surrogate_key(['base64_url', '_extract_ts', 'id', 'vehicle_id', 'trip_id']) }} as key,
+        gtfs_dataset_key,
+        dt,
+        hour,
+        base64_url,
+        _extract_ts,
+        _config_extract_ts,
+        _gtfs_dataset_name,
+
+        header_timestamp,
+        header_version,
+        header_incrementality,
+
+        id,
+
+        trip_update_timestamp,
+        trip_update_delay,
+
+        vehicle_id,
+        vehicle_label,
+        vehicle_license_plate,
+
+        trip_id,
+        trip_route_id,
+        trip_direction_id,
+        trip_start_time,
+        trip_start_date,
+        trip_schedule_relationship,
+
+        stop_time_updates,
+    FROM keying
+)
+
+
+SELECT * FROM fct_trip_updates_messages
