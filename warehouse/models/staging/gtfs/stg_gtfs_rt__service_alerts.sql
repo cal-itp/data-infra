04424ad2b9ea04e20fc6ab50c070241413bfe9b6
Laurie Merrell <laurie.m@jarv.us>
2023-01-05 12:18:17 -0600

    handle language translations unnesting

--- warehouse/models/staging/gtfs/stg_gtfs_rt__service_alerts.sql
+++ warehouse/models/staging/gtfs/stg_gtfs_rt__service_alerts.sql
@@ -1,44 +1,45 @@
-WITH external_service_alerts AS (
+WITH base_gtfs_rt__first_unnest_service_alerts AS (
     SELECT *
-    FROM {{ source('external_gtfs_rt', 'service_alerts') }}
+    FROM {{ ref('base_gtfs_rt__first_unnest_service_alerts') }}
 ),
 
 stg_gtfs_rt__service_alerts AS (
     SELECT
+        msg_key,
         dt,
         hour,
         base64_url,
 
-        metadata.extract_ts AS _extract_ts,
-        metadata.extract_config.extracted_at AS _config_extract_ts,
-        metadata.extract_config.name AS _name,
+        _extract_ts,
+        _config_extract_ts,
+        _name,
 
-        TIMESTAMP_SECONDS(header.timestamp) AS header_timestamp,
-        header.incrementality AS header_incrementality,
-        header.gtfsRealtimeVersion AS header_version,
+        header_timestamp,
+        header_incrementality,
+        header_version,
 
         id,
 
-        unnested_active_period.start AS active_period_start,
-        unnested_active_period.end AS active_period_end,
+        active_period_start,
+        active_period_end,
 
-        unnested_informed_entity.agencyId AS informed_entity_agency_id,
-        unnested_informed_entity.routeId AS informed_entity_route_id,
-        unnested_informed_entity.routeType AS informed_entity_route_type,
-        unnested_informed_entity.directionId AS informed_entity_direction_id,
-        unnested_informed_entity.trip.tripId AS informed_entity_trip_id,
-        unnested_informed_entity.trip.routeId AS informed_entity_trip_route_id,
-        unnested_informed_entity.trip.directionId AS informed_entity_trip_direction_id,
-        unnested_informed_entity.trip.startTime AS informed_entity_trip_start_time,
-        unnested_informed_entity.trip.startDate AS informed_entity_trip_start_date,
-        unnested_informed_entity.trip.scheduleRelationship AS informed_entity_trip_schedule_relationship,
-        unnested_informed_entity.stopId AS informed_entity_stop_id,
+        informed_entity_agency_id,
+        informed_entity_route_id,
+        informed_entity_route_type,
+        informed_entity_direction_id,
+        informed_entity_trip_id,
+        informed_entity_trip_route_id,
+        informed_entity_trip_direction_id,
+        informed_entity_trip_start_time,
+        informed_entity_trip_start_date,
+        informed_entity_trip_schedule_relationship,
+        informed_entity_stop_id,
 
-        alert.cause AS cause,
-        alert.effect AS effect,
+        cause,
+        effect,
 
         unnested_url_translation.text AS url_text,
-        unnested_url_translation.text AS url_language,
+        unnested_url_translation.language AS url_language,
 
         unnested_header_text_translation.text AS header_text_text,
         unnested_header_text_translation.language AS header_text_language,
@@ -52,17 +53,20 @@ stg_gtfs_rt__service_alerts AS (
         unnested_tts_description_text_translation.text AS tts_description_text_text,
         unnested_tts_description_text_translation.language AS tts_description_text_language,
 
-        alert.severityLevel AS severity_level
-    FROM external_service_alerts
+        severity_level
+    FROM base_gtfs_rt__first_unnest_service_alerts
     -- https://stackoverflow.com/questions/44918108/google-bigquery-i-lost-null-row-when-using-unnest-function
     -- these arrays have nulls
-    LEFT JOIN UNNEST(external_service_alerts.alert.activePeriod) AS unnested_active_period
-    LEFT JOIN UNNEST(external_service_alerts.alert.informedEntity) AS unnested_informed_entity
-    LEFT JOIN UNNEST(external_service_alerts.alert.url.translation) AS unnested_url_translation
-    LEFT JOIN UNNEST(external_service_alerts.alert.headerText.translation) AS unnested_header_text_translation
-    LEFT JOIN UNNEST(external_service_alerts.alert.descriptionText.translation) AS unnested_description_text_translation
-    LEFT JOIN UNNEST(external_service_alerts.alert.ttsHeaderText.translation) AS unnested_tts_header_text_translation
-    LEFT JOIN UNNEST(external_service_alerts.alert.ttsDescriptionText.translation) AS unnested_tts_description_text_translation
+    LEFT JOIN UNNEST(base_gtfs_rt__first_unnest_service_alerts.header_text.translation) AS unnested_header_text_translation
+    LEFT JOIN UNNEST(base_gtfs_rt__first_unnest_service_alerts.description_text.translation) AS unnested_description_text_translation
+    LEFT JOIN UNNEST(base_gtfs_rt__first_unnest_service_alerts.tts_header_text.translation) AS unnested_tts_header_text_translation
+    LEFT JOIN UNNEST(base_gtfs_rt__first_unnest_service_alerts.tts_description_text.translation) AS unnested_tts_description_text_translation
+    LEFT JOIN UNNEST(base_gtfs_rt__first_unnest_service_alerts.url.translation) AS unnested_url_translation
+    -- filter where languages match, otherwise we get cartesian product of language combinations
+    WHERE COALESCE(unnested_description_text_translation.language, unnested_header_text_translation.language, 'language') = COALESCE(unnested_header_text_translation.language, 'language')
+       AND COALESCE(unnested_tts_header_text_translation.language, unnested_header_text_translation.language, 'language') = COALESCE(unnested_header_text_translation.language, 'language')
+       AND COALESCE(unnested_tts_description_text_translation.language, unnested_header_text_translation.language, 'language') = COALESCE(unnested_header_text_translation.language, 'language')
+       AND COALESCE(unnested_url_translation.language, unnested_header_text_translation.language, 'language') = COALESCE(unnested_header_text_translation.language, 'language')
 )
 
 SELECT * FROM stg_gtfs_rt__service_alerts
