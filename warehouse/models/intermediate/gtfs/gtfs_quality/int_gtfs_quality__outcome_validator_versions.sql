2acdec905b39c4768647e9bf095b7418a20980ec
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2023-01-09 12:21:59 -0500

    Post-storm analysis dbt cleanup (#2136)
    * run full refresh tests even if run failed
    
    * remove extra key
    
    * adjust dbt and v2 rt validation resource requests and threads
    
    * wip on handling different validator versions in schedule notices
    
    * add validator version to key
    
    * relax tests slightly
    
    * add yaml for int_gtfs_quality__outcome_validator_versions

--- warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__outcome_validator_versions.sql
+++ warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__outcome_validator_versions.sql
@@ -5,12 +5,13 @@ WITH validation_notices AS (
     FROM {{ ref('stg_gtfs_schedule__validation_notices') }}
 ),
 
-int_gtfs_quality__schedule_validation_severities AS (
+int_gtfs_quality__outcome_validator_versions AS (
     SELECT DISTINCT
-        code,
-        severity,
+        dt,
+        base64_url,
+        ts,
         gtfs_validator_version,
     FROM validation_notices
 )
 
-SELECT * FROM int_gtfs_quality__schedule_validation_severities
+SELECT * FROM int_gtfs_quality__outcome_validator_versions
