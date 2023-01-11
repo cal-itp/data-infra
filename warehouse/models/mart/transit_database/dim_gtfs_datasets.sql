d09512d65253019469b219726a8bd28ee3559ff2
Laurie Merrell <laurie.m@jarv.us>
2023-01-11 15:57:59 -0600

    fixes to gtfs datasets

--- warehouse/models/mart/transit_database/dim_gtfs_datasets.sql
+++ warehouse/models/mart/transit_database/dim_gtfs_datasets.sql
@@ -16,7 +16,7 @@ dim_gtfs_datasets AS (
         future_uri,
         deprecated_date,
         data_quality_pipeline,
-        datasets.key AS schedule_to_use_for_rt_validation_gtfs_dataset_key,
+        schedule_to_use_for_rt_validation_gtfs_dataset_key,
         base64_url,
         _is_current,
         _valid_from,
