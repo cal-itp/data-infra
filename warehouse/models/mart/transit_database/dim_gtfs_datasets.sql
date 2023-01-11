543e3c4081e6f9a065321a4e74c7e9b1e16baf8f
Laurie Merrell <laurie.m@jarv.us>
2023-01-11 11:07:37 -0600

    remove join

--- warehouse/models/mart/transit_database/dim_gtfs_datasets.sql
+++ warehouse/models/mart/transit_database/dim_gtfs_datasets.sql
@@ -7,24 +7,20 @@ WITH dim AS (
 
 dim_gtfs_datasets AS (
     SELECT
-        dim.key,
-        dim.name,
-        dim.type,
-        dim.regional_feed_type,
-        dim.uri,
-        dim.future_uri,
-        dim.deprecated_date,
-        dim.data_quality_pipeline,
+        key,
+        name,
+        type,
+        regional_feed_type,
+        uri,
+        future_uri,
+        deprecated_date,
+        data_quality_pipeline,
         datasets.key AS schedule_to_use_for_rt_validation_gtfs_dataset_key,
-        dim.base64_url,
-        (dim._is_current AND datasets._is_current) AS _is_current,
-        GREATEST(dim._valid_from, datasets._valid_from) AS _valid_from,
-        LEAST(dim._valid_to, datasets._valid_to) AS _valid_to
+        base64_url,
+        _is_current,
+        _valid_from,
+        _valid_to
     FROM dim
-    LEFT JOIN dim AS datasets
-        ON dim.schedule_to_use_for_rt_validation_gtfs_dataset_key = datasets.original_record_id
-        AND dim._valid_from < datasets._valid_to
-        AND dim._valid_to > datasets._valid_from
 )
 
 SELECT * FROM dim_gtfs_datasets
