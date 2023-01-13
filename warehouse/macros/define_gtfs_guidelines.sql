2a722d633dfb05b35f15c24ef3c58d88bd64d48f
Scott Owades <jeremyscottowades@gmail.com>
2023-01-12 18:38:15 -0800

    begin building test for recent extreme daily check results

--- warehouse/macros/define_gtfs_guidelines.sql
+++ warehouse/macros/define_gtfs_guidelines.sql
@@ -1,4 +1,4 @@
--- declare checks
+-- CHECK NAMES
 {% macro static_feed_downloaded_successfully() %}
 "GTFS schedule feed downloads successfully"
 {% endmacro %}
@@ -152,7 +152,10 @@
 "The GTFS Schedule API endpoint is configured to report the file modification date"
 {% endmacro %}
 
--- declare features
+--
+-- FEATURE NAMES
+--
+
 {% macro compliance_schedule() %}
 "Compliance (Schedule)"
 {% endmacro %}
@@ -201,7 +204,10 @@
 "Up-to-Dateness"
 {% endmacro %}
 
--- columns
+--
+-- COLUMNS
+--
+
 {% macro gtfs_guidelines_columns() %}
 date,
 calitp_itp_id,
@@ -212,7 +218,9 @@ status,
 feature
 {% endmacro %}
 
--- queries
+--
+-- QUERIES
+--
 -- For use in int_gtfs_quality__persistent_ids_schedule:
 {% macro ids_version_compare_aggregate(id, dim) %}
 (
@@ -264,6 +272,7 @@ feature
 
 -- For use in int_gtfs_quality__persistent_ids_schedule:
 {% macro max_new_id_ratio(table_name) %}
+
     MAX({{ table_name }}.id_added * 100 / {{ table_name }}.ids_current_feed )
        OVER (
            PARTITION BY t1.feed_key
@@ -271,3 +280,23 @@ feature
            ROWS BETWEEN 30 PRECEDING AND CURRENT ROW
         )
 {% endmacro %}
+
+--
+-- TESTS
+--
+
+{% test extreme_results(model) %}
+{{ config(severity = 'warn') }}
+
+    SELECT * FROM
+    (
+        SELECT
+            date,
+            COUNT(CASE WHEN status = 'PASS' THEN 1 END) * 100.0 / COUNT(*) AS percent_pass
+        FROM {{ model }}
+       WHERE date > CURRENT_DATE - 30
+       GROUP BY 1
+    )
+    WHERE percent_pass = 100 OR percent_pass = 0
+
+{% endtest %}
