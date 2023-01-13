d1ea434054d0df5a891281cddf2a871af0b44e0b
Andrew Vaccaro <atvaccaro@gmail.com>
2023-01-13 16:49:44 -0500

    fix bad macro

--- warehouse/macros/make_schedule_file_dimension_from_dim_schedule_feeds.sql
+++ warehouse/macros/make_schedule_file_dimension_from_dim_schedule_feeds.sql
@@ -33,7 +33,7 @@ SELECT
     t2._valid_from,
     t2._valid_to,
     t2._is_current
-FROM {{ gtfs_file_table }} AS t1
+FROM {{ gtfs_file_table.identifier }} AS t1
 INNER JOIN dim_schedule_feeds AS t2
     ON t1.ts = t2._valid_from
     AND t1.base64_url = t2.base64_url
