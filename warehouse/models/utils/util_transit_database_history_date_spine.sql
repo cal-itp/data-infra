511a405c9d87ab189902c19491718c513111c7b8
Laurie Merrell <laurie.m@jarv.us>
2023-01-19 10:30:25 -0600

    add comment per pr review

--- warehouse/models/utils/util_transit_database_history_date_spine.sql
+++ warehouse/models/utils/util_transit_database_history_date_spine.sql
@@ -1,5 +1,6 @@
 {{ config(materialized='ephemeral') }}
 
+-- 2022-06-29 is the first date that v2 airtable data was extracted
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2022-06-29' AS datetime)",
