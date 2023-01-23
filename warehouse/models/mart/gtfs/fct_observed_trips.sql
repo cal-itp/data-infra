fc1d74ee99f03b24d7347b18f84d0b1957138806
Andrew Vaccaro <atvaccaro@gmail.com>
2023-01-23 17:13:44 -0500

    fix column refs, add test

--- warehouse/models/mart/gtfs/fct_observed_trips.sql
+++ warehouse/models/mart/gtfs/fct_observed_trips.sql
@@ -3,7 +3,7 @@
         materialized='incremental',
         incremental_strategy='insert_overwrite',
         partition_by={
-            'field': 'date',
+            'field': 'dt',
             'data_type': 'date',
             'granularity': 'day',
         },
@@ -22,7 +22,7 @@ vehicle_positions AS (
 fct_observed_trips AS (
     SELECT
         {{ dbt_utils.surrogate_key([
-            'date',
+            'dt',
             'base64_url',
             'trip_id',
             'trip_route_id',
@@ -30,11 +30,19 @@ fct_observed_trips AS (
             'trip_start_time',
             'trip_start_date',
         ]) }} as key,
-        tu.* EXCEPT (key),
-        vp.* EXCEPT (key),
-    FROM trip_updates tu
-    FULL OUTER JOIN vehicle_positions vp
-        USING (date, base64_url, trip_id, trip_route_id, trip_direction_id, trip_start_time, trip_start_date)
+        dt,
+        base64_url,
+        tu.num_distinct_message_ids AS tu_num_distinct_message_ids,
+        tu.min_trip_update_timestamp AS tu_min_trip_update_timestamp,
+        tu.max_trip_update_timestamp AS tu_max_trip_update_timestamp,
+        tu.max_delay AS tu_max_delay,
+        tu.num_skipped_stops AS tu_num_skipped_stops,
+        vp.num_distinct_message_ids AS vp_num_distinct_message_ids,
+        vp.min_trip_update_timestamp AS vp_min_trip_update_timestamp,
+        vp.max_trip_update_timestamp AS vp_max_trip_update_timestamp,
+    FROM trip_updates AS tu
+    FULL OUTER JOIN vehicle_positions AS vp
+        USING (dt, base64_url, trip_id, trip_route_id, trip_direction_id, trip_start_time, trip_start_date)
 )
 
 SELECT * FROM fct_observed_trips
