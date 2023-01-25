414b7c543f9b4115c7f5a72f0014bfcdf1c59edc
Andrew Vaccaro <atvaccaro@gmail.com>
2023-01-25 12:09:49 -0500

    wip moving to actual RT feed association

--- warehouse/models/mart/gtfs/fct_observed_trips.sql
+++ warehouse/models/mart/gtfs/fct_observed_trips.sql
@@ -27,7 +27,7 @@ fct_observed_trips AS (
     SELECT
         {{ dbt_utils.surrogate_key([
             'dt',
-            'base64_url',
+            'associated_schedule_gtfs_dataset_key',
             'trip_id',
             'trip_route_id',
             'trip_direction_id',
@@ -35,7 +35,7 @@ fct_observed_trips AS (
             'trip_start_date',
         ]) }} as key,
         dt,
-        base64_url,
+        ,
         tu.num_distinct_message_ids AS tu_num_distinct_message_ids,
         tu.min_trip_update_timestamp AS tu_min_trip_update_timestamp,
         tu.max_trip_update_timestamp AS tu_max_trip_update_timestamp,
@@ -46,11 +46,31 @@ fct_observed_trips AS (
         vp.max_trip_update_timestamp AS vp_max_trip_update_timestamp,
         sa.num_distinct_message_ids AS sa_num_distinct_message_ids,
         sa.service_alert_message_keys AS sa_service_alert_message_key,
+    FROM fct_daily_rt_feed_files AS ff
+    LEFT JOIN dim_provider_gtfs_data AS pgd
+        ON ff.gtfs_dataset_key =
+    -- in RT world can use associated_schedule_gtfs_dataset_key
+    FULL OUTER JOIN trip_updates AS tu
+        ON ff.date = tu.dt
+        AND
     FROM trip_updates AS tu
+    LEFT JOIN fct_daily_rt_feed_files AS tu
     FULL OUTER JOIN vehicle_positions AS vp
-        USING (dt, base64_url, trip_id, trip_route_id, trip_direction_id, trip_start_time, trip_start_date)
+        ON tu.dt = vp.du
+        AND tu.provider_gtfs_data = vp.provider_gtfs_data
+        AND tu.trip_id = vp.trip_id
+        AND tu.trip_route_id = vp.trip_route_id
+        AND tu.trip_direction_id = vp.trip_direction_id
+        AND tu.trip_start_time = vp.trip_start_time
+        AND tu.trip_start_date = vp.trip_start_date
     FULL OUTER JOIN service_alerts AS sa
-        USING (dt, base64_url, trip_id, trip_route_id, trip_direction_id, trip_start_time, trip_start_date)
+        ON tu.dt = sa.du
+        AND tu.provider_gtfs_data = sa.provider_gtfs_data
+        AND tu.trip_id = sa.trip_id
+        AND tu.trip_route_id = sa.trip_route_id
+        AND tu.trip_direction_id = sa.trip_direction_id
+        AND tu.trip_start_time = sa.trip_start_time
+        AND tu.trip_start_date = sa.trip_start_date
 )
 
 SELECT * FROM fct_observed_trips
