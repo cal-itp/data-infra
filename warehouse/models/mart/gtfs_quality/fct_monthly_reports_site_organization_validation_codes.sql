e548a62dc872fe09b8d1e9943543ba9baa56753c
Laurie Merrell <laurie.m@jarv.us>
2023-01-26 12:08:09 -0600

    refactor monthly organization validations

--- warehouse/models/mart/gtfs_quality/fct_monthly_reports_site_organization_validation_codes.sql
+++ warehouse/models/mart/gtfs_quality/fct_monthly_reports_site_organization_validation_codes.sql
@@ -4,32 +4,36 @@ WITH fct_daily_schedule_feed_validation_notices AS (
     SELECT * FROM {{ ref('fct_daily_schedule_feed_validation_notices') }}
 ),
 
+idx_monthly_reports_site AS (
+    SELECT *
+    FROM {{ ref('idx_monthly_reports_site') }}
+),
+
 int_gtfs__organization_dataset_map AS (
-
-    SELECT *,
-        DATE_ADD(LAST_DAY(date, MONTH), INTERVAL 1 DAY) AS publish_date
+    SELECT *
     FROM {{ ref('int_gtfs_quality__organization_dataset_map') }}
-    WHERE reports_site_assessed
-
 ),
 
 fct_monthly_reports_site_organization_validation_codes AS (
     SELECT DISTINCT
-        {{ dbt_utils.surrogate_key(['organization_source_record_id',
-                                    'publish_date',
-                                    'code',
+        {{ dbt_utils.surrogate_key(['idx.organization_source_record_id',
+                                    'idx.publish_date',
+                                    'notices.code',
         ]) }} AS key,
-        organization_name,
-        organization_source_record_id,
-        organization_itp_id,
-        publish_date,
-        code,
-        severity,
-        validation_validator_version
+        idx.organization_name,
+        idx.organization_source_record_id,
+        idx.organization_itp_id,
+        idx.publish_date,
+        notices.code,
+        notices.severity,
+        notices.validation_validator_version
     FROM fct_daily_schedule_feed_validation_notices AS notices
     INNER JOIN int_gtfs__organization_dataset_map AS orgs
         ON notices.date = orgs.date
         AND notices.feed_key = orgs.schedule_feed_key
+    INNER JOIN idx_monthly_reports_site AS idx
+        ON notices.date BETWEEN idx.date_start AND idx.date_end
+        AND orgs.organization_source_record_id = idx.organization_source_record_id
     WHERE total_notices > 0
     QUALIFY validation_validator_version = MAX(validation_validator_version)
         OVER (PARTITION BY publish_date, organization_source_record_id)
