6a92294e8a7482b7313660ed885d9dbc6c5dff7f
Laurie Merrell <laurie.m@jarv.us>
2023-01-26 11:22:26 -0600

    add base64 url to daily validation notices

--- warehouse/models/mart/gtfs_quality/fct_daily_schedule_feed_validation_notices.sql
+++ warehouse/models/mart/gtfs_quality/fct_daily_schedule_feed_validation_notices.sql
@@ -4,10 +4,6 @@ WITH fct_daily_schedule_feeds AS (
     SELECT * FROM {{ ref('fct_daily_schedule_feeds') }}
 ),
 
-dim_schedule_feeds AS (
-    SELECT * FROM {{ ref('dim_schedule_feeds') }}
-),
-
 validation_codes AS (
     SELECT * FROM {{ ref('int_gtfs_quality__schedule_validation_severities') }}
 ),
@@ -46,6 +42,7 @@ fct_daily_schedule_feed_validation_notices AS (
         ]) }} AS key,
         daily_feeds.date,
         daily_feeds.feed_key,
+        daily_feeds.base64_url,
         outcomes.validation_validator_version,
         codes.code,
         codes.severity,
@@ -56,17 +53,15 @@ fct_daily_schedule_feed_validation_notices AS (
             CASE WHEN validation_success THEN 0 END
         ) AS total_notices,
     FROM fct_daily_schedule_feeds AS daily_feeds
-    LEFT JOIN dim_schedule_feeds AS dim_feeds
-        ON daily_feeds.feed_key = dim_feeds.key
     LEFT JOIN first_outcome_per_version AS outcomes
-        ON dim_feeds.key = outcomes.feed_key
+        ON daily_feeds.base64_url = outcomes.base64_url
     LEFT JOIN validation_codes AS codes
         ON outcomes.validation_validator_version = codes.gtfs_validator_version
     LEFT JOIN validation_notices AS notices
         ON codes.code = notices.code
         AND outcomes.base64_url = notices.base64_url
         AND outcomes.extract_ts = notices.ts
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 )
 
 SELECT * FROM fct_daily_schedule_feed_validation_notices
