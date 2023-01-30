6af78843174ef6d7d37017b59f9491a029fd29fa
Andrew Vaccaro <atvaccaro@gmail.com>
2023-01-30 15:10:32 -0500

    few more pk flags, and split mart yaml into fct/dim

--- warehouse/models/mart/gtfs/dim_fare_transfer_rules.sql
+++ warehouse/models/mart/gtfs/dim_fare_transfer_rules.sql
@@ -5,6 +5,23 @@ WITH make_dim AS (
     ) }}
 ),
 
+-- typical pattern for letting us join on nulls
+with_identifier AS (
+    SELECT *, {{ dbt_utils.surrogate_key(['from_leg_group_id', 'to_leg_group_id', 'fare_product_id', 'transfer_count', 'duration_limit']) }} AS fare_transfer_rule_identifier,
+    FROM make_dim
+),
+
+bad_rows AS (
+    SELECT
+        base64_url,
+        ts,
+        {{ dbt_utils.surrogate_key(['from_leg_group_id', 'to_leg_group_id', 'fare_product_id', 'transfer_count', 'duration_limit']) }} AS fare_transfer_rule_identifier,
+        TRUE AS warning_duplicate_primary_key
+    FROM make_dim
+    GROUP BY 1, 2, 3
+    HAVING COUNT(*) > 1
+),
+
 dim_fare_transfer_rules AS (
     SELECT
         {{ dbt_utils.surrogate_key(['feed_key', 'from_leg_group_id', 'to_leg_group_id', 'fare_product_id', 'transfer_count', 'duration_limit']) }} AS key,
@@ -17,8 +34,11 @@ dim_fare_transfer_rules AS (
         duration_limit_type,
         fare_transfer_type,
         fare_product_id,
+        COALESCE(warning_duplicate_primary_key, FALSE) AS warning_duplicate_primary_key,
         _feed_valid_from,
-    FROM make_dim
+    FROM with_identifier
+    LEFT JOIN bad_rows
+        USING (base64_url, ts, fare_transfer_rule_identifier)
 )
 
 SELECT * FROM dim_fare_transfer_rules
