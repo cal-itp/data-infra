941ef5deb5e582c39d9e7463fef8816069a16303
Charlie Costanzo <cvc5185@gmail.com>
2023-02-02 11:52:28 -0500

    Revise logic in daily payments tests to reflect date spine (#2240)
    * refactored logic to make more sense with date_spine
    
    * reformat spacing

--- warehouse/models/payments_views/payments_daily_transaction_deltas.sql
+++ warehouse/models/payments_views/payments_daily_transaction_deltas.sql
@@ -1,7 +1,5 @@
 WITH payments_rides AS (
-
     SELECT * FROM {{ ref('payments_rides') }}
-
 ),
 
 payments_tests_date_spine AS (
@@ -9,7 +7,6 @@ payments_tests_date_spine AS (
 ),
 
 extract_count_date AS (
-
     SELECT
 
         participant_id,
@@ -22,9 +19,21 @@ extract_count_date AS (
     GROUP BY transaction_date, participant_id
 ),
 
+add_date_spine AS (
+    SELECT
 
-calculate_relative_difference AS (
+        t1.participant_id,
+        t1.day_history AS transaction_date,
 
+        t2.ridership_count
+
+    FROM payments_tests_date_spine AS t1
+    LEFT JOIN extract_count_date AS t2
+        ON (t1.day_history = t2.transaction_date)
+            AND (t1.participant_id = t2.participant_id)
+),
+
+payments_daily_transaction_deltas AS (
     SELECT
 
         *,
@@ -37,25 +46,7 @@ calculate_relative_difference AS (
             OVER (PARTITION BY participant_id ORDER BY transaction_date)) * 100
         AS relative_difference
 
-    FROM extract_count_date
-
-),
-
-payments_daily_transaction_deltas AS (
-
-    SELECT
-
-        t1.participant_id,
-        t1.day_history AS transaction_date,
-
-        t2.ridership_count,
-        t2.relative_difference
-
-    FROM payments_tests_date_spine AS t1
-    LEFT JOIN calculate_relative_difference AS t2
-        ON (t1.day_history = t2.transaction_date)
-            AND (t1.participant_id = t2.participant_id)
-
+    FROM add_date_spine
 )
 
 SELECT * FROM payments_daily_transaction_deltas
