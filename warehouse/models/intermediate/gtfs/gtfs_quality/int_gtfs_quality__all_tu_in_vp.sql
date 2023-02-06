12d50b6bdc118b7b5229de901991a39ba65a1571
Scott Owades <jeremyscottowades@gmail.com>
2023-02-06 10:36:27 -0800

    fix typo

--- warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__all_tu_in_vp.sql
+++ warehouse/models/intermediate/gtfs/gtfs_quality/int_gtfs_quality__all_tu_in_vp.sql
@@ -40,7 +40,7 @@ joined AS (
     ON idx.date = f.dt
     AND quartet.trip_updates_gtfs_dataset_key = f.tu_gtfs_dataset_key
     -- Joining on vp_gtfs_dataset_key would ensure that vp_num_distinct_message_ids was never null
-    -- The below join condition may be unnecessary, though it could rule out potential cases where one TU feed is mapped to multiple scheduel feeds
+    -- The below join condition may be unnecessary, though it could rule out potential cases where one TU feed is mapped to multiple schedule feeds
     AND quartet.associated_schedule_gtfs_dataset_key = f.schedule_to_use_for_rt_validation_gtfs_dataset_key
     AND f.tu_num_scheduled_canceled_added_stops > 0
     GROUP BY 1,2
