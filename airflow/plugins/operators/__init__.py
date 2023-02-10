1fef0e9df1a9a09fba4df613c8cfd56917b9e0bf
Andrew Vaccaro <atvaccaro@gmail.com>
2023-02-10 11:32:17 -0500

    delete python taskflow operator

--- airflow/plugins/operators/__init__.py
+++ airflow/plugins/operators/__init__.py
@@ -6,6 +6,5 @@ from operators.external_table import ExternalTable
 from operators.gtfs_csv_to_jsonl import GtfsGcsToJsonlOperator
 from operators.once_off_external_task_sensor import OnceOffExternalTaskSensor
 from operators.pod_operator import PodOperator
-from operators.python_taskflow_operator import PythonTaskflowOperator
 from operators.sql_query_operator import SqlQueryOperator
 from operators.sql_to_warehouse_operator import SqlToWarehouseOperator
