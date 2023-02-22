8941bf2311f0e5bd27575cd04c732a29e181d751
Laurie Merrell <laurie.m@jarv.us>
2023-02-22 15:28:23 -0600

    rearrange folder structure

--- warehouse/models/intermediate/gtfs_quality/int_gtfs_quality__feed_version_history.sql
+++ warehouse/models/intermediate/gtfs_quality/int_gtfs_quality__feed_version_history.sql
@@ -9,11 +9,11 @@ WITH distinct_feed_versions AS (
 int_gtfs_quality__feed_version_history AS (
     SELECT base64_url,
            feed_key,
-           LAG (feed_key) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS prev_feed_key,
-           LEAD (feed_key) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_key,
-           LEAD (EXTRACT(date FROM _valid_from)) OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_valid_from,
+           LAG(feed_key) OVER(PARTITION BY base64_url ORDER BY _valid_from ASC) AS prev_feed_key,
+           LEAD(feed_key) OVER(PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_key,
+           LEAD(EXTRACT(date FROM _valid_from)) OVER(PARTITION BY base64_url ORDER BY _valid_from ASC) AS next_feed_valid_from,
            EXTRACT(date FROM _valid_from) AS valid_from,
-           ROW_NUMBER () OVER (PARTITION BY base64_url ORDER BY _valid_from ASC) feed_version_number
+           ROW_NUMBER() OVER(PARTITION BY base64_url ORDER BY _valid_from ASC) AS feed_version_number
       FROM distinct_feed_versions
 )
 
