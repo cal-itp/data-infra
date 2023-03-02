b2dfb2c32cd0cac93d6989b9c17d65ffe8d6e2a7
Laurie Merrell <laurie.m@jarv.us>
2023-03-01 18:02:57 -0600

    minor logic fixes to int_gtfs_quality__schedule_download_success

--- warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__schedule_download_success.sql
+++ warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__schedule_download_success.sql
@@ -11,19 +11,24 @@ fct_schedule_feed_downloads AS (
 ),
 
 daily_feed_download_unzip_success AS (
-    SELECT DISTINCT date,
-           base64_url
+    SELECT date,
+           base64_url,
+           LOGICAL_OR(download_success) AS download_success,
+           LOGICAL_OR(unzip_success) AS unzip_success
       FROM fct_schedule_feed_downloads
-     WHERE download_success
-       AND unzip_success
+      GROUP BY 1, 2
 ),
 
 int_gtfs_quality__schedule_download_success AS (
     SELECT
         idx.* EXCEPT(status),
         CASE
-            WHEN d.base64_url IS NOT null THEN {{ guidelines_pass_status() }}
-            WHEN idx.has_schedule_url THEN {{ guidelines_fail_status() }}
+            WHEN download_success AND unzip_success THEN {{ guidelines_pass_status() }}
+            -- means we attempted to download and failed
+            WHEN idx.has_schedule_url AND d.base64_url IS NOT NULL THEN {{ guidelines_fail_status() }}
+            -- means we did not attempt to download on this day for whatever reason
+            WHEN idx.has_schedule_url AND d.base64_url IS NULL THEN {{ guidelines_na_check_status() }}
+            -- fill in n/a entities etc.
             ELSE idx.status
         END AS status
     FROM guideline_index idx
