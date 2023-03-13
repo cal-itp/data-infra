a971a55fdb29c990e19824ee562a567e9865dac7
Laurie Merrell <laurie.m@jarv.us>
2023-03-13 13:26:13 -0500

    fix date handling for shapes valid

--- warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__shapes_valid.sql
+++ warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__shapes_valid.sql
@@ -4,10 +4,20 @@ WITH guideline_index AS (
     WHERE check = {{ shapes_valid() }}
 ),
 
+shape_validation_notices AS (
+    SELECT *
+    FROM {{ ref('fct_daily_schedule_feed_validation_notices') }}
+    WHERE code IN (
+            'decreasing_shape_distance',
+            'equal_shape_distance_diff_coordinates',
+            'equal_shape_distance_same_coordinates',
+            'decreasing_or_equal_shape_distance'
+            )
+),
+
 -- For this check we are only looking for errors related to shapes
-shape_validation_notices_by_day AS (
+feed_shape_validation_notices AS (
     SELECT
-        date,
         feed_key,
         SUM(total_notices) AS validation_notices
     FROM {{ ref('fct_daily_schedule_feed_validation_notices') }}
@@ -17,12 +27,12 @@ shape_validation_notices_by_day AS (
             'equal_shape_distance_same_coordinates',
             'decreasing_or_equal_shape_distance'
             )
-    GROUP BY feed_key, date
+    GROUP BY feed_key
 ),
 
 check_start AS (
     SELECT MIN(date) AS first_check_date
-    FROM shape_validation_notices_by_day
+    FROM shape_validation_notices
 ),
 
 int_gtfs_quality__shapes_valid AS (
@@ -41,8 +51,8 @@ int_gtfs_quality__shapes_valid AS (
         END AS status
     FROM guideline_index idx
     CROSS JOIN check_start
-    LEFT JOIN shape_validation_notices_by_day
-      ON idx.schedule_feed_key = shape_validation_notices_by_day.feed_key
+    LEFT JOIN feed_shape_validation_notices
+      ON idx.schedule_feed_key = feed_shape_validation_notices.feed_key
 )
 
 SELECT * FROM int_gtfs_quality__shapes_valid
