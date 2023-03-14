9eeeeb40deed1a6b2037cda2c81fc9f95d19da94
Laurie Merrell <laurie.m@jarv.us>
2023-03-14 08:55:39 -0500

    feed info technical contact

--- warehouse/macros/define_gtfs_guidelines.sql
+++ warehouse/macros/define_gtfs_guidelines.sql
@@ -101,7 +101,7 @@
 {% endmacro %}
 
 {% macro technical_contact_listed() %}
-"Technical contact is listed in feed_contact_email field within the feed_info.txt file"
+"Feed has feed_info.txt file and technical contact is listed in feed_contact_email field within the feed_info.txt file"
 {% endmacro %}
 
 {% macro no_rt_validation_errors_vp() %}
--- warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__technical_contact_listed.sql
+++ warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__technical_contact_listed.sql
@@ -1,26 +1,59 @@
-WITH feed_guideline_index AS (
-    SELECT * FROM {{ ref('int_gtfs_quality__schedule_feed_guideline_index') }}
+WITH guideline_index AS (
+    SELECT *
+    FROM {{ ref('int_gtfs_quality__guideline_checks_index') }}
+    WHERE check = {{ technical_contact_listed() }}
 ),
 
 dim_feed_info AS (
     SELECT * FROM {{ ref('dim_feed_info') }}
 ),
 
--- Joins our feed-level check with feed_guideline_index, where each feed will have a row for every day it is active
+files AS (
+    SELECT * FROM {{ ref('fct_schedule_feed_files') }}
+),
+
+check_technical_contact AS (
+    SELECT
+        feed_key,
+        LOGICAL_AND(feed_contact_email IS NOT NULL) AS has_contact
+    FROM dim_feed_info
+    GROUP BY 1
+),
+
+feed_has_feed_info AS (
+    SELECT
+        feed_key,
+        LOGICAL_OR(gtfs_filename = "feed_info") AS has_feed_info
+    FROM files
+    GROUP BY 1
+),
+
+check_start AS (
+    SELECT MIN(_feed_valid_from) AS first_check_date
+    FROM dim_feed_info
+),
+
 int_gtfs_quality__technical_contact_listed AS (
     SELECT
-        t1.date,
-        t1.feed_key,
-        {{ technical_contact_listed() }} AS check,
-        {{ technical_contact_availability() }} AS feature,
+        idx.* EXCEPT(status),
         CASE
-            WHEN LOGICAL_AND(t2.feed_contact_email IS NOT NULL) THEN {{ guidelines_pass_status() }}
-            ELSE {{ guidelines_fail_status() }}
+            WHEN has_schedule_feed
+                THEN
+                    CASE
+                        WHEN has_feed_info AND has_contact THEN {{ guidelines_pass_status() }}
+                        -- TODO: could add special handling for April 16, 2021 (start of checks)
+                        WHEN CAST(idx.date AS TIMESTAMP) < first_check_date THEN {{ guidelines_na_too_early_status() }}
+                        WHEN NOT has_feed_info OR NOT has_contact THEN {{ guidelines_fail_status() }}
+                        WHEN has_feed_info IS NULL THEN {{ guidelines_na_check_status() }}
+                    END
+            ELSE idx.status
         END AS status
-    FROM feed_guideline_index AS t1
-    LEFT JOIN dim_feed_info AS t2
-       ON t1.feed_key = t2.feed_key
-    GROUP BY 1, 2, 3, 4
+    FROM guideline_index idx
+    CROSS JOIN check_start
+    LEFT JOIN feed_has_feed_info
+        ON idx.schedule_feed_key = feed_has_feed_info.feed_key
+    LEFT JOIN check_technical_contact
+        ON idx.schedule_feed_key = check_technical_contact.feed_key
 )
 
 SELECT * FROM int_gtfs_quality__technical_contact_listed
