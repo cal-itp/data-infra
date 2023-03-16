4078a96e1b7cb6c9d33256fef42932dbcebefbce
Andrew Vaccaro <atvaccaro@gmail.com>
2023-03-16 14:39:53 -0400

    create v2 littlepay parse to jsonl operator and use it

--- airflow/plugins/operators/__init__.py
+++ airflow/plugins/operators/__init__.py
@@ -4,4 +4,5 @@ from operators.amplitude_to_flattened_json import AmplitudeToFlattenedJSONOperat
 from operators.external_table import ExternalTable
 from operators.gtfs_csv_to_jsonl import GtfsGcsToJsonlOperator
 from operators.littlepay_raw_sync import LittlepayRawSync
+from operators.littlepay_to_jsonl import LittlepayToJSONL
 from operators.pod_operator import PodOperator
