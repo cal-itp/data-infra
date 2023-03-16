fe9b1e80b30339a81be17667efd8e8c512d1c1fc
Laurie <55149902+lauriemerrell@users.noreply.github.com>
2023-03-16 13:33:39 -0500

    Migrate guideline check indices: Batch 2 (#2370)
    * wip on trip planner checks, tbd delete the unused rt one and keep testing
    
    * make yaml actually work
    
    * remove unneeded rt model
    
    * reindex no schedule validation errors
    
    * remove all existing models and just start over on yaml
    
    * shapes file present
    
    * migrate complete wheelchair info
    
    * shapes for all trips
    
    * add comment
    
    * migrate include tts
    
    * migrate no shapes validations
    
    * remove non-applicable comment
    
    * fix date handling for shapes valid
    
    * remove redundant clause
    
    * refactor pathways check to check for file presence
    
    * feed info technical contact
    
    * add check for shapes file existing per pr review
    
    * add comments per pr review

--- warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__shapes_valid.sql
+++ warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__shapes_valid.sql
@@ -1,11 +1,18 @@
-WITH feed_guideline_index AS (
-    SELECT * FROM {{ ref('int_gtfs_quality__schedule_feed_guideline_index') }}
+WITH guideline_index AS (
+    SELECT *
+    FROM {{ ref('int_gtfs_quality__guideline_checks_index') }}
+    WHERE check = {{ shapes_valid() }}
+),
+
+files AS (
+    SELECT * FROM {{ ref('fct_schedule_feed_files') }}
 ),
 
 -- For this check we are only looking for errors related to shapes
-validation_fact_daily_feed_codes_shape_related AS (
-    SELECT * FROM {{ ref('fct_daily_schedule_feed_validation_notices') }}
-     WHERE code IN (
+shape_validation_notices AS (
+    SELECT *
+    FROM {{ ref('fct_daily_schedule_feed_validation_notices') }}
+    WHERE code IN (
             'decreasing_shape_distance',
             'equal_shape_distance_diff_coordinates',
             'equal_shape_distance_same_coordinates',
@@ -13,29 +20,49 @@ validation_fact_daily_feed_codes_shape_related AS (
             )
 ),
 
-shape_validation_notices_by_day AS (
+feed_shape_validation_notices AS (
     SELECT
         feed_key,
-        date,
-        SUM(total_notices) as validation_notices
-    FROM validation_fact_daily_feed_codes_shape_related
-    GROUP BY feed_key, date
+        SUM(total_notices) AS validation_notices
+    FROM shape_validation_notices
+    GROUP BY 1
+),
+
+feed_has_shapes AS (
+    SELECT
+        feed_key,
+        LOGICAL_OR(gtfs_filename = "shapes") AS has_shapes
+    FROM files
+    GROUP BY 1
+),
+
+check_start AS (
+    SELECT MIN(date) AS first_check_date
+    FROM shape_validation_notices
 ),
 
 int_gtfs_quality__shapes_valid AS (
     SELECT
-        idx.date,
-        idx.feed_key,
-        {{ shapes_valid() }} AS check,
-        {{ accurate_service_data() }} AS feature,
+        idx.* EXCEPT(status),
+        validation_notices,
+        has_shapes,
         CASE
-            WHEN notices.validation_notices = 0 THEN {{ guidelines_pass_status() }}
-            WHEN notices.validation_notices > 0 THEN {{ guidelines_fail_status() }}
+            WHEN has_schedule_feed
+                THEN
+                    CASE
+                        WHEN has_shapes AND validation_notices = 0 THEN {{ guidelines_pass_status() }}
+                        WHEN idx.date < first_check_date THEN {{ guidelines_na_too_early_status() }}
+                        WHEN feed_shape_validation_notices.feed_key IS NULL OR NOT has_shapes THEN {{ guidelines_na_check_status() }}
+                        WHEN has_shapes AND validation_notices > 0 THEN {{ guidelines_fail_status() }}
+                    END
+            ELSE idx.status
         END AS status
-    FROM feed_guideline_index idx
-    LEFT JOIN shape_validation_notices_by_day notices
-        ON idx.feed_key = notices.feed_key
-            AND idx.date = notices.date
+    FROM guideline_index idx
+    CROSS JOIN check_start
+    LEFT JOIN feed_has_shapes
+        ON idx.schedule_feed_key = feed_has_shapes.feed_key
+    LEFT JOIN feed_shape_validation_notices
+      ON idx.schedule_feed_key = feed_shape_validation_notices.feed_key
 )
 
 SELECT * FROM int_gtfs_quality__shapes_valid
