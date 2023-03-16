ce510a40fd53540e878773c19a3d61755844930c
Laurie <55149902+lauriemerrell@users.noreply.github.com>
2023-03-16 13:33:39 -0500

    Migrate guideline check indices: Batch 2 (#2370)
    * wip on trip planner checks, tbd delete the unused rt one and keep testing
    
    * make yaml actually work
    
    * remove unneeded rt model
    
    * reindex no schedule validation errors
    
    * remove all existing models and just start over on yaml
    
    * shapes file present
    
    * migrate complete wheelchair info
    
    * shapes for all trips
    
    * add comment
    
    * migrate include tts
    
    * migrate no shapes validations
    
    * remove non-applicable comment
    
    * fix date handling for shapes valid
    
    * remove redundant clause
    
    * refactor pathways check to check for file presence
    
    * feed info technical contact
    
    * add check for shapes file existing per pr review
    
    * add comments per pr review

--- warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__complete_wheelchair_accessibility_data.sql
+++ warehouse/models/intermediate/gtfs_quality/guidelines_checks/int_gtfs_quality__complete_wheelchair_accessibility_data.sql
@@ -1,5 +1,7 @@
-WITH feed_guideline_index AS (
-    SELECT * FROM {{ ref('int_gtfs_quality__schedule_feed_guideline_index') }}
+WITH guideline_index AS (
+    SELECT *
+    FROM {{ ref('int_gtfs_quality__guideline_checks_index') }}
+    WHERE check = {{ complete_wheelchair_accessibility_data() }}
 ),
 
 dim_trips AS (
@@ -13,7 +15,7 @@ dim_stops AS (
 feed_trips_summary AS (
    SELECT
        feed_key,
-       COUNTIF(wheelchair_accessible IS NOT NULL AND CAST(wheelchair_accessible AS string) != "0") AS ct_trips_accessibility_info,
+       COUNTIF(wheelchair_accessible IS NOT NULL AND CAST(wheelchair_accessible AS STRING) != "0") AS ct_trips_accessibility_info,
        COUNT(*) AS ct_trips
     FROM dim_trips
    GROUP BY feed_key
@@ -22,27 +24,40 @@ feed_trips_summary AS (
 feed_stops_summary AS (
    SELECT
        feed_key,
-       COUNTIF(wheelchair_boarding IS NOT NULL AND CAST(wheelchair_boarding AS string) != "0") AS ct_stops_accessibility_info,
+       COUNTIF(wheelchair_boarding IS NOT NULL AND CAST(wheelchair_boarding AS STRING) != "0") AS ct_stops_accessibility_info,
        COUNT(*) AS ct_stops
     FROM dim_stops
    GROUP BY feed_key
 ),
 
+check_start AS (
+    SELECT MIN(_feed_valid_from) AS first_check_date
+    FROM dim_trips
+),
+
 int_gtfs_quality__complete_wheelchair_accessibility_data AS (
     SELECT
-        t1.date,
-        t1.feed_key,
-        {{ complete_wheelchair_accessibility_data() }} AS check,
-        {{ accurate_accessibility_data() }} AS feature,
+        idx.* EXCEPT(status),
         CASE
-            WHEN t2.ct_trips_accessibility_info = t2.ct_trips AND t3.ct_stops_accessibility_info = t3.ct_stops THEN {{ guidelines_pass_status() }}
-            ELSE {{ guidelines_fail_status() }}
-        END AS status,
-      FROM feed_guideline_index t1
-      LEFT JOIN feed_trips_summary t2
-             ON t1.feed_key = t2.feed_key
-      LEFT JOIN feed_stops_summary t3
-             ON t1.feed_key = t3.feed_key
+            WHEN has_schedule_feed
+                THEN
+                    CASE
+                        WHEN trips.ct_trips_accessibility_info = trips.ct_trips
+                            AND stops.ct_stops_accessibility_info = stops.ct_stops
+                            THEN {{ guidelines_pass_status() }}
+                        -- TODO: we might be able to handle these if we wanted to shift to noon or similar -- only "too early" are on April 21, 2021 specifically
+                        -- but impact is small (many checks not assessessed this far back) so probably not worth it right now
+                        WHEN CAST(idx.date AS TIMESTAMP) < first_check_date THEN {{ guidelines_na_too_early_status() }}
+                        ELSE {{ guidelines_fail_status() }}
+                    END
+            ELSE idx.status
+        END AS status
+    FROM guideline_index AS idx
+    CROSS JOIN check_start
+    LEFT JOIN feed_trips_summary AS trips
+            ON idx.schedule_feed_key = trips.feed_key
+    LEFT JOIN feed_stops_summary AS stops
+            ON idx.schedule_feed_key = stops.feed_key
 )
 
 SELECT * FROM int_gtfs_quality__complete_wheelchair_accessibility_data
