9863bd6f343f731fc0b99953cab196ac3f024656
Charles Costanzo <cvc5185@gmail.com>
2023-03-17 12:19:24 -0400

    added more aliasing to dim_organizations to be explicit

--- warehouse/models/mart/transit_database/dim_organizations.sql
+++ warehouse/models/mart/transit_database/dim_organizations.sql
@@ -18,29 +18,35 @@ bridge_organizations_x_county_geography AS (
 
 dim_organizations AS (
     SELECT
+
         -- key
         dim.key,
         dim.source_record_id,
+
         ntd.key AS annual_database_agency_information_2021_key,
+
         -- attributes
         dim.name,
         dim.organization_type,
-        roles,
-        itp_id,
-        details,
-        caltrans_district,
-        website,
-        reporting_category,
-        hubspot_company_record_id,
-        gtfs_static_status,
-        gtfs_realtime_status,
-        assessment_status,
-        manual_check__contact_on_website,
-        alias,
+        dim.roles,
+        dim.itp_id,
+        dim.details,
+        dim.caltrans_district,
+        dim.website,
+        dim.reporting_category,
+        dim.hubspot_company_record_id,
+        dim.gtfs_static_status,
+        dim.gtfs_realtime_status,
+        dim.assessment_status,
+        dim.manual_check__contact_on_website,
+        dim.alias,
+
         geography_bridge.name AS hq_county_geography,
+
         dim._is_current,
         dim._valid_from,
         dim._valid_to
+
     FROM dim
     LEFT JOIN ntd_agency_to_organization ntd_to_org
         ON dim.source_record_id = ntd_to_org.organization_record_id
