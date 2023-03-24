25c0fad3d5d43bdfecef377edf157a1d49aa61d2
natam1 <natalielamberg@gmail.com>
2023-03-23 19:34:57 -0700

    use fct_observed_trips

--- warehouse/models/mart/ad_hoc/gtfs_scheduled_vs_vehicle_positions.sql
+++ warehouse/models/mart/ad_hoc/gtfs_scheduled_vs_vehicle_positions.sql
@@ -1,15 +1,16 @@
 WITH
-vp_trips AS (
+rt_trips AS (
   SELECT
-      gtfs_dataset_key AS vp_gtfs_dataset_key,
-      base64_url,
+      -- vp_gtfs_dataset_key,
+      -- vp_base64_url,
       dt AS service_date,
-      _gtfs_dataset_name AS vp_gtfs_dataset_name,
+      schedule_to_use_for_rt_validation_gtfs_dataset_key,
       trip_route_id,
-      count(distinct trip_id) AS num_vp_trips
-  FROM {{ ref('fct_vehicle_locations') }}
-  WHERE ((dt = '2023-02-08'))
-  GROUP BY 1, 2, 3, 4, 5),
+      trip_id AS vp_trip_id,
+      -- sum(vp_num_distinct_message_ids) AS sum_vp_num_dist_message_ids,
+  FROM {{ ref('fct_observed_trips') }}
+  WHERE dt = '2023-02-08'
+),
 
 sched_trips AS(
   SELECT 
@@ -19,50 +20,58 @@ sched_trips AS(
     service_date,
     route_id,
     route_short_name,
-    count(distinct trip_id) AS num_sched_trips
-FROM {{ref('fct_daily_scheduled_trips')}}
-WHERE ((service_date = '2023-02-08'))
-GROUP BY 1, 2, 3, 4, 5, 6),
+    trip_id AS sched_trip_id
+FROM {{ ref('fct_daily_scheduled_trips') }}
+WHERE service_date = '2023-02-08'
+),
 
 rt_sched_joined AS (
   SELECT
     T1.sched_gtfs_dataset_key,
-    T2.vp_gtfs_dataset_key,
+    -- T2.vp_gtfs_dataset_key,
+    -- T2.vp_base64_url,
     T1.service_date,
     T1.agency_id,
     T1.route_id,
     T2.trip_route_id,
     T1.route_short_name,
     T1.sched_gtfs_dataset_name,
-    T2.vp_gtfs_dataset_name,
-    T1.num_sched_trips,
-    T2.num_vp_trips
+    T1.sched_trip_id,
+    T2.vp_trip_id
   FROM sched_trips as T1
-  LEFT JOIN vp_trips as T2
+  LEFT JOIN rt_trips as T2
     ON
-      T1.route_id = T2.trip_route_id
+      T1.sched_gtfs_dataset_key = T2.schedule_to_use_for_rt_validation_gtfs_dataset_key
+      AND T1.route_id = T2.trip_route_id
+      AND T1.sched_trip_id = T2.vp_trip_id
       AND T1.service_date = T2.service_date
-  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-)
-,
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+),
 
-gtfs_scheduled_vs_vehicle_positions AS (
+gtfs_rt_vs_sched_trips_counts AS (
   SELECT
     T1.sched_gtfs_dataset_key,
-    T1.vp_gtfs_dataset_key,
+    -- T1.vp_gtfs_dataset_key,
+    -- T1.vp_base64_url,
     T1.service_date,
     T2.organization_name,
     T1.route_id,
     T1.route_short_name,
     T1.sched_gtfs_dataset_name,
-    T1.vp_gtfs_dataset_name,
-    T1.num_sched_trips,
-    T1.num_vp_trips,
-    num_vp_trips/num_sched_trips AS pct_trips_w_vp
+    count(DISTINCT T1.sched_trip_id) AS num_sched_trip_ids,
+    count(DISTINCT T1.vp_trip_id) AS num_vp_trip_ids
   FROM rt_sched_joined as T1
-    LEFT JOIN {{ref('dim_provider_gtfs_data')}} as T2
+    LEFT JOIN {{ ref('dim_provider_gtfs_data') }}  as T2
       ON T1.sched_gtfs_dataset_key = T2.schedule_gtfs_dataset_key
-  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  GROUP BY 1, 2, 3, 4, 5, 6
+),
+
+gtfs_scheduled_vs_vehicle_positions AS (
+SELECT 
+  *,
+  num_vp_trip_ids/num_sched_trip_ids AS pct_trips_w_vp
+  FROM gtfs_rt_vs_sched_trips_counts
+  ORDER BY route_id
 )
 
 SELECT *
