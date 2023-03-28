d3f57d4712586668a1d4013e5b1dbaefe9654989
Soren Spicknall <soren@spicknall.us>
2023-03-28 16:04:56 -0500

    Initial definitions for GTFS datasets open data

---
+++ warehouse/models/mart/gtfs_schedule_latest/dim_gtfs_datasets_latest.sql
@@ -0,0 +1,30 @@
+WITH
+unfiltered_entries_latest AS (
+    {{ get_latest_schedule_data(
+    table_name = ref('dim_gtfs_datasets'),
+    clean_table_name = 'dim_gtfs_datasets'
+    ) }}
+),
+
+bridge_schedule_dataset_for_validation AS (
+    SELECT * FROM {{ ref('bridge_schedule_dataset_for_validation') }}
+    where _is_current
+),
+
+dim_gtfs_datasets_latest AS (
+    SELECT
+        unfiltered_entries_latest.name,
+        unfiltered_entries_latest.type,
+        unfiltered_entries_latest.regional_feed_type,
+        unfiltered_entries_latest.uri,
+        unfiltered_entries_latest.base64_url,
+        validation_schedule.base64_url AS schedule_to_use_for_rt_validation_base64_url
+    FROM unfiltered_entries_latest
+    LEFT JOIN bridge_schedule_dataset_for_validation
+        ON unfiltered_entries_latest.key = bridge_schedule_dataset_for_validation.gtfs_dataset_key
+    LEFT JOIN unfiltered_entries_latest AS validation_schedule
+        ON bridge_schedule_dataset_for_validation.schedule_to_use_for_rt_validation_gtfs_dataset_key = validation_schedule.key
+    WHERE unfiltered_entries_latest.deprecated_date IS NULL
+)
+
+SELECT * FROM dim_gtfs_datasets_latest
