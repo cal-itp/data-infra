b300fad5629e6edb6488527eda73a2b1b6e55922
Laurie Merrell <laurie.m@jarv.us>
2023-04-06 14:47:39 -0500

    remove unnecessary tz validation - stops

--- warehouse/models/staging/gtfs/stg_gtfs_schedule__stops.sql
+++ warehouse/models/staging/gtfs/stg_gtfs_schedule__stops.sql
@@ -27,14 +27,6 @@ stg_gtfs_schedule__stops AS (
         SAFE_CAST({{ trim_make_empty_string_null('wheelchair_boarding') }} AS INTEGER) AS wheelchair_boarding,
         {{ trim_make_empty_string_null('level_id') }} AS level_id,
         {{ trim_make_empty_string_null('platform_code') }} AS platform_code,
-        -- enumerate the values that we have encountered in this field
-        -- because we have confirmed that they work in BigQuery, i.e., they can be passed to the TIMESTAMP function and do not return an error
-        -- if we encounter a new value we want to alert and check
-        CASE
-            WHEN {{ trim_make_empty_string_null('stop_timezone') }} IN ('America/Los_Angeles', 'US/Pacific', 'America/Vancouver',
-                'America/New_York', 'America/Phoenix', 'US/Arizona', 'America/Chicago',
-                'America/Denver', 'America/Boise') THEN {{ trim_make_empty_string_null('stop_timezone') }}
-        END AS stop_timezone_valid_tz
     FROM external_stops
 )
 
