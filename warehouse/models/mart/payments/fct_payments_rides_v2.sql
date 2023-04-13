d8f12bb158f116a7aecf94d2f1c49b34d9ec95be
Charles Costanzo <cvc5185@gmail.com>
2023-04-13 11:22:42 -0400

    add fct_payments_rides_v2 row access policy for CCJPA

--- warehouse/models/mart/payments/fct_payments_rides_v2.sql
+++ warehouse/models/mart/payments/fct_payments_rides_v2.sql
@@ -21,6 +21,11 @@
     principals = ['serviceAccount:clean-air-payments-user@cal-itp-data-infra.iam.gserviceaccount.com']
 ) }}",
 " {{ create_row_access_policy(
+    filter_column = 'participant_id',
+    filter_value = 'ccjpa',
+    principals = ['serviceAccount:ccjpa-payments-user@cal-itp-data-infra.iam.gserviceaccount.com']
+) }}",
+" {{ create_row_access_policy(
     principals = ['serviceAccount:metabase@cal-itp-data-infra.iam.gserviceaccount.com',
                   'serviceAccount:bq-transform-svcacct@cal-itp-data-infra.iam.gserviceaccount.com',
                   'group:cal-itp@jarv.us',
