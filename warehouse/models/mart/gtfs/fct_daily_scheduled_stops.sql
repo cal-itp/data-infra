16fa2f9ba201d21fcafff225d502caabd15e5e3d
Laurie Merrell <laurie.m@jarv.us>
2023-04-21 11:39:09 -0500

    use service date for scheduled stops

--- warehouse/models/mart/gtfs/fct_daily_scheduled_stops.sql
+++ warehouse/models/mart/gtfs/fct_daily_scheduled_stops.sql
@@ -24,15 +24,23 @@ dim_routes AS (
 stops_by_day_by_route AS (
 
     SELECT
-        DATE(TIMESTAMP_ADD(
-            {{ gtfs_noon_minus_twelve_hours('trips.service_date', 'trips.feed_timezone') }},
-            INTERVAL stop_times.arrival_sec SECOND
-            ), "America/Los_Angeles") AS stop_arrival_date_pacific,
+        trips.service_date,
         trips.feed_key,
         COALESCE(CAST(trips.route_type AS INT), 1000) AS route_type,
         stop_times.stop_id,
+        trips.feed_timezone,
 
         COUNT(*) AS stop_events_count_by_route,
+
+        MIN(DATETIME(TIMESTAMP_ADD(
+            {{ gtfs_noon_minus_twelve_hours('trips.service_date', 'trips.feed_timezone') }},
+            INTERVAL stop_times.arrival_sec SECOND
+            ), "America/Los_Angeles")) AS first_stop_arrival_datetime_pacific,
+
+        MAX(DATETIME(TIMESTAMP_ADD(
+            {{ gtfs_noon_minus_twelve_hours('trips.service_date', 'trips.feed_timezone') }},
+            INTERVAL stop_times.departure_sec SECOND
+            ), "America/Los_Angeles")) AS last_stop_departure_datetime_pacific,
 
         LOGICAL_OR(
             trips.contains_warning_duplicate_stop_times_primary_key
@@ -48,7 +56,7 @@ stops_by_day_by_route AS (
     LEFT JOIN fct_daily_scheduled_trips AS trips
         ON trips.feed_key = stop_times.feed_key
             AND trips.trip_id = stop_times.trip_id
-    GROUP BY 1, 2, 3, 4
+    GROUP BY 1, 2, 3, 4, 5
 
 ),
 
@@ -56,11 +64,15 @@ stops_by_day AS (
 
     SELECT
 
-        stop_arrival_date_pacific,
+        service_date,
+        feed_timezone,
         feed_key,
         stop_id,
 
         SUM(stop_events_count_by_route) AS stop_event_count,
+
+        MIN(first_stop_arrival_datetime_pacific) AS first_stop_arrival_datetime_pacific,
+        MAX(last_stop_departure_datetime_pacific) AS last_stop_departure_datetime_pacific,
 
         LOGICAL_OR(
             contains_warning_duplicate_stop_times_primary_key
@@ -73,7 +85,7 @@ stops_by_day AS (
         ) AS contains_warning_missing_foreign_key_stop_id
 
     FROM stops_by_day_by_route
-    GROUP BY 1, 2, 3
+    GROUP BY 1, 2, 3, 4
 ),
 
 pivot_to_route_type AS (
@@ -82,7 +94,7 @@ pivot_to_route_type AS (
     FROM
         (SELECT
 
-            stop_arrival_date_pacific,
+            service_date,
             feed_key,
             route_type,
             stop_id,
@@ -99,13 +111,16 @@ pivot_to_route_type AS (
 fct_daily_scheduled_stops AS (
     SELECT
 
-        {{ dbt_utils.generate_surrogate_key(['pivoted.stop_arrival_date_pacific', 'stops.key']) }} AS key,
+        {{ dbt_utils.generate_surrogate_key(['pivoted.service_date', 'stops.key']) }} AS key,
 
-        pivoted.stop_arrival_date_pacific,
+        pivoted.service_date,
         pivoted.feed_key,
         pivoted.stop_id,
 
+        stops_by_day.feed_timezone,
         stops_by_day.stop_event_count,
+        stops_by_day.first_stop_arrival_datetime_pacific,
+        stops_by_day.last_stop_departure_datetime_pacific,
 
         pivoted.route_type_0,
         pivoted.route_type_1,
@@ -141,7 +156,7 @@ fct_daily_scheduled_stops AS (
         AND pivoted.feed_key = stops.feed_key
     LEFT JOIN stops_by_day
         ON pivoted.stop_id = stops_by_day.stop_id
-        AND pivoted.stop_arrival_date_pacific = stops_by_day.stop_arrival_date_pacific
+        AND pivoted.service_date = stops_by_day.service_date
         AND pivoted.feed_key = stops_by_day.feed_key
 )
 
