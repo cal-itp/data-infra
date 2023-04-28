f7786f7125e85e178d2e96575290834eb0a3a244
Laurie Merrell <laurie.m@jarv.us>
2023-04-28 10:07:09 -0500

    clean up comments

--- warehouse/macros/gtfs_interval_to_seconds.sql
+++ warehouse/macros/gtfs_interval_to_seconds.sql
@@ -1,5 +1,5 @@
 {% macro gtfs_interval_to_seconds(gtfs_interval_field) %}
--- convert a BigQuery hours to seconds interval (assumed to be from a GTFS "Time" fied) to a count of seconds
+-- convert a BigQuery hours to seconds interval (assumed to be from a GTFS "Time" field) to a count of seconds
 
 EXTRACT(HOUR FROM {{ gtfs_interval_field }}) * 3600
     + EXTRACT(MINUTE FROM {{ gtfs_interval_field }}) * 60
