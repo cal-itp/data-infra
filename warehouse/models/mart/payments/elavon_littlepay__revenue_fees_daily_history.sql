d54d819d9e1bd4e0474cd00fe6e9a984a36987b4
Charles Costanzo <cvc5185@gmail.com>
2023-04-28 12:29:20 -0400

    cleaned up old mvp workaround tables

--- warehouse/models/mart/payments/elavon_littlepay__revenue_fees_daily_history.sql
+++ warehouse/models/mart/payments/elavon_littlepay__revenue_fees_daily_history.sql
@@ -1,7 +1,8 @@
 {{ config(materialized='table') }}
 
-WITH stg_elavon__billing AS (
-    SELECT * FROM  {{ ref('stg_elavon__billing') }}
+WITH elavon_billing AS (
+    SELECT * FROM  {{ ref('fct_elavon__transactions') }}
+    WHERE batch_type = 'B'
 ),
 
 fct_payments_rides_v2 AS (
@@ -18,12 +19,11 @@ billing_agg AS(
 
         customer_name,
         payment_date,
+        -- this same treatment for the other customers?
         IF(customer_name = 'MST TAP TO RIDE', 'mst', customer_name) as participant_id,
         SUM(amount) AS billing_sum
 
-    FROM stg_elavon__billing
-    -- this is a quick and dirty dedupe and needs to be replaced
-    WHERE dt = '2023-04-10'
+    FROM elavon_billing
     GROUP BY customer_name, payment_date
 ),
 
@@ -36,7 +36,7 @@ littlepay_agg AS (
     GROUP BY participant_id, transaction_date_pacific
 ),
 
-stg_fee_comparison AS (
+elavon_littlepay__revenue_fees_daily_history AS (
 
     SELECT
         t1.participant_id,
@@ -50,4 +50,4 @@ stg_fee_comparison AS (
         ON (t1.day_history = t3.payment_date) AND (t1.participant_id = t3.participant_id)
 )
 
-SELECT *  FROM stg_fee_comparison
+SELECT *  FROM elavon_littlepay__revenue_fees_daily_history
