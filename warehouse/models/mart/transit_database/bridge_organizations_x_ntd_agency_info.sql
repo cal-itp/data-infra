d6cfa99b7fc80e13432c5a7c21fe2fa65ea7e90f
Soren Spicknall <soren@spicknall.us>
2023-05-11 10:31:23 -0500

    Return bridge table to previous state

--- warehouse/models/mart/transit_database/bridge_organizations_x_ntd_agency_info.sql
+++ warehouse/models/mart/transit_database/bridge_organizations_x_ntd_agency_info.sql
@@ -7,22 +7,23 @@ WITH orgs AS ( -- noqa
 
 ntd AS ( -- noqa
     SELECT *
-    FROM {{ ref('ntd_agency_to_organization') }}
+    FROM {{ ref('int_transit_database__ntd_agency_info_dim') }}
 ),
 
 bridge_organizations_x_ntd_agency_info AS (
     SELECT
         orgs.key AS organization_key,
-        orgs.source_record_id,
         orgs.name AS organization_name,
+        ntd.key AS ntd_agency_info_key,
         ntd.ntd_id,
-        ntd.organization_record_id,
-        orgs._is_current,
-        orgs._valid_from,
-        orgs._valid_to
+        (orgs._is_current AND ntd._is_current) AS _is_current,
+        GREATEST(orgs._valid_from, ntd._valid_from) AS _valid_from,
+        LEAST(orgs._valid_to, ntd._valid_to) AS _valid_to
     FROM orgs
     INNER JOIN ntd
-        ON orgs.source_record_id = ntd.organization_record_id
+        ON orgs.ntd_agency_info_key = ntd.source_record_id
+        AND orgs._valid_from < ntd._valid_to
+        AND orgs._valid_to > ntd._valid_from
 )
 
 SELECT * FROM bridge_organizations_x_ntd_agency_info
