8755c00a1928898a7e9d97c25b1f0c7e26cfbdcf
Laurie Merrell <laurie.m@jarv.us>
2023-05-11 08:44:00 -0500

    working wip for service alerts

--- warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
+++ warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
@@ -1,66 +1,45 @@
 {{
     config(
-        materialized='incremental',
-        incremental_strategy='insert_overwrite',
-        partition_by={
-            'field': 'dt',
-            'data_type': 'date',
-            'granularity': 'day',
-        },
+        materialized='table',
         cluster_by='base64_url',
     )
 }}
 
+
 WITH service_alerts AS (
-    SELECT 
-        *,
-        -- per spec, start/end is +/- infinity if null: https://gtfs.org/realtime/reference/#message-timerange
-        -- use placeholders instead
-        COALESCE(TIMESTAMP_SECONDS(unnested_active_period.start), TIMESTAMP(DATE(1900,1,1))) AS active_period_start_ts,
-        COALESCE(TIMESTAMP_SECONDS(unnested_active_period.end), TIMESTAMP(DATE(2099,1,1))) AS active_period_end_ts
-    FROM {{ ref('fct_service_alerts_messages_unnested') }}
-    WHERE {{ gtfs_rt_dt_where() }}
-        -- TODO: support route_id/direction_id/start_time as a trip identifier
-        -- as of 2023-04-20, there are no cases of service alert messages where trip ID is not populated and trip.route ID is populated
-        -- so if trip_id is not populated we assume it's not a trip-level alert
-        AND trip_id IS NOT NULL
+    SELECT * FROM {{ ref('int_gtfs_rt__service_alerts_trip_day_map_grouping') }}
 ),
 
 fct_service_alerts_trip_summaries AS (
     SELECT
         -- https://gtfs.org/realtime/reference/#message-tripdescriptor
         {{ dbt_utils.generate_surrogate_key([
-            'dt',
+            'calculated_service_date',
             'base64_url',
             'trip_id',
-            'trip_route_id',
-            'trip_direction_id',
             'trip_start_time',
-            'trip_start_date',
         ]) }} as key,
-        -- TODO: once #2457 merges, we should use the schedule feed timezone rather than just Pacific
-        -- we need to get individual trip instances that can be merged with schedule feed trip instances
-        COALESCE(
-            PARSE_DATE("%Y%m%d",trip_start_date),
-            DATE(header_timestamp, "America/Los_Angeles"),
-            DATE(_extract_ts, "America/Los_Angeles")) AS calculated_service_date_pacific,
+        calculated_service_date,
         base64_url,
         trip_id,
         trip_route_id,
         trip_direction_id,
         trip_start_time,
         trip_start_date,
-        COUNT(DISTINCT id) AS num_distinct_message_ids,
-        COUNT(DISTINCT header_timestamp) AS num_distinct_header_timestamps,
-        ARRAY_AGG(DISTINCT service_alert_message_key) AS service_alert_message_keys,
-        MIN(_extract_ts) AS min_extract_ts,
-        MAX(_extract_ts) AS max_extract_ts,
-        MIN(header_timestamp) AS min_header_timestamp,
-        MAX(header_timestamp) AS max_header_timestamp,
+        trip_schedule_relationship,
+        feed_timezone,
+        COUNT(DISTINCT unnested_message_ids) AS num_distinct_message_ids,
+        COUNT(DISTINCT unested_header_timestamps) AS num_distinct_header_timestamps,
+        COUNT(DISTINCT unnested_message_keys) AS num_distinct_message_keys,
+        MIN(min_extract_ts) AS min_extract_ts,
+        MAX(max_extract_ts) AS max_extract_ts,
+        MIN(min_header_timestamp) AS min_header_timestamp,
+        MAX(max_header_timestamp) AS max_header_timestamp
     FROM service_alerts
-    -- we only want to use alerts that were actually *active*
-    WHERE header_timestamp BETWEEN active_period_start_ts AND active_period_end_ts
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
+    LEFT JOIN UNNEST(message_ids_array) AS unnested_message_ids
+    LEFT JOIN UNNEST(header_timestamps_array) AS unested_header_timestamps
+    LEFT JOIN UNNEST(message_keys_array) AS unnested_message_keys
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 )
 
 SELECT * FROM fct_service_alerts_trip_summaries
