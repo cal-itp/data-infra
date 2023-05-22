9ea39f80e9796f1e04473061a378156ad790fe1c
Laurie Merrell <laurie.m@jarv.us>
2023-05-22 08:34:45 -0500

    address failing tests in long_calendar

--- warehouse/models/intermediate/gtfs/int_gtfs_schedule__long_calendar.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_schedule__long_calendar.sql
@@ -4,6 +4,9 @@
 WITH dim_calendar AS (
     SELECT *
     FROM {{ ref('dim_calendar') }}
+    -- in May 2023 we got a feed where there were some rows with start_date < end_date
+    -- this breaks generate_date_array below
+    WHERE start_date <= end_date
 ),
 
 int_gtfs_schedule__long_calendar AS (
@@ -29,6 +32,9 @@ int_gtfs_schedule__long_calendar AS (
     -- one row per day between calendar service start and end date
     -- https://stackoverflow.com/questions/38694040/how-to-generate-date-series-to-occupy-absent-dates-in-google-biqquery/58169269#58169269
     LEFT JOIN UNNEST(GENERATE_DATE_ARRAY(start_date, LEAST(end_date, DATE_ADD(CURRENT_DATE(), INTERVAL 1 YEAR)))) AS dt
+    -- in May 2023 we got a feed that was publishing service through 2025 / 2026
+    -- drop service that is more than a year away
+    WHERE start_date < DATE_ADD(CURRENT_DATE(), INTERVAL 1 YEAR)
 )
 
 SELECT *
