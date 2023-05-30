20398e4428f14612423166255408c7b6d7a9ee0b
Andrew Vaccaro <atvaccaro@users.noreply.github.com>
2023-05-30 11:37:25 -0400

    Bring back cml CI and change what we print (#2605)
    * calculate service date for trip updates - wip
    
    * fix tu time zones & start on sa, wip
    
    * more service alerts wip
    
    * working wip for service alerts
    
    * non-working wip; need to go refactor service alerts
    
    * untested wip, move to two-step grouping
    
    * sa wip
    
    * working refactored service alerts
    
    * semi-working wip; need to handle trip schedule relationship
    
    * start using window functions
    
    * Revert "Switch GitHub comment action (#2495)"
    
    This reverts commit 890b87e5c083642e17438a590e71d4dc1ffbbfe1.
    
    * maybe fix cml, only show changed incremental models
    
    * add note about incremental models
    
    * missed this
    
    * switch to jinja for report building; start to tweak output
    
    * make mypy pass
    
    * run the dbt comment step on any PR not just main
    
    * change ci warehouse report templating
    
    * continue working on CI report
    
    * clean up; make mypy pass
    
    * we do not always have a catalog entry
    
    * fix mistake, only large tables should be red
    
    * clean up run viz too
    
    * start addressing PR comments
    
    * address PR comment
    
    * format markdown table
    
    * also check incremental models downstream of changing models
    
    * need to include the downstream stuff in viz
    
    * remove a log line, add another
    
    * make mypy happy
    
    * use an enum for viz artifact type
    
    * tweak a bit more
    
    ---------
    
    Co-authored-by: Laurie Merrell <laurie.m@jarv.us>

---
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt__trip_updates_trip_day_map_grouping.sql
@@ -0,0 +1,77 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='insert_overwrite',
+        partition_by={
+            'field': 'calculated_service_date',
+            'data_type': 'date',
+            'granularity': 'day',
+        },
+        cluster_by='base64_url',
+    )
+}}
+
+WITH stop_time_updates AS (
+    SELECT *
+    FROM {{ ref('fct_stop_time_updates') }}
+    WHERE {{ gtfs_rt_dt_where() }}
+),
+
+rt_feeds AS (
+    SELECT *
+    FROM {{ ref('fct_daily_rt_feed_files') }}
+),
+
+schedule_feeds AS (
+    SELECT *
+    FROM {{ ref('dim_schedule_feeds') }}
+),
+
+-- group by *both* the UTC date that data was scraped (dt) *and* calculated service date
+-- so that in the mart we can get just service date-level data
+-- this allows us to handle the dt/service_date mismatch by grouping in two stages
+int_gtfs_rt__trip_updates_trip_day_map_grouping AS (
+    SELECT
+        -- try to figure out what the service date would be to join back with schedule: fall back from explicit to imputed
+        -- TODO: it's possible that this could lead to some weirdness around midnight Pacific / in feed timezone
+        -- if `trip_start_date` is not set we theoretically should be trying to grab the date of the first arrival time per trip
+        -- because trip updates may be generated hours before the beginning of the actual trip activity
+        -- however the fact that this would occur near date boundaries is precisely why it's a bit tricky to pick the right first arrival time if trip start date is not populated
+        dt,
+        COALESCE(
+            PARSE_DATE("%Y%m%d", trip_start_date),
+            DATE(header_timestamp, schedule_feeds.feed_timezone),
+            DATE(_extract_ts, schedule_feeds.feed_timezone)) AS calculated_service_date,
+        stop_time_updates.base64_url,
+        trip_id,
+        trip_route_id,
+        trip_direction_id,
+        trip_start_time,
+        trip_start_date,
+        trip_schedule_relationship,
+        schedule_feeds.feed_timezone,
+        ARRAY_AGG(DISTINCT id) AS message_ids_array,
+        ARRAY_AGG(DISTINCT header_timestamp) AS header_timestamps_array,
+        ARRAY_AGG(DISTINCT trip_update_timestamp IGNORE NULLS) AS trip_update_timestamps_array,
+        ARRAY_AGG(DISTINCT _trip_updates_message_key) AS message_keys_array,
+        MIN(_extract_ts) AS min_extract_ts,
+        MAX(_extract_ts) AS max_extract_ts,
+        MIN(header_timestamp) AS min_header_timestamp,
+        MAX(header_timestamp) AS max_header_timestamp,
+        MIN(trip_update_timestamp) AS min_trip_update_timestamp,
+        MAX(trip_update_timestamp) AS max_trip_update_timestamp,
+        MAX(trip_update_delay) AS max_delay,
+        ARRAY_AGG(DISTINCT CASE WHEN schedule_relationship = 'SKIPPED' THEN stop_id END IGNORE NULLS) AS skipped_stops_array,
+        ARRAY_AGG(DISTINCT CASE WHEN schedule_relationship = 'SCHEDULED' THEN stop_id END IGNORE NULLS) AS scheduled_stops_array,
+        ARRAY_AGG(DISTINCT CASE WHEN schedule_relationship = 'CANCELED' THEN stop_id END IGNORE NULLS) AS canceled_stops_array,
+        ARRAY_AGG(DISTINCT CASE WHEN schedule_relationship = 'ADDED' THEN stop_id END IGNORE NULLS) AS added_stops_array,
+    FROM stop_time_updates
+    LEFT JOIN rt_feeds
+        ON stop_time_updates.base64_url = rt_feeds.base64_url
+        AND stop_time_updates.dt = rt_feeds.date
+    LEFT JOIN schedule_feeds
+        ON rt_feeds.schedule_feed_key = schedule_feeds.key
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+)
+
+SELECT * FROM int_gtfs_rt__trip_updates_trip_day_map_grouping
--- warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
+++ warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
@@ -1,49 +1,82 @@
 {{
     config(
-        materialized='incremental',
-        incremental_strategy='insert_overwrite',
-        partition_by={
-            'field': 'dt',
-            'data_type': 'date',
-            'granularity': 'day',
-        },
+        materialized='table',
         cluster_by='base64_url',
     )
 }}
 
-WITH service_alerts AS (
-    SELECT * FROM {{ ref('fct_service_alerts_messages_unnested') }}
-    WHERE {{ gtfs_rt_dt_where() }}
-),
 
-fct_service_alerts_trip_summaries AS (
-    SELECT
-        -- https://gtfs.org/realtime/reference/#message-tripdescriptor
+WITH service_alerts AS (
+    SELECT *,
+            -- https://gtfs.org/realtime/reference/#message-tripdescriptor
         {{ dbt_utils.generate_surrogate_key([
-            'dt',
+            'calculated_service_date',
             'base64_url',
             'trip_id',
-            'trip_route_id',
-            'trip_direction_id',
             'trip_start_time',
-            'trip_start_date',
         ]) }} as key,
-        dt,
+    FROM {{ ref('int_gtfs_rt__service_alerts_trip_day_map_grouping') }}
+),
+
+re_aggregate AS (
+    SELECT
+        key,
+        calculated_service_date,
         base64_url,
         trip_id,
         trip_route_id,
         trip_direction_id,
         trip_start_time,
         trip_start_date,
-        COUNT(DISTINCT id) AS num_distinct_message_ids,
-        COUNT(DISTINCT header_timestamp) AS num_distinct_header_timestamps,
-        ARRAY_AGG(DISTINCT service_alert_message_key) AS service_alert_message_keys,
-        MIN(_extract_ts) AS min_extract_ts,
-        MAX(_extract_ts) AS max_extract_ts,
-        MIN(header_timestamp) AS min_header_timestamp,
-        MAX(header_timestamp) AS max_header_timestamp,
+        trip_schedule_relationship,
+        feed_timezone,
+        COUNT(DISTINCT unnested_message_ids) AS num_distinct_message_ids,
+        COUNT(DISTINCT unested_header_timestamps) AS num_distinct_header_timestamps,
+        COUNT(DISTINCT unnested_message_keys) AS num_distinct_message_keys,
+        MIN(min_extract_ts) AS min_extract_ts,
+        MAX(max_extract_ts) AS max_extract_ts,
+        MIN(min_header_timestamp) AS min_header_timestamp,
+        MAX(max_header_timestamp) AS max_header_timestamp,
+        ARRAY_AGG(DISTINCT unnested_alert_content)
+            AS alert_content_array
     FROM service_alerts
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
-)
+    LEFT JOIN UNNEST(message_ids_array) AS unnested_message_ids
+    LEFT JOIN UNNEST(header_timestamps_array) AS unested_header_timestamps
+    LEFT JOIN UNNEST(message_keys_array) AS unnested_message_keys
+    LEFT JOIN UNNEST(alert_content_array) AS unnested_alert_content
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+),
+
+fct_service_alerts_trip_summaries AS (
+    SELECT
+        key,
+        calculated_service_date,
+        base64_url,
+        trip_id,
+        trip_route_id,
+        trip_direction_id,
+        trip_start_time,
+        trip_start_date,
+        trip_schedule_relationship,
+        feed_timezone,
+        num_distinct_message_ids,
+        num_distinct_header_timestamps,
+        num_distinct_message_keys,
+        min_extract_ts,
+        max_extract_ts,
+        min_header_timestamp,
+        max_header_timestamp,
+        ARRAY_AGG(
+            STRUCT<message_id string, cause string, effect string, header string, description string >
+                (JSON_VALUE(unnested_alert, '$.message_id'),
+                JSON_VALUE(unnested_alert, '$.cause'),
+                JSON_VALUE(unnested_alert, '$.effect'),
+                JSON_VALUE(unnested_alert, '$.header'),
+                JSON_VALUE(unnested_alert, '$.description')))
+        AS alert_content_array
+    FROM re_aggregate
+    LEFT JOIN UNNEST(alert_content_array) AS unnested_alert
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
+    )
 
 SELECT * FROM fct_service_alerts_trip_summaries
