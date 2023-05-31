bff446dbbf5e3b8093e2189eab15366f552c5420
Andrew Vaccaro <atvaccaro@gmail.com>
2023-05-30 21:34:00 -0400

    just use window function

--- warehouse/models/mart/gtfs/dim_areas.sql
+++ warehouse/models/mart/gtfs/dim_areas.sql
@@ -5,17 +5,6 @@ WITH make_dim AS (
     ) }}
 ),
 
-bad_rows AS (
-    SELECT
-        base64_url,
-        ts,
-        area_id,
-        TRUE AS warning_duplicate_gtfs_key
-    FROM make_dim
-    GROUP BY base64_url, ts, area_id
-    HAVING COUNT(*) > 1
-),
-
 dim_areas AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['feed_key', '_line_number']) }} AS key,
@@ -24,14 +13,16 @@ dim_areas AS (
         area_id,
         area_name,
         base64_url,
-        COALESCE(warning_duplicate_gtfs_key, FALSE) AS warning_duplicate_gtfs_key,
+        COUNT(
+            *
+        ) OVER (
+            PARTITION BY feed_key, area_id
+        ) > 1 AS warning_duplicate_gtfs_key,
         _dt,
         _feed_valid_from,
         _line_number,
         feed_timezone,
     FROM make_dim
-    LEFT JOIN bad_rows
-        USING (base64_url, ts, area_id)
 )
 
 SELECT * FROM dim_areas
