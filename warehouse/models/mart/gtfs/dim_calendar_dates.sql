ddcd4b453f1b65fd33d5d1a93127beb1ca17e73b
Andrew Vaccaro <atvaccaro@gmail.com>
2023-06-01 11:47:08 -0400

    make dim_calendar_dates unique as natural key level

--- warehouse/models/mart/gtfs/dim_calendar_dates.sql
+++ warehouse/models/mart/gtfs/dim_calendar_dates.sql
@@ -19,6 +19,10 @@ dim_calendar_dates AS (
         _line_number,
         feed_timezone,
     FROM make_dim
+    -- filter rather than flag; lots of downstream models expect uniqueness
+    -- we could remove if we handled it everywhere but these are mostly full duplicates
+    -- and they occur on many feeds
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY feed_key, service_id, date ORDER BY _line_number) = 1
 )
 
 SELECT * FROM dim_calendar_dates
