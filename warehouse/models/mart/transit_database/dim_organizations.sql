1cd08aead4249aef2c9bb3e1dd6aeab27b71ec74
Laurie Merrell <laurie.m@jarv.us>
2023-06-05 11:22:24 -0500

    comment / doc clarifications

--- warehouse/models/mart/transit_database/dim_organizations.sql
+++ warehouse/models/mart/transit_database/dim_organizations.sql
@@ -10,14 +10,10 @@ ntd_agency_to_organization AS (
 
 dim_organizations AS (
     SELECT
-
-        -- key
-        dim.key,
-        dim.source_record_id,
-
-        -- attributes
+        key,
+        source_record_id,
         name,
-        dim.organization_type,
+        organization_type,
         roles,
         itp_id,
         details,
@@ -31,21 +27,21 @@ dim_organizations AS (
         manual_check__contact_on_website,
         alias,
         is_public_entity,
-        -- use same cutover date as public currently operating logic for assessment status
+        -- use same May 23, 2023 cutover date as `assessment_status` --> `public_currently_operating` in downstream models for consistency
         CASE
-            WHEN dim._valid_from >= '2023-05-23' THEN raw_ntd_id
+            WHEN _valid_from >= '2023-05-23' THEN raw_ntd_id
             ELSE ntd_to_org.ntd_id
         END AS ntd_id,
         public_currently_operating,
         public_currently_operating_fixed_route,
 
-        dim._is_current,
-        dim._valid_from,
-        dim._valid_to
+        _is_current,
+        _valid_from,
+        _valid_to
 
     FROM dim
     LEFT JOIN ntd_agency_to_organization ntd_to_org
-        ON dim.source_record_id = ntd_to_org.organization_record_id
+        ON source_record_id = ntd_to_org.organization_record_id
 )
 
 SELECT * FROM dim_organizations
