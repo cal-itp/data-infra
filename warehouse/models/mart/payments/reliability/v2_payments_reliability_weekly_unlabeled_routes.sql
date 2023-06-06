efac1862c7a1f1d6f6c25fca89fb3e7ae34ef16d
Charles Costanzo <cvc5185@gmail.com>
2023-06-06 10:38:49 -0400

    create new weekly unlabeled routes tracking table, add 'monthly' to title of original table

--- warehouse/models/mart/payments/reliability/v2_payments_reliability_weekly_unlabeled_routes.sql
+++ warehouse/models/mart/payments/reliability/v2_payments_reliability_weekly_unlabeled_routes.sql
@@ -2,31 +2,31 @@ WITH payments_rides AS (
     SELECT * FROM {{ ref('fct_payments_rides_v2') }}
 ),
 
-payments_tests_monthly_date_spine AS (
-    SELECT * FROM {{ ref('payments_tests_monthly_date_spine') }}
+payments_tests_weekly_date_spine AS (
+    SELECT * FROM {{ ref('payments_tests_weekly_date_spine') }}
 ),
 
 count_rides AS (
     SELECT
 
         spine.participant_id,
-        spine.month_start,
+        spine.week_start,
         COUNTIF(route_id = 'Route Z') AS n_route_z_rides,
         COUNTIF(route_id IS NULL) AS n_null_rides,
         COUNTIF(route_id = '') AS n_empty_string_rides,
         COUNT(*) AS n_all_rides
 
-    FROM payments_tests_monthly_date_spine AS spine
+    FROM payments_tests_weekly_date_spine AS spine
     INNER JOIN payments_rides
-        ON (spine.participant_id = payments_rides.participant_id) AND transaction_date_pacific >= month_start AND transaction_date_pacific <= month_end
-    GROUP BY month_start, participant_id
+        ON (spine.participant_id = payments_rides.participant_id) AND transaction_date_pacific >= week_start AND transaction_date_pacific <= week_end
+    GROUP BY week_start, participant_id
 ),
 
 aggregations_and_date_spine AS (
     SELECT
 
         date_spine.participant_id,
-        date_spine.month_start,
+        date_spine.week_start,
         count_rides.n_route_z_rides,
         count_rides.n_null_rides,
         count_rides.n_empty_string_rides,
@@ -36,25 +36,25 @@ aggregations_and_date_spine AS (
 
         SAFE_DIVIDE((count_rides.n_route_z_rides + count_rides.n_null_rides + count_rides.n_empty_string_rides), count_rides.n_all_rides) * 100 AS pct_unlabeled_rides_to_total_rides
 
-    FROM payments_tests_monthly_date_spine AS date_spine
+    FROM payments_tests_weekly_date_spine AS date_spine
     LEFT JOIN count_rides
-        USING (participant_id, month_start)
+        USING (participant_id, week_start)
 ),
 
-v2_payments_reliability_unlabeled_routes AS (
+v2_payments_reliability_weekly_unlabeled_routes AS (
     SELECT
 
         participant_id,
-        month_start,
+        week_start,
         n_route_z_rides,
         n_null_rides,
         total_unlabeled_rides,
         n_empty_string_rides,
         n_all_rides,
         pct_unlabeled_rides_to_total_rides,
-        RANK() OVER (PARTITION BY participant_id ORDER BY month_start DESC) AS recency_rank
+        RANK() OVER (PARTITION BY participant_id ORDER BY week_start DESC) AS recency_rank
 
     FROM aggregations_and_date_spine
 )
 
-SELECT * FROM v2_payments_reliability_unlabeled_routes
+SELECT * FROM v2_payments_reliability_weekly_unlabeled_routes
