97cead62baa638a7ad4beec12024708d5a74c3ea
Laurie Merrell <laurie.m@jarv.us>
2023-06-21 09:39:17 -0500

    refactor service alerts trip summaries based on new message keying

--- warehouse/models/intermediate/gtfs/int_gtfs_rt__service_alerts_trip_day_map_grouping.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt__service_alerts_trip_day_map_grouping.sql
@@ -21,27 +21,13 @@ WITH service_alerts AS (
         AND trip_id IS NOT NULL
 ),
 
-rt_feeds AS (
-    SELECT *
-    FROM {{ ref('fct_daily_rt_feed_files') }}
-),
-
-schedule_feeds AS (
-    SELECT *
-    FROM {{ ref('dim_schedule_feeds') }}
-),
-
 -- group by *both* the UTC date that data was scraped (dt) *and* calculated service date
 -- so that in the mart we can get just service date-level data
 -- this allows us to handle the dt/service_date mismatch by grouping in two stages
 grouped AS (
     SELECT
-        -- try to figure out what the service date would be to join back with schedule: fall back from explicit to imputed
         dt,
-        COALESCE(
-            PARSE_DATE("%Y%m%d", trip_start_date),
-            DATE(header_timestamp, schedule_feeds.feed_timezone),
-            DATE(_extract_ts, schedule_feeds.feed_timezone)) AS calculated_service_date,
+        calculated_service_date,
         service_alerts.base64_url,
         trip_id,
         trip_route_id,
@@ -49,7 +35,7 @@ grouped AS (
         trip_start_time,
         trip_start_date,
         trip_schedule_relationship,
-        schedule_feeds.feed_timezone,
+        schedule_feed_timezone,
         -- what we really want here is an array of the distinct structs, but you can't DISTINCT structs
         -- so we turn them into JSON strings which we can turn back into structs later
         ARRAY_AGG(DISTINCT
@@ -66,11 +52,6 @@ grouped AS (
         MIN(header_timestamp) AS min_header_timestamp,
         MAX(header_timestamp) AS max_header_timestamp,
     FROM service_alerts
-    LEFT JOIN rt_feeds
-        ON service_alerts.base64_url = rt_feeds.base64_url
-        AND service_alerts.dt = rt_feeds.date
-    LEFT JOIN schedule_feeds
-        ON rt_feeds.schedule_feed_key = schedule_feeds.key
     -- we only want to use alerts that were actually *active*
     WHERE header_timestamp BETWEEN active_period_start_ts AND active_period_end_ts
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
@@ -95,7 +76,7 @@ int_gtfs_rt__service_alerts_trip_day_map_grouping AS (
         trip_start_time,
         trip_start_date,
         trip_schedule_relationship,
-        feed_timezone,
+        schedule_feed_timezone,
         alert_content_array,
         message_ids_array,
         extract_ts_array,
--- warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
+++ warehouse/models/mart/gtfs/fct_service_alerts_trip_summaries.sql
@@ -19,24 +19,36 @@ window_functions AS (
             ORDER BY min_header_timestamp
             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
         ) AS starting_schedule_relationship,
-    LAST_VALUE(trip_schedule_relationship)
-        OVER (
-            PARTITION BY key
-            ORDER BY max_header_timestamp
-            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-        ) AS ending_schedule_relationship,
-    FIRST_VALUE(trip_direction_id)
+        LAST_VALUE(trip_schedule_relationship)
+            OVER (
+                PARTITION BY key
+                ORDER BY max_header_timestamp
+                ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS ending_schedule_relationship,
+        FIRST_VALUE(trip_route_id)
         OVER (
             PARTITION BY key
             ORDER BY min_header_timestamp
             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-        ) AS starting_direction_id,
-    LAST_VALUE(trip_direction_id)
-        OVER (
-            PARTITION BY key
-            ORDER BY max_header_timestamp
-            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-        ) AS ending_direction_id,
+        ) AS starting_route_id,
+        LAST_VALUE(trip_route_id)
+            OVER (
+                PARTITION BY key
+                ORDER BY max_header_timestamp
+                ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS ending_route_id,
+        FIRST_VALUE(trip_direction_id)
+            OVER (
+                PARTITION BY key
+                ORDER BY min_header_timestamp
+                ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS starting_direction_id,
+        LAST_VALUE(trip_direction_id)
+            OVER (
+                PARTITION BY key
+                ORDER BY max_header_timestamp
+                ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+            ) AS ending_direction_id,
     FROM service_alerts
 ),
 
@@ -101,7 +113,9 @@ non_array_agg AS(
         trip_route_id,
         trip_start_time,
         trip_start_date,
-        feed_timezone,
+        schedule_feed_timezone,
+        starting_route_id,
+        ending_route_id,
         starting_schedule_relationship,
         ending_schedule_relationship,
         starting_direction_id,
@@ -111,7 +125,7 @@ non_array_agg AS(
         MIN(min_header_timestamp) AS min_header_timestamp,
         MAX(max_header_timestamp) AS max_header_timestamp,
     FROM window_functions
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 ),
 
 fct_service_alerts_trip_summaries AS (
@@ -122,29 +136,27 @@ fct_service_alerts_trip_summaries AS (
         trip_id,
         trip_start_time,
         trip_start_date,
-        feed_timezone,
+        schedule_feed_timezone,
         starting_schedule_relationship,
         ending_schedule_relationship,
         starting_direction_id,
         ending_direction_id,
         starting_route_id,
         ending_route_id,
-        starting_direction_id,
-        ending_direction_id,
         min_extract_ts,
         max_extract_ts,
         TIMESTAMP_DIFF(max_extract_ts, min_extract_ts, MINUTE) AS extract_duration_minutes,
         DATETIME(min_extract_ts, "America/Los_Angeles") AS min_extract_datetime_pacific,
         DATETIME(max_extract_ts, "America/Los_Angeles") AS max_extract_datetime_pacific,
-        DATETIME(min_extract_ts, feed_timezone) AS min_extract_datetime_local_tz,
-        DATETIME(max_extract_ts, feed_timezone) AS max_extract_datetime_local_tz,
+        DATETIME(min_extract_ts, schedule_feed_timezone) AS min_extract_datetime_local_tz,
+        DATETIME(max_extract_ts, schedule_feed_timezone) AS max_extract_datetime_local_tz,
         min_header_timestamp,
         max_header_timestamp,
         TIMESTAMP_DIFF(max_header_timestamp, min_header_timestamp, MINUTE) AS header_duration_minutes,
         DATETIME(min_extract_ts, "America/Los_Angeles") AS min_header_datetime_pacific,
         DATETIME(max_extract_ts, "America/Los_Angeles") AS max_header_datetime_pacific,
-        DATETIME(min_extract_ts, feed_timezone) AS min_header_datetime_local_tz,
-        DATETIME(max_extract_ts, feed_timezone) AS max_header_datetime_local_tz,
+        DATETIME(min_extract_ts, schedule_feed_timezone) AS min_header_datetime_local_tz,
+        DATETIME(max_extract_ts, schedule_feed_timezone) AS max_header_datetime_local_tz,
         num_distinct_message_ids,
         num_distinct_header_timestamps,
         num_distinct_message_keys,
