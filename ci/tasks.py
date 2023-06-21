6bcde57a925843a012c1e77193173d490b85fe28
Andrew Vaccaro <atvaccaro@gmail.com>
2023-06-21 16:06:18 -0400

    start on pydantic with invoke, and change test consumer to test

--- ci/tasks.py
+++ ci/tasks.py
@@ -1,14 +1,52 @@
 import os
+from enum import Enum
 from pathlib import Path
+from typing import Any, Generator, List, Optional
 
 import git
-from decouple import Config, RepositoryEnv
 from invoke import Result, task
+from pydantic import BaseSettings
 
 
-@task
-def fail(c):
-    c.run("this does not exist")
+class ReleaseDriver(str, Enum):
+    helm = "helm"
+    kustomize = "kustomize"
+
+
+class Release(BaseSettings):
+    name: str
+    release_driver: ReleaseDriver
+
+    # for helm
+    release_namespace: Optional[str]
+    release_helm_name: Optional[str]
+    release_helm_chart: Optional[Path]
+    release_helm_values: Optional[List[Path]]
+
+    # for kustomize
+    release_kustomize_dir: Optional[Path]
+
+    class Config:
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name == "release_helm_values":
+                return raw_val.split(":")
+            return cls.json_loads(raw_val)
+
+
+def get_releases(
+    channel: str,
+    releases_dir="./vars/releases",
+    driver: Optional[ReleaseDriver] = None,
+) -> Generator[Release, None, None]:
+    for release in Path(releases_dir).glob(f"{channel}-*"):
+        r = Release(
+            name=str(release),
+            _env_file=release,
+            _env_file_encoding="utf-8",
+        )
+        if not driver or r.release_driver == driver:
+            yield r
 
 
 @task
@@ -16,20 +54,16 @@ def kdiff(
     c,
     channel,
     app=None,
-    releases_dir="./vars/releases",
     outfile=os.getenv("GITHUB_OUTPUT"),
 ):
     repo = git.Repo(".", search_parent_directories=True)
 
     full_diff = ""
 
-    for release in Path(releases_dir).glob(f"{channel}-*"):
-        env = Config(RepositoryEnv(release))
-        if env.get("RELEASE_DRIVER") == "kustomize" and (
-            not app or app == release.name
-        ):
+    for release in get_releases(channel, driver=ReleaseDriver.kustomize):
+        if not app or app == release.name:
             kustomize_dir = Path(repo.working_tree_dir) / Path(
-                env.get("RELEASE_KUSTOMIZE_DIR")
+                release.release_kustomize_dir
             )
             result: Result = c.run(
                 f"kubectl diff -k {kustomize_dir}",
@@ -43,3 +77,4 @@ def kdiff(
         print(f"writing to {outfile}")
         with open(outfile, "a") as f:
             f.write(f"DIFF={full_diff}")
+    print(flush=True)
