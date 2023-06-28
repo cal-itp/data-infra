c0c8ed5d4783394858c0201eabb51a20fa08d6eb
Laurie Merrell <laurie.m@jarv.us>
2023-06-28 16:44:32 -0500

    refactored vehicle locations... includes rows with no trip id & the mtc regional feed

--- warehouse/models/mart/gtfs/fct_vehicle_locations.sql
+++ warehouse/models/mart/gtfs/fct_vehicle_locations.sql
@@ -1,40 +1,55 @@
-WITH
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'key',
+        cluster_by = ['calculated_service_date', 'base64_url'],
+    )
+}}
 
--- base64_url
--- header_timestamp
--- vehicle_timestamp
--- vehicle_id
--- trip_id
--- is unique
-fct_vehicle_positions_messages AS (
+WITH fct_vehicle_positions_messages AS (
     SELECT * FROM {{ ref('fct_vehicle_positions_messages') }}
+    WHERE {{ incremental_where() }}
+),
+
+vp_trips AS (
+    SELECT
+        calculated_service_date,
+        base64_url,
+        trip_id,
+        trip_start_time,
+        trip_instance_key
+    FROM {{ ref('fct_vehicle_positions_trip_summaries') }}
 ),
 
 coalesced_and_filtered AS (
     SELECT * EXCEPT (key),
         COALESCE(vehicle_timestamp, header_timestamp) AS location_timestamp
     FROM fct_vehicle_positions_messages
-    WHERE trip_id IS NOT NULL
-        AND _gtfs_dataset_name != 'Bay Area 511 Regional VehiclePositions'
 ),
 
 deduped AS (
     SELECT *,
-        {{ dbt_utils.generate_surrogate_key(['dt', 'base64_url', 'location_timestamp', 'vehicle_id', 'vehicle_label', 'trip_id']) }} AS key,
-        {{ dbt_utils.generate_surrogate_key(['dt', 'base64_url', 'vehicle_id', 'vehicle_label', 'trip_id']) }} AS vehicle_trip_key
+        {{ dbt_utils.generate_surrogate_key(['calculated_service_date', 'base64_url', 'location_timestamp', 'vehicle_id', 'vehicle_label', 'trip_id', 'trip_start_time']) }} AS key,
+        {{ dbt_utils.generate_surrogate_key(['calculated_service_date', 'base64_url', 'vehicle_id', 'vehicle_label', 'trip_id', 'trip_start_time']) }} AS vehicle_trip_key
     FROM coalesced_and_filtered
     QUALIFY ROW_NUMBER() OVER (
-        -- the dt is necessary to preserve partition elimination in downstream queries
-        PARTITION BY dt, base64_url, location_timestamp, vehicle_id, vehicle_label, trip_id
-        ORDER BY NULL
+        PARTITION BY calculated_service_date, base64_url, location_timestamp, vehicle_id, vehicle_label, trip_id, trip_start_time
+        ORDER BY position_latitude, position_longitude
     ) = 1
 ),
 
 fct_vehicle_locations AS (
-    SELECT *,
-        LEAD(key) OVER (PARTITION BY dt, base64_url, vehicle_trip_key ORDER BY location_timestamp) AS next_location_key,
-        ST_GEOGPOINT(position_longitude, position_latitude) AS location
+    SELECT deduped.*,
+        LEAD(key) OVER (PARTITION BY vehicle_trip_key ORDER BY location_timestamp) AS next_location_key,
+        ST_GEOGPOINT(position_longitude, position_latitude) AS location,
+        trip_instance_key
     FROM deduped
+    LEFT JOIN vp_trips
+        ON deduped.calculated_service_date = vp_trips.calculated_service_date
+        AND deduped.trip_id = vp_trips.trip_id
+        AND deduped.base64_url = vp_trips.base64_url
+        -- this is often null but we need to include it for frequency based trips
+        AND COALESCE(deduped.trip_start_time, "") = COALESCE(vp_trips.trip_start_time, "")
 )
 
 SELECT * FROM fct_vehicle_locations
