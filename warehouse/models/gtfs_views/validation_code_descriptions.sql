d1a134aca538671618a03e2dce5f6d5e9a2d2c29
Soren Spicknall <soren.s@jarv.us>
2023-07-11 13:47:10 -0500

    Remove unused models and related references (#2791)

--- warehouse/models/gtfs_views/validation_code_descriptions.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-WITH validation_code_descriptions AS (
-    SELECT *
-    FROM {{ source('gtfs_schedule_history',
-    'validation_code_descriptions') }}
-)
-
-SELECT * FROM validation_code_descriptions
