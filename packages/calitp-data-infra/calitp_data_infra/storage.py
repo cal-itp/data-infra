305a70a072f56da07bf32a74fc9fcbaad2fbb754
Andrew Vaccaro <atvaccaro@gmail.com>
2023-07-24 16:07:48 -0400

    remove calitp-data-infra dependency on calitp-data

--- packages/calitp-data-infra/calitp_data_infra/storage.py
+++ packages/calitp-data-infra/calitp_data_infra/storage.py
@@ -27,8 +27,6 @@ import backoff
 import gcsfs  # type: ignore
 import humanize
 import pendulum
-from calitp_data.config import get_bucket, require_pipeline  # type: ignore
-from calitp_data.storage import get_fs  # type: ignore
 from google.cloud import storage  # type: ignore
 from pydantic import (
     BaseModel,
@@ -48,63 +46,13 @@ JSONL_EXTENSION = ".jsonl"
 JSONL_GZIP_EXTENSION = f"{JSONL_EXTENSION}.gz"
 
 
-@require_pipeline("save_to_gcfs")
-def save_to_gcfs(
-    src_path,
-    dst_path,
-    gcs_project="cal-itp-data-infra",
-    bucket=None,
-    use_pipe=False,
-    verbose=True,
-    **kwargs,
-):
-    """Convenience function for saving files from disk to google cloud storage.
-
-    Arguments:
-        src_path: path to file being saved.
-        dst_path: path to bucket subdirectory (e.g. "path/to/dir").
-    """
-
-    bucket = get_bucket() if bucket is None else bucket
-
-    full_dst_path = bucket + "/" + str(dst_path)
-
-    fs = get_fs(gcs_project)
-
-    if verbose:
-        print("Saving to:", full_dst_path)
-
-    if not use_pipe:
-        fs.put(str(src_path), full_dst_path, **kwargs)
+def get_fs(gcs_project="", **kwargs):
+    if os.environ.get("CALITP_AUTH") == "cloud":
+        return gcsfs.GCSFileSystem(project=gcs_project, token="cloud", **kwargs)
     else:
-        fs.pipe(str(full_dst_path), src_path, **kwargs)
-
-    return full_dst_path
-
-
-def read_gcfs(src_path, dst_path=None, gcs_project="", bucket=None, verbose=True):
-    """
-    Arguments:
-        src_path: path to file being read from google cloud.
-        dst_path: optional path to save file directly on disk.
-    """
-
-    bucket = get_bucket() if bucket is None else bucket
-
-    fs = get_fs(gcs_project)
-
-    full_src_path = bucket + "/" + str(src_path)
-
-    if verbose:
-        print(f"Reading file: {full_src_path}")
-
-    if dst_path is None:
-        return fs.open(full_src_path)
-    else:
-        # TODO: in this case, dump directly to disk, rather opening
-        raise NotImplementedError()
-
-    return full_src_path
+        return gcsfs.GCSFileSystem(
+            project=gcs_project, token="google_default", **kwargs
+        )
 
 
 def make_name_bq_safe(name: str):
