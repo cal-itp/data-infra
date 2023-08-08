6c37bb60a28c9275effb42fef877080b30a2be6f
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-08-08 18:57:33 +0000

    start month/year/route/shape grain and add lookup to dim_shape_arrays

--- warehouse/models/mart/ad_hoc/fct_daily_scheduled_shapes_monthly_aggregations.sql
+++ warehouse/models/mart/ad_hoc/fct_daily_scheduled_shapes_monthly_aggregations.sql
@@ -1,100 +1,79 @@
 {{ config(materialized='table') }}
 
 WITH dim_gtfs_datasets AS (
-    SELECT * FROM {{ ref('dim_gtfs_datasets') }}
+    SELECT
+        key,
+        name,
+        source_record_id
+    FROM {{ ref('dim_gtfs_datasets') }}
 ),
 
 fct_scheduled_trips AS (
     SELECT *
     FROM {{ ref('fct_scheduled_trips') }}
-    WHERE service_date >= '2023-06-01' AND service_date < '2023-06-04'
+    #WHERE service_date < CURRENT_DATE()
+    WHERE service_date >= '2023-06-01' AND service_date < '2023-06-02'
 ),
 
-fct_daily_scheduled_shapes AS (
+dim_shapes_arrays AS (
 
     SELECT
-        shape_array_key,
-        n_trips,
-        pt_array
-
-    FROM {{ ref('fct_daily_scheduled_shapes') }}
-    WHERE service_date >= '2023-06-01' AND service_date < '2023-06-04'
-),
-
-extract_trip_date_types AS (
-
-    SELECT DISTINCT
-        gtfs_dataset_key,
-        route_id,
+        base64_url,
         shape_id,
         shape_array_key,
-        direction_id,
-        EXTRACT(month FROM service_date) AS month,
-        EXTRACT(year FROM service_date) AS year,
+        pt_array
 
-    FROM fct_scheduled_trips
+    FROM {{ ref('dim_shapes_arrays') }}
+    WHERE {{ incremental_where(default_start_var='GTFS_SCHEDULE_START',
+                               this_dt_column='_feed_valid_from',
+                               filter_dt_column='_feed_valid_from',
+                               dev_lookback_days = None)
+           }}
 ),
 
 trips_by_shape AS (
 
-    SELECT
-
-        trips.gtfs_dataset_key,
-        trips.route_id,
-        trips.shape_id,
-        trips.shape_array_key,
-        trips.direction_id,
-        trips.month,
-        trips.year,
-
-        shapes.n_trips,
-
-    FROM extract_trip_date_types AS trips
-    LEFT JOIN fct_daily_scheduled_shapes AS shapes
-        ON (shapes.shape_array_key = trips.shape_array_key)
-),
-
-shape_aggregations AS (
-
-    SELECT
+    SELECT DISTINCT
         gtfs_dataset_key,
+        base64_url,
         route_id,
         shape_id,
-        shape_array_key,
-        direction_id,
-        month,
-        year,
+        COUNT(trip_id) AS n_trips,
+        EXTRACT(month FROM service_date) AS month,
+        EXTRACT(year FROM service_date) AS year,
 
-        SUM(n_trips) as n_trips,
-
-    FROM trips_by_shape
-    GROUP BY 1, 2, 3, 4, 5, 6, 7
+    FROM fct_scheduled_trips
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY
+                               gtfs_dataset_key, base64_url, route_id,
+                               month, year,
+                               ORDER BY n_trips desc) = 1
 ),
 
-fct_daily_scheduled_shapes_monthly_aggregations AS (
+fct_monthly_shapes AS (
 
     SELECT
-        dim_gtfs_datasets.name,
+
         dim_gtfs_datasets.source_record_id,
+        dim_gtfs_datasets.name,
 
-        shape_aggregations.route_id,
-        shape_aggregations.shape_id,
-        shape_aggregations.direction_id,
-        shape_aggregations.month,
-        shape_aggregations.year,
-        shape_aggregations.n_trips,
+        trips.gtfs_dataset_key,
+        trips.base64_url,
+        trips.route_id,
+        trips.shape_id,
+        trips.month,
+        trips.year,
+        trips.n_trips,
 
-        shapes.pt_array,
+        shapes.shape_array_key,
+        shapes.pt_array
 
-    FROM shape_aggregations
+    FROM trips_by_shape AS trips
     LEFT JOIN dim_gtfs_datasets
-        ON (shape_aggregations.gtfs_dataset_key = dim_gtfs_datasets.key)
-    LEFT JOIN fct_daily_scheduled_shapes AS shapes
-        ON (shapes.shape_array_key = shape_aggregations.shape_array_key)
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY
-                               name, source_record_id, route_id, shape_id,
-                               direction_id, month, year
-                              ORDER BY n_trips desc) = 1
+        ON trips.gtfs_dataset_key = dim_gtfs_datasets.key
+    INNER JOIN dim_shape_arrays AS shapes
+        ON trips.base64_url = shapes.base64_url
+        AND trips.shape_id = shapes.shape_id
+    GROUP BY 1, 2, 4, 5, 6, 7, 8
 )
 
-SELECT * FROM fct_daily_scheduled_shapes_monthly_aggregations
+SELECT * FROM fct_monthly_shapes
