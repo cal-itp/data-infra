9fb8a6682a87a127f4e0b1c2a79537d70e70b811
tiffanychu90 <tiffany.chu@dot.ca.gov>
2023-08-09 16:46:52 +0000

    move dim_gtfs_datasets up, add month_last_day

--- warehouse/models/mart/gtfs/fct_monthly_shapes.sql
+++ warehouse/models/mart/gtfs/fct_monthly_shapes.sql
@@ -8,7 +8,7 @@ WITH dim_gtfs_datasets AS (
 fct_scheduled_trips AS (
     SELECT *
     FROM {{ ref('fct_scheduled_trips') }}
-    WHERE service_date >= '2023-07-29' AND service_date <= '2023-07-31'
+    WHERE service_date >= '2023-07-01'
 ),
 
 dim_shapes_arrays AS (
@@ -21,7 +21,7 @@ dim_shapes_arrays AS (
 
     FROM {{ ref('dim_shapes_arrays') }}
     WHERE {{ incremental_where(default_start_var='GTFS_SCHEDULE_START',
-                               this_dt_column='_feed_valid_from',
+                               this_dt_column='month_last_day',
                                filter_dt_column='_feed_valid_from',
                                dev_lookback_days = None)
            }}
@@ -30,26 +30,36 @@ dim_shapes_arrays AS (
 trips_by_shape AS (
 
     SELECT
-        base64_url,
-        route_id,
-        shape_id,
+
+        dim_gtfs_datasets.source_record_id,
+        dim_gtfs_datasets.name,
+
+        trips.base64_url,
+        trips.route_id,
+        trips.shape_id,
         EXTRACT(month FROM service_date) AS month,
         EXTRACT(year FROM service_date) AS year,
+        LAST_DAY(service_date, MONTH) AS month_last_day,
         COUNT(*) AS n_trips,
 
-    FROM fct_scheduled_trips
-    GROUP BY 1, 2, 3, 4, 5
+    FROM fct_scheduled_trips as trips
+    LEFT JOIN dim_gtfs_datasets
+        ON trips.gtfs_dataset_key = dim_gtfs_datasets.key
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
     QUALIFY ROW_NUMBER() OVER (PARTITION BY
-                               base64_url, route_id, month, year
+                               source_record_id, route_id, month_last_day
                                ORDER BY n_trips DESC) = 1
 ),
 
 fct_monthly_shapes AS (
 
     SELECT
-        dim_gtfs_datasets.source_record_id,
-        dim_gtfs_datasets.name,
+        {{ dbt_utils.generate_surrogate_key([
+            'trips.base64_url', 'trips.route_id',
+            'trips.month', 'trips.year']) }} AS key,
 
+        trips.source_record_id,
+        trips.name,
         trips.base64_url,
         trips.route_id,
         trips.shape_id,
@@ -60,11 +70,10 @@ fct_monthly_shapes AS (
         shapes.pt_array
 
     FROM trips_by_shape AS trips
-    INNER JOIN dim_gtfs_datasets
-        ON trips.base64_url = dim_gtfs_datasets.base64_url
     INNER JOIN dim_shapes_arrays AS shapes
         ON trips.base64_url = shapes.base64_url
         AND trips.shape_id = shapes.shape_id
+    WHERE trips.month_last_day <= CURRENT_DATE()
 )
 
 SELECT * FROM fct_monthly_shapes
