4e113f2947a42db1e5de0ca49f4e71491e0e7264
Laurie Merrell <laurie.m@jarv.us>
2023-10-05 09:09:24 -0500

    remove over-aggressive file deduping

--- warehouse/macros/littlepay_staging_transforms.sql
+++ warehouse/macros/littlepay_staging_transforms.sql
@@ -32,17 +32,7 @@ CASE
 END
 {% endmacro %}
 
-{% macro qualify_dedupe_lp_files(instance_col = 'instance', file_dt_col = 'littlepay_export_date', file_ts_col = 'littlepay_export_ts', ts_col = 'ts') %}
-
--- remove duplicate instances of the same file (file defined as date-level update from LP)
--- partition by file date, order by LP-defined timestamp (most recent first), and then order by our extract timestamp (most recent first)
--- use dense rank instead of row number because we need to allow all rows from a given file to be included (allow ties)
-QUALIFY DENSE_RANK()
-    OVER (PARTITION BY {{ instance_col }}, {{ file_dt_col }} ORDER BY {{ file_ts_col }} DESC, {{ ts_col }} DESC) = 1
-
-{% endmacro %}
-
-{% macro qualify_dedupe_full_duplicate_lp_rows(content_hash_col = 'content_hash', file_ts_col = 'littlepay_export_ts', line_number_col = '_line_number') %}
+{% macro qualify_dedupe_full_duplicate_lp_rows(content_hash_col = '_content_hash', file_ts_col = 'littlepay_export_ts', line_number_col = '_line_number') %}
 
 -- remove full duplicate rows where *all* content is the same
 -- get most recent instance across files and then highest-line-number instance within most recent file
