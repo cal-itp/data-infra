8b1194e07f185c059bc7b5703a1510125cbfbf83
Vladimir Jimenez <allejo@me.com>
2023-11-06 22:02:54 -0800

    Update query to be dbt-friendly

--- warehouse/models/mart/transit_database/dim_mobility_mart_providers.sql
+++ warehouse/models/mart/transit_database/dim_mobility_mart_providers.sql
@@ -1,21 +1,59 @@
+{{ config(materialized='table') }}
+
 -- An Organization (i.e. dim_organizations) can provide multiple different
 -- Services (i.e. dim_services). Each Service will have its own funding sources,
 -- GTFS feeds, and target counties.
 
+WITH organizations AS (
+   SELECT * FROM {{ ref('dim_organizations') }}
+   WHERE _is_current
+),
+
+services AS (
+    SELECT * FROM {{ ref('dim_services')}}
+    WHERE _is_current
+),
+
+county_geogs AS (
+    SELECT * FROM {{ ref('dim_county_geography') }}
+)
+
+orgs_hq_bridge AS (
+    SELECT * FROM {{ ref('bridge_organizations_x_headquarters_county_geography') }}
+)
+
+orgs_services_bridge AS (
+   SELECT * FROM {{ ref('bridge_organizations_x_services_managed') }}
+   WHERE _is_current
+),
+
+services_funding_bridge AS (
+   SELECT * FROM {{ ref('bridge_services_x_funding_programs') }}
+   WHERE _is_current
+),
+
+gtfs_services AS (
+   SELECT * FROM {{ ref('dim_gtfs_service_data') }}
+   WHERE _is_current
+),
+
+gtfs_datasets AS (
+    SELECT * FROM {{ ref('dim_gtfs_datasets') }}
+   WHERE _is_current
+)
+
 -- This query will collect every funding source an Organization has via the
 -- services it provides.
-WITH funding_by_organization AS (
+funding_by_organization AS (
     SELECT
-        orgs.key AS organization_key,
-        ARRAY_AGG(services_x_funding.funding_program_name) AS funding_sources
+        orgs.key AS org_key,
+        ARRAY_AGG(services_funding_bridge.funding_program_name) AS funding_sources
     FROM
-        mart_transit_database.dim_organizations orgs
+        organizations orgs
     INNER JOIN
-        mart_transit_database.bridge_organizations_x_services_managed orgs_x_services ON orgs_x_services.organization_key = orgs.key AND orgs_x_services._is_current IS TRUE
+        orgs_services_bridge ON orgs_services_bridge.org_key = orgs.key
     INNER JOIN
-        mart_transit_database.bridge_services_x_funding_programs services_x_funding ON orgs_x_services.service_key = services_x_funding.service_key AND services_x_funding._is_current IS TRUE
-    WHERE
-        orgs._is_current IS TRUE
+        services_funding_bridge ON orgs_services_bridge.service_key = services_funding_bridge.service_key
     GROUP BY
         orgs.key
 ),
@@ -29,28 +67,29 @@ WITH funding_by_organization AS (
 annual_ntd AS (
     SELECT *
     FROM mart_ntd.dim_annual_ntd_agency_information
-    WHERE
-        year = (SELECT MAX(year) FROM mart_ntd.dim_annual_ntd_agency_information)
-        AND state = 'CA'
+    WHERE state = "CA"
+
+    -- We only want data from the latest data from NTD. In the rare edge case
+    -- that an NTD ID is removed and no longer exists in the current year's
+    -- dataset, then replace `DENSE_RANK()` with `ROW_NUMBER()` and add a
+    -- `PARTITION BY ntd_id` inside of the `OVER()`.
+    QUALIFY DENSE_RANK() OVER(ORDER BY year DESC) = 1
 ),
 
 gtfs_data_by_organization AS (
     SELECT
-        orgs.key AS organization_key,
+        orgs.key AS org_key,
         ARRAY_AGG(gtfs_datasets.uri) AS gtfs_uris
-    FROM mart_transit_database.dim_organizations orgs
+    FROM organizations orgs
     LEFT JOIN
-        mart_transit_database.bridge_organizations_x_services_managed orgs_x_services ON orgs.key = orgs_x_services.organization_key
+        orgs_services_bridge ON orgs.key = orgs_services_bridge.org_key
     LEFT JOIN
-        mart_transit_database.dim_gtfs_service_data gtfs_service ON orgs_x_services.service_key = gtfs_service.service_key
+        gtfs_services ON orgs_services_bridge.service_key = gtfs_services.service_key
     LEFT JOIN
-        mart_transit_database.dim_gtfs_datasets gtfs_datasets ON gtfs_service.gtfs_dataset_key = gtfs_datasets.key
+        gtfs_datasets ON gtfs_services.gtfs_dataset_key = gtfs_datasets.key
     WHERE
-        orgs._is_current IS TRUE
-        AND orgs.public_currently_operating IS TRUE
-        AND gtfs_service._is_current IS TRUE
-        AND gtfs_service.customer_facing IS TRUE
-        AND gtfs_datasets._is_current IS TRUE
+        orgs.public_currently_operating IS TRUE
+        AND gtfs_services.customer_facing IS TRUE
         -- We only want to track schedule feeds for right now
         AND gtfs_datasets.type = 'schedule'
     GROUP BY
@@ -59,16 +98,13 @@ gtfs_data_by_organization AS (
 
 serviced_counties_by_organization AS (
     SELECT
-        orgs.key AS organization_key,
+        orgs.key AS org_key,
         ARRAY_CONCAT_AGG(services.operating_counties) AS operating_counties
-    FROM mart_transit_database.dim_organizations orgs
+    FROM organizations orgs
     LEFT JOIN
-        mart_transit_database.bridge_organizations_x_services_managed orgs_x_services ON orgs_x_services.organization_key = orgs.key
+        orgs_services_bridge orgs_services_bridge ON orgs_services_bridge.org_key = orgs.key
     LEFT JOIN
-        mart_transit_database.dim_services services ON services.key = orgs_x_services.service_key
-    WHERE
-        orgs._is_current IS TRUE
-        AND services._is_current IS TRUE
+        services ON services.key = orgs_services_bridge.service_key
     GROUP BY
         orgs.key
 ),
@@ -78,7 +114,7 @@ mobility_market_providers AS (
         annual_ntd.agency_name AS agency_name,
         annual_ntd.ntd_id AS ntd_id,
         annual_ntd.city AS hq_city,
-        orgs_x_hq.county_geography_name AS hq_county,
+        orgs_hq_bridge.county_geography_name AS hq_county,
         ARRAY(
             SELECT DISTINCT
                 county
@@ -107,17 +143,17 @@ mobility_market_providers AS (
     FROM
         annual_ntd
     LEFT JOIN
-        mart_transit_database.dim_organizations orgs ON orgs.ntd_id = annual_ntd.ntd_id AND orgs._is_current IS TRUE
+        organizations orgs ON orgs.ntd_id = annual_ntd.ntd_id
     LEFT JOIN
-        mart_transit_database.bridge_organizations_x_headquarters_county_geography orgs_x_hq ON orgs.key = orgs_x_hq.organization_key
+        orgs_hq_bridge ON orgs.key = orgs_hq_bridge.org_key
     LEFT JOIN
-        mart_transit_database.dim_county_geography county_geog ON orgs_x_hq.county_geography_key = county_geog.key
+        county_geogs ON orgs_hq_bridge.county_geography_key = county_geogs.key
     LEFT JOIN
-        funding_by_organization ON funding_by_organization.organization_key = orgs.key
+        funding_by_organization ON funding_by_organization.org_key = orgs.key
     LEFT JOIN
-        gtfs_data_by_organization ON gtfs_data_by_organization.organization_key = orgs.key
+        gtfs_data_by_organization ON gtfs_data_by_organization.org_key = orgs.key
     LEFT JOIN
-        serviced_counties_by_organization ON serviced_counties_by_organization.organization_key = orgs.key
+        serviced_counties_by_organization ON serviced_counties_by_organization.org_key = orgs.key
     ORDER BY
         annual_ntd.ntd_id
 )
