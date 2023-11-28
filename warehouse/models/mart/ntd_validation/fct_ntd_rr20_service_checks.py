e6f7c5ab2f77a71ac6db422edfe2c8970d81d55e
Kim Engie <kim.engie@slalom.com>
2023-11-28 11:11:35 -0800

    remove excel conversion from dbt model

--- warehouse/models/mart/ntd_validation/fct_ntd_rr20_service_checks.py
+++ warehouse/models/mart/ntd_validation/fct_ntd_rr20_service_checks.py
@@ -176,44 +176,6 @@ def model(dbt, session):
                              frpt_checks, rev_speed_checks, 
                              tph_checks, voms0_check], 
                              ignore_index=True).sort_values(by="Organization")
-    
-    ## Part 1: save Excel file to GCS
-    GCS_FILE_PATH_VALIDATED = f"gs://calitp-ntd-report-validation/validation_reports_{this_year}" 
-    with pd.ExcelWriter(f"{GCS_FILE_PATH_VALIDATED}/rr20_service_check_report_{this_date}.xlsx") as writer:
-        rr20_checks.to_excel(writer, sheet_name="rr20_checks_full", index=False, startrow=2)
-
-        workbook = writer.book
-        worksheet = writer.sheets["rr20_checks_full"]
-        cell_highlight = workbook.add_format({
-            'fg_color': 'yellow',
-            'bold': True,
-            'border': 1
-        })
-        report_title = "NTD Data Validation Report"
-        title_format = workbook.add_format({
-                'bold': True,
-                'valign': 'center',
-                'align': 'left',
-                'font_color': '#1c639e',
-                'font_size': 15
-                })
-        subtitle = "Reduced Reporting RR-20: Validation Warnings"
-        subtitle_format = workbook.add_format({
-            'bold': True,
-            'align': 'left',
-            'font_color': 'black',
-            'font_size': 19
-            })
-        
-        worksheet.write('A1', report_title, title_format)
-        worksheet.merge_range('A2:C2', subtitle, subtitle_format)
-        worksheet.write('G3', 'Agency Response', cell_highlight)
-        worksheet.write('H3', 'Response Date', cell_highlight)
-        worksheet.set_column(0, 0, 35) #col A width
-        worksheet.set_column(1, 3, 22) #cols B-D width
-        worksheet.set_column(4, 4, 11) #col D width
-        worksheet.set_column(5, 6, 53) #col E-G width
-        worksheet.freeze_panes('B4')
 
     logger.info(f"RR-20 service data checks conducted on {this_date} is complete!")
 
