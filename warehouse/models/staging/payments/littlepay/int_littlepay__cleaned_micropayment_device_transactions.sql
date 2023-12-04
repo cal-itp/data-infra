f6c970d7feeb204a52bd467a61abb083e2b064ed
Laurie Merrell <laurie.m@jarv.us>
2023-12-04 13:35:13 -0600

    standardize sql in cleaned device transactions

--- warehouse/models/staging/payments/littlepay/int_littlepay__cleaned_micropayment_device_transactions.sql
+++ warehouse/models/staging/payments/littlepay/int_littlepay__cleaned_micropayment_device_transactions.sql
@@ -1,60 +1,58 @@
-with
-
-stg_littlepay__micropayment_device_transactions AS (
+WITH stg_littlepay__micropayment_device_transactions AS (
     SELECT * FROM {{ ref('stg_littlepay__micropayment_device_transactions') }}
 ),
+
 stg_littlepay__micropayments AS (
     SELECT * FROM {{ ref('stg_littlepay__micropayments') }}
 ),
 
-deduped_micropayment_device_transaction_ids as (
-    select distinct
+deduped_micropayment_device_transaction_ids AS (
+    SELECT DISTINCT
         littlepay_transaction_id,
         micropayment_id
-    from stg_littlepay__micropayment_device_transactions
+    FROM stg_littlepay__micropayment_device_transactions
 ),
-
 
 -- Some transactions are associated with more than one DEBIT micropayment. This
 -- should not happen. In the query below, we identify the micropayment_id of the
 -- pending micropayment records that are no longer valid because they've been
 -- superceded by a completed micropayment.
 --
--- See https://github.com/cal-itp/data-infra/issues/647 for the explanation.
-invalid_micropayment_device_transaction_ids as (
-    select
+-- See https://github.com/cal-itp/data-infra/ISsues/647 for the explanation.
+invalid_micropayment_device_transaction_ids AS (
+    SELECT
         littlepay_transaction_id,
-        m1.micropayment_id
+        first_micropayment.micropayment_id
 
-    from deduped_micropayment_device_transaction_ids as mdt1
-    inner join stg_littlepay__micropayments as m1
-        on mdt1.micropayment_id = m1.micropayment_id
+    FROM deduped_micropayment_device_transaction_ids AS micropayment_device_transaction
+    INNER JOIN stg_littlepay__micropayments AS first_micropayment
+        ON micropayment_device_transaction.micropayment_id = first_micropayment.micropayment_id
 
-    inner join deduped_micropayment_device_transaction_ids as mdt2 using (littlepay_transaction_id)
-    inner join stg_littlepay__micropayments as m2
-        on mdt2.micropayment_id = m2.micropayment_id
+    INNER JOIN deduped_micropayment_device_transaction_ids AS second_micropayment_device_transaction USING (littlepay_transaction_id)
+    INNER JOIN stg_littlepay__micropayments AS second_micropayment
+        ON second_micropayment_device_transaction.micropayment_id = second_micropayment.micropayment_id
 
-    where m1.micropayment_id != m2.micropayment_id
-        and m1.charge_type = 'pending_charge_fare'
-        and m2.charge_type = 'complete_variable_fare'
+    WHERE first_micropayment.micropayment_id != second_micropayment.micropayment_id
+        AND first_micropayment.charge_type = 'pending_charge_fare'
+        AND second_micropayment.charge_type = 'complete_variable_fare'
 ),
 
-cleaned_micropayment_device_transaction_ids as (
-    select
+cleaned_micropayment_device_transaction_ids AS (
+    SELECT
         littlepay_transaction_id,
         micropayment_id
-    from deduped_micropayment_device_transaction_ids
-    left join invalid_micropayment_device_transaction_ids as invalid
-        using (littlepay_transaction_id, micropayment_id)
-    where invalid.littlepay_transaction_id is null
+    FROM deduped_micropayment_device_transaction_ids
+    LEFT JOIN invalid_micropayment_device_transaction_ids AS invalid
+        USING (littlepay_transaction_id, micropayment_id)
+    WHERE invalid.littlepay_transaction_id IS null
 ),
 
-int_littlepay__cleaned_micropayment_device_transactions as (
+int_littlepay__cleaned_micropayment_device_transactions AS (
 
-    select distinct *
-    from stg_littlepay__micropayment_device_transactions
-    inner join cleaned_micropayment_device_transaction_ids
-        using (littlepay_transaction_id, micropayment_id)
+    SELECT DISTINCT *
+    FROM stg_littlepay__micropayment_device_transactions
+    INNER JOIN cleaned_micropayment_device_transaction_ids
+        USING (littlepay_transaction_id, micropayment_id)
 )
 
-select * from int_littlepay__cleaned_micropayment_device_transactions
+SELECT * FROM int_littlepay__cleaned_micropayment_device_transactions
