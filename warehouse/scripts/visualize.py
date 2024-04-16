aeafd89cd214009bdc592e15a682ee4132728fc3
Soren Spicknall <soren@spicknall.us>
2024-04-16 15:32:12 -0500

    Experiment with >0 threshold for modified_models list

--- warehouse/scripts/visualize.py
+++ warehouse/scripts/visualize.py
@@ -290,13 +290,15 @@ def ci_report(
         ]
     ).result
 
-    typer.secho(f"Visualizing the following models: {modified_models}")
-    assert isinstance(modified_models, list)
-    viz(
-        ArtifactType.manifest,
-        include=modified_models,
-        output=Path("./target/dag.png"),
-    )
+
+    if len(modified_models > 0):
+        typer.secho(f"Visualizing the following models: {modified_models}")
+        assert isinstance(modified_models, list)
+        viz(
+            ArtifactType.manifest,
+            include=modified_models,
+            output=Path("./target/dag.png"),
+        )
 
     env = Environment(
         loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
