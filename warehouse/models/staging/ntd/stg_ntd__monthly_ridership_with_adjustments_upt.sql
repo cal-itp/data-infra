a1684bc241d1c6d51f38154f524c8485aae2194a
V <vevetron@gmail.com>
2024-04-23 10:46:27 -0700

    Filters out totals columns

--- warehouse/models/staging/ntd/stg_ntd__monthly_ridership_with_adjustments_upt.sql
+++ warehouse/models/staging/ntd/stg_ntd__monthly_ridership_with_adjustments_upt.sql
@@ -8,6 +8,7 @@ stg_ntd__monthly_ridership_with_adjustments_upt AS(
     SELECT *
     FROM source
     -- we can have old months data in the source so this gets only the latest extract
-    QUALIFY DENSE_RANK() OVER (ORDER BY ts DESC) = 1
+    Qualify DENSE_RANK() OVER (ORDER BY ts DESC) = 1
 )
 Select * FROM stg_ntd__monthly_ridership_with_adjustments_upt
+where mode in ("DR","FB","LR","MB","SR","TB","VP","CB","RB","CR","YR","MG","MO","AR","TR","HR","OR","IP","AG")
