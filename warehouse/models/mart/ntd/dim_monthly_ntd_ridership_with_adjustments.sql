eabfbe6e9c923e5c1334861cb91f1feea9786314
V <156962295+vevetron@users.noreply.github.com>
2024-06-28 13:54:37 -0700

    Minor ridership fixes (#3378)
    * feat(dim_monthly_ntd_ridership_with_adjustments): adds a period_year_month column to make reporting by date easier,  adds service_mode and adds two more modes to service_type
    
    * feat(dim_annual_ntd_agency_service): adds new category service_type to make analysis easier on the different modes

--- warehouse/models/mart/ntd/dim_monthly_ntd_ridership_with_adjustments.sql
+++ warehouse/models/mart/ntd/dim_monthly_ntd_ridership_with_adjustments.sql
@@ -14,9 +14,15 @@ select
     agency,
     mode_type_of_service_status,
     mode,
+    CASE
+        WHEN mode IN ('AR', 'CC', 'CR', 'HR', 'YR', 'IP', 'LR', 'MG', 'SR', 'TR', 'MB', 'RB', 'CB', 'TB', 'FB', 'IP', 'MO', 'AG') THEN 'Fixed Route'
+        WHEN mode IN ('DR', 'DT', 'VP', 'JT', 'PB') THEN 'Demand Response'
+        ELSE 'Unknown' -- mode is null sometimes
+    END AS service_type,
     _3_mode,
     tos,
     legacy_ntd_id,
+    concat(period_year, '-',  lpad(period_month, 2, '0')) as period_year_month,
     period_year,
     period_month,
     upt,
