e67267a56bc01e0ad8c779ff569fc3a4f934fede
Charles Costanzo <cvc5185@gmail.com>
2024-08-13 11:25:18 -0400

    got 2022 ntd data scraped

--- script/scrape_ntd_api.py
+++ script/scrape_ntd_api.py
@@ -31,21 +31,16 @@ poetry run python scrape_ntd.py monthly-ridership-with-adjustments 2024 https://
 poetry run python scrape_ntd.py annual-database-service 2022 https://www.transit.dot.gov/sites/fta.dot.gov/files/2024-04/2022%20Service.xlsx
 """
 
+import csv
 import gzip
 import os
 from typing import ClassVar, List
 
 import humanize
-import pandas as pd  # type: ignore
 import pendulum
 import requests
 import typer
-from calitp_data_infra.storage import (  # type: ignore
-    PartitionedGCSArtifact,
-    get_fs,
-    make_name_bq_safe,
-)
-from pydantic import HttpUrl, parse_obj_as
+from calitp_data_infra.storage import PartitionedGCSArtifact, get_fs  # type: ignore
 
 CALITP_BUCKET__NTD_DATA_PRODUCTS = os.environ["CALITP_BUCKET__NTD_DATA_PRODUCTS"]
 
@@ -57,7 +52,7 @@ class NtdDataProductExtract(PartitionedGCSArtifact):
     product: str
     ts: pendulum.DateTime
     year: int
-    file_url: HttpUrl
+    api_endpoint: str
 
     @property
     def table(self) -> str:
@@ -69,58 +64,55 @@ class NtdDataProductExtract(PartitionedGCSArtifact):
 
 
 def main(
-    product: str,
-    year: int,
-    api_endpoint: str,
+    # product: str,
+    # year: int,
+    # api_endpoint: str,
 ):
-    # validated_url = parse_obj_as(HttpUrl, file_url)
-    typer.secho(f"reading file from url {api_endpoint}", fg=typer.colors.MAGENTA)
-    start = pendulum.now(tz="Etc/UTC")
-    api_endpoint = "https://data.transportation.gov/api/odata/v4/" + api_endpoint
+    filename = "ntd_endpoints.csv"
 
-    api_content = requests.get(api_endpoint).content
-    #print(api_content)
-    # Save initial excel files to the bucket as "raw"
-    api_extract = NtdDataProductExtract(
-        product=product + "_raw",
-        ts=start,
-        year=year,
-        file_url=api_endpoint,
-        filename=f"{product}_raw.json",
-    )
-    typer.secho(
-        f"saving {humanize.naturalsize(len(api_content))} bytes to {api_extract.path}"
-    )
-    api_extract.save_content(fs=get_fs(), content=api_content)
+    with open(filename, "r") as csvfile:
+        ntd_endpoints = csv.reader(csvfile)
+        for row in ntd_endpoints:
+            # print(row[2])
 
-    #df_dict = pd.read_excel(excel_content, sheet_name=None, engine="openpyxl")
+            # ntd_endpoints = pd.read_csv('ntd_endpoints.csv')
+            # for endpoint in ntd_endpoints:
+            #     print(endpoint)
 
-    # Rename all columns in dictonary, then fix the key(tab) name of the dictionary
-    # for key, df in df_dict.items():
-    #     df = df.rename(make_name_bq_safe, axis="columns")
-    #     typer.secho(
-    #         f"read {df.shape[0]} rows and {df.shape[1]} columns",
-    #         fg=typer.colors.MAGENTA,
-    #     )
+            # print(ntd_endpoints.full_name)
+            # for endpoint in ntd_endpoints:
 
-    #     gzipped_content = gzip.compress(
-    #         df.to_json(orient="records", lines=True).encode()
-    #     )
+            # print(endpoint)
+            typer.secho(
+                f"reading file from api_endpoint: {row[4]}", fg=typer.colors.MAGENTA
+            )
+            start = pendulum.now(tz="Etc/UTC")
+            api_endpoint = (
+                "https://data.transportation.gov/resource/"
+                + row[4]
+                + ".json"
+                + "?$limit=5000000"
+            )
 
-    #     tab_name = ""
-    #     if len(df_dict.keys()) > 1:
-    #         tab_name = "/" + make_name_bq_safe(key)
-    #     extract = NtdDataProductExtract(
-    #         product=product + tab_name,
-    #         ts=start,
-    #         year=year,
-    #         file_url=validated_url,
-    #         filename=f"{product}.jsonl.gz",
-    #     )
-    #     typer.secho(
-    #         f"saving {humanize.naturalsize(len(gzipped_content))} bytes to {extract.path}"
-    #     )
-    #     extract.save_content(fs=get_fs(), content=gzipped_content)
+            api_content = requests.get(api_endpoint).content
+
+            # print(api_content)
+            # Save initial json files to the bucket as "raw"
+            api_extract = NtdDataProductExtract(
+                product=row[2],
+                ts=start,
+                year=row[0],
+                api_endpoint=row[4],
+                filename=f"{row[2]}.jsonl.gz",
+            )
+
+            gzipped_content = gzip.compress(api_content)  # .encode()
+
+            typer.secho(
+                f"saving {humanize.naturalsize(len(gzipped_content))} bytes to {api_extract.path}"
+            )
+
+            api_extract.save_content(fs=get_fs(), content=gzipped_content)
 
 
 if __name__ == "__main__":
