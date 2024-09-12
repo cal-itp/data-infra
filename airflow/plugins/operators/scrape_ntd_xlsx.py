1db17c608b323dae5c87b2f0b9e69d51b5921690
Charles Costanzo <cvc5185@gmail.com>
2024-09-12 17:27:04 -0400

    cleaned XLSX operator, refactored

--- airflow/plugins/operators/scrape_ntd_xlsx.py
+++ airflow/plugins/operators/scrape_ntd_xlsx.py
@@ -1,13 +1,15 @@
 import gzip
 
 # import os
+import logging
 from io import BytesIO
 from typing import ClassVar, List
 
 import pandas as pd  # type: ignore
 import pendulum
 import requests
-import typer
+
+# import typer
 from calitp_data_infra.storage import (  # type: ignore
     PartitionedGCSArtifact,
     get_fs,
@@ -25,63 +27,13 @@ RAW_XLSX_BUCKET = "gs://calitp-ntd-xlsx-products-raw"
 CLEAN_XLSX_BUCKET = "gs://calitp-ntd-xlsx-products-clean"
 
 
-class NtdDataProductXLSXExtractRaw(PartitionedGCSArtifact):
+class NtdDataProductXLSXExtract(PartitionedGCSArtifact):
     bucket: ClassVar[str] = RAW_XLSX_BUCKET
     year: str
     product: str
     execution_ts: pendulum.DateTime = pendulum.now()
     dt: pendulum.Date = execution_ts.date()
-    file_url: HttpUrl
-    partition_names: ClassVar[List[str]] = ["dt", "execution_ts"]
-
-    @property
-    def table(self) -> str:
-        return self.product
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    def fetch_from_ntd_xlsx(self, file_url):
-        validated_url = parse_obj_as(HttpUrl, file_url)
-
-        typer.secho(f"reading file from url {validated_url}", fg=typer.colors.MAGENTA)
-
-        """
-        From here:
-        need to figure out logging, replace secho, add more for the requests
-        need to figure out if moving to one class is possible
-        need to finalize naming conventions
-        need to do all of this for API
-        """
-
-        try:
-            excel_content = requests.get(validated_url).content
-
-            if excel_content is None or len(excel_content) == 0:
-                typer.secho(
-                    f"There is no data to download for {self.year} / {self.product}. Ending pipeline.",
-                    fg=typer.colors.MAGENTA,
-                )
-                pass
-            else:
-                typer.secho(
-                    f"Downloaded {self.product} data for {self.year} with {len(excel_content)} rows!",
-                    fg=typer.colors.MAGENTA,
-                )
-
-                return excel_content
-
-        except requests.exceptions.RequestException as e:
-            # need to figure out logging quick
-            print(f"An error occurred: {e}")
-
-
-class NtdDataProductXLSXExtractClean(PartitionedGCSArtifact):
-    bucket: ClassVar[str] = CLEAN_XLSX_BUCKET
-    year: str
-    product: str
-    execution_ts: pendulum.DateTime = pendulum.now()
-    dt: pendulum.Date = execution_ts.date()
+    file_url: HttpUrl = None
     partition_names: ClassVar[List[str]] = ["dt", "execution_ts"]
 
     @property
@@ -94,6 +46,33 @@ class NtdDataProductXLSXExtractClean(PartitionedGCSArtifact):
 
     class Config:
         arbitrary_types_allowed = True
+
+    def fetch_from_ntd_xlsx(self, file_url):
+        validated_url = parse_obj_as(HttpUrl, file_url)
+
+        logging.info(f"reading file from url {validated_url}")
+
+        try:
+            excel_content = requests.get(validated_url).content
+
+            if excel_content is None or len(excel_content) == 0:
+                logging.info(
+                    f"There is no data to download for {self.year} / {self.product}. Ending pipeline."
+                )
+
+                pass
+
+            else:
+                logging.info(
+                    f"Downloaded {self.product} data for {self.year} with {len(excel_content)} rows!"
+                )
+
+                return excel_content
+
+        except requests.exceptions.RequestException as e:
+            logging.info(f"An error occurred: {e}")
+
+            raise
 
 
 class NtdDataProductXLSXOperator(BaseOperator):
@@ -110,13 +89,8 @@ class NtdDataProductXLSXOperator(BaseOperator):
         self.product = product
         self.xlsx_file_url = xlsx_file_url
 
-        typer.secho(
-            f"file url is {self.xlsx_file_url} and file type is {type(self.xlsx_file_url)}",
-            fg=typer.colors.MAGENTA,
-        )
-
         # Save initial excel files to the bucket as "raw"
-        self.raw_excel_extract = NtdDataProductXLSXExtractRaw(
+        self.raw_excel_extract = NtdDataProductXLSXExtract(
             year=year,
             product=product + "_raw",
             file_url=self.xlsx_file_url,
@@ -129,6 +103,9 @@ class NtdDataProductXLSXOperator(BaseOperator):
         excel_content = self.raw_excel_extract.fetch_from_ntd_xlsx(
             self.raw_excel_extract.file_url
         )
+        logging.info(
+            f"file url is {self.xlsx_file_url} and file type is {type(self.xlsx_file_url)}"
+        )
 
         self.raw_excel_extract.save_content(fs=get_fs(), content=excel_content)
 
@@ -137,10 +114,8 @@ class NtdDataProductXLSXOperator(BaseOperator):
 
         for key, df in df_dict.items():
             df = df.rename(make_name_bq_safe, axis="columns")
-            typer.secho(
-                f"read {df.shape[0]} rows and {df.shape[1]} columns",
-                fg=typer.colors.MAGENTA,
-            )
+
+            logging.info(f"read {df.shape[0]} rows and {df.shape[1]} columns")
 
             self.clean_gzipped_content = gzip.compress(
                 df.to_json(orient="records", lines=True).encode()
@@ -150,7 +125,7 @@ class NtdDataProductXLSXOperator(BaseOperator):
 
             tab_name = make_name_bq_safe(key)
 
-            self.clean_excel_extract = NtdDataProductXLSXExtractClean(
+            self.clean_excel_extract = NtdDataProductXLSXExtract(
                 year=self.year,
                 product=self.product + "/" + tab_name,
                 filename=f"{self.year}__{self.product}__{tab_name}.jsonl.gz",
