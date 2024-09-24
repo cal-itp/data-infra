86ab19e13acdc41cfb04cf8cf979da068db747d0
Charlie Costanzo <cvc5185@gmail.com>
2024-09-24 14:15:04 -0400

    Bring new NTD endpoint sources into the warehouse as staging (#3467)
    * initial dbt yml for stg and src, as well as initial staging tables for 2022 nts data
    
    * take most recent extract on execution_ts
    
    * revise comment with accurate description of sync
    
    * added mvp src, stg, and models for ntd ridership data
    
    * revised ridership source schema name that contained a typo

---
+++ warehouse/models/staging/ntd_annual_data_tables/2022/stg_ntd_annual_data__2022__service_by_agency.sql
@@ -0,0 +1,19 @@
+WITH external_2022_service_by_agency AS (
+    SELECT *
+    FROM {{ source('external_ntd__annual_reporting', '2022__service_by_agency') }}
+),
+
+get_latest_extract AS(
+
+    SELECT *
+    FROM external_2022_service_by_agency
+    -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+    QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+),
+
+stg_ntd_annual_data__2022__service_by_agency AS (
+    SELECT *
+    FROM get_latest_extract
+)
+
+SELECT * FROM stg_ntd_annual_data__2022__service_by_agency
