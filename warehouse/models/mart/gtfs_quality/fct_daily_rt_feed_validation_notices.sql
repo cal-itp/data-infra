ca9fbf8383c9666e2a28e7adb6d7abd9c69b622a
Doc Ritezel <doc@ministryofvelocity.com>
2024-10-17 15:28:24 -0700

    Optimize fct_daily_rt_feed_validation_notices
    [#3481]
    
    Signed-off-by: Erika Pacheco <erika@ministryofvelocity.com>

--- warehouse/models/mart/gtfs_quality/fct_daily_rt_feed_validation_notices.sql
+++ warehouse/models/mart/gtfs_quality/fct_daily_rt_feed_validation_notices.sql
@@ -1,11 +1,16 @@
 {{ config(materialized='table') }}
 
-WITH fct_daily_rt_feed_files AS (
-    SELECT * FROM {{ ref('fct_daily_rt_feed_files') }}
-),
-
-codes AS (
-    SELECT * FROM {{ ref('stg_gtfs_quality__rt_validation_code_descriptions') }}
+WITH daily_feeds_codes AS (
+    SELECT
+        daily_feeds.date,
+        daily_feeds.base64_url,
+        daily_feeds.key,
+        daily_feeds.parse_success_file_count,
+        codes.code,
+        codes.description,
+        codes.is_critical
+    FROM {{ ref('fct_daily_rt_feed_files') }} AS daily_feeds
+    CROSS JOIN {{ ref('stg_gtfs_quality__rt_validation_code_descriptions') }} AS codes
 ),
 
 outcomes AS (
@@ -16,42 +21,46 @@ notices AS (
     SELECT * FROM {{ ref('int_gtfs_quality__rt_validation_notices') }}
 ),
 
--- This CTE starts with daily RT feeds, then pulls in validation outcomes
--- to give us a row per actual validation execution (outcome) that we have;
--- then cross-join codes to give us "buckets" per outcome since the
--- underlying notices will not contain a code that did not trigger a notice;
--- finally, we aggregate everything back up to the date/URL/code level
--- and count how many parsed and validated files we have, and sum the
--- notice occurrences; if we have successful validations but no occurrences,
--- we assume the feeds passed the code 100% of the time
+-- This table reports on each entry in the daily GTFS-RT feeds table.
+-- For each feed, on each day, create a row for the codes table, which
+-- contains a potential validation error with its description and error level.
+-- For each feed/day/code, retrieve outcomes and notices, then report on
+-- the total number of extractions, successes, exceptions, occurrences of
+-- that specific code, and the GTFS-RT validator version used.
+-- If a code does not appear for a feed on a day, total_notices will be 0.
+-- If there a feed was not validated on a day, total_notices will be NULL.
 fct_daily_rt_feed_validation_notices AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['daily_feeds.date', 'daily_feeds.base64_url', 'codes.code', 'codes.is_critical']) }} AS key,
-        daily_feeds.date,
-        daily_feeds.base64_url,
-        codes.code,
-        codes.description,
-        codes.is_critical,
+        {{ dbt_utils.generate_surrogate_key(['daily_feeds_codes.date', 'daily_feeds_codes.base64_url', 'daily_feeds_codes.code', 'daily_feeds_codes.is_critical']) }} AS key,
+        daily_feeds_codes.date,
+        daily_feeds_codes.base64_url,
+        daily_feeds_codes.code,
+        daily_feeds_codes.description,
+        daily_feeds_codes.is_critical,
         -- TODO: at some point, these codes will be versioned by validator version
         ARRAY_AGG(DISTINCT notices.gtfs_validator_version IGNORE NULLS) AS gtfs_validator_versions,
-        SUM(daily_feeds.parse_success_file_count) / COUNT(daily_feeds.key) AS parsed_files,
+        SUM(daily_feeds_codes.parse_success_file_count) / COUNT(daily_feeds_codes.key) AS parsed_files,
         COUNT(DISTINCT outcomes.extract_ts) AS validated_extracts,
         COUNTIF(outcomes.validation_success) AS validation_successes,
         COUNT(outcomes.validation_exception) AS validation_exceptions,
         COALESCE(
-            SUM(ARRAY_LENGTH(occurrence_list)),
-            CASE WHEN COUNTIF(outcomes.validation_success) > 0 THEN 0 END
-        ) AS total_notices,
-    FROM fct_daily_rt_feed_files AS daily_feeds
+          SUM(ARRAY_LENGTH(notices.occurrence_list)),
+          CASE WHEN COUNTIF(outcomes.validation_success) > 0 THEN 0 END
+        ) AS total_notices
+    FROM daily_feeds_codes
     LEFT JOIN outcomes
-        ON daily_feeds.base64_url = outcomes.base64_url
-        AND daily_feeds.date = EXTRACT(DATE FROM outcomes.extract_ts)
-    CROSS JOIN codes
+         ON daily_feeds_codes.base64_url = outcomes.base64_url
+         AND daily_feeds_codes.date = EXTRACT(DATE FROM outcomes.extract_ts)
     LEFT JOIN notices
-        ON outcomes.extract_ts = notices.ts
-        AND outcomes.base64_url = notices.base64_url
-        AND codes.code = notices.error_message_validation_rule_error_id
-    GROUP BY 1, 2, 3, 4, 5, 6
+         ON outcomes.extract_ts = notices.ts
+         AND daily_feeds_codes.base64_url = notices.base64_url
+         AND daily_feeds_codes.code = notices.error_message_validation_rule_error_id
+    GROUP BY
+        daily_feeds_codes.date,
+        daily_feeds_codes.base64_url,
+        daily_feeds_codes.code,
+        daily_feeds_codes.description,
+        daily_feeds_codes.is_critical
 )
 
 SELECT * FROM fct_daily_rt_feed_validation_notices
