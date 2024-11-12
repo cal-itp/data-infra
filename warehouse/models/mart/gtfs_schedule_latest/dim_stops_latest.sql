0af175908240237f8d7da6fe78dc3b8cdebbc095
Charles Costanzo <cvc5185@gmail.com>
2024-11-12 11:45:40 -0500

    add initial source, staging, and docs for state geoportal scrape

--- warehouse/models/mart/gtfs_schedule_latest/dim_stops_latest.sql
+++ warehouse/models/mart/gtfs_schedule_latest/dim_stops_latest.sql
@@ -4,29 +4,6 @@ dim_stops_latest AS (
     table_name = ref('dim_stops'),
     clean_table_name = 'dim_stops'
     ) }}
-),
-
-buffer_geometry_table AS (
-    SELECT
-        ST_BUFFER(geometry,
-            30.48) AS buffer_geometry
-    FROM {{ ref('state_highway_network') }}
-),
-
-
-current_stops AS (
-    SELECT
-        pt_geom,
-        key
-    FROM {{ ref('dim_stops_latest') }}
 )
 
-
-SELECT
-    current_stops.*
-FROM buffer_geometry_table, current_stops
-WHERE ST_DWITHIN(
-        buffer_geometry_table.buffer_geometry,current_stops.pt_geom, 0)
-
-
--- SELECT * FROM dim_stops_latest
+SELECT * FROM dim_stops_latest
