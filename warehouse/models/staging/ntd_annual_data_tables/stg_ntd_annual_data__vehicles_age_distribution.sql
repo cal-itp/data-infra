7b2d4b1275fd19923e98dec781f59a9b0272764a
Charles Costanzo <cvc5185@gmail.com>
2024-11-13 16:17:36 -0500

    replace all staging tables with new year-agnostic naming

---
+++ warehouse/models/staging/ntd_annual_data_tables/stg_ntd_annual_data__vehicles_age_distribution.sql
@@ -0,0 +1,19 @@
+WITH external_vehicles_age_distribution AS (
+    SELECT *
+    FROM {{ source('external_ntd__annual_reporting', 'multi_year__vehicles_age_distribution') }}
+),
+
+get_latest_extract AS(
+
+    SELECT *
+    FROM external_vehicles_age_distribution
+    -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+    QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+),
+
+stg_ntd_annual_data__vehicles_age_distribution AS (
+    SELECT *
+    FROM get_latest_extract
+)
+
+SELECT * FROM stg_ntd_annual_data__vehicles_age_distribution
