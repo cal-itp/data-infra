51db7d0aecd9ff2beeca84d5eecc564c4b3be240
Charles Costanzo <cvc5185@gmail.com>
2024-11-13 13:08:40 -0500

    dag to scrape the current ridership URL from the NTD portal

---
+++ airflow/dags/scrape-ntd-ridership-xlsx-url/scrape-ntd-ridership-xlsx-url.py
@@ -0,0 +1,40 @@
+# ---
+# python_callable: scrape_ntd_ridership_xlsx_url
+# provide_context: true
+# ---
+import logging
+
+import requests
+from bs4 import BeautifulSoup
+from pydantic import HttpUrl, parse_obj_as
+
+from airflow.models import Variable
+
+
+def scrape_ntd_ridership_xlsx_url():
+    # page to find download URL on
+    url = "https://www.transit.dot.gov/ntd/data-product/monthly-module-raw-data-release"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    # Look for an anchor tag where the href ends with '.xlsx' and starts with '/sites/fta.dot.gov/files/'
+    link = soup.find(
+        "a",
+        href=lambda href: href
+        and href.startswith("/sites/fta.dot.gov/files/")
+        and href.endswith(".xlsx"),
+    )
+
+    # Extract the href if the link is found
+    file_link = link["href"] if link else None
+
+    updated_url = "https://www.transit.dot.gov" + file_link
+    # print('https://www.transit.dot.gov' + file_link)
+
+    validated_url = parse_obj_as(HttpUrl, updated_url)
+
+    logging.info(f"Validated URL: {validated_url}.")
+
+    # Set or overwrite the variable
+    # Will this work?
+    Variable.set("current_ntd_ridership_url", validated_url)
