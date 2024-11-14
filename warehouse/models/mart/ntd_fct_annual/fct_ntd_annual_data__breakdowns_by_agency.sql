7d312098e3c34cea5de8f0564da6020ad90d5b66
Charles Costanzo <cvc5185@gmail.com>
2024-11-14 16:48:20 -0500

    test commit

--- warehouse/models/mart/ntd_fct_annual/fct_ntd_annual_data__breakdowns_by_agency.sql
+++ warehouse/models/mart/ntd_fct_annual/fct_ntd_annual_data__breakdowns_by_agency.sql
@@ -2,7 +2,7 @@ WITH staging_breakdowns_by_agency AS (
     SELECT *
     FROM {{ ref('staging', 'stg_ntd_annual_data__breakdowns_by_agency') }}
 ),
-
+-- test
 fct_ntd_annual_data__breakdowns_by_agency AS (
     SELECT *
     FROM staging_breakdowns_by_agency
