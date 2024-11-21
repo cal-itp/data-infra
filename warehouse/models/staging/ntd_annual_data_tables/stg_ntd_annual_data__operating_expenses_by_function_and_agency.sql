c280e487df82e438f6dbac1de7f72b896dc2798f
Charlie Costanzo <cvc5185@gmail.com>
2024-11-21 16:13:17 -0500

    Rename ntd api tables (#3546)
    * make ntd table API sync for annual data year-agnostic
    
    * revise sync dag ymls
    
    * rename paths and destination tables in external table config, rename files
    
    * replace all staging tables with new year-agnostic naming
    
    * replace the staging tables referenced in ntd mart tables with new year-agnostic naming convention
    
    * revise testing for year values to include the year 2023
    
    * revise ntd annual reporting mart table documentation to no longer include references to the year 2022 specifically

---
+++ warehouse/models/staging/ntd_annual_data_tables/stg_ntd_annual_data__operating_expenses_by_function_and_agency.sql
@@ -0,0 +1,19 @@
+WITH external_operating_expenses_by_function_and_agency AS (
+    SELECT *
+    FROM {{ source('external_ntd__annual_reporting', 'multi_year__operating_expenses_by_function_and_agency') }}
+),
+
+get_latest_extract AS(
+
+    SELECT *
+    FROM external_operating_expenses_by_function_and_agency
+    -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+    QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+),
+
+stg_ntd_annual_data__operating_expenses_by_function_and_agency AS (
+    SELECT *
+    FROM get_latest_extract
+)
+
+SELECT * FROM stg_ntd_annual_data__operating_expenses_by_function_and_agency
---
+++ warehouse/models/staging/ntd_annual_data_tables/stg_ntd_annual_data__service_by_mode_and_time_period.sql
@@ -0,0 +1,19 @@
+WITH external_service_by_mode_and_time_period AS (
+    SELECT *
+    FROM {{ source('external_ntd__annual_reporting', 'multi_year__service_by_mode_and_time_period') }}
+),
+
+get_latest_extract AS(
+
+    SELECT *
+    FROM external_service_by_mode_and_time_period
+    -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+    QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+),
+
+stg_ntd_annual_data__service_by_mode_and_time_period AS (
+    SELECT *
+    FROM get_latest_extract
+)
+
+SELECT * FROM stg_ntd_annual_data__service_by_mode_and_time_period
