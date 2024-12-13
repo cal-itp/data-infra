1b379540468f5a9fb48f8394e354a167933c5bce
Charles Costanzo <cvc5185@gmail.com>
2024-12-13 16:00:27 -0500

    add mart tables

---
+++ warehouse/models/mart/ntd_fct_annual/fct_ntd_annual_data__2023_agency_information.sql
@@ -0,0 +1,11 @@
+WITH staging_agency_information AS (
+    SELECT *
+    FROM {{ ref('stg_ntd_annual_data__2023_agency_information') }}
+),
+
+fct_ntd_annual_data__2023_agency_information AS (
+    SELECT *
+    FROM staging_agency_information
+)
+
+SELECT * FROM fct_ntd_annual_data__2023_agency_information
