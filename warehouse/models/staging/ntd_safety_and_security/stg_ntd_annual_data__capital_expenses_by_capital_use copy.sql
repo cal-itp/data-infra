df160e3737b41049b56c2f2e54a4f114dd42b263
Charles Costanzo <cvc5185@gmail.com>
2024-12-12 21:21:24 -0500

    bringing ntd safety anf security tables into the warehouse as staging and mart

---
+++ warehouse/models/staging/ntd_safety_and_security/stg_ntd_annual_data__capital_expenses_by_capital_use copy.sql
@@ -0,0 +1,19 @@
+WITH external_capital_expenses_by_capital_use AS (
+    SELECT *
+    FROM {{ source('external_ntd__safety_and_security', 'historical__fra_regulated_mode_major_security_events') }}
+),
+
+get_latest_extract AS(
+
+    SELECT *
+    FROM external_capital_expenses_by_capital_use
+    -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+    QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+),
+
+stg_ntd_annual_data__capital_expenses_by_capital_use AS (
+    SELECT *
+    FROM get_latest_extract
+)
+
+SELECT * FROM stg_ntd_annual_data__capital_expenses_by_capital_use
