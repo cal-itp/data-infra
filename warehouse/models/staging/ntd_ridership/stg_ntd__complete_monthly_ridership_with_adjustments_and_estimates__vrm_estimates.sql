13e87815545898b9e61af494085350dc113590f8
Charles Costanzo <cvc5185@gmail.com>
2024-12-16 10:06:18 -0500

    new naming conventions for ridership tables

--- warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates.sql
+++ warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates.sql
@@ -4,11 +4,11 @@ WITH
           FROM {{ source('external_ntd__ridership', 'historical__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates') }}
     ),
 
-    stg_ntd_ridership_historical__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates AS(
+    stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates AS(
         SELECT *
           FROM source
         -- we pull the whole table every month in the pipeline, so this gets only the latest extract
         QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
     )
 
-SELECT * FROM stg_ntd_ridership_historical__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates
+SELECT * FROM stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__vrm_estimates
