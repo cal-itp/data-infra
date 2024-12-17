782953173e68a619ade4a2714b3546aed0f64649
Charles Costanzo <cvc5185@gmail.com>
2024-12-16 19:19:20 -0500

    add explicit column names for mart ntd safety and security tables

--- warehouse/models/mart/ntd_safety_and_security/fct_major_safety_events.sql
+++ warehouse/models/mart/ntd_safety_and_security/fct_major_safety_events.sql
@@ -8,4 +8,27 @@ fct_major_safety_events AS (
     FROM staging_major_safety_events
 )
 
-SELECT * FROM fct_major_safety_events
+SELECT
+    other,
+    worker,
+    minor_nonphysical_assaults_on_other_transit_workers,
+    total_injuries,
+    non_physical_assaults_on_operators_security_events_only_,
+    total_incidents,
+    minor_physical_assaults_on_operators,
+    minor_physical_assaults_on_other_transit_workers,
+    location_group,
+    location,
+    eventtype,
+    additional_assault_information,
+    sftsecfl,
+    yr,
+    modecd,
+    mo,
+    typeofservicecd,
+    reportername,
+    customer,
+    ntdid,
+    dt,
+    execution_ts
+FROM fct_major_safety_events
