08831085951b0c9ab9702c2f877a9a5ff233c9a5
Charles Costanzo <cvc5185@gmail.com>
2024-12-16 19:19:00 -0500

    add explicit column names for staging ntd safety and security tables

--- warehouse/models/staging/ntd_safety_and_security/stg_ntd__major_safety_events.sql
+++ warehouse/models/staging/ntd_safety_and_security/stg_ntd__major_safety_events.sql
@@ -1,10 +1,9 @@
 WITH external_major_safety_events AS (
     SELECT *
-    FROM {{ source('external_ntd__safety_and_security', 'historical__major_safety_events') }}
+    FROM {{ source('external_ntd__safety_and_security', 'multi_year__major_safety_events') }}
 ),
 
 get_latest_extract AS(
-
     SELECT *
     FROM external_major_safety_events
     -- we pull the whole table every month in the pipeline, so this gets only the latest extract
@@ -16,4 +15,27 @@ stg_ntd__major_safety_events AS (
     FROM get_latest_extract
 )
 
-SELECT * FROM stg_ntd__major_safety_events
+SELECT
+    other,
+    worker,
+    minor_nonphysical_assaults_on_other_transit_workers,
+    total_injuries,
+    non_physical_assaults_on_operators_security_events_only_,
+    total_incidents,
+    minor_physical_assaults_on_operators,
+    minor_physical_assaults_on_other_transit_workers,
+    location_group,
+    location,
+    eventtype,
+    additional_assault_information,
+    sftsecfl,
+    yr,
+    modecd,
+    mo,
+    typeofservicecd,
+    reportername,
+    customer,
+    ntdid,
+    dt,
+    execution_ts
+FROM stg_ntd__major_safety_events
