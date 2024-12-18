d3f09148698da07ec5c3cc7fd19b8c4fb096cd1e
Charles Costanzo <cvc5185@gmail.com>
2024-12-18 14:38:24 -0500

    expand dynamic ntd url scrape to cover agency data for multiple years

--- airflow/dags/sync_ntd_data_xlsx/scrape_ntd_xlsx_urls.py
+++ airflow/dags/sync_ntd_data_xlsx/scrape_ntd_xlsx_urls.py
@@ -10,7 +10,8 @@ from pydantic import HttpUrl, parse_obj_as
 
 xlsx_urls = {
     "ridership_url": "https://www.transit.dot.gov/ntd/data-product/monthly-module-raw-data-release",
-    "agency_url": "https://www.transit.dot.gov/ntd/data-product/2023-annual-database-agency-information",
+    "2022_agency_url": "https://www.transit.dot.gov/ntd/data-product/2022-annual-database-agency-information",
+    "2023_agency_url": "https://www.transit.dot.gov/ntd/data-product/2023-annual-database-agency-information",
     "contractual_relationship_url": "https://www.transit.dot.gov/ntd/data-product/2023-annual-database-contractual-relationship",
 }
 
@@ -46,36 +47,3 @@ def scrape_ntd_xlsx_urls(**context):
         logging.info(f"Validated URL: {validated_url}.")
 
         push_url_to_xcom(key=key, scraped_url=validated_url, context=context)
-
-
-# # pushes the scraped URL value to XCom
-# def push_url_to_xcom(scraped_url, context):
-#     task_instance = context["ti"]
-#     task_instance.xcom_push(key="current_url", value=scraped_url)
-
-
-# # Look for an anchor tag where the href ends with '.xlsx' and starts with '/sites/fta.dot.gov/files/'
-# def href_matcher(href):
-#     return (
-#         href and href.startswith("/sites/fta.dot.gov/files/") and href.endswith(".xlsx")
-#     )
-
-
-# def scrape_ntd_xlsx_urls(**context):
-#     # page to find download URL
-#     url = "https://www.transit.dot.gov/ntd/data-product/monthly-module-raw-data-release"
-#     req = requests.get(url)
-#     soup = BeautifulSoup(req.text, "html.parser")
-
-#     link = soup.find("a", href=href_matcher)
-
-#     # Extract the href if the link is found
-#     file_link = link["href"] if link else None
-
-#     updated_url = f"https://www.transit.dot.gov{file_link}"
-
-#     validated_url = parse_obj_as(HttpUrl, updated_url)
-
-#     logging.info(f"Validated URL: {validated_url}.")
-
-#     push_url_to_xcom(scraped_url=validated_url, context=context)
