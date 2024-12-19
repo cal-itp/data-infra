36c4bebcce69acf3280f432475ed9bf2fd666707
Charlie Costanzo <cvc5185@gmail.com>
2024-12-18 22:34:25 -0500

    NTD: staging and mart tables for 2022 agency data (#3605)
    * add new tables to yml files
    
    * add new staging and mart tables for 2022 agency information
    
    * fix duplicate entries in yml file

---
+++ warehouse/models/mart/ntd_annual_reporting/dim_2022_agency_information.sql
@@ -0,0 +1,56 @@
+WITH staging_agency_information AS (
+    SELECT *
+    FROM {{ ref('stg_ntd__2022_agency_information') }}
+),
+
+dim_2022_agency_information AS (
+    SELECT *
+    FROM staging_agency_information
+)
+
+SELECT
+    number_of_state_counties,
+    tam_tier,
+    personal_vehicles,
+    uza_name,
+    tribal_area_name,
+    service_area_sq_miles,
+    voms_do,
+    url,
+    region,
+    state_admin_funds_expended,
+    zip_code_ext,
+    zip_code,
+    ueid,
+    address_line_2,
+    number_of_counties_with_service,
+    reporter_acronym,
+    original_due_date,
+    sq_miles,
+    address_line_1,
+    p_o__box,
+    fy_end_date,
+    service_area_pop,
+    state,
+    subrecipient_type,
+    population,
+    reporting_module,
+    volunteer_drivers,
+    doing_business_as,
+    reporter_type,
+    legacy_ntd_id,
+    total_voms,
+    primary_uza_uace_code,
+    reported_by_name,
+    fta_recipient_id,
+    city,
+    voms_pt,
+    organization_type,
+    agency_name,
+    ntd_id,
+    reported_by_ntd_id,
+    density,
+    state_parent_ntd_id,
+    dt,
+    execution_ts
+FROM dim_2022_agency_information
