fa511f5c292ab28cea53f217af35256086cd14fd
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-01-11 18:06:50 +0000

    use st_equals for checking if location is the same

--- warehouse/models/mart/gtfs/fct_vehicle_locations_dwell.sql
+++ warehouse/models/mart/gtfs/fct_vehicle_locations_dwell.sql
@@ -10,92 +10,82 @@ WITH fct_vehicle_locations AS (
         next_location_key,
 
     FROM {{ ref('fct_vehicle_locations') }}
-    WHERE gtfs_dataset_name="LA DOT VehiclePositions" AND service_date = "2025-01-07" --AND trip_instance_key="0000a63ce280462e6eed4f3ae92df16d"
+    WHERE gtfs_dataset_name="LA DOT VehiclePositions" AND service_date = "2025-01-07" AND (trip_instance_key="0000a63ce280462e6eed4f3ae92df16d" OR trip_instance_key="ec14aa25e209f90ed025450c18519814")
     ORDER by service_date, trip_instance_key, location_timestamp
     ),
 
-current_vp AS (
-    SELECT
-        service_date,
-        trip_instance_key,
-        key,
-        location,
-        location_timestamp,
-        next_location_key,
-    FROM fct_vehicle_locations
-),
 
 get_next AS (
     SELECT
         key AS next_location_key,
         location,
         location_timestamp
-    FROM current_vp
+    FROM fct_vehicle_locations
 ),
 
 vp_groupings AS (
     SELECT
-        current_vp.service_date,
-        current_vp.trip_instance_key,
-        current_vp.key,
-        current_vp.location_timestamp,
+        fct_vehicle_locations.service_date,
+        fct_vehicle_locations.trip_instance_key,
+        fct_vehicle_locations.key,
+        --fct_vehicle_locations.location,
+        fct_vehicle_locations.location_timestamp,
         get_next.location_timestamp AS next_location_timestamp,
-        ST_X(current_vp.location) AS current_longitude,
-        ST_Y(current_vp.location) AS current_latitude,
-        ST_X(get_next.location) AS next_longitude,
-        ST_Y(get_next.location) AS next_latitude,
-    FROM current_vp
-    LEFT JOIN get_next
-        ON current_vp.next_location_key = get_next.next_location_key
-),
-
-vp_grouping_agg AS (
-    SELECT
-        vp_groupings.service_date,
-        vp_groupings.trip_instance_key,
-        vp_groupings.key,
-        vp_groupings.location_timestamp,
-        vp_groupings.current_longitude,
-        vp_groupings.next_longitude,
-        vp_groupings.current_latitude,
-        vp_groupings.next_latitude,
+        --get_next.location AS next_location,
         CASE
-            WHEN vp_groupings.current_longitude = vp_groupings.next_longitude AND vp_groupings.current_latitude = vp_groupings.next_latitude
+            WHEN ST_EQUALS(fct_vehicle_locations.location, get_next.location)
             THEN 0
             ELSE 1
         END AS new_group,
-    FROM vp_groupings
+    FROM fct_vehicle_locations
+    LEFT JOIN get_next
+        ON fct_vehicle_locations.next_location_key = get_next.next_location_key
 ),
 
 grouped AS (
     SELECT
         key,
-        vp_grouping_agg.current_longitude,
-        vp_grouping_agg.current_latitude,
         SUM(new_group)
             OVER (
                 PARTITION BY service_date, trip_instance_key
                 ORDER BY location_timestamp
                 ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
             )  AS vp_group,
-    FROM vp_grouping_agg
+    FROM vp_groupings
 ),
 
 merged AS (
     SELECT
-        current_vp.trip_instance_key AS trip_instance_key,
-        current_vp.service_date AS service_date,
-        MIN(current_vp.key) AS key,
+        fct_vehicle_locations.trip_instance_key AS trip_instance_key,
+        fct_vehicle_locations.service_date AS service_date,
+        MIN(fct_vehicle_locations.key) AS key,
         grouped.vp_group AS vp_group,
-        COUNT(current_vp.key) AS n_vp,
-        MIN(current_vp.location_timestamp) AS location_timestamp,
-        MAX(current_vp.location_timestamp) AS moving_timestamp,
-        ST_GEOGPOINT(grouped.current_longitude, grouped.current_latitude) AS location,
-    FROM current_vp
-    INNER JOIN grouped
-        ON current_vp.key = grouped.key
-    GROUP BY service_date, trip_instance_key, vp_group, current_longitude, current_latitude
-    ORDER BY service_date, trip_instance_key, vp_group, location_timestamp
+        COUNT(*) AS n_vp,
+        MIN(fct_vehicle_locations.location_timestamp) AS location_timestamp,
+        MAX(fct_vehicle_locations.location_timestamp) AS moving_timestamp,
+    FROM fct_vehicle_locations
+    LEFT JOIN grouped
+        ON fct_vehicle_locations.key = grouped.key
+    GROUP BY service_date, trip_instance_key, vp_group
+),
+
+fct_vehicle_locations_dwell AS (
+    SELECT
+        merged.trip_instance_key AS trip_instance_key,
+        merged.service_date AS service_date,
+        merged.key AS key,
+        merged.vp_group as vp_group, -- this column can be deleted, but use this to check, because vp_group=11 should have 2, but sometimes has count of 1.
+        merged.n_vp as n_vp,
+        merged.location_timestamp AS location_timestamp,
+        merged.moving_timestamp AS moving_timestamp,
+        fct_vehicle_locations.location AS location
+    FROM merged
+    LEFT JOIN fct_vehicle_locations
+        ON merged.key = fct_vehicle_locations.key
 )
 
-SELECT * FROM merged
+-- can we roll in n_vp + location into merged
+-- without the last merge? how to group with location (geography) type?
+-- expect to see n_vp have values 1, 2, 3 for these 2 trips
+
+SELECT * FROM fct_vehicle_locations_dwell
