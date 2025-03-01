7a1ecbc36e877d48c6669fbe7f691f23c0fb00eb
Erika Pacheco <erika@ministryofvelocity.com>
2025-02-28 17:23:10 -0800

    Remove is_gke

--- airflow/plugins/operators/pod_operator.py
+++ airflow/plugins/operators/pod_operator.py
@@ -17,31 +17,26 @@ def PodOperator(*args, **kwargs):
     if "startup_timeout_seconds" not in kwargs:
         kwargs["startup_timeout_seconds"] = 300
 
-    is_gke = kwargs.pop("is_gke", False)  # we want to always pop()
-
     if "secrets" in kwargs:
         kwargs["secrets"] = map(lambda d: Secret(**d), kwargs["secrets"])
 
-    # do we ever have non-GKE pods?
-    if os.environ["AIRFLOW_ENV"] == "development" or is_gke:
-        return GKEPodOperator(
-            *args,
-            in_cluster=False,
-            project_id="cal-itp-data-infra",  # there currently isn't a staging cluster
-            location=kwargs.pop("pod_location", os.environ["POD_LOCATION"]),
-            cluster_name=kwargs.pop("cluster_name", os.environ["POD_CLUSTER_NAME"]),
-            namespace=namespace,
-            image_pull_policy="Always",
-            **kwargs,
-        )
+    location = kwargs.pop("pod_location", os.environ["POD_LOCATION"])
+    cluster_name = kwargs.pop("cluster_name", os.environ["POD_CLUSTER_NAME"])
 
-    else:
-        return KubernetesPodOperator(*args, namespace=namespace, **kwargs)
+    return GKEPodOperator(
+        *args,
+        in_cluster=False,
+        project_id="cal-itp-data-infra",  # there currently isn't a staging cluster
+        location=location,
+        cluster_name=cluster_name,
+        namespace=namespace,
+        image_pull_policy="Always",
+        **kwargs,
+    )
 
 
 PodOperator._gusty_parameters = (
     *inspect.signature(KubernetesPodOperator.__init__).parameters.keys(),
-    "is_gke",
     "pod_location",
     "cluster_name",
 )
