9e46eef98bfde4fae46213793ae29880e1ab09bb
Erika Pacheco <erika@ministryofvelocity.com>
2025-03-04 17:42:53 -0800

    Add payments cosmos dag
    Signed-off-by: Doc Ritezel <doc@ministryofvelocity.com>

---
+++ airflow/dags/cosmos_payment_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from cosmos import DbtDag, ProfileConfig, ProjectConfig, RenderConfig
+
+cosmos_payment = DbtDag(
+    # dbt/cosmos-specific parameters
+    project_config=ProjectConfig(dbt_project_path="data/warehouse"),
+    profile_config=ProfileConfig(
+        target_name="staging",
+        profile_name="calitp_warehouse",
+        profiles_yml_filepath="data/warehouse/profiles.yml",
+    ),
+    render_config=RenderConfig(
+        select=[
+            "+path:models/staging/payments+",
+            "+path:models/intermediate/payments+",
+            "+path:models/mart/payments+",
+        ],
+    ),
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="cosmos_payment",
+    tags=["dbt", "payments"],
+)
