a6ea74e0c3e5b000d6d0740d44675642f90d4f54
Erika Pacheco <erika@ministryofvelocity.com>
2025-03-04 17:42:53 -0800

    Add payments cosmos dag
    Signed-off-by: Doc Ritezel <doc@ministryofvelocity.com>

--- iac/cal-itp-data-infra-staging/airflow/us/variables.tf
+++ iac/cal-itp-data-infra-staging/airflow/us/variables.tf
@@ -1,3 +1,22 @@
+locals {
+  composer_files = setunion(
+    fileset("../../../../airflow", "dags/**/*.py"),
+    fileset("../../../../airflow", "dags/**/*.yml"),
+    fileset("../../../../airflow", "dags/**/*.md"),
+    fileset("../../../../airflow", "plugins/**/*.py")
+  )
+
+  warehouse_files = setunion(
+    fileset("../../../../warehouse", "dbt_project.yml"),
+    fileset("../../../../warehouse", "packages.yml"),
+    fileset("../../../../warehouse", "profiles.yml"),
+    fileset("../../../../warehouse", "macros/**/*"),
+    fileset("../../../../warehouse", "models/**/*"),
+    fileset("../../../../warehouse", "seeds/**/*"),
+    fileset("../../../../warehouse", "tests/**/*")
+  )
+}
+
 data "terraform_remote_state" "networks" {
   backend = "gcs"
 
@@ -14,4 +33,24 @@ data "terraform_remote_state" "gcs" {
     bucket = "calitp-staging-gcp-components-tfstate"
     prefix = "cal-itp-data-infra-staging/gcs"
   }
+}
+
+data "google_storage_bucket_object_content" "calitp-staging-dbt-manifest" {
+  name   = "latest/manifest.json"
+  bucket = data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-dbt-artifacts_id
+}
+
+data "google_storage_bucket_object_content" "calitp-staging-dbt-catalog" {
+  name   = "latest/catalog.json"
+  bucket = data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-dbt-artifacts_id
+}
+
+data "google_storage_bucket_object_content" "calitp-staging-dbt-run_results" {
+  name   = "latest/run_results.json"
+  bucket = data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-dbt-artifacts_id
+}
+
+data "google_storage_bucket_object_content" "calitp-staging-dbt-index" {
+  name   = "latest/index.html"
+  bucket = data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-dbt-artifacts_id
 }
