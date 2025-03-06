e3eca81e48ec76d710ccf318df1d8f5030c49647
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-03-06 14:25:17 -0800

    Airflow tests now run with database management

--- airflow/tests/conftest.py
+++ airflow/tests/conftest.py
@@ -1,9 +1,10 @@
-import subprocess
 import datetime
+import os
+import subprocess
+import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "../plugins"))
 
 def pytest_sessionstart(session):
+    subprocess.run(["airflow", "db", "clean", "-y", "--clean-before-timestamp", str(datetime.datetime.now(datetime.timezone.utc))])
     subprocess.run(["airflow", "db", "init"])
-
-def pytest_sessionfinish(session):
-    subprocess.run(["airflow", "db", "clean", "-y", "--clean-before-timestamp", str(datetime.datetime.today())])
