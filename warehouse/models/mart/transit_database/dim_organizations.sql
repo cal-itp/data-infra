4122659f1d3936e7538a6444377fe6298ac60760
Charles Costanzo <cvc5185@gmail.com>
2025-03-07 14:40:59 -0500

    enrich dim_organizations with caltrans district info from improved pattern, remove deprecated airtable caltrans district columns from stg/int tables

--- warehouse/models/mart/transit_database/dim_organizations.sql
+++ warehouse/models/mart/transit_database/dim_organizations.sql
@@ -1,15 +1,37 @@
 {{ config(materialized='table') }}
 
-WITH dim AS (
+WITH int_organizations_dim AS (
     SELECT * FROM {{ ref('int_transit_database__organizations_dim') }}
 ),
 
+<<<<<<< HEAD
 -- This is deprecated, what replaced the source of truth for ntd_id < '2023-05-23' that was previously found in ntd_to_org.ntd_id ??
 -- ntd_agency_to_organization AS (
 --     SELECT * FROM {{ ref('_deprecated__ntd_agency_to_organization') }}
 -- ),
 
 mpo_rtpa AS (
+=======
+bridge_organizations_x_headquarters_county_geography AS (
+    SELECT * FROM {{ ref('bridge_organizations_x_headquarters_county_geography') }}
+),
+
+dim_county_geography AS (
+    SELECT * FROM {{ ref('dim_county_geography') }}
+),
+
+join_caltrans_district AS (
+    SELECT
+        int_organizations_dim.*,
+        dim_county_geography.caltrans_district,
+        dim_county_geography.caltrans_district_name
+    FROM int_organizations_dim
+    LEFT JOIN bridge_organizations_x_headquarters_county_geography ON int_organizations_dim.key = bridge_organizations_x_headquarters_county_geography.organization_key
+        LEFT JOIN dim_county_geography ON bridge_organizations_x_headquarters_county_geography.county_geography_key = dim_county_geography.key
+),
+
+dim_organizations AS (
+>>>>>>> 53e471e4 (enrich dim_organizations with caltrans district info from improved pattern, remove deprecated airtable caltrans district columns from stg/int tables)
     SELECT
         key,
         source_record_id,
@@ -30,6 +52,7 @@ dim_organizations AS (
         itp_id,
         details,
         caltrans_district,
+        caltrans_district_name,
         website,
         reporting_category,
         hubspot_company_record_id,
@@ -55,11 +78,15 @@ dim_organizations AS (
         _valid_from,
         _valid_to
 
+<<<<<<< HEAD
     FROM dim
     LEFT JOIN ntd_agency_to_organization ntd_to_org
         ON source_record_id = ntd_to_org.organization_record_id
     LEFT JOIN mpo_rtpa mr_rtpa ON dim.rtpa = mr_rtpa.source_record_id
     LEFT JOIN mpo_rtpa mr_mpo ON dim.mpo = mr_mpo.source_record_id
+=======
+    FROM join_caltrans_district
+>>>>>>> 53e471e4 (enrich dim_organizations with caltrans district info from improved pattern, remove deprecated airtable caltrans district columns from stg/int tables)
 
 )
 
