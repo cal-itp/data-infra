5ebe0a9ed9b710f183f7e099e9be0ceb1e77ddb3
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-03-18 18:23:29 +0000

    materalize table for sample, only keep 1 test

--- warehouse/models/mart/gtfs/fct_vehicle_locations_grouped.sql
+++ warehouse/models/mart/gtfs/fct_vehicle_locations_grouped.sql
@@ -1,14 +1,6 @@
 {{
     config(
-        materialized='incremental',
-        incremental_strategy='insert_overwrite',
-        partition_by = {
-            'field': 'service_date',
-            'data_type': 'date',
-            'granularity': 'day',
-        },
-        cluster_by=['service_date', 'base64_url'],
-        on_schema_change='append_new_columns'
+        materialized='table'
     )
 }}
 
@@ -25,7 +17,7 @@ WITH fct_vehicle_locations AS (
         location,
         -- rather than using next_location_key, use lag to calculate direction from previous
     FROM {{ ref('fct_vehicle_locations') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
+    WHERE service_date = DATE('2025-02-12') AND base64_url = 'aHR0cHM6Ly9uZXh0Y2NidXMub3JnL2d0ZnNydC92ZWhpY2xlcw=='
 ),
 
 lat_lon AS (
@@ -124,4 +116,4 @@ fct_grouped_locations AS (
     GROUP BY gtfs_dataset_key, base64_url, gtfs_dataset_name, schedule_gtfs_dataset_key, service_date, trip_instance_key, vp_group, ST_ASTEXT(fct_vehicle_locations.location)
 )
 
-SELECT * FROM fct_grouped_locations WHERE trip_instance_key IS NOT NULL
+SELECT * FROM fct_grouped_locations
