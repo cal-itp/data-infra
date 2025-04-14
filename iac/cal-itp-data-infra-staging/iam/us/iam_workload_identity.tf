da5180483a26638d3101578430f0308a4f128777
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-04-14 10:17:04 -0700

    Add Composer service account

--- iac/cal-itp-data-infra-staging/iam/us/iam_workload_identity.tf
+++ iac/cal-itp-data-infra-staging/iam/us/iam_workload_identity.tf
@@ -1,7 +1,3 @@
-locals {
-  github_repository_name = "cal-itp/data-infra"
-}
-
 resource "google_iam_workload_identity_pool" "github-actions--pool" {
   workload_identity_pool_id = "github-actions-pool"
 }
@@ -21,16 +17,4 @@ resource "google_iam_workload_identity_pool_provider" "github-actions--provider"
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
-}
-
-resource "google_service_account_iam_member" "github-actions-terraform" {
-  service_account_id = google_service_account.github-actions-terraform.id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-actions--pool.name}/attribute.repository/${local.github_repository_name}"
-}
-
-resource "google_service_account_iam_member" "github-actions-service-account" {
-  service_account_id = google_service_account.github-actions-service-account.id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-actions--pool.name}/attribute.repository/${local.github_repository_name}"
 }
--- iac/cal-itp-data-infra-staging/iam/us/project_iam_member.tf
+++ iac/cal-itp-data-infra-staging/iam/us/project_iam_member.tf
@@ -239,3 +239,12 @@ resource "google_project_iam_member" "github-actions-service-account" {
   member  = "serviceAccount:${google_service_account.github-actions-service-account.email}"
   project = "cal-itp-data-infra-staging"
 }
+
+resource "google_project_iam_member" "custom_service_account" {
+  for_each = toset([
+    "roles/composer.worker"
+  ])
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.composer-service-account.email}"
+  project = "cal-itp-data-infra-staging"
+}
