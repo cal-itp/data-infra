8a29341c115a8953533568286ab79edc08c08619
Charlie Costanzo <cvc5185@gmail.com>
2025-04-16 14:54:20 -0400

    modify organization tables for enrichment (#3810)
    * modify organization tables for enrichment
    
    * add handling of ntd_agency_info_key
    
    * add new handling for ntd_id
    
    * create latest dim organizations table with caltrans district
    
    * remove deprecated references for ntd_id seed file
    
    * join in ntd_id from ntd agency info table
    
    * use joined in ntd_id from ntd agency info table for CASE/WHEN ntd_id statement
    
    * new ntd enrichment pattern from ntd agency info table
    
    * clarify comments:
    
    * remove comments to assist with review from dim_organizations

--- warehouse/models/mart/transit_database/dim_organizations.sql
+++ warehouse/models/mart/transit_database/dim_organizations.sql
@@ -4,10 +4,6 @@ WITH dim AS (
     SELECT * FROM {{ ref('int_transit_database__organizations_dim') }}
 ),
 
-ntd_agency_to_organization AS (
-    SELECT * FROM {{ ref('_deprecated__ntd_agency_to_organization') }}
-),
-
 mpo_rtpa AS (
     SELECT
         key,
@@ -41,8 +37,9 @@ dim_organizations AS (
         -- use same May 23, 2023 cutover date as `assessment_status` --> `public_currently_operating` in downstream models for consistency
         CASE
             WHEN _valid_from >= '2023-05-23' THEN raw_ntd_id
-            ELSE ntd_to_org.ntd_id
+            ELSE ntd_id
         END AS ntd_id,
+        ntd_agency_info_key,
         ntd_id_2022,
         mr_rtpa.key AS rtpa_key,
         mr_rtpa.name AS rtpa_name,
@@ -55,8 +52,6 @@ dim_organizations AS (
         _valid_to
 
     FROM dim
-    LEFT JOIN ntd_agency_to_organization ntd_to_org
-        ON source_record_id = ntd_to_org.organization_record_id
     LEFT JOIN mpo_rtpa mr_rtpa ON dim.rtpa = mr_rtpa.source_record_id
     LEFT JOIN mpo_rtpa mr_mpo ON dim.mpo = mr_mpo.source_record_id
 
