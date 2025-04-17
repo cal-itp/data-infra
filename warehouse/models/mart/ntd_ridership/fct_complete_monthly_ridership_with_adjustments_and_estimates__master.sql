4bc2264ecca4db32b8c1446e55e7b65a13dc1ff9
Charles Costanzo <cvc5185@gmail.com>
2025-04-17 18:56:41 -0400

    feat: enrich the tables in mart.ntd_ridership with caltrans_district and mild refactoring

--- warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates__master.sql
+++ warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates__master.sql
@@ -3,39 +3,51 @@ WITH staging_master AS (
     FROM {{ ref('stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__master') }}
 ),
 
+current_dim_organizations AS (
+    SELECT
+        ntd_id,
+        caltrans_district AS caltrans_district_current,
+        caltrans_district_name AS caltrans_district_name_current
+    FROM {{ ref('dim_organizations_latest_with_caltrans_district') }}
+),
+
 fct_complete_monthly_ridership_with_adjustments_and_estimates__master AS (
-    SELECT *
-    FROM staging_master
+    SELECT
+        stg.ntd_id,
+        stg.legacy_ntd_id,
+        stg.agency,
+        stg.mode,
+        stg.tos,
+        stg._3_mode,
+        stg.mode_type_of_service_status,
+        stg.reporter_type,
+        stg.organization_type,
+        stg.hq_city,
+        stg.hq_state,
+        stg.uace_cd,
+        stg.uza_name,
+        stg.uza_sq_miles,
+        stg.uza_population,
+        stg.service_area_population,
+        stg.service_area_sq_miles,
+        stg.last_closed_report_year,
+        stg.last_closed_fy_end_month,
+        stg.last_closed_fy_end_year,
+        stg.passenger_miles_fy,
+        stg.unlinked_passenger_trips_fy,
+        stg.avg_trip_length_fy,
+        stg.fares_fy,
+        stg.operating_expenses_fy,
+        stg.avg_cost_per_trip_fy,
+        stg.avg_fares_per_trip_fy,
+
+        orgs.caltrans_district_current,
+        orgs.caltrans_district_name_current,
+
+        stg.dt,
+        stg.execution_ts
+    FROM staging_master AS stg
+    LEFT JOIN current_dim_organizations AS orgs USING (ntd_id)
 )
 
-SELECT
-    ntd_id,
-    legacy_ntd_id,
-    agency,
-    mode,
-    tos,
-    _3_mode,
-    mode_type_of_service_status,
-    reporter_type,
-    organization_type,
-    hq_city,
-    hq_state,
-    uace_cd,
-    uza_name,
-    uza_sq_miles,
-    uza_population,
-    service_area_population,
-    service_area_sq_miles,
-    last_closed_report_year,
-    last_closed_fy_end_month,
-    last_closed_fy_end_year,
-    passenger_miles_fy,
-    unlinked_passenger_trips_fy,
-    avg_trip_length_fy,
-    fares_fy,
-    operating_expenses_fy,
-    avg_cost_per_trip_fy,
-    avg_fares_per_trip_fy,
-    dt,
-    execution_ts
-FROM fct_complete_monthly_ridership_with_adjustments_and_estimates__master
+SELECT * FROM fct_complete_monthly_ridership_with_adjustments_and_estimates__master
--- warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates__vrm.sql
+++ warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates__vrm.sql
@@ -3,9 +3,22 @@ WITH staging_vrm AS (
     FROM {{ ref('stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__vrm') }}
 ),
 
+current_dim_organizations AS (
+    SELECT
+        ntd_id,
+        caltrans_district AS caltrans_district_current,
+        caltrans_district_name AS caltrans_district_name_current
+    FROM {{ ref('dim_organizations_latest_with_caltrans_district') }}
+),
+
 fct_complete_monthly_ridership_with_adjustments_and_estimates__vrm AS (
-    SELECT *
-    FROM staging_vrm
+    SELECT
+        stg.*,
+
+        orgs.caltrans_district_current,
+        orgs.caltrans_district_name_current
+    FROM staging_vrm AS stg
+    LEFT JOIN current_dim_organizations AS orgs USING (ntd_id)
 )
 
 SELECT * FROM fct_complete_monthly_ridership_with_adjustments_and_estimates__vrm
