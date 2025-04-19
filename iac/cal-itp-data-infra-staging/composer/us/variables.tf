7cd67dc987d7d204d471055553e1a9b11b8caca0
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-04-19 10:33:09 -0700

    Set staging composer environment variables from airflow/.staging.env

--- iac/cal-itp-data-infra-staging/composer/us/variables.tf
+++ iac/cal-itp-data-infra-staging/composer/us/variables.tf
@@ -1,12 +1,25 @@
 locals {
+  # This regular expression corresponds to the Python package name specification
+  # https://packaging.python.org/en/latest/specifications/name-normalization/
+  python_package_regex  = "(?P<name>[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9])(?P<version>.*)"
   requirements_txt_path = abspath("../../../../airflow/requirements.txt")
   requirements = tolist([
-    for line in split("\n", trimspace(file(local.requirements_txt_path))):
-      regex("(?P<name>[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9])(?P<version>.*)", line)
+    for line in split("\n", trimspace(file(local.requirements_txt_path))) :
+    regex(local.python_package_regex, line)
   ])
   pypi_packages = tomap({
-    for requirement in local.requirements:
-      requirement.name => requirement.version
+    for requirement in local.requirements :
+    requirement.name => requirement.version
+  })
+
+  env_path = abspath("../../../../airflow/.staging.env")
+  env = tolist([
+    for line in split("\n", trimspace(file(local.env_path))) :
+    regex("(?P<name>[A-Z0-9_]+)=(?P<value>.*)", line)
+  ])
+  env_variables = tomap({
+    for variable in local.env :
+    variable.name => variable.value
   })
 }
 
