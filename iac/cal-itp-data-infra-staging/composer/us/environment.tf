e2afb8f8d1625b0775f906d633f38cf4c0bfd52e
Doc Ritezel <doc@ministryofvelocity.com>
2025-04-23 14:52:59 -0700

    Add staging composer configuration (#3850)
    * Add staging composer configuration
    
    * Set staging composer packages from airflow/requirements.txt
    
    * Set staging composer environment variables from airflow/.staging.env

--- iac/cal-itp-data-infra-staging/composer/us/environment.tf
+++ iac/cal-itp-data-infra-staging/composer/us/environment.tf
@@ -8,12 +8,20 @@ resource "google_composer_environment" "calitp-staging-composer" {
   }
 
   config {
-    software_config {
-      image_version = "composer-2.8.3-airflow-2.6.3"
-    }
-
     node_config {
       service_account = data.terraform_remote_state.iam.outputs.google_service_account_composer-service-account_id
+    }
+
+    software_config {
+      image_version = "composer-2.8.3-airflow-2.6.3"
+
+      airflow_config_overrides = {
+        core-dags_are_paused_at_creation = "True"
+      }
+
+      pypi_packages = local.pypi_packages
+
+      env_variables = local.env_variables
     }
   }
 }
--- iac/cal-itp-data-infra-staging/composer/us/variables.tf
+++ iac/cal-itp-data-infra-staging/composer/us/variables.tf
@@ -1,3 +1,28 @@
+locals {
+  # This regular expression corresponds to the Python package name specification
+  # https://packaging.python.org/en/latest/specifications/name-normalization/
+  python_package_regex  = "(?P<name>[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9])(?P<version>.*)"
+  requirements_txt_path = abspath("../../../../airflow/requirements.txt")
+  requirements = tolist([
+    for line in split("\n", trimspace(file(local.requirements_txt_path))) :
+    regex(local.python_package_regex, line)
+  ])
+  pypi_packages = tomap({
+    for requirement in local.requirements :
+    requirement.name => requirement.version
+  })
+
+  env_path = abspath("../../../../airflow/.staging.env")
+  env = tolist([
+    for line in split("\n", trimspace(file(local.env_path))) :
+    regex("(?P<name>[A-Z0-9_]+)=(?P<value>.*)", line)
+  ])
+  env_variables = tomap({
+    for variable in local.env :
+    variable.name => variable.value
+  })
+}
+
 data "terraform_remote_state" "gcs" {
   backend = "gcs"
 
