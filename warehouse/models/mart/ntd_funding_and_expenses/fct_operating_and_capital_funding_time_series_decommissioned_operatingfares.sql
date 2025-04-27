24c38f83ea71a856d2317e645c010404d4081123
Charles Costanzo <cvc5185@gmail.com>
2025-04-27 15:14:02 -0400

    feat: last two intermediate tables to unpivot, new preferred column order for marts

--- warehouse/models/mart/ntd_funding_and_expenses/fct_operating_and_capital_funding_time_series_decommissioned_operatingfares.sql
+++ warehouse/models/mart/ntd_funding_and_expenses/fct_operating_and_capital_funding_time_series_decommissioned_operatingfares.sql
@@ -1,6 +1,6 @@
-WITH staging_operating_and_capital_funding_time_series_decommissioned_operatingfares AS (
+WITH intermediate_operating_and_capital_funding_time_series_decommissioned_operatingfares AS (
     SELECT *
-    FROM {{ ref('stg_ntd__operating_and_capital_funding_time_series__decommissioned_operatingfares') }}
+    FROM {{ ref('int_ntd__operating_and_capital_funding_time_series_decommissioned_operatingfares') }}
 ),
 
 current_dim_organizations AS (
@@ -13,55 +13,28 @@ current_dim_organizations AS (
 
 fct_operating_and_capital_funding_time_series_decommissioned_operatingfares AS (
     SELECT
-        stg._2017,
-        stg._2016,
-        stg._2014,
-        stg._2012,
-        stg._2010,
-        stg._2009,
-        stg._2008,
-        stg._2007,
-        stg._2005,
-        stg._2013,
-        stg._2002,
-        stg._2006,
-        stg._2000,
-        stg._2004,
-        stg._2003,
-        stg._1999,
-        stg._1997,
-        stg._2011,
-        stg._1995,
-        stg._1994,
-        stg._2017_status,
-        stg.agency_status,
-        stg._1992,
-        stg.uza_population,
-        stg.uza_area_sq_miles,
-        stg._2001,
-        stg._1996,
-        stg._1991,
-        stg.uza,
-        stg.city,
-        stg._1998,
-        stg._2015,
-        stg.primary_uza_name,
-        stg.legacy_ntd_id,
-        stg.census_year,
-        stg._1993,
-        stg.reporting_module,
-        stg.last_report_year,
-        stg.state,
-        stg.reporter_type,
-        stg.agency_name,
-        stg.ntd_id,
+        int.ntd_id,
+        int.city,
+        int.state,
+        int.agency_name,
+        int._2017_status,
+        int.agency_status,
+        int.uza_population,
+        int.uza_area_sq_miles,
+        int.uza,
+        int.primary_uza_name,
+        int.legacy_ntd_id,
+        int.census_year,
+        int.reporting_module,
+        int.last_report_year,
+        int.reporter_type,
 
         orgs.caltrans_district_current,
         orgs.caltrans_district_name_current,
 
-        stg.dt,
-        stg.execution_ts
-    FROM staging_operating_and_capital_funding_time_series_decommissioned_operatingfares AS stg
+        int.dt,
+        int.execution_ts
+    FROM intermediate_operating_and_capital_funding_time_series_decommissioned_operatingfares AS int
     LEFT JOIN current_dim_organizations AS orgs USING (ntd_id)
 )
 
