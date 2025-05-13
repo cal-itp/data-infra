ec1b3411df15d17c625a3e6efe53c2f42ab712dd
Charles Costanzo <cvc5185@gmail.com>
2025-05-12 20:32:08 -0400

    ntd_safety_and_security normalize grouping column names, carry through to mart, enrich from dim agencies table

--- warehouse/models/mart/ntd_safety_and_security/fct_fra_regulated_mode_major_security_events.sql
+++ warehouse/models/mart/ntd_safety_and_security/fct_fra_regulated_mode_major_security_events.sql
@@ -7,27 +7,24 @@ dim_agency_information AS (
     SELECT
         ntd_id,
         year,
+        agency_name,
         city,
         state,
+        caltrans_district_current,
+        caltrans_district_name_current
     FROM {{ ref('dim_agency_information') }}
-),
-
-current_dim_organizations AS (
-    SELECT
-        ntd_id,
-        caltrans_district AS caltrans_district_current,
-        caltrans_district_name AS caltrans_district_name_current
-    FROM {{ ref('dim_organizations_latest_with_caltrans_district') }}
 ),
 
 fct_fra_regulated_mode_major_security_events AS (
     SELECT
-        stg.agency AS agency_name,
         stg.ntd_id,
         stg.year,
 
+        agency.agency_name,
         agency.city,
         agency.state,
+        agency.caltrans_district_current,
+        agency.caltrans_district_name_current,
 
         stg.other_vehicle_action,
         stg.manufacturer_description,
@@ -115,14 +112,10 @@ fct_fra_regulated_mode_major_security_events AS (
         stg.other_fatalities,
         stg.pedestrian_not_in_crosswalk,
         stg.number_of_derailed_cars,
-
-        orgs.caltrans_district_current,
-        orgs.caltrans_district_name_current,
-
+        stg.agency AS source_agency,
         stg.dt,
         stg.execution_ts
     FROM staging_fra_regulated_mode_major_security_events AS stg
-    LEFT JOIN current_dim_organizations AS orgs USING (ntd_id)
     LEFT JOIN dim_agency_information AS agency
         ON stg.ntd_id = agency.ntd_id
             AND stg.year = agency.year
