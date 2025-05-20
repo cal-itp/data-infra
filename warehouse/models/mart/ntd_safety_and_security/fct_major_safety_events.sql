10531e2ed96f059a9dd65b16a49d41c395d4a859
Charles Costanzo <cvc5185@gmail.com>
2025-05-20 15:51:40 -0400

    create content hash columns for tables without clear keying, rename contracts table as fct, other ordering and cleaning

--- warehouse/models/mart/ntd_safety_and_security/fct_major_safety_events.sql
+++ warehouse/models/mart/ntd_safety_and_security/fct_major_safety_events.sql
@@ -17,7 +17,6 @@ dim_agency_information AS (
 
 fct_major_safety_events AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['stg.ntd_id', 'stg.year', 'stg.month', 'stg.mode', 'stg.type_of_service', 'stg.location_group', 'stg.location', 'stg.event_type']) }} AS key,
         stg.ntd_id,
         stg.year,
         stg.month,
@@ -46,7 +45,15 @@ fct_major_safety_events AS (
         stg.sftsecfl,
         stg.reportername,
         stg.dt,
-        stg.execution_ts
+        stg.execution_ts,
+        {{ dbt_utils.generate_surrogate_key(['stg.ntd_id', 'stg.year', 'stg.month',
+            'agency.agency_name', 'agency.city', 'agency.state', 'agency.caltrans_district_current',
+            'agency.caltrans_district_name_current', 'stg.mode', 'stg.type_of_service', 'stg.location_group',
+            'stg.location', 'stg.event_type', 'stg.customer', 'stg.other', 'stg.worker',
+            'stg.minor_nonphysical_assaults_on_other_transit_workers', 'stg.total_injuries',
+            'stg.non_physical_assaults_on_operators_security_events_only_', 'stg.total_incidents',
+            'stg.minor_physical_assaults_on_operators', 'stg.minor_physical_assaults_on_other_transit_workers',
+            'stg.additional_assault_information', 'stg.sftsecfl', 'stg.reportername']) }} AS _content_hash
     FROM staging_major_safety_events AS stg
     LEFT JOIN dim_agency_information AS agency
         ON stg.ntd_id = agency.ntd_id
