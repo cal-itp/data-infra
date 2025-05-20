245e9e0979ce46d9052ebfd134c996104bf5418c
Charles Costanzo <cvc5185@gmail.com>
2025-05-20 14:14:36 -0400

    reformat ntd staging tables for dbt best practices

--- warehouse/models/staging/ntd_annual_reporting/stg_ntd__funding_sources_directly_generated.sql
+++ warehouse/models/staging/ntd_annual_reporting/stg_ntd__funding_sources_directly_generated.sql
@@ -11,36 +11,35 @@ get_latest_extract AS(
 ),
 
 stg_ntd__funding_sources_directly_generated AS (
-    SELECT *
+    SELECT
+        SAFE_CAST(advertising AS NUMERIC) AS advertising,
+        {{ trim_make_empty_string_null('advertising_questionable') }} AS advertising_questionable,
+        {{ trim_make_empty_string_null('agency') }} AS agency,
+        SAFE_CAST(agency_voms AS NUMERIC) AS agency_voms,
+        {{ trim_make_empty_string_null('city') }} AS city,
+        SAFE_CAST(concessions AS NUMERIC) AS concessions,
+        {{ trim_make_empty_string_null('concessions_questionable') }} AS concessions_questionable,
+        SAFE_CAST(fares AS NUMERIC) AS fares,
+        {{ trim_make_empty_string_null('fares_questionable') }} AS fares_questionable,
+        {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
+        {{ trim_make_empty_string_null('organization_type') }} AS organization_type,
+        SAFE_CAST(other AS NUMERIC) AS other,
+        {{ trim_make_empty_string_null('other_questionable') }} AS other_questionable,
+        SAFE_CAST(park_and_ride AS NUMERIC) AS park_and_ride,
+        {{ trim_make_empty_string_null('park_and_ride_questionable') }} AS park_and_ride_questionable,
+        SAFE_CAST(primary_uza_population AS NUMERIC) AS primary_uza_population,
+        SAFE_CAST(purchased_transportation AS NUMERIC) AS purchased_transportation,
+        {{ trim_make_empty_string_null('purchased_transportation_1') }} AS purchased_transportation_1,
+        SAFE_CAST(report_year AS INT64) AS report_year,
+        {{ trim_make_empty_string_null('reporter_type') }} AS reporter_type,
+        {{ trim_make_empty_string_null('state') }} AS state,
+        SAFE_CAST(total AS NUMERIC) AS total,
+        {{ trim_make_empty_string_null('total_questionable') }} AS total_questionable,
+        {{ trim_make_empty_string_null('uace_code') }} AS uace_code,
+        {{ trim_make_empty_string_null('uza_name') }} AS uza_name,
+        dt,
+        execution_ts
     FROM get_latest_extract
 )
 
-SELECT
-    SAFE_CAST(advertising AS NUMERIC) AS advertising,
-    {{ trim_make_empty_string_null('advertising_questionable') }} AS advertising_questionable,
-    {{ trim_make_empty_string_null('agency') }} AS agency,
-    SAFE_CAST(agency_voms AS NUMERIC) AS agency_voms,
-    {{ trim_make_empty_string_null('city') }} AS city,
-    SAFE_CAST(concessions AS NUMERIC) AS concessions,
-    {{ trim_make_empty_string_null('concessions_questionable') }} AS concessions_questionable,
-    SAFE_CAST(fares AS NUMERIC) AS fares,
-    {{ trim_make_empty_string_null('fares_questionable') }} AS fares_questionable,
-    {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
-    {{ trim_make_empty_string_null('organization_type') }} AS organization_type,
-    SAFE_CAST(other AS NUMERIC) AS other,
-    {{ trim_make_empty_string_null('other_questionable') }} AS other_questionable,
-    SAFE_CAST(park_and_ride AS NUMERIC) AS park_and_ride,
-    {{ trim_make_empty_string_null('park_and_ride_questionable') }} AS park_and_ride_questionable,
-    SAFE_CAST(primary_uza_population AS NUMERIC) AS primary_uza_population,
-    SAFE_CAST(purchased_transportation AS NUMERIC) AS purchased_transportation,
-    {{ trim_make_empty_string_null('purchased_transportation_1') }} AS purchased_transportation_1,
-    SAFE_CAST(report_year AS INT64) AS report_year,
-    {{ trim_make_empty_string_null('reporter_type') }} AS reporter_type,
-    {{ trim_make_empty_string_null('state') }} AS state,
-    SAFE_CAST(total AS NUMERIC) AS total,
-    {{ trim_make_empty_string_null('total_questionable') }} AS total_questionable,
-    {{ trim_make_empty_string_null('uace_code') }} AS uace_code,
-    {{ trim_make_empty_string_null('uza_name') }} AS uza_name,
-    dt,
-    execution_ts
-FROM stg_ntd__funding_sources_directly_generated
+SELECT * FROM stg_ntd__funding_sources_directly_generated
--- warehouse/models/staging/ntd_annual_reporting/stg_ntd__operating_expenses_by_function_and_agency.sql
+++ warehouse/models/staging/ntd_annual_reporting/stg_ntd__operating_expenses_by_function_and_agency.sql
@@ -11,29 +11,28 @@ get_latest_extract AS(
 ),
 
 stg_ntd__operating_expenses_by_function_and_agency AS (
-    SELECT *
+    SELECT
+        {{ trim_make_empty_string_null('agency') }} AS agency,
+        {{ trim_make_empty_string_null('city') }} AS city,
+        SAFE_CAST(max_agency_voms AS NUMERIC) AS max_agency_voms,
+        {{ trim_make_empty_string_null('max_organization_type') }} AS max_organization_type,
+        SAFE_CAST(max_primary_uza_population AS NUMERIC) AS max_primary_uza_population,
+        {{ trim_make_empty_string_null('max_reporter_type') }} AS max_reporter_type,
+        {{ trim_make_empty_string_null('max_uace_code') }} AS max_uace_code,
+        {{ trim_make_empty_string_null('max_uza_name') }} AS max_uza_name,
+        {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
+        SAFE_CAST(report_year AS INT64) AS report_year,
+        {{ trim_make_empty_string_null('state') }} AS state,
+        SAFE_CAST(sum_facility_maintenance AS NUMERIC) AS sum_facility_maintenance,
+        SAFE_CAST(sum_general_administration AS NUMERIC) AS sum_general_administration,
+        SAFE_CAST(sum_reduced_reporter_expenses AS NUMERIC) AS sum_reduced_reporter_expenses,
+        SAFE_CAST(sum_separate_report_amount AS NUMERIC) AS sum_separate_report_amount,
+        SAFE_CAST(sum_vehicle_maintenance AS NUMERIC) AS sum_vehicle_maintenance,
+        SAFE_CAST(sum_vehicle_operations AS NUMERIC) AS sum_vehicle_operations,
+        SAFE_CAST(total AS NUMERIC) AS total,
+        dt,
+        execution_ts
     FROM get_latest_extract
 )
 
-SELECT
-    {{ trim_make_empty_string_null('agency') }} AS agency,
-    {{ trim_make_empty_string_null('city') }} AS city,
-    SAFE_CAST(max_agency_voms AS NUMERIC) AS max_agency_voms,
-    {{ trim_make_empty_string_null('max_organization_type') }} AS max_organization_type,
-    SAFE_CAST(max_primary_uza_population AS NUMERIC) AS max_primary_uza_population,
-    {{ trim_make_empty_string_null('max_reporter_type') }} AS max_reporter_type,
-    {{ trim_make_empty_string_null('max_uace_code') }} AS max_uace_code,
-    {{ trim_make_empty_string_null('max_uza_name') }} AS max_uza_name,
-    {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
-    SAFE_CAST(report_year AS INT64) AS report_year,
-    {{ trim_make_empty_string_null('state') }} AS state,
-    SAFE_CAST(sum_facility_maintenance AS NUMERIC) AS sum_facility_maintenance,
-    SAFE_CAST(sum_general_administration AS NUMERIC) AS sum_general_administration,
-    SAFE_CAST(sum_reduced_reporter_expenses AS NUMERIC) AS sum_reduced_reporter_expenses,
-    SAFE_CAST(sum_separate_report_amount AS NUMERIC) AS sum_separate_report_amount,
-    SAFE_CAST(sum_vehicle_maintenance AS NUMERIC) AS sum_vehicle_maintenance,
-    SAFE_CAST(sum_vehicle_operations AS NUMERIC) AS sum_vehicle_operations,
-    SAFE_CAST(total AS NUMERIC) AS total,
-    dt,
-    execution_ts
-FROM stg_ntd__operating_expenses_by_function_and_agency
+SELECT * FROM stg_ntd__operating_expenses_by_function_and_agency
