a6db9191cd3be33b560ad04dfaa1c7b59de6ef2f
Charles Costanzo <cvc5185@gmail.com>
2025-05-20 17:13:04 -0400

    revise date cutover logic

--- warehouse/models/intermediate/payments/littlepay_feed_unions/int_littlepay__unioned_device_transaction_purchases.sql
+++ warehouse/models/intermediate/payments/littlepay_feed_unions/int_littlepay__unioned_device_transaction_purchases.sql
@@ -14,10 +14,10 @@ device_transaction_purchases_v3 AS (
         -- Keep all records for Nevada County Connects and SacRT, these agencies didn't have a competing feed v1 so we keep it all
         instance IN ('nevada-county-connects', 'sacrt')
 
-        -- For the following participants only, keep records after 5/17/2025 (cutover date for agencies that had both feeds at some point)
+        -- For the following participants only, keep records including and after 5/17/2025 (cutover date for agencies that had both feeds at some point)
         OR (
             instance IN ('clean-air-express', 'mendocino-transit-authority', 'ccjpa', 'atn', 'mst', 'lake-transit-authority', 'sbmtd', 'humboldt-transit-authority', 'redwood-coast-transit')
-            AND littlepay_export_date > '2025-05-17'
+            AND littlepay_export_date >= '2025-05-17'
         )
 ),
 
