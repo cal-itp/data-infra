1107b459da13791703e99015e8c60c0f95e7ee79
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-05-27 16:38:30 -0700

    Members of the public see staging dbt docs behind HTTPS [#3884]

--- iac/cal-itp-data-infra-staging/cdn/us/backend_bucket.tf
+++ iac/cal-itp-data-infra-staging/cdn/us/backend_bucket.tf
@@ -1,7 +1,16 @@
+resource "google_compute_managed_ssl_certificate" "calitp-staging" {
+  name = "calitp-staging-certificate"
+
+  managed {
+    domains = ["dbt-docs-staging.dds.dot.ca.gov."]
+  }
+}
+
 resource "google_compute_backend_bucket" "calitp-staging-dbt-docs" {
   name        = "calitp-staging-dbt-docs-backend-bucket"
   bucket_name = data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-dbt-docs_name
   enable_cdn  = true
+
   cdn_policy {
     cache_mode        = "CACHE_ALL_STATIC"
     client_ttl        = 3600
@@ -12,21 +21,58 @@ resource "google_compute_backend_bucket" "calitp-staging-dbt-docs" {
   }
 }
 
-resource "google_compute_url_map" "default" {
-  name            = "http-lb"
+resource "google_compute_url_map" "calitp-staging-https" {
+  name            = "calitp-staging-https-load-balancer"
   default_service = google_compute_backend_bucket.calitp-staging-dbt-docs.id
+
+  host_rule {
+    path_matcher = "dbt-docs"
+    hosts        = ["dbt-docs-staging.dds.dot.ca.gov"]
+  }
+
+  path_matcher {
+    name            = "dbt-docs"
+    default_service = google_compute_backend_bucket.calitp-staging-dbt-docs.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_bucket.calitp-staging-dbt-docs.id
+    }
+  }
 }
 
-resource "google_compute_target_http_proxy" "default" {
-  name    = "http-lb-proxy"
-  url_map = google_compute_url_map.default.id
+resource "google_compute_target_https_proxy" "calitp-staging-https" {
+  name             = "calitp-staging-https-proxy"
+  url_map          = google_compute_url_map.calitp-staging-https.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.calitp-staging.id]
 }
 
-resource "google_compute_global_forwarding_rule" "default" {
-  name                  = "http-lb-forwarding-rule"
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL"
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.default.id
-  ip_address            = data.terraform_remote_state.networks.outputs.google_compute_network_static-load-balancer-address_id
+resource "google_compute_global_forwarding_rule" "calitp-staging-https" {
+  name        = "calitp-staging-https-forwarding-rule"
+  ip_protocol = "TCP"
+  port_range  = 443
+  target      = google_compute_target_https_proxy.calitp-staging-https.id
+  ip_address  = data.terraform_remote_state.networks.outputs.google_compute_network_static-load-balancer-address_id
+}
+
+resource "google_compute_url_map" "calitp-staging-http" {
+  name = "calitp-staging-http-load-balancer"
+
+  default_url_redirect {
+    strip_query    = false
+    https_redirect = true
+  }
+}
+
+resource "google_compute_target_http_proxy" "calitp-staging-http" {
+  name    = "calitp-staging-http-proxy"
+  url_map = google_compute_url_map.calitp-staging-http.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "calitp-staging-http" {
+  name        = "calitp-staging-http-forwarding-rule"
+  ip_protocol = "TCP"
+  port_range  = 80
+  target      = google_compute_target_http_proxy.calitp-staging-http.self_link
+  ip_address  = data.terraform_remote_state.networks.outputs.google_compute_network_static-load-balancer-address_id
 }
