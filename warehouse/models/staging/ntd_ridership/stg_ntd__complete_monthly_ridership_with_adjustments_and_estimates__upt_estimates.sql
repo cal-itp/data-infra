4819086e32f8df338427739d7d57ace281c52604
Charles Costanzo <cvc5185@gmail.com>
2025-06-03 11:35:43 -0400

    reformat ntd ridership staging models

--- warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates.sql
+++ warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates.sql
@@ -1,15 +1,14 @@
-WITH
-    source AS (
-        SELECT *
-          FROM {{ source('external_ntd__ridership', 'historical__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates') }}
-    ),
+WITH source AS (
+  SELECT *
+  FROM {{ source('external_ntd__ridership', 'historical__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates') }}
+),
 
-    stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates AS(
-        SELECT * REPLACE ({{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id)
-        FROM source
-        -- we pull the whole table every month in the pipeline, so this gets only the latest extract
-        QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
-    )
+stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates__upt_estimates AS(
+  SELECT * REPLACE ({{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id)
+  FROM source
+  -- we pull the whole table every month in the pipeline, so this gets only the latest extract
+  QUALIFY DENSE_RANK() OVER (ORDER BY execution_ts DESC) = 1
+)
 
 SELECT
   top_150,
