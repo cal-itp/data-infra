b1a00d16a1d54f1a6c4e29dfe88b2838b2717849
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-06-04 19:18:03 +0000

    get monthly trips + shapes aggregation

--- warehouse/models/mart/gtfs/fct_monthly_route_service_by_timeofday.sql
+++ warehouse/models/mart/gtfs/fct_monthly_route_service_by_timeofday.sql
@@ -1,31 +1,31 @@
 {{ config(materialized='table') }}
 
-WITH dim_gtfs_datasets AS (
-    SELECT * FROM {{ ref('dim_gtfs_datasets') }}
+WITH trips AS (
+    SELECT * FROM {{ ref('fct_scheduled_trips') }}
+    WHERE EXTRACT(YEAR FROM service_date) = 2025 --WHERE service_date < DATE_TRUNC(CURRENT_DATE(), MONTH)
 ),
 
--- keep the most recent of month_last_day
-feeds AS (
+dim_shapes_arrays AS (
     SELECT
-        gtfs_dataset_key as gtfs_key,
-        MAX(LAST_DAY(date, MONTH)) as max_date
-    FROM {{ ref('fct_daily_schedule_feeds') }}
-    WHERE date = LAST_DAY(date, MONTH)
-    GROUP BY 1
+        key,
+        pt_array
+    FROM {{ ref('dim_shapes_arrays') }}
 ),
 
-fct_scheduled_trips AS (
-    SELECT *
-    FROM {{ ref('fct_scheduled_trips') }}
-    INNER JOIN feeds
-        ON feeds.gtfs_key = fct_scheduled_trips.gtfs_dataset_key
-    WHERE fct_scheduled_trips.service_date <= feeds.max_date
-),
-
-extract_trip_date_types AS (
+monthly_trips AS (
 
     SELECT
 
+        gtfs_dataset_key,
+        name,
+        EXTRACT(month FROM service_date) AS month,
+        EXTRACT(year FROM service_date) AS year,
+        CASE
+            WHEN EXTRACT(DAYOFWEEK FROM service_date) = 1 THEN "Sunday"
+            WHEN EXTRACT(DAYOFWEEK FROM service_date) = 7 THEN "Saturday"
+            ELSE "Weekday"
+        END
+        AS day_type,
         CASE
             WHEN EXTRACT(hour FROM trip_first_departure_datetime_pacific) < 4 THEN "Owl"
             WHEN EXTRACT(hour FROM trip_first_departure_datetime_pacific) < 7 THEN "Early AM"
@@ -35,81 +35,38 @@ extract_trip_date_types AS (
             ELSE "Evening"
         END
         AS time_of_day,
-
-        gtfs_dataset_key,
-        route_id,
+        --route_id, # this might change over longer time periods
+        direction_id,
         route_short_name,
         route_long_name,
-        EXTRACT(hour FROM trip_first_departure_datetime_pacific) AS hour,
-        EXTRACT(month FROM service_date) AS month,
-        EXTRACT(year FROM service_date) AS year,
-        EXTRACT(DAYOFWEEK from service_date) AS day_type,
-        service_hours
+        CONCAT(COALESCE(trips.route_short_name, ""), ' ', COALESCE(trips.route_long_name, "")) AS route_name,
+        route_desc,
+        route_color,
+        route_text_color,
+        trip_id,
+        shape_id,
+        shape_array_key,
 
-    FROM fct_scheduled_trips
+        AVG(service_hours) AS service_hours,
+        --SUM(service_hours) AS ttl_service_hours,
+        COUNT(DISTINCT trip_instance_key) as n_trips,
+        COUNT(DISTINCT service_date) as n_days,
+
+
+    FROM trips
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
 
 ),
 
-service_with_daypart AS (
+monthly_trips_with_geom AS (
     SELECT
+        monthly_trips.*,
+        dim_shapes_arrays.pt_array
 
-        dim_gtfs_datasets.name,
-        dim_gtfs_datasets.source_record_id,
-
-        trips.time_of_day,
-        trips.hour,
-        trips.month,
-        trips.year,
-        trips.day_type,
-        trips.route_id,
-        trips.route_short_name,
-        trips.route_long_name,
-        trips.service_hours,
-
-    FROM extract_trip_date_types AS trips
-    LEFT JOIN dim_gtfs_datasets
-        ON (trips.gtfs_dataset_key = dim_gtfs_datasets.key)
-
-),
-
-daypart_aggregations AS (
-    SELECT
-
-        name,
-        source_record_id,
-        route_id,
-        route_short_name,
-        route_long_name,
-        time_of_day,
-        month,
-        year,
-        day_type,
-
-        COUNT(*) AS n_trips,
-        SUM(service_hours) AS ttl_service_hours
-
-    FROM service_with_daypart
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
-),
-
-fct_monthly_route_service_by_timeofday AS (
-    SELECT
-       {{ dbt_utils.generate_surrogate_key(['source_record_id', 'route_id', 'route_short_name', 'route_long_name', 'time_of_day', 'month', 'year', 'day_type']) }} AS key,
-        name,
-        source_record_id,
-        route_id,
-        route_short_name,
-        route_long_name,
-        time_of_day,
-        month,
-        year,
-        day_type,
-        n_trips,
-        ttl_service_hours
-
-    FROM daypart_aggregations
-    WHERE ttl_service_hours IS NOT NULL
-
+    FROM monthly_trips
+    INNER JOIN dim_shapes_arrays
+        ON monthly_trips.shape_array_key = dim_shapes_arrays.key
 )
 
-SELECT * FROM fct_monthly_route_service_by_timeofday
+
+SELECT * FROM monthly_trips_with_geom
