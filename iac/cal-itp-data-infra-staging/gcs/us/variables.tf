5b54792497b4359e0fb0655b6e00bdd18ac483f2
Doc Ritezel <doc@ministryofvelocity.com>
2025-06-04 16:15:43 -0700

    Simplify staging environment bucket configuration
    Signed-off-by: Erika Pacheco <erika@ministryofvelocity.com>

--- iac/cal-itp-data-infra-staging/gcs/us/variables.tf
+++ iac/cal-itp-data-infra-staging/gcs/us/variables.tf
@@ -1,3 +1,35 @@
+locals {
+  environment_buckets = toset([
+    "calitp-staging-aggregator-scraper",
+    "calitp-staging-airtable",
+    "calitp-staging-amplitude-benefits-events",
+    "calitp-staging-elavon-parsed",
+    "calitp-staging-elavon-raw",
+    "calitp-staging-gtfs-download-config",
+    "calitp-staging-gtfs-rt-parsed",
+    "calitp-staging-gtfs-rt-raw-v2",
+    "calitp-staging-gtfs-rt-validation",
+    "calitp-staging-gtfs-schedule-parsed",
+    "calitp-staging-gtfs-schedule-parsed-hourly",
+    "calitp-staging-gtfs-schedule-raw-v2",
+    "calitp-staging-gtfs-schedule-unzipped",
+    "calitp-staging-gtfs-schedule-unzipped-hourly",
+    "calitp-staging-gtfs-schedule-validation",
+    "calitp-staging-gtfs-schedule-validation-hourly",
+    "calitp-staging-ntd-api-products",
+    "calitp-staging-ntd-xlsx-products-clean",
+    "calitp-staging-ntd-xlsx-products-raw",
+    "calitp-staging-payments-littlepay-parsed",
+    "calitp-staging-payments-littlepay-parsed-v3",
+    "calitp-staging-payments-littlepay-raw",
+    "calitp-staging-payments-littlepay-raw-v3",
+    "calitp-staging-publish",
+    "calitp-staging-sentry",
+    "calitp-staging-state-geoportal-scrape",
+    "calitp-staging-gtfs-download-config-test",
+  ])
+}
+
 data "terraform_remote_state" "iam" {
   backend = "gcs"
 
