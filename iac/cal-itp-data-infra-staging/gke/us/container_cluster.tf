086fac52852756d9a6f9ee2cba71d82b2c45a9b1
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-06-11 15:28:16 -0700

    Create kubernetes namespace for airflow jobs

--- iac/cal-itp-data-infra-staging/gke/us/container_cluster.tf
+++ iac/cal-itp-data-infra-staging/gke/us/container_cluster.tf
@@ -3,9 +3,11 @@ resource "google_container_cluster" "airflow-jobs-staging" {
   location = "us-west2"
   project  = "cal-itp-data-infra-staging"
 
-  enable_autopilot = true
-
-  network = data.terraform_remote_state.networks.outputs.google_compute_network_tfer--default_self_link
-
+  enable_autopilot    = true
   deletion_protection = false
+  network             = data.terraform_remote_state.networks.outputs.google_compute_network_tfer--default_self_link
+
+  secret_manager_config {
+    enabled = true
+  }
 }
