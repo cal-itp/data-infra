35fa94caf2f9c95d17cc9f4a1f472468ce66c622
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-06-16 16:15:43 -0700

    Add IAM binding for audit sink

--- iac/cal-itp-data-infra-staging/logging/us/audit_config.tf
+++ iac/cal-itp-data-infra-staging/logging/us/audit_config.tf
@@ -1,11 +1,3 @@
-# resource "google_project_iam_audit_config" "audit" {
-#   project = "cal-itp-data-infra-staging"
-
-#   audit_log_config {
-#     log_type = "ADMIN_READ"
-#   }
-# }
-
 resource "google_bigquery_dataset" "audit" {
   dataset_id                 = "audit"
   location                   = "us-west2"
@@ -13,7 +5,7 @@ resource "google_bigquery_dataset" "audit" {
   delete_contents_on_destroy = true
 }
 
-resource "google_logging_project_sink" "audit_sink" {
+resource "google_logging_project_sink" "audit" {
   name        = "bigquery-sink"
   destination = "bigquery.googleapis.com/projects/cal-itp-data-infra-staging/datasets/${google_bigquery_dataset.audit.dataset_id}"
   filter      = <<-EOT
@@ -26,4 +18,14 @@ resource "google_logging_project_sink" "audit_sink" {
   bigquery_options {
     use_partitioned_tables = true # always true if it is false, logs cant export to the bigquery
   }
+}
+
+resource "google_bigquery_dataset_iam_binding" "audit" {
+  project    = "cal-itp-data-infra-staging"
+  dataset_id = google_bigquery_dataset.audit.dataset_id
+  role       = "roles/bigquery.dataEditor"
+  members = [
+    google_logging_project_sink.audit.writer_identity,
+  ]
+  depends_on = [google_bigquery_dataset.audit]
 }
