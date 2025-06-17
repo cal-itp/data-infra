af4ca9251817897a6afb52937c3d445664e4c05d
Charles Costanzo <cvc5185@gmail.com>
2025-06-17 19:33:28 -0400

    refactor keying location in ntd_assets, hardcode out failing test rows

--- warehouse/models/mart/ntd_assets/fct_asset_inventory_time_series_avg_standing_capacity.sql
+++ warehouse/models/mart/ntd_assets/fct_asset_inventory_time_series_avg_standing_capacity.sql
@@ -1,6 +1,11 @@
 WITH intermediate_asset_inventory_time_series_avg_standing_capacity AS (
     SELECT *
     FROM {{ ref('int_ntd__asset_inventory_time_series_avg_standing_capacity') }}
+    -- remove bad rows for 'Advance Transit, Inc. NH' and 'Southern Teton Area Rapid Transit'
+    WHERE key NOT IN ('e41f3812655066d28ec4bbc851545517','7d3e30725b3fa42c6d1722308f9cc855','f5f160d19e3753e3a99d9ad55b4f2210',
+        'da108425cb2696446aa1017bca72340f','0fab2ef186a2a74edc98d16427d4d61a','a31019318eddb35b747ab79470e10017',
+        'd6809f84a9d19808f8b1f013fc1cd537','73f01d2aa1c268ec1dafbcf1fdaa84fc','98692053a5a16aae8ef8e2579f19b8a3',
+        '5b13563073a95faa05c9da4f77c0b3a8','564993fcc3a920cc0800005f3af9fd73','c3ae0b0299c10ffa25e1193404762136')
 ),
 
 dim_agency_information AS (
@@ -17,7 +22,7 @@ dim_agency_information AS (
 
 fct_asset_inventory_time_series_avg_standing_capacity AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['int.ntd_id', 'int.year', 'int.legacy_ntd_id', 'int.mode', 'int.service']) }} AS key,
+        int.key,
         int.ntd_id,
         int.year,
 
