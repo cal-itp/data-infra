09fb05863e1033895e1542000dea2cff86c5d6aa
Rae Bonfanti <raebot@users.noreply.github.com>
2025-06-23 21:40:45 -0700

    Add helpers for accessing geospatial files in GCS

---
+++ packages/calitp-data-analysis/calitp_data_analysis/gcs_geopandas.py
@@ -0,0 +1,37 @@
+import geopandas
+import gcsfs
+import google.auth
+
+class GCSGeoPandas:
+    """
+    GCSGeoPandas contains authentication helpers for interacting with Google Cloud   Storage with GeoPandas
+    """
+
+    def __init__(self):
+        """Sets instance token to credentials returned from Google auth request
+        """
+        credentials, _ = google.auth.default()
+        self.token = credentials
+
+    def gcs_filesystem(self, **kwargs):
+        """Returns a Google Cloud Storage Filesystem
+        """
+        return gcsfs.GCSFileSystem(token=self.token, **kwargs)
+
+    def read_parquet(self, path, *args, **kwargs):
+        """Delegates to geopandas.read_parquet with storage option token
+
+        Passes the auth credentials from Google auth as storage option token
+        """
+        storage_options = kwargs.get("storage_options", {}) | {'token': self.token}
+        return geopandas.read_parquet(path, storage_options=storage_options, *args, **kwargs)
+
+    def geo_data_frame_to_parquet(self, geo_data_frame, path, *args, **kwargs):
+        """Delegates to .to_parquet on the passed geo_data_frame, providing Google Cloud Storage file system
+
+        Fetches gcsfs.GCSFileSystem instance
+
+        Passes the auth credentials from Google Cloud Storage as storage option token
+        """
+        gcs_filesystem = self.gcs_filesystem()
+        return geo_data_frame.to_parquet(path, filesystem=gcs_filesystem, *args, **kwargs)
---
+++ packages/calitp-data-analysis/tests/test_gcs_geopandas.py
@@ -0,0 +1,42 @@
+import pytest
+import geopandas
+import gcsfs
+
+from calitp_data_analysis.gcs_geopandas import GCSGeoPandas
+
+TOKEN = 'faketoken'
+PATH = 'https://www.example.com/path/to/file'
+ANY_OTHER_ARGUMENT = 'fakeargument'
+GCS_FILESYSTEM = 'fakegcsfilesystem'
+
+@pytest.fixture
+def gcs_geopandas(mocker):
+    return GCSGeoPandas()
+
+@pytest.fixture
+def gcs_auth_setup(mocker):
+    mocker.patch('google.auth.default', return_value=(TOKEN, None))
+
+def test_gcs_filesystem(mocker, gcs_auth_setup, gcs_geopandas):
+    mocker.patch('gcsfs.GCSFileSystem')
+
+    gcs_geopandas.gcs_filesystem()
+
+    gcsfs.GCSFileSystem.assert_called_once_with(token=TOKEN)
+
+def test_read_parquet(mocker, gcs_auth_setup, gcs_geopandas):
+    mocker.patch('geopandas.read_parquet')
+
+    gcs_geopandas.read_parquet(PATH, ANY_OTHER_ARGUMENT)
+
+    geopandas.read_parquet.assert_called_once_with(PATH, ANY_OTHER_ARGUMENT, storage_options={'token': TOKEN})
+
+def test_geo_data_frame_to_parquet(mocker, gcs_auth_setup, gcs_geopandas):
+    mocker.patch('gcsfs.GCSFileSystem', return_value=GCS_FILESYSTEM)
+    geo_data_frame = mocker.Mock()
+    geo_data_frame.to_parquet = mocker.Mock()
+
+    gcs_geopandas.geo_data_frame_to_parquet(geo_data_frame, PATH, ANY_OTHER_ARGUMENT)
+
+    gcsfs.GCSFileSystem.assert_called_once_with(token=TOKEN)
+    geo_data_frame.to_parquet.assert_called_once_with(PATH, ANY_OTHER_ARGUMENT, filesystem=GCS_FILESYSTEM)
