08a4abf329f710511a160960a22c9dfcc2464d22
Rae Bonfanti <raebot@users.noreply.github.com>
2025-06-24 15:20:41 -0700

    Run Black on new GCS GeoPandas helpers

--- packages/calitp-data-analysis/calitp_data_analysis/gcs_geopandas.py
+++ packages/calitp-data-analysis/calitp_data_analysis/gcs_geopandas.py
@@ -1,6 +1,7 @@
-import geopandas
 import gcsfs
+import geopandas
 import google.auth
+
 
 class GCSGeoPandas:
     """
@@ -8,14 +9,12 @@ class GCSGeoPandas:
     """
 
     def __init__(self):
-        """Sets instance token to credentials returned from Google auth request
-        """
+        """Sets instance token to credentials returned from Google auth request"""
         credentials, _ = google.auth.default()
         self.token = credentials
 
     def gcs_filesystem(self, **kwargs):
-        """Returns a Google Cloud Storage Filesystem
-        """
+        """Returns a Google Cloud Storage Filesystem"""
         return gcsfs.GCSFileSystem(token=self.token, **kwargs)
 
     def read_parquet(self, path, *args, **kwargs):
@@ -23,8 +22,10 @@ class GCSGeoPandas:
 
         Passes the auth credentials from Google auth as storage option token
         """
-        storage_options = kwargs.get("storage_options", {}) | {'token': self.token}
-        return geopandas.read_parquet(path, storage_options=storage_options, *args, **kwargs)
+        storage_options = kwargs.get("storage_options", {}) | {"token": self.token}
+        return geopandas.read_parquet(
+            path, storage_options=storage_options, *args, **kwargs
+        )
 
     def geo_data_frame_to_parquet(self, geo_data_frame, path, *args, **kwargs):
         """Delegates to .to_parquet on the passed geo_data_frame, providing Google Cloud Storage file system
@@ -34,4 +35,6 @@ class GCSGeoPandas:
         Passes the auth credentials from Google Cloud Storage as storage option token
         """
         gcs_filesystem = self.gcs_filesystem()
-        return geo_data_frame.to_parquet(path, filesystem=gcs_filesystem, *args, **kwargs)
+        return geo_data_frame.to_parquet(
+            path, filesystem=gcs_filesystem, *args, **kwargs
+        )
