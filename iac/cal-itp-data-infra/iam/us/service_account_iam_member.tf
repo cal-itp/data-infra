e4e871623f4e33dd25afb599fb458801c4799305
Jeff Parr <jeffparr100@gmail.com>
2025-07-03 16:05:26 -0700

    Add missing workload identity federation for reports (#4043)
    Signed-off-by: Doc Ritezel <doc@ministryofvelocity.com>

--- iac/cal-itp-data-infra/iam/us/service_account_iam_member.tf
+++ iac/cal-itp-data-infra/iam/us/service_account_iam_member.tf
@@ -16,6 +16,12 @@ resource "google_service_account_iam_member" "github-actions-service-account_gtf
   role               = "roles/iam.workloadIdentityUser"
 }
 
+resource "google_service_account_iam_member" "github-actions-service-account_reports" {
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-actions.name}/attribute.repository/${local.reports_github_repository_name}"
+  service_account_id = google_service_account.github-actions-service-account.id
+  role               = "roles/iam.workloadIdentityUser"
+}
+
 resource "google_service_account_iam_member" "github-actions--github-actions-services-accoun" {
   service_account_id = "projects/cal-itp-data-infra/serviceAccounts/github-actions-services-accoun@cal-itp-data-infra.iam.gserviceaccount.com"
   role               = "roles/iam.workloadIdentityUser"
