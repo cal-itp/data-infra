2bfe24b2220d1cd669d6037020162399913c14be
Charles Costanzo <cvc5185@gmail.com>
2025-07-08 09:48:53 -0400

    rearrange filter clause for bad data in mart.ntd_assets

--- warehouse/models/mart/ntd_assets/fct_asset_inventory_time_series_avg_fleet_age.sql
+++ warehouse/models/mart/ntd_assets/fct_asset_inventory_time_series_avg_fleet_age.sql
@@ -1,11 +1,6 @@
 WITH intermediate_asset_inventory_time_series_avg_fleet_age AS (
     SELECT *
     FROM {{ ref('int_ntd__asset_inventory_time_series_avg_fleet_age') }}
-    -- remove bad rows for 'Advance Transit, Inc. NH' and 'Southern Teton Area Rapid Transit'
-    WHERE key NOT IN ('f5f160d19e3753e3a99d9ad55b4f2210','e41f3812655066d28ec4bbc851545517','7d3e30725b3fa42c6d1722308f9cc855',
-        'da108425cb2696446aa1017bca72340f','c3ae0b0299c10ffa25e1193404762136','a31019318eddb35b747ab79470e10017',
-        'd6809f84a9d19808f8b1f013fc1cd537','0fab2ef186a2a74edc98d16427d4d61a','5b13563073a95faa05c9da4f77c0b3a8',
-        '73f01d2aa1c268ec1dafbcf1fdaa84fc','564993fcc3a920cc0800005f3af9fd73','98692053a5a16aae8ef8e2579f19b8a3')
 ),
 
 dim_agency_information AS (
@@ -56,6 +51,11 @@ fct_asset_inventory_time_series_avg_fleet_age AS (
     LEFT JOIN dim_agency_information AS agency
         ON int.ntd_id = agency.ntd_id
             AND int.year = agency.year
+    -- remove bad rows for 'Advance Transit, Inc. NH' and 'Southern Teton Area Rapid Transit'
+    WHERE int.key NOT IN ('f5f160d19e3753e3a99d9ad55b4f2210','e41f3812655066d28ec4bbc851545517','7d3e30725b3fa42c6d1722308f9cc855',
+        'da108425cb2696446aa1017bca72340f','c3ae0b0299c10ffa25e1193404762136','a31019318eddb35b747ab79470e10017',
+        'd6809f84a9d19808f8b1f013fc1cd537','0fab2ef186a2a74edc98d16427d4d61a','5b13563073a95faa05c9da4f77c0b3a8',
+        '73f01d2aa1c268ec1dafbcf1fdaa84fc','564993fcc3a920cc0800005f3af9fd73','98692053a5a16aae8ef8e2579f19b8a3')
 )
 
 SELECT * FROM fct_asset_inventory_time_series_avg_fleet_age
