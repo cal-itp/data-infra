9ec3158917cb284363abf00f7157327ce5fcaa8e
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2025-07-09 14:54:08 -0700

    `fct_vehicle_locations` as array / linestring (#3994)
    * condense vp into array/linestring
    
    * remove fct_vehicle_locations_grouped
    
    * add docs for vp path
    
    * add column where timestamp is seconds
    
    * change partition/clustering for monthly trips
    
    * partition by first day of month
    
    * make incremental, add condition to run when full month is available

---
+++ warehouse/models/mart/gtfs/fct_vehicle_locations_path.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='insert_overwrite',
+        partition_by={
+            'field': 'service_date',
+            'data_type': 'date',
+            'granularity': 'day',
+        },
+        cluster_by='base64_url',
+    )
+}}
+
+WITH fct_vehicle_locations AS (
+    SELECT *
+    FROM {{ ref('fct_vehicle_locations') }}
+),
+
+-- collect points into an array
+initial_pt_array AS (
+    SELECT
+        gtfs_dataset_key,
+        gtfs_dataset_name,
+        base64_url,
+        service_date,
+        schedule_gtfs_dataset_key,
+        schedule_name,
+        schedule_feed_key,
+        trip_id,
+        trip_instance_key,
+        -- don't try to make LINESTRING because of this issue:
+        -- https://stackoverflow.com/questions/58234223/st-makeline-discarding-duplicate-points-even-if-not-consecutive
+        -- also: https://gis.stackexchange.com/questions/426188/can-i-represent-a-route-that-doubles-back-on-itself-in-bigquery-with-a-linestrin
+        -- so instead this is just an array of WKT points
+        ARRAY_AGG(
+            -- ignore nulls so it doesn't error out if there's a null point
+            location IGNORE NULLS
+            ORDER BY DATETIME(location_timestamp, "America/Los_Angeles")
+        ) AS pt_array,
+        -- count number of rows so we can check for nulls (drops) later
+        ARRAY_AGG(
+            DATETIME(location_timestamp, "America/Los_Angeles") IGNORE NULLS
+            ORDER BY location_timestamp
+        ) AS location_timestamp_pacific,
+        ARRAY_AGG(
+            EXTRACT(HOUR FROM DATETIME(location_timestamp, "America/Los_Angeles")) * 3600
+              + EXTRACT(MINUTE FROM DATETIME(location_timestamp, "America/Los_Angeles")) * 60
+              + EXTRACT(SECOND FROM DATETIME(location_timestamp, "America/Los_Angeles"))
+             IGNORE NULLS
+            ORDER BY location_timestamp
+        ) AS pacific_seconds,
+        COUNT(*) AS n_vp,
+
+    FROM fct_vehicle_locations
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+),
+
+fct_vehicle_locations_path AS (
+    SELECT *
+    FROM initial_pt_array
+    -- drop shapes that had nulls
+    WHERE ARRAY_LENGTH(pt_array) = n_vp
+)
+
+SELECT * FROM fct_vehicle_locations_path
