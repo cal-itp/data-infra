0fd1c6698bd1c8813ff566e9ab8070578fd99f94
Doc Ritezel <doc@ministryofvelocity.com>
2025-07-15 16:36:38 -0700

    Clean up airflow code (#4070)

--- airflow/plugins/operators/__init__.py
+++ airflow/plugins/operators/__init__.py
@@ -6,7 +6,6 @@ from operators.aggregator_to_gcs_operator import (
 from operators.airtable_to_gcs_operator import AirtableToGCSOperator
 from operators.blackcat_to_gcs_operator import BlackCatToGCSOperator
 from operators.external_table import ExternalTable
-from operators.gtfs_csv_to_jsonl import GtfsGcsToJsonlOperator
 from operators.gtfs_csv_to_jsonl_hourly import GtfsGcsToJsonlOperatorHourly
 from operators.kuba_to_gcs_operator import KubaToGCSOperator
 from operators.littlepay_raw_sync import LittlepayRawSync
