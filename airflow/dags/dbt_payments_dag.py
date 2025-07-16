10977c697c7e9bbb0e3690a1231bfaa4499b58a6
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-07-15 17:31:12 -0700

    Switch to LatestOnlyOperator

--- airflow/dags/dbt_payments_dag.py
+++ airflow/dags/dbt_payments_dag.py
@@ -1,39 +1,49 @@
 import os
 from datetime import datetime
 
-from cosmos import DbtDag, ProfileConfig, ProjectConfig, RenderConfig
+from cosmos import DbtTaskGroup, ProfileConfig, ProjectConfig, RenderConfig
 from cosmos.constants import TestBehavior
+
+from airflow import DAG
+from airflow.operators.latest_only import LatestOnlyOperator
 
 DBT_TARGET = os.environ.get("DBT_TARGET")
 
-dbt_payments = DbtDag(
-    # dbt/cosmos-specific parameters
-    project_config=ProjectConfig(
-        dbt_project_path="/home/airflow/gcs/data/warehouse",
-        manifest_path="/home/airflow/gcs/data/warehouse/target/manifest.json",
-        project_name="calitp_warehouse",
-        seeds_relative_path="seeds/",
-    ),
-    profile_config=ProfileConfig(
-        target_name=DBT_TARGET,
-        profile_name="calitp_warehouse",
-        profiles_yml_filepath="/home/airflow/gcs/data/warehouse/profiles.yml",
-    ),
-    render_config=RenderConfig(
-        select=[
-            "+path:models/staging/payments+",
-            "+path:models/intermediate/payments+",
-            "+path:models/mart/payments+",
-        ],
-        test_behavior=TestBehavior.AFTER_ALL,
-    ),
-    operator_args={"install_deps": True},
+with DAG(
+    dag_id="dbt_payments",
+    tags=["dbt", "payments"],
     # Tuesday, Wednesday, Friday at 7am PDT/8am PST (2pm UTC)
     schedule="0 14 * * 2,3,5",
     start_date=datetime(2025, 7, 6),
     catchup=False,
-    latest_only=True,
-    dag_id="dbt_payments",
-    tags=["dbt", "payments"],
-    default_args={"retries": 0},
-)
+):
+    latest_only = LatestOnlyOperator(task_id="latest_only", depends_on_past=False)
+
+    dbt_payments = DbtTaskGroup(
+        group_id="dbt_payments",
+        project_config=ProjectConfig(
+            dbt_project_path="/home/airflow/gcs/data/warehouse",
+            manifest_path="/home/airflow/gcs/data/warehouse/target/manifest.json",
+            project_name="calitp_warehouse",
+            seeds_relative_path="seeds/",
+        ),
+        profile_config=ProfileConfig(
+            target_name=DBT_TARGET,
+            profile_name="calitp_warehouse",
+            profiles_yml_filepath="/home/airflow/gcs/data/warehouse/profiles.yml",
+        ),
+        render_config=RenderConfig(
+            select=[
+                "+path:models/staging/payments+",
+                "+path:models/intermediate/payments+",
+                "+path:models/mart/payments+",
+            ],
+            test_behavior=TestBehavior.AFTER_ALL,
+        ),
+        operator_args={
+            "install_deps": True,
+        },
+        default_args={"retries": 0},
+    )
+
+    latest_only >> dbt_payments
