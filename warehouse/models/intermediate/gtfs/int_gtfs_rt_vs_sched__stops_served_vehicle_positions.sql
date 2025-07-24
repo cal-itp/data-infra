188ba1737682ce50f9b281bc8ae4f9e70a34185a
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-07-24 20:35:34 +0000

    use hausdorff and move step earlier?

--- warehouse/models/intermediate/gtfs/int_gtfs_rt_vs_sched__stops_served_vehicle_positions.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt_vs_sched__stops_served_vehicle_positions.sql
@@ -18,7 +18,9 @@ WITH trips AS (
         trip_id,
         iteration_num,
         trip_instance_key,
-    FROM {{ ref('fct_scheduled_trips') }}
+    --FROM {{ ref('fct_scheduled_trips') }}
+    FROM `cal-itp-data-infra.mart_gtfs.fct_scheduled_trips`
+    WHERE service_date = "2025-07-11"
 ),
 
 vp_path AS (
@@ -27,8 +29,11 @@ vp_path AS (
         base64_url,
         schedule_feed_key,
         trip_instance_key,
-        ST_MAKELINE(pt_array) AS pt_array
-    FROM {{ ref('fct_vehicle_locations_path') }}
+        ST_SIMPLIFY(ST_MAKELINE(pt_array), 5) AS pt_array
+
+    --FROM {{ ref('fct_vehicle_locations_path') }}
+    FROM `cal-itp-data-infra-staging.tiffany_mart_gtfs.test_vp_path`
+    WHERE service_date = "2025-07-11"
 ),
 
 stop_times_grouped AS (
@@ -51,38 +56,46 @@ vp_with_stops AS (
         trips.iteration_num,
         trips.trip_instance_key,
 
-        stop_times_grouped.stop_pt_array,
-        stop_times_grouped.stop_id_array,
+        --stop_times_grouped.stop_pt_array,
+        --stop_times_grouped.stop_id_array,
 
         vp_path.base64_url AS vp_base64_url,
-        vp_path.pt_array,
+        --vp_path.pt_array,
+
+        pt_geom,
+        stop_id,
+        ST_HAUSDORFFDWITHIN(
+            pt_geom, vp_path.pt_array, 25
+        ) AS is_within_25m,
 
     FROM trips
     INNER JOIN stop_times_grouped
         ON stop_times_grouped.feed_key = trips.feed_key
         AND stop_times_grouped.trip_id = trips.trip_id
         AND stop_times_grouped.iteration_num = trips.iteration_num
-    LEFT JOIN vp_path
+    INNER JOIN vp_path
         ON trips.service_date = vp_path.service_date
         AND trips.feed_key = vp_path.schedule_feed_key
         AND trips.trip_instance_key = vp_path.trip_instance_key
-),
+    LEFT JOIN UNNEST(stop_times_grouped.stop_pt_array) AS pt_geom
+    LEFT JOIN UNNEST(stop_times_grouped.stop_id_array) AS stop_id
+)
 
 -- unnest the arrays so that every stop_id/stop pt_geom is a row
 -- becomes trip-stop grain
-unnested_stops AS (
-    SELECT
-        vp_with_stops.* EXCEPT(stop_pt_array, stop_id_array),
-        stop_id,
+--unnested_stops AS (
+--    SELECT
+--        vp_with_stops.* EXCEPT(stop_pt_array, stop_id_array),
+--        stop_id,
 
         -- tried buffering stop pt geom to various distances,
         -- but BQ resources were exhausted working with ST_SIMPLIFY(pt_array, 5)
         -- so let's use distance to get at a similar metric
-        ROUND(ST_DISTANCE(pt_geom, pt_array), 2) AS meters_to_vp
+        --ROUND(ST_DISTANCE(pt_geom, pt_array), 2) AS meters_to_vp
 
-    FROM vp_with_stops
-    LEFT JOIN UNNEST(stop_pt_array) AS pt_geom
-    LEFT JOIN UNNEST(stop_id_array) AS stop_id
-)
+--    FROM vp_with_stops
+--    LEFT JOIN UNNEST(stop_pt_array) AS pt_geom
+--    LEFT JOIN UNNEST(stop_id_array) AS stop_id
+--)
 
-SELECT * FROM unnested_stops
+SELECT * FROM vp_with_stops
--- warehouse/models/mart/gtfs/fct_vehicle_positions_stop_metrics.sql
+++ warehouse/models/mart/gtfs/fct_vehicle_positions_stop_metrics.sql
@@ -16,25 +16,36 @@ WITH stops_served_vp AS (
     FROM {{ ref('int_gtfs_rt_vs_sched__stops_served_vehicle_positions') }}
 ),
 
+stops_grouped AS (
+    SELECT
+        *,
+        CASE
+          WHEN meters_to_vp <= 10 THEN TRUE
+          ELSE FALSE
+        END AS is_near_10m,
+        CASE WHEN meters_to_vp <= 25 THEN TRUE
+          ELSE FALSE
+        END AS is_near_25m,
+    FROM stops_served_vp
+),
+
 trip_counts_by_group AS (
     SELECT
         vp_base64_url,
         schedule_feed_key,
         schedule_gtfs_dataset_key,
         service_date,
-
         stop_id,
-
-        COUNTIF(near_stop_50m) AS near_50m,
-        COUNTIF(near_stop_25m) AS near_25m,
-        COUNTIF(near_stop_10m) AS near_10m,
+    
+        COUNTIF(is_near_10m) AS near_10m,
+        COUNTIF(is_near_25m) AS near_25m,
         COUNT(*) AS n_vp_trips,
 
-    FROM stops_served_vp
+    FROM stops_grouped
     GROUP BY GROUPING SETS(
         vp_base64_url, schedule_feed_key, schedule_gtfs_dataset_key,
         service_date, stop_id,
-        near_stop_50m, near_stop_25m, near_stop_10m)
+        is_near_25m, is_near_10m)
     -- grouping set will return the null placeholder
     -- which captures the groups were near_stop_* = false for every stop
 ),
