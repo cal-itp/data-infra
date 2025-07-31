f8aff673aa4483b3cc3ab3cefc39ee6af0944e65
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-07-31 18:06:37 +0000

    use dt as partition, hardcode to 1 date

--- warehouse/models/mart/gtfs/fct_stop_time_updates_week.sql
+++ warehouse/models/mart/gtfs/fct_stop_time_updates_week.sql
@@ -2,18 +2,19 @@
     config(
         materialized='incremental',
         partition_by={
-            'field': 'service_date',
+            'field': 'dt',
             'data_type': 'date',
             'granularity': 'day'
-        }, cluster_by=['service_date', 'base64_url']
+        }, cluster_by=['dt', 'base64_url']
     )
 }}
 
 WITH fct_stop_time_updates_filtered AS (
     SELECT *
-    FROM {{ ref('fct_stop_time_updates') }}
+    FROM `cal-itp-data-infra.mart_gtfs.fct_stop_time_updates`
+    --{{ ref('fct_stop_time_updates') }}
     -- add extra date boundaries to grab relevant service_dates
-    WHERE dt >= '2025-06-21' AND dt <= '2025-06-29'
+    WHERE dt = '2025-06-21'
 )
 
 SELECT * FROM fct_stop_time_updates_filtered
