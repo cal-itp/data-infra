c1483a6c03c293c84ab4b2f81205f3e34641935b
farhad <farhad.salemi@calitp.org>
2025-08-05 04:44:40 +0000

    Remove old output.tf to fix duplicate outputs

--- iac/cal-itp-data-infra/cloud-functions/us/output.tf
+++
@@ -1,7 +0,0 @@
-output "function_name" {
-  value = google_cloudfunctions2_function.update_airtable.name
-}
-
-output "function_uri" {
-  value = google_cloudfunctions2_function.update_airtable.service_config[0].uri
-}
