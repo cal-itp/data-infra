9e116a905bc16c0ad08787ff5fae6567ce8548b0
Charles Costanzo <cvc5185@gmail.com>
2025-08-07 16:45:39 -0400

    move entity mapping join on _in_use_from/to dates to appropriate location

--- warehouse/models/mart/payments/fct_payments_aggregations.sql
+++ warehouse/models/mart/payments/fct_payments_aggregations.sql
@@ -57,9 +57,7 @@ join_orgs AS (
         orgs.name AS organization_name,
         orgs.source_record_id AS organization_source_record_id,
     FROM join_payments
-    LEFT JOIN payments_entity_mapping
-        ON join_payments.participant_id = payments_entity_mapping.participant_id
-        AND CAST(join_payments.aggregation_datetime AS TIMESTAMP) BETWEEN CAST(payments_entity_mapping._in_use_from AS TIMESTAMP) AND CAST(payments_entity_mapping._in_use_until AS TIMESTAMP)
+    LEFT JOIN payments_entity_mapping USING (participant_id)
     LEFT JOIN orgs
         ON payments_entity_mapping.organization_source_record_id = orgs.source_record_id
         AND CAST(join_payments.aggregation_datetime AS TIMESTAMP) BETWEEN orgs._valid_from AND orgs._valid_to
