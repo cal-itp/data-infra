5c5e2148978705df16bc53a1aa3d8deb81376ce7
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-08-13 23:01:30 +0000

    make schedule trips daily and month tables, but partition it

--- warehouse/models/mart/gtfs/fct_monthly_scheduled_trips.sql
+++ warehouse/models/mart/gtfs/fct_monthly_scheduled_trips.sql
@@ -1,8 +1,6 @@
 {{
     config(
-        materialized='incremental',
-        incremental_strategy='insert_overwrite',
-        unique_key = "key",
+        materialized='table',
         partition_by={
             'field': 'month_first_day',
             'data_type': 'date',
@@ -14,17 +12,18 @@
 
 
 WITH trips AS (
-    SELECT * FROM {{ ref('fct_scheduled_trips') }}
+    SELECT *
+    FROM {{ ref('fct_scheduled_trips') }}
     -- only run if new month is available. select dates <= last day of prior month
-    WHERE service_date <= LAST_DAY(
+    WHERE service_date >= "2025-01-01" AND service_date <= LAST_DAY(
         DATE_SUB(CURRENT_DATE("America/Los_Angeles"), INTERVAL 1 MONTH)
     )
 ),
 
+
 monthly_trips AS (
 
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['year', 'month', 'name', 'day_type', 'trip_id', 'iteration_num']) }} AS key,
         gtfs_dataset_key,
         name,
 
@@ -32,14 +31,8 @@ monthly_trips AS (
         EXTRACT(year FROM service_date) AS year,
         DATE_TRUNC(service_date, MONTH) AS month_first_day,
 
-        CASE
-            WHEN EXTRACT(DAYOFWEEK FROM service_date) = 1 THEN "Sunday"
-            WHEN EXTRACT(DAYOFWEEK FROM service_date) = 7 THEN "Saturday"
-            ELSE "Weekday"
-        END
-        AS day_type,
+        {{ generate_day_type('service_date') }} AS day_type,
         time_of_day,
-        --route_id, # this might change over longer time periods
         direction_id,
         route_short_name,
         route_long_name,
@@ -52,12 +45,13 @@ monthly_trips AS (
         shape_id,
         shape_array_key,
 
+        ARRAY_AGG(DISTINCT route_id IGNORE NULLS) AS route_id_array,
         AVG(service_hours) AS service_hours,
         COUNT(DISTINCT trip_instance_key) as n_trips,
         COUNT(DISTINCT service_date) as n_days,
 
     FROM trips
-    GROUP BY 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
 
 )
 
