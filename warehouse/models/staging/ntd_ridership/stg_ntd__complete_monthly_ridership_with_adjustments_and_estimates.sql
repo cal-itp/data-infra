c22632ff92a999129d83f6a5a6587e820f381b3f
Charles Costanzo <cvc5185@gmail.com>
2025-09-10 14:15:22 -0400

    revert table source config, define data types appropriately, reorder columns

--- warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates.sql
+++ warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates.sql
@@ -12,13 +12,15 @@ get_latest_extract AS(
 
 stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates AS (
     SELECT
+        {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
+        {{ trim_make_empty_string_null('agency') }} AS agency,
+        {{ trim_make_empty_string_null('tos') }} AS tos,
+        {{ trim_make_empty_string_null('mode') }} AS mode,
         {{ trim_make_empty_string_null('agency_mode_tos_date') }} AS agency_mode_tos_date,
         SAFE_CAST(voms AS NUMERIC) AS voms,
         SAFE_CAST(upt AS NUMERIC) AS upt,
         {{ trim_make_empty_string_null('_3_mode') }} AS _3_mode,
-        {{ trim_make_empty_string_null('tos') }} AS tos,
         SAFE_CAST(vrm AS NUMERIC) AS vrm,
-        {{ trim_make_empty_string_null('mode') }} AS mode,
         {{ trim_make_empty_string_null('uza_name') }} AS uza_name,
         SAFE_CAST(uace_cd AS NUMERIC) AS uace_cd,
         {{ trim_make_empty_string_null('fta_region') }} AS fta_region,
@@ -28,8 +30,6 @@ stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates AS (
         SAFE_CAST(vrh AS NUMERIC) AS vrh,
         {{ trim_make_empty_string_null('legacy_ntd_id') }} AS legacy_ntd_id,
         SAFE_CAST(date AS DATETIME) AS date,
-        {{ trim_make_empty_string_null('agency') }} AS agency,
-        {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
         dt,
         execution_ts,
     FROM get_latest_extract
