f2e3ccb6f1b1539c35081fb915fe97a6d71dff82
Jeff Parr <jeffparr100@gmail.com>
2025-09-12 09:52:45 -0700

    Staging sftp endpoint for enghouse (#4232)
    * Staging sftp endpoint for enghouse
    
    references: #4048
    
    # Conflicts:
    #	iac/cal-itp-data-infra-staging/gcs/us/storage_bucket.tf
    #	iac/cal-itp-data-infra-staging/gcs/us/storage_bucket_iam_member.tf
    
    * Add static ip address to enghouse sftp load balancer on staging

--- iac/cal-itp-data-infra-staging/gcs/us/storage_bucket_iam_member.tf
+++ iac/cal-itp-data-infra-staging/gcs/us/storage_bucket_iam_member.tf
@@ -40,6 +40,12 @@ resource "google_storage_bucket_iam_member" "calitp-staging-composer" {
   role   = "roles/storage.legacyBucketOwner"
 }
 
+resource "google_storage_bucket_iam_member" "enghouse-raw-sftp-service-account" {
+  bucket = google_storage_bucket.cal-itp-data-infra-enghouse-raw.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.terraform_remote_state.iam.outputs.google_service_account_sftp-pod-service-account_email}"
+}
+
 resource "google_storage_bucket_iam_member" "calitp-staging-pytest" {
   bucket = google_storage_bucket.calitp-staging-pytest.name
   member = "projectViewer:cal-itp-data-infra-staging"
