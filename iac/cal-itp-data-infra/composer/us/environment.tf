b787d1a0f0a88fa3e9fa409911d37073b2170cc9
Doc Ritezel <doc@ministryofvelocity.com>
2025-09-15 17:46:13 -0700

    Set composer environment size to medium
    Signed-off-by: Erika Pacheco <erika@ministryofvelocity.com>

--- iac/cal-itp-data-infra/composer/us/environment.tf
+++ iac/cal-itp-data-infra/composer/us/environment.tf
@@ -33,7 +33,7 @@ resource "google_composer_environment" "calitp-composer" {
       }
     }
 
-    environment_size = "ENVIRONMENT_SIZE_SMALL"
+    environment_size = "ENVIRONMENT_SIZE_MEDIUM"
 
     software_config {
       image_version = "composer-2.8.6-airflow-2.7.3"
@@ -43,16 +43,16 @@ resource "google_composer_environment" "calitp-composer" {
         core-dag_file_processor_timeout            = 1200
         core-dagbag_import_timeout                 = 600
         core-dags_are_paused_at_creation           = true
+        email-email_backend                        = "airflow.utils.email.send_email_smtp"
+        email-email_conn_id                        = "smtp_postmark"
+        email-from_email                           = "bot@calitp.org"
         scheduler-min_file_process_interval        = 120
         scheduler-scheduler_health_check_threshold = 120
         secrets-backend                            = "airflow.providers.google.cloud.secrets.secret_manager.CloudSecretManagerBackend"
+        smtp-smtp_mail_from                        = "bot@calitp.org"
+        smtp-smtp_starttls                         = true
         webserver-reload_on_plugin_change          = true
         webserver-show_trigger_form_if_no_params   = true
-        email-email_backend                        = "airflow.utils.email.send_email_smtp"
-        email-from_email                           = "bot@calitp.org"
-        email-email_conn_id                        = "smtp_postmark"
-        smtp-smtp_starttls                         = true
-        smtp-smtp_mail_from                        = "bot@calitp.org"
       }
 
       pypi_packages = local.pypi_packages
