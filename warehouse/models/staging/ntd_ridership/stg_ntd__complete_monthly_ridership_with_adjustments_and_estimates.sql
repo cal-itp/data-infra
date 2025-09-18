f8dec4ca941082770d243bba2e0fda5c66e84e9d
Charles Costanzo <cvc5185@gmail.com>
2025-09-18 12:37:40 -0400

    restore existing transformations to staging table

--- warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates.sql
+++ warehouse/models/staging/ntd_ridership/stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates.sql
@@ -12,8 +12,12 @@ get_latest_extract AS(
 
 stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates AS (
     SELECT
-        {{ trim_make_empty_string_null('CAST(ntd_id AS STRING)') }} AS ntd_id,
+        FORMAT("%05d", CAST(CAST(ntd_id AS NUMERIC) AS INT64)) AS ntd_id,
         {{ trim_make_empty_string_null('agency') }} AS agency,
+        SAFE_CAST(date AS DATETIME) AS date,
+        FORMAT_DATE('%m', stg.date) AS period_month,
+        FORMAT_DATE('%Y', stg.date) AS period_year,
+        FORMAT_DATE('%Y-%m', stg.date) AS period_year_month,
         {{ trim_make_empty_string_null('tos') }} AS tos,
         {{ trim_make_empty_string_null('mode') }} AS mode,
         {{ trim_make_empty_string_null('agency_mode_tos_date') }} AS agency_mode_tos_date,
@@ -22,17 +26,18 @@ stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates AS (
         {{ trim_make_empty_string_null('_3_mode') }} AS _3_mode,
         SAFE_CAST(vrm AS NUMERIC) AS vrm,
         {{ trim_make_empty_string_null('uza_name') }} AS uza_name,
-        SAFE_CAST(uace_cd AS NUMERIC) AS uace_cd,
+        FORMAT("%05d", CAST(uace_cd AS INT64)) AS uace_cd,
         {{ trim_make_empty_string_null('fta_region') }} AS fta_region,
         {{ trim_make_empty_string_null('state') }} AS state,
         {{ trim_make_empty_string_null('reporter_type') }} AS reporter_type,
         {{ trim_make_empty_string_null('mode_type_of_service_status') }} AS mode_type_of_service_status,
         SAFE_CAST(vrh AS NUMERIC) AS vrh,
         {{ trim_make_empty_string_null('legacy_ntd_id') }} AS legacy_ntd_id,
-        SAFE_CAST(date AS DATETIME) AS date,
         dt,
         execution_ts,
     FROM get_latest_extract
+    WHERE ntd_id IS NOT NULL
+
 )
 
 SELECT * FROM stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates
