4eda0442023c55cd9f14ec599480b90070e93455
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-10-02 15:15:10 -0700

    Add sentinel log setup

---
+++ iac/cal-itp-data-infra/sentinel/us/output.tf
@@ -0,0 +1,11 @@
+output "GCP_project_id" {
+  value = data.google_project.project.project_id
+}
+
+output "GCP_project_number" {
+  value = data.google_project.project.number
+}
+
+output "GCP_subscription_name" {
+  value = google_pubsub_subscription.sentinel-subscription.name
+}
