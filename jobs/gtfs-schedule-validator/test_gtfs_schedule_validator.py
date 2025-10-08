17726fed1778e52d6666ebb6041bd0f14f74e0d4
Laurie Merrell <laurie.m@jarv.us>
2025-10-08 17:44:19 -0500

    start stubbing out gtfs schedule validator tasks; doesn't run because of missing env var

--- jobs/gtfs-schedule-validator/test_gtfs_schedule_validator.py
+++ jobs/gtfs-schedule-validator/test_gtfs_schedule_validator.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+import pytest
 import toml
-from gtfs_schedule_validator_hourly import __version__
+from gtfs_schedule_validator_hourly import __version__, app
+from typer.testing import CliRunner
 
 
 def test_version():
@@ -20,3 +22,21 @@ def test_versions_are_in_sync():
     package_init_version = __version__
 
     assert package_init_version == pyproject_version
+
+
+class TestGtfsScheduleValidator:
+    @pytest.fixture
+    def runner(self) -> CliRunner:
+        return CliRunner()
+
+    def test_no_extracts(self, runner):
+        hour_start = "2020-02-20T20:00"
+        result = runner.invoke(
+            app,
+            [
+                "validate-hour",
+                hour_start,
+            ],
+        )
+        assert result.exit_code == 0
+        assert "found 0 extracts to process, exiting" in result.stdout
