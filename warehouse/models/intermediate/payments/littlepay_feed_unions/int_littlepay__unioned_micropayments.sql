f2cc09656e70a2dbae30db3d95a02e0cd3cfab71
Charles Costanzo <cvc5185@gmail.com>
2025-10-09 15:53:15 -0400

    reverse logic

--- warehouse/models/intermediate/payments/littlepay_feed_unions/int_littlepay__unioned_micropayments.sql
+++ warehouse/models/intermediate/payments/littlepay_feed_unions/int_littlepay__unioned_micropayments.sql
@@ -11,12 +11,12 @@ micropayments_v3 AS (
     SELECT *
     FROM {{ ref('stg_littlepay__micropayments_v3') }}
     WHERE
-        -- Keep all records for Nevada County Connects and SacRT, these agencies didn't have a competing feed v1 so we keep it all
-        participant_id IN ('nevada-county-connects', 'sacrt')
+        -- Keep all records for agencies that didn't have a competing feed v1
+        participant_id NOT IN ('clean-air-express', 'mendocino-transit-authority', 'ccjpa', 'atn', 'mst', 'lake-transit-authority', 'sbmtd', 'humboldt-transit-authority', 'redwood-coast-transit')
 
         -- For the following participants only, keep records including and after 5/17/2025 (cutover date for agencies that had both feeds at some point)
         OR (
-            participant_id NOT IN ('nevada-county-connects', 'sacrt')
+            participant_id IN ('clean-air-express', 'mendocino-transit-authority', 'ccjpa', 'atn', 'mst', 'lake-transit-authority', 'sbmtd', 'humboldt-transit-authority', 'redwood-coast-transit')
             AND littlepay_export_date >= '2025-05-17'
         )
 ),
