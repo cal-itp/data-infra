eaa0e67bd91ddcc6cbc9cd74048b72b6fd10de2d
Erika Pacheco <erika@ministryofvelocity.com>
2025-10-28 23:39:47 -0700

    Set up Airflow Slack URL using Terraform
    [#4363]

--- iac/cal-itp-data-infra-staging/composer/us/environment.tf
+++ iac/cal-itp-data-infra-staging/composer/us/environment.tf
@@ -95,6 +95,7 @@ resource "google_composer_environment" "calitp-staging-composer" {
         "CALITP_BUCKET__PUBLISH"                               = "gs://${data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-publish_name}",
         "CALITP_BUCKET__SENTRY_EVENTS"                         = "gs://${data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-sentry_name}",
         "CALITP_BUCKET__STATE_GEOPORTAL_DATA_PRODUCTS"         = "gs://${data.terraform_remote_state.gcs.outputs.google_storage_bucket_calitp-staging-state-geoportal-scrape_name}",
+        "CALITP_SLACK_URL_KEY"                                 = data.google_secret_manager_secret_version.slack-airflow-url.secret_data
       })
     }
   }
