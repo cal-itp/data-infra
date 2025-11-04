6f28fbb5e767c6e668684b4dc4c6cf21d42364b5
Doc Ritezel <doc@ministryofvelocity.com>
2025-11-04 09:59:57 -0800

    Upgrade production to composer-2.10.2-airflow-2.9.3 (#4463)

--- iac/cal-itp-data-infra/composer/us/environment.tf
+++ iac/cal-itp-data-infra/composer/us/environment.tf
@@ -36,7 +36,7 @@ resource "google_composer_environment" "calitp-composer" {
     environment_size = "ENVIRONMENT_SIZE_MEDIUM"
 
     software_config {
-      image_version = "composer-2.10.1-airflow-2.9.3"
+      image_version = "composer-2.10.2-airflow-2.9.3"
 
       airflow_config_overrides = {
         celery-worker_concurrency                  = 4
