c44e4c2e8b209a549388e4ebc2fee6d3a3203004
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-07 17:02:22 -0800

    Allow serving pages within JupyterHub image

--- images/jupyter-singleuser/jupyter_notebook_config.py
+++ images/jupyter-singleuser/jupyter_notebook_config.py
@@ -1,3 +1,9 @@
 c.FileCheckpoints.checkpoint_dir = (  # noqa: F821
     "/home/jovyan/.jupyter/.ipynb_checkpoints/"
 )
+c.JupyterHub.subdomain_host = "https://notebooks.calitp.org"  # noqa: F821
+c.Spawner.args = [  # noqa: F821
+    "--VoilaConfiguration.enable_nbextensions=True",
+    "--VoilaConfiguration.file_whitelist=['.*']",
+    '--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors \'self\'"}}',
+]
