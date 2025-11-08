44a19936aa53ce351641c7d9c458bf8fae3c51d1
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-07 17:06:53 -0800

    Add JH prototype image profile for 2025.11.7

--- images/jupyter-singleuser/jupyter_notebook_config.py
+++ images/jupyter-singleuser/jupyter_notebook_config.py
@@ -1,7 +1,6 @@
 c.FileCheckpoints.checkpoint_dir = (  # noqa: F821
     "/home/jovyan/.jupyter/.ipynb_checkpoints/"
 )
-c.JupyterHub.subdomain_host = "https://notebooks.calitp.org"  # noqa: F821
 c.Spawner.args = [  # noqa: F821
     "--VoilaConfiguration.enable_nbextensions=True",
     "--VoilaConfiguration.file_whitelist=['.*']",
