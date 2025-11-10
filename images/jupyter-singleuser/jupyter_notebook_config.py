05091e700bc94f93f7838fcdc6086c5c22d0700a
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-10 11:32:54 -0800

    Fix "Render Notebook with Voila" in JupyterHub (#4487)
    * Allow serving pages within JupyterHub image
    * [images/jupyter-singleuser] Bump version
    * Add JH prototype image profile for 2025.11.7

--- images/jupyter-singleuser/jupyter_notebook_config.py
+++ images/jupyter-singleuser/jupyter_notebook_config.py
@@ -1,3 +1,8 @@
 c.FileCheckpoints.checkpoint_dir = (  # noqa: F821
     "/home/jovyan/.jupyter/.ipynb_checkpoints/"
 )
+c.Spawner.args = [  # noqa: F821
+    "--VoilaConfiguration.enable_nbextensions=True",
+    "--VoilaConfiguration.file_whitelist=['.*']",
+    '--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors \'self\'"}}',
+]
