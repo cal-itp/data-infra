73563db1a3fbe63d369545f43e1277c24cd5aa74
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-11-11 01:01:02 +0000

    summarize fct_observed_trips to month/day_type/route-direction

---
+++ warehouse/models/mart/gtfs_rollup/fct_monthly_rt_route_direction_summary.sql
@@ -0,0 +1,76 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['month_first_day', 'schedule_name']
+    )
+}}
+
+WITH observed_trips AS (
+    SELECT *
+    FROM `cal-itp-data-infra.mart_gtfs.fct_observed_trips` --{{ ref('fct_observed_trips') }}
+    WHERE service_date >= "2025-10-01"
+    -- incremental; partitioned by service_date
+    -- clustered by service_date, schedule_base64_url
+),
+
+scheduled_trips AS (
+    SELECT
+        service_date,
+        base64_url,
+        trip_instance_key,
+        route_short_name,
+        route_long_name,
+        direction_id,
+
+    FROM `cal-itp-data-infra.mart_gtfs.fct_scheduled_trips` -- {{ ref('fct_scheduled_trips') }}
+    WHERE service_date >= "2025-10-01"
+    -- table; clustered by service_date'
+),
+
+route_direction_aggregation AS (
+    SELECT
+        EXTRACT(month FROM observed_trips.service_date) AS month,
+        EXTRACT(year FROM observed_trips.service_date) AS year,
+        DATE_TRUNC(observed_trips.service_date, MONTH) AS month_first_day,
+        {{ generate_day_type('observed_trips.service_date') }} AS day_type,
+
+        -- identifiers
+        observed_trips.schedule_base64_url,
+        observed_trips.tu_name,
+        observed_trips.vp_name,
+        observed_trips.schedule_name,
+        observed_trips.tu_base64_url,
+        observed_trips.vp_base64_url,
+
+        -- route direction columns
+        CONCAT(COALESCE(scheduled_trips.route_short_name, ""), ' ', COALESCE(scheduled_trips.route_long_name, "")) AS route_name,
+        scheduled_trips.direction_id,
+
+        -- metrics from trip updates
+        SUM(observed_trips.tu_num_distinct_updates) AS tu_num_distinct_updates,
+        SUM(observed_trips.tu_num_distinct_updates) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_num_distinct_updates, -- check column naming pattern for daily avg
+        SUM(observed_trips.tu_num_skipped_stops) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_num_skipped_stops,
+        SUM(observed_trips.tu_num_canceled_stops) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_num_canceled_stops,
+        SUM(observed_trips.tu_num_added_stops) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_num_added_stops,
+        SUM(observed_trips.tu_num_scheduled_stops) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_num_scheduled_stops,
+        COUNTIF(appeared_in_tu IS TRUE) AS n_tu_trips, -- total number of weekday trips with trip updates
+        COUNTIF(appeared_in_tu IS TRUE) / COUNT(DISTINCT observed_trips.service_date) AS daily_tu_trips, -- the average number of weekday trips with trip updates
+
+        -- metrics from vehicle positions
+        SUM(observed_trips.vp_num_distinct_updates) AS vp_num_distinct_updates,
+        SUM(observed_trips.vp_num_distinct_updates) / COUNT(DISTINCT observed_trips.service_date) AS daily_vp_num_distinct_updates,
+        COUNTIF(appeared_in_vp IS TRUE) AS n_vp_trips,
+        COUNTIF(appeared_in_vp IS TRUE) / COUNT(DISTINCT observed_trips.service_date) AS daily_vp_trips,
+
+        COUNT(DISTINCT observed_trips.trip_instance_key) AS n_trips,
+        COUNT(DISTINCT observed_trips.service_date) AS n_days,
+
+    FROM observed_trips
+    INNER JOIN scheduled_trips
+      ON observed_trips.service_date = scheduled_trips.service_date
+      AND observed_trips.schedule_base64_url = scheduled_trips.base64_url
+      AND observed_trips.trip_instance_key = scheduled_trips.trip_instance_key
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+)
+
+SELECT * FROM route_direction_aggregation
