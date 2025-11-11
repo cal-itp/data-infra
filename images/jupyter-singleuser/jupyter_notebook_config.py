822a33c2d57d9a5fce5a0d9a5a0f3015e55dc4ca
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-11 11:21:18 -0800

    Fix "Render Notebook with Voila" in JupyterHub, attempt 4 (#4501)
    * Fix 'Render Notebook with Voila' in JupyterHub
    * [images/jupyter-singleuser] Bump version
    * Change JH prototype image profile to 2025.11.10b

--- images/jupyter-singleuser/jupyter_notebook_config.py
+++ images/jupyter-singleuser/jupyter_notebook_config.py
@@ -6,5 +6,7 @@ c.JupyterHub.tornado_settings = {  # noqa: F821
     "headers": {"Content-Security-Policy": "frame-ancestors *;"}
 }
 c.Spawner.args = [  # noqa: F821
-    '--ServerApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors *;"}}'
+    '--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors *;"}}'
 ]
+
+print(f"jupyter_notebook_config: {c}")  # noqa: F821
