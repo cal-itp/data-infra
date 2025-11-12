417052fb9200c28f552754dac3357cca1a61ff79
Rae Bonfanti <raebot@users.noreply.github.com>
2025-11-11 16:01:25 -0800

    Fix "Render Notebook with Voila" in JupyterHub, attempt 5 (#4503)
    * set log level to debug for JupyterHub
    * [images/jupyter-singleuser] Bump version
    * Change JH prototype image profile to 2025.11.11a

--- images/jupyter-singleuser/jupyter_notebook_config.py
+++ images/jupyter-singleuser/jupyter_notebook_config.py
@@ -1,12 +1,3 @@
-c.FileCheckpoints.checkpoint_dir = (  # noqa: F821
-    "/home/jovyan/.jupyter/.ipynb_checkpoints/"
-)
+c = get_config()  # noqa
 
-c.JupyterHub.tornado_settings = {  # noqa: F821
-    "headers": {"Content-Security-Policy": "frame-ancestors *;"}
-}
-c.Spawner.args = [  # noqa: F821
-    '--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors *;"}}'
-]
-
-print(f"jupyter_notebook_config: {c}")  # noqa: F821
+c.FileCheckpoints.checkpoint_dir = "/home/jovyan/.jupyter/.ipynb_checkpoints/"
