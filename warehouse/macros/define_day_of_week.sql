5cc7107ae02cdc53e409ad1edf1eb61d8dc32392
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-11-13 04:03:29 +0000

    get route_id combined with route_name working

--- warehouse/macros/define_day_of_week.sql
+++ warehouse/macros/define_day_of_week.sql
@@ -8,3 +8,16 @@
     END
 
 {% endmacro %}
+
+{% macro generate_time_of_day_hours(time_of_day_column) %}
+    CASE
+        -- hours 0-3; 20-23
+        WHEN {{ time_of_day_column }} = "Owl" OR {{ time_of_day_column }} = "Evening" THEN 4
+        -- hours 4-6; 7-9
+        WHEN {{ time_of_day_column }}  = "Early AM" OR {{ time_of_day_column }}  = "AM Peak" THEN 3
+        -- hours 10-13, 14; 15-19
+        WHEN {{ time_of_day_column }}  = "Midday" OR {{ time_of_day_column }}  = "PM Peak" THEN 5
+        ELSE NULL
+    END
+
+{% endmacro %}
--- warehouse/macros/gtfs_route_labels.sql
+++ warehouse/macros/gtfs_route_labels.sql
@@ -2,45 +2,60 @@
     CASE
         -- operators using hyphens
         WHEN (
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Monterey Salinas") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "LA Metro") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "BART") OR
-             CONTAINS_SUBSTR({{ operator_name_column  }}, "MVGO")
-        ) THEN TRIM(SPLIT({{ route_id_column }}, "-")[SAFE_OFFSET(0)])
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Monterey Salinas") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "LA Metro") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "BART") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "MVGO")
+        ) THEN TRIM(SPLIT( {{ route_id_column }}, "-")[SAFE_OFFSET(0)])
         -- operators using underscores
-        WHEN CONTAINS_SUBSTR({{ operator_name_column  }}, "Roseville") THEN TRIM(SPLIT({{ route_id_column }}, "_")[SAFE_OFFSET(0)])
+        WHEN CONTAINS_SUBSTR( {{ operator_name_column  }}, "Roseville")
+            THEN TRIM(SPLIT( {{ route_id_column }}, "_")[SAFE_OFFSET(0)])
         ELSE TRIM( {{ route_id_column }} )
 
     END
 
 {% endmacro %}
 
-{% macro get_combined_route_name(operator_name_column, route_short_name_column, route_long_name_column) %}
+{% macro get_combined_route_name(operator_name_column, route_id_column, route_short_name_column, route_long_name_column) %}
 -- base off of latest commit
 -- https://github.com/cal-itp/data-analyses/blob/59dff810bc963de265709e803a166bc83d5aa3d7/rt_segment_speeds/segment_speed_utils/time_series_utils.py#L42-L105
     CASE
         -- operators where route_short_name adds duplicate info, just use route_long_name
         --Ex: route_short_name = 2; route_long_name = Route 2; combined name would be 2 Route 2
         WHEN (
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Antelope Valley Transit Authority") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Bay Area 511 ACE") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Bay Area 511 Emery Go-Round") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Bay Area 511 Petaluma") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Beach Cities GMV") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Bear") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Commerce") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Humboldt") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "LA DOT") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Lawndale Beat GMV") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Redding") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Redwood Coast") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Santa Maria") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "StanRTA") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Victor Valley GMV") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Visalia") OR
-            CONTAINS_SUBSTR({{ operator_name_column  }}, "Yolobus")
-        ) THEN {{ route_long_name_column }}
-        ELSE CONCAT( {{ route_short_name_column }}, " ", {{ route_long_name_column }} )
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Antelope Valley Transit Authority") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Bay Area 511 ACE") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Bay Area 511 Emery Go-Round") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Bay Area 511 Petaluma") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Beach Cities GMV") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Bear") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Commerce") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Humboldt") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "LA DOT") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Lawndale Beat GMV") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Redding") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Redwood Coast") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Santa Maria") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "StanRTA") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Victor Valley GMV") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Visalia") OR
+            CONTAINS_SUBSTR( {{ operator_name_column }}, "Yolobus")
+        ) THEN TRIM(
+            CONCAT(
+                {{ parse_route_id(operator_name_column, route_id_column) }},
+                "__",
+                COALESCE( {{ route_long_name_column }}, "")
+            )
+        )
+        ELSE TRIM(
+            CONCAT(
+                {{ parse_route_id(operator_name_column, route_id_column) }},
+                "__",
+                COALESCE( {{ route_short_name_column }}, ""),
+                " ",
+                COALESCE( {{ route_long_name_column }}, "")
+            )
+        )
 
     END
 
