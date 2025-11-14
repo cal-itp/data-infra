d77f4abc4eabb815e8c4bf3764bb6ff8006f6b32
Laurie Merrell <laurie.m@jarv.us>
2025-11-14 15:29:25 -0600

    revert changes to testing, do in a separate follow-up

--- jobs/gtfs-schedule-validator/test_gtfs_schedule_validator.py
+++ jobs/gtfs-schedule-validator/test_gtfs_schedule_validator.py
@@ -1,10 +1,7 @@
 from pathlib import Path
 
-# import pytest
 import toml
-from gtfs_schedule_validator_hourly import __version__  # , app
-
-# from typer.testing import CliRunner
+from gtfs_schedule_validator_hourly import __version__
 
 
 def test_version():
@@ -23,21 +20,3 @@ def test_versions_are_in_sync():
     package_init_version = __version__
 
     assert package_init_version == pyproject_version
-
-
-# class TestGtfsScheduleValidator:
-#     @pytest.fixture
-#     def runner(self) -> CliRunner:
-#         return CliRunner()
-
-# def test_no_extracts(self, runner):
-#     hour_start = "2020-02-20T20"
-#     result = runner.invoke(
-#         app,
-#         [
-#             "validate-hour",
-#             hour_start,
-#         ],
-#     )
-#     # assert result.exit_code == 0
-#     assert "found 0 extracts to process, exiting" in result.stdout
