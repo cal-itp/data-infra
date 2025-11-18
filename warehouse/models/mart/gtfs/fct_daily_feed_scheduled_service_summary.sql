6cd4968a82f4b60ea1c2fe3c7718bb4d7692e125
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-11-18 23:34:32 +0000

    add new columns for schedule service summary

--- warehouse/models/mart/gtfs/fct_daily_feed_scheduled_service_summary.sql
+++ warehouse/models/mart/gtfs/fct_daily_feed_scheduled_service_summary.sql
@@ -1,4 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    cluster_by = ['service_date'])
+}}
 
 WITH fct_daily_schedule_feeds AS (
     SELECT
@@ -8,25 +11,33 @@ WITH fct_daily_schedule_feeds AS (
 ),
 
 fct_scheduled_trips AS (
-
     SELECT *
     FROM {{ ref('fct_scheduled_trips') }}
+),
 
+fct_scheduled_stops AS (
+    SELECT
+        service_date,
+        feed_key,
+        stop_id,
+    FROM {{ ref('fct_daily_scheduled_stops') }}
 ),
 
 -- service that is actually scheduled
 summarize_service AS (
 
     SELECT
-        service_date,
-        feed_key,
+        fct_scheduled_trips.service_date,
+        fct_scheduled_trips.feed_key,
         gtfs_dataset_key,
         SUM(service_hours) AS ttl_service_hours,
         COUNT(DISTINCT trip_id) AS n_trips,
         MIN(trip_first_departure_sec) AS first_departure_sec,
         MAX(trip_last_arrival_sec) AS last_arrival_sec,
         SUM(num_stop_times) AS num_stop_times,
-        COUNT(DISTINCT route_id) AS n_routes,
+        COUNT(DISTINCT fct_scheduled_trips.route_id) AS n_routes,
+        COUNT(DISTINCT fct_scheduled_trips.shape_id) AS n_shapes,
+        COUNT(DISTINCT stop_id) AS n_stops,
         LOGICAL_OR(
             contains_warning_duplicate_stop_times_primary_key
         ) AS contains_warning_duplicate_stop_times_primary_key,
@@ -38,7 +49,10 @@ summarize_service AS (
         ) AS contains_warning_missing_foreign_key_stop_id
 
     FROM fct_scheduled_trips
-    WHERE service_date < CURRENT_DATE()
+    INNER JOIN fct_scheduled_stops
+        ON fct_scheduled_trips.service_date = fct_scheduled_stops.service_date
+        AND fct_scheduled_trips.feed_key = fct_scheduled_stops.feed_key
+    WHERE fct_scheduled_trips.service_date < CURRENT_DATE()
     GROUP BY service_date, feed_key, gtfs_dataset_key
 ),
 
@@ -55,6 +69,7 @@ fct_daily_feed_scheduled_service_summary AS (
         service.last_arrival_sec,
         COALESCE(service.num_stop_times, 0) AS num_stop_times,
         COALESCE(service.n_routes, 0) AS n_routes,
+        COALESCE(service.n_stops, 0) AS n_stops,
         service.contains_warning_duplicate_stop_times_primary_key,
         service.contains_warning_duplicate_trip_primary_key,
         service.contains_warning_missing_foreign_key_stop_id
