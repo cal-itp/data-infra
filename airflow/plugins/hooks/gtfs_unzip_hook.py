3d6d27934811903940331ae13f8aff4a132082c8
Erika Pacheco <erika@ministryofvelocity.com>
2025-12-05 17:19:20 -0800

    Analyst sees a consolidated GTFS Schedule unzip and parse workflow

--- airflow/plugins/hooks/gtfs_unzip_hook.py
+++ airflow/plugins/hooks/gtfs_unzip_hook.py
@@ -7,6 +7,30 @@ import pendulum
 from airflow.hooks.base import BaseHook
 
 
+class GTFSExtractedFile:
+    def __init__(
+        self,
+        current_date: pendulum.DateTime,
+        config: dict[str, str],
+        filename: str,
+        original_filename: str,
+        content: bytes,
+    ) -> None:
+        self.filename = filename
+        self.original_filename = original_filename
+        self.content = content
+        self.current_date = current_date
+        self.config = config
+
+    def metadata(self) -> dict[str, str]:
+        return {
+            "filename": self.filename,
+            "original_filename": self.original_filename,
+            "ts": self.current_date.isoformat(),
+            "extract_config": self.config,
+        }
+
+
 class GTFSUnzipResult:
     def __init__(
         self,
@@ -19,9 +43,7 @@ class GTFSUnzipResult:
         self._md5hash = hashlib.md5()
         self._files = []
         self._directories = []
-        self._content_name = None
-        self._content_original_name = None
-        self._content = None
+        self._extracted_files = []
 
     def valid(self) -> bool:
         if not self._directories:
@@ -56,10 +78,16 @@ class GTFSUnzipResult:
     def add_hash(self, content: bytes) -> None:
         self._md5hash.update(content)
 
-    def add_content(self, name: str, original_name: str, content: bytes) -> None:
-        self._content_name = name
-        self._content_original_name = original_name
-        self._content = content
+    def add_content(self, name: str, original_filename: str, content: bytes) -> None:
+        self._extracted_files.append(
+            GTFSExtractedFile(
+                filename=name,
+                original_filename=original_filename,
+                content=content,
+                config=self.extract_config(),
+                current_date=self.current_date,
+            )
+        )
 
     def files(self) -> list[str]:
         return sorted(self._files)
@@ -67,23 +95,11 @@ class GTFSUnzipResult:
     def directories(self) -> list[str]:
         return sorted(self._directories)
 
-    def content(self) -> bytes:
-        return self._content
-
     def md5hash(self) -> str:
         return self._md5hash.hexdigest()
 
-    def extracted_files(self) -> list[str]:
-        if self._content_name is None:
-            return []
-        return [
-            {
-                "filename": self._content_name,
-                "original_filename": self._content_original_name,
-                "ts": self.current_date.isoformat(),
-                "extract_config": self.extract_config(),
-            }
-        ]
+    def extracted_files(self) -> list[GTFSExtractedFile]:
+        return self._extracted_files
 
     def results(self) -> dict:
         return {
@@ -93,7 +109,7 @@ class GTFSUnzipResult:
             "zipfile_extract_md5hash": self.md5hash(),
             "zipfile_files": self.files(),
             "zipfile_dirs": self.directories(),
-            "extracted_files": self.extracted_files(),
+            "extracted_files": [ef.metadata() for ef in self.extracted_files()],
         }
 
 
@@ -105,7 +121,7 @@ class GTFSZip:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def extract(self, path_to_extract: str, result: GTFSUnzipResult) -> None:
+    def extract(self, filenames: str, result: GTFSUnzipResult) -> None:
         with zipfile.ZipFile(self.filename, "r") as zip_file:
             for name in zip_file.namelist():
                 if name.startswith("__MACOSX"):
@@ -119,24 +135,22 @@ class GTFSZip:
                 with zip_file.open(file_path) as child_file:
                     content = child_file.read()
                     result.add_hash(content)
-                    if (
-                        result.valid()
-                        and os.path.basename(file_path) == path_to_extract
-                    ):
+                    filename = os.path.basename(file_path)
+                    if result.valid() and filename in filenames:
                         result.add_content(
-                            name=path_to_extract,
-                            original_name=file_path,
+                            name=filename,
+                            original_filename=file_path,
                             content=content,
                         )
 
 
 class GTFSUnzipHook(BaseHook):
-    filename: str
+    filenames: list[str]
     current_date: pendulum.DateTime
 
-    def __init__(self, filename: str, current_date: pendulum.DateTime):
+    def __init__(self, filenames: list[str], current_date: pendulum.DateTime):
         super().__init__()
-        self.filename = filename
+        self.filenames = filenames
         self.current_date = current_date
 
     def run(
@@ -149,7 +163,7 @@ class GTFSUnzipHook(BaseHook):
 
         try:
             GTFSZip(filename=zipfile_path).extract(
-                path_to_extract=self.filename, result=result
+                filenames=self.filenames, result=result
             )
             if not result.valid():
                 raise ValueError(
