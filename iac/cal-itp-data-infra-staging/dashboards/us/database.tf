72b8fb6585799fcc07b9679593a5afa97e695171
Erika Pacheco <erika@ministryofvelocity.com>
2025-12-11 14:33:04 -0800

    Add mart_gtfs_audit dbt dataset to Metabase Staging

--- iac/cal-itp-data-infra-staging/dashboards/us/database.tf
+++ iac/cal-itp-data-infra-staging/dashboards/us/database.tf
@@ -9,6 +9,6 @@ resource "metabase_database" "bigquery" {
     service_account_key      = base64decode(google_service_account_key.metabase-staging-key.private_key)
     project_id               = "cal-itp-data-infra-staging"
     dataset_filters_type     = "inclusion"
-    dataset_filters_patterns = "mart_gtfs"
+    dataset_filters_patterns = "mart_gtfs,mart_gtfs_audit"
   }
 }
