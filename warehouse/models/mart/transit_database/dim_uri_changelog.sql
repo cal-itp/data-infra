104c16d94959f391a7f449bf884e772b4182e1f4
Anna Paten <anna.paten@dot.ca.gov>
2025-12-11 23:51:26 +0000

    linting (#4482)

--- warehouse/models/mart/transit_database/dim_uri_changelog.sql
+++ warehouse/models/mart/transit_database/dim_uri_changelog.sql
@@ -5,13 +5,13 @@ WITH dim AS (
     FROM {{ ref('int_transit_database__uri_changelog') }}
 ),
 dim_uri_changelog AS (
-    SELECT 
+    SELECT
         source_record_id,
         name,
         uri,
         first_downloaded_dt,
         last_downloaded_dt,
-    FROM dim 
+    FROM dim
     ORDER BY first_downloaded_dt DESC
 )
 SELECT * from dim_uri_changelog
