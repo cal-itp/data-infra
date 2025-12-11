2c9af9b7fba182c51db05cc8b1907169d34c4ab0
Anna Paten <anna.paten@dot.ca.gov>
2025-12-11 23:40:22 +0000

    fixed dbt errors (#4428)

--- warehouse/models/mart/transit_database/dim_uri_changelog.sql
+++ warehouse/models/mart/transit_database/dim_uri_changelog.sql
@@ -6,12 +6,12 @@ WITH dim AS (
 ),
 dim_uri_changelog AS (
     SELECT 
-        id,
+        source_record_id,
         name,
         uri,
         first_downloaded_dt,
         last_downloaded_dt,
-    FROM lagged_uri_table 
+    FROM dim 
     ORDER BY first_downloaded_dt DESC
 )
 SELECT * from dim_uri_changelog
