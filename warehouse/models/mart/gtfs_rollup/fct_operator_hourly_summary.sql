eaf58e8c31c7f35c067fd3311176a69eb47fdcaf
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-12-12 00:27:13 +0000

    fix hours that pass 24th hour

--- warehouse/models/mart/gtfs_rollup/fct_operator_hourly_summary.sql
+++ warehouse/models/mart/gtfs_rollup/fct_operator_hourly_summary.sql
@@ -31,17 +31,35 @@ deduped_analysis_name AS (
     ) = 1
 ),
 
+trips_by_departure_hour AS (
+   SELECT
+      *,
+        -- since trip_first_departure_sec can span beyond the 24 hour period
+        -- for this aggregation, we'll be getting hours that are beyond 24
+        -- edge cases might get swap dates near end of the month, but for the most part, this is fine
+        CAST(TRUNC(trips.trip_first_departure_sec / 3600) AS INT) AS departure_hour_raw,
+    FROM trips
+),
+
 hourly_aggregation AS (
     SELECT
         trips.month_first_day,
         trips.name,
         deduped_analysis_name.analysis_name,
         trips.day_type,
-        CAST(TRUNC(trips.trip_first_departure_sec / 3600) AS INT) AS departure_hour,
+        -- since trip_first_departure_sec can span beyond the 24 hour period
+        -- for this aggregation, we'll be getting hours that are beyond 24
+        -- edge cases might get swap dates near end of the month, but for the most part, this is fine
+        CASE
+          WHEN departure_hour_raw >= 24 THEN departure_hour_raw - 24
+          ELSE departure_hour_raw
+        END AS departure_hour,
 
-        COUNT(*) AS n_trips
+        COUNT(*) AS n_trips,
+        ROUND(SUM(service_hours), 2) AS service_hours,
+        COALESCE(ROUND(SUM(flex_service_hours), 2), 0) AS flex_service_hours
 
-    FROM trips
+    FROM trips_by_departure_hour AS trips
     INNER JOIN deduped_analysis_name
         ON trips.name = deduped_analysis_name.name
     GROUP BY 1, 2, 3, 4, 5
