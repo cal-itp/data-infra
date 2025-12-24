7520ad177a29595de6f47c807a16d88a27a44b95
Daniel Huang <tihuang@gmail.com>
2025-12-24 12:04:49 -0800

    add project_id to GTFS-RT alert resources and condition SQL query

--- iac/cal-itp-data-infra/logging/us/gtfs_rt_alert.tf
+++ iac/cal-itp-data-infra/logging/us/gtfs_rt_alert.tf
@@ -1,4 +1,9 @@
+# the empty data block is required to inherit the `provider "google"` block from provider.tf
+data "google_project" "project" {
+}
+
 resource "google_monitoring_notification_channel" "email" {
+  project      = data.google_project.project.project_id
   type         = "email"
   display_name = "GTFS-RT Archivers Email Alert Channel"
   description  = "Send gtfs-rt archivers alerts to email."
@@ -8,6 +13,7 @@ resource "google_monitoring_notification_channel" "email" {
 }
 
 resource "google_monitoring_alert_policy" "gtfs_rt_bucket_writes_alert" {
+  project               = data.google_project.project.project_id
   display_name          = "GTFS-RT Bucket Writes Alert"
   combiner              = "OR"
   notification_channels = [google_monitoring_notification_channel.email.name]
@@ -15,7 +21,7 @@ resource "google_monitoring_alert_policy" "gtfs_rt_bucket_writes_alert" {
     display_name = "Less than 1500 writes to specific bucket in 15 minutes"
     condition_sql {
       query = <<-QUERY
-      SELECT count(*) AS row_count FROM `projects/your-project-id/logs/cloudaudit.googleapis.com%2Fdata_access`
+      SELECT count(*) AS row_count FROM `projects/${data.google_project.project.project_id}/logs/cloudaudit.googleapis.com%2Fdata_access`
       WHERE resource.type = "gcs_bucket"
       AND protoPayload.methodName = "storage.objects.create"
       AND resource.labels.bucket_name = "calitp-gtfs-rt-raw-v2"
@@ -31,7 +37,7 @@ resource "google_monitoring_alert_policy" "gtfs_rt_bucket_writes_alert" {
     }
   }
   documentation {
-    content   = "This alert triggers when the number of writes to 'calitp-gtfs-rt-raw-v2/trip_updates' is less than 1500 in 15 minutes."
+    content   = "This alert triggers when the number of writes to 'calitp-gtfs-rt-raw-v2/trip_updates' is less than 7500 in 15 minutes."
     mime_type = "text/markdown"
   }
 }
