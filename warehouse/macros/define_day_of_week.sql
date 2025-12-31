a399863d689f7de1b03f33ea87db1c8c4c7be453
tiffanychu90 <tiffany.ku@dot.ca.gov>
2025-12-31 16:38:45 +0000

    add time-of-day macro

--- warehouse/macros/define_day_of_week.sql
+++ warehouse/macros/define_day_of_week.sql
@@ -9,14 +9,27 @@
 
 {% endmacro %}
 
+{% macro generate_time_of_day_column(hour_column) %}
+    CASE
+        WHEN {{ hour_column }} < 4 OR ( {{ hour_column }} >= 24 AND {{ hour_column }} < 28 ) THEN "owl"
+        WHEN {{ hour_column }} < 7 OR ( {{ hour_column }} >= 28 AND {{ hour_column }} < 31 ) THEN "early_am"
+        WHEN {{ hour_column }} < 10 OR ( {{ hour_column }} >= 31 AND {{ hour_column }} < 34 )THEN "am_peak"
+        WHEN {{ hour_column }} < 15 OR ( {{ hour_column }} >= 34 AND {{ hour_column }} < 39 ) THEN "midday"
+        WHEN {{ hour_column }} < 20 OR ( {{ hour_column }} >= 39 AND {{ hour_column }} < 44 ) THEN "pm_peak"
+        WHEN {{ hour_column }} < 24 OR ( {{ hour_column }} >= 44 AND {{ hour_column }} < 48 ) THEN "evening"
+        ELSE NULL
+    END
+
+{% endmacro %}
+
 {% macro generate_time_of_day_hours(time_of_day_column) %}
     CASE
         -- hours 0-3; 20-23
-        WHEN {{ time_of_day_column }} = "Owl" OR {{ time_of_day_column }} = "Evening" THEN 4
+        WHEN {{ time_of_day_column }} = "owl" OR {{ time_of_day_column }} = "evening" THEN 4
         -- hours 4-6; 7-9
-        WHEN {{ time_of_day_column }}  = "Early AM" OR {{ time_of_day_column }}  = "AM Peak" THEN 3
+        WHEN {{ time_of_day_column }}  = "early_am" OR {{ time_of_day_column }}  = "am_peak" THEN 3
         -- hours 10-14; 15-19
-        WHEN {{ time_of_day_column }}  = "Midday" OR {{ time_of_day_column }}  = "PM Peak" THEN 5
+        WHEN {{ time_of_day_column }}  = "midday" OR {{ time_of_day_column }}  = "pm_peak" THEN 5
         ELSE NULL
     END
 
