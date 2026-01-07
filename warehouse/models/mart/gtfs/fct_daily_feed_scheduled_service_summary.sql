5c82d0b51d2a592a35a343fe2e41c46d677dcbe2
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-01-07 02:24:29 +0000

    refactor schedule feed summary, also add gtfs_dataset_name

--- warehouse/models/mart/gtfs/fct_daily_feed_scheduled_service_summary.sql
+++ warehouse/models/mart/gtfs/fct_daily_feed_scheduled_service_summary.sql
@@ -1,18 +1,22 @@
-{{ config(
-    materialized='table',
-    cluster_by = ['service_date'])
+{{
+    config(
+        materialized='table',
+        cluster_by = ['service_date']
+    )
 }}
 
 WITH fct_daily_schedule_feeds AS (
     SELECT
         *,
         EXTRACT(DAYOFWEEK FROM date) AS day_num
-    FROM {{ ref('fct_daily_schedule_feeds') }}
+    FROM `cal-itp-data-infra.mart_gtfs.fct_daily_schedule_feeds` AS t1
+    WHERE t1.date >= "2022-01-01"--{{ ref('fct_daily_schedule_feeds') }}
 ),
 
 fct_scheduled_trips AS (
     SELECT *
-    FROM {{ ref('fct_scheduled_trips') }}
+    FROM `cal-itp-data-infra.mart_gtfs.fct_scheduled_trips`--{{ ref('fct_scheduled_trips') }}
+    WHERE service_date >= "2022-01-01"
 ),
 
 fct_scheduled_stops AS (
@@ -20,24 +24,22 @@ fct_scheduled_stops AS (
         service_date,
         feed_key,
         stop_id,
-    FROM {{ ref('fct_daily_scheduled_stops') }}
+    FROM `cal-itp-data-infra.mart_gtfs.fct_daily_scheduled_stops`--{{ ref('fct_daily_scheduled_stops') }}
+    WHERE service_date >= "2022-01-01"
 ),
 
--- service that is actually scheduled
-summarize_service AS (
-
+trip_summary AS (
     SELECT
-        fct_scheduled_trips.service_date,
-        fct_scheduled_trips.feed_key,
+        service_date,
+        feed_key,
         gtfs_dataset_key,
         SUM(service_hours) AS ttl_service_hours,
         COUNT(DISTINCT trip_id) AS n_trips,
         MIN(trip_first_departure_sec) AS first_departure_sec,
         MAX(trip_last_arrival_sec) AS last_arrival_sec,
         SUM(num_stop_times) AS num_stop_times,
-        COUNT(DISTINCT fct_scheduled_trips.route_id) AS n_routes,
-        COUNT(DISTINCT fct_scheduled_trips.shape_id) AS n_shapes,
-        COUNT(DISTINCT stop_id) AS n_stops,
+        COUNT(DISTINCT route_id) AS n_routes,
+        COUNT(DISTINCT shape_id) AS n_shapes,
         LOGICAL_OR(
             contains_warning_duplicate_stop_times_primary_key
         ) AS contains_warning_duplicate_stop_times_primary_key,
@@ -47,13 +49,29 @@ summarize_service AS (
         LOGICAL_OR(
             contains_warning_missing_foreign_key_stop_id
         ) AS contains_warning_missing_foreign_key_stop_id
-
     FROM fct_scheduled_trips
-    INNER JOIN fct_scheduled_stops
-        ON fct_scheduled_trips.service_date = fct_scheduled_stops.service_date
-        AND fct_scheduled_trips.feed_key = fct_scheduled_stops.feed_key
-    WHERE fct_scheduled_trips.service_date < CURRENT_DATE()
+    WHERE service_date < CURRENT_DATE()
     GROUP BY service_date, feed_key, gtfs_dataset_key
+),
+
+stop_summary AS (
+    SELECT
+        service_date,
+        feed_key,
+        COUNT(DISTINCT stop_id) AS n_stops
+    FROM fct_scheduled_stops
+    GROUP BY service_date, feed_key
+),
+
+-- service that is actually scheduled
+summarize_service AS (
+    SELECT
+        trip_summary.*,
+        stop_summary.* EXCEPT(service_date, feed_key)
+    FROM trip_summary
+    INNER JOIN stop_summary
+        ON trip_summary.service_date = stop_summary.service_date
+        AND trip_summary.feed_key = stop_summary.feed_key
 ),
 
 -- left join with feeds to include information about feeds with no service scheduled
@@ -63,6 +81,7 @@ fct_daily_feed_scheduled_service_summary AS (
         DATE(feeds.date) AS service_date,
         feeds.feed_key,
         feeds.gtfs_dataset_key,
+        feeds.gtfs_dataset_name,
         COALESCE(service.ttl_service_hours, 0) AS ttl_service_hours,
         COALESCE(service.n_trips, 0) AS n_trips,
         service.first_departure_sec,
