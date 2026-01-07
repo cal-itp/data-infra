003796ca008cee7b255f8a9037ceadd67d5be46f
Erika Pacheco <erika@ministryofvelocity.com>
2026-01-07 14:52:25 -0800

    Move code from materialized view 'int_transit_database__gtfs_datasets_dim' to a new GTFS download config view 'stg_gtfs_schedule__download_configs'
    [#4653]

--- warehouse/models/intermediate/transit_database/dimensions/int_transit_database__gtfs_datasets_dim.sql
+++ warehouse/models/intermediate/transit_database/dimensions/int_transit_database__gtfs_datasets_dim.sql
@@ -1,49 +1,7 @@
 {{ config(materialized='table') }}
 
 WITH int_transit_database__gtfs_datasets_dim AS (
-    SELECT key,
-           source_record_id,
-           name,
-           data,
-           data_quality_pipeline,
-           manual_check__link_to_dataset_on_website,
-           manual_check__accurate_shapes,
-           manual_check__data_license,
-           manual_check__authentication_acceptable,
-           manual_check__stable_url,
-           manual_check__localized_stop_tts,
-           manual_check__grading_scheme_v1,
-           regional_feed_type,
-           fares_v2_status,
-           fares_notes,
-           pathways_status,
-           schedule_comments,
-           uri,
-           future_uri,
-           pipeline_url,
-           aggregated_to_gtfs_dataset_key,
-           provider_gtfs_capacity,
-           notes,
-           provider,
-           operator,
-           gtfs_service_mapping,
-           dataset_producers,
-           schedule_to_use_for_rt_validation_gtfs_dataset_key,
-           dataset_publisher,
-           deprecated_date,
-           authorization_url_parameter_name,
-           url_secret_key_name,
-           authorization_header_parameter_name,
-           header_secret_key_name,
-           url_to_encode,
-           base64_url,
-           type,
-           private_dataset,
-           analysis_name,
-           _is_current,
-           _valid_from,
-           _valid_to
-      FROM {{ ref('int_gtfs_datasets') }}
+    SELECT * FROM {{ ref('stg_gtfs_schedule__download_configs') }}
 )
 
 SELECT * FROM int_transit_database__gtfs_datasets_dim
