bc727a631a6475fde10f32f64132475125852563
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-01-09 18:06:17 +0000

    use incremental_where macro

--- warehouse/models/mart/gtfs/fct_stop_time_metrics.sql
+++ warehouse/models/mart/gtfs/fct_stop_time_metrics.sql
@@ -13,8 +13,11 @@
 WITH int_tu_trip_stop AS (
     SELECT *
     FROM {{ ref('int_gtfs_rt__trip_updates_trip_stop_day_map_grouping') }}
-    WHERE dt >= "2025-12-01" AND dt <= "2025-12-02"
-
+    WHERE {{ incremental_where(
+        default_start_var='PROD_GTFS_RT_START',
+        this_dt_column="service_date",
+        filter_dt_column="dt"
+    ) }} AND dt >= "2025-12-01"
 ),
 
 trip_stop_with_trip_keys AS (
