1da4a996b54941c490c50e1b9663f414246d78b0
Erika <erika@ministryofvelocity.com>
2026-01-12 13:42:06 -0800

    Updates to the new combined GTFS Download DAG `download_parse_and_validate_gtfs` (#4676)
    * Fix sequence and trigger rule to not skip download config task
    
    [#4653]
    
    * Add specific pools to GTFS schedule tasks
    
    * Generate download results file when download file exists but results is missing
    
    * Remove temp rule for larger files

--- airflow/dags/download_parse_and_validate_gtfs.py
+++ airflow/dags/download_parse_and_validate_gtfs.py
@@ -89,6 +89,7 @@ def download_parse_and_validate_gtfs():
         feed_type="schedule",
         source_bucket=os.getenv("CALITP_BUCKET__GTFS_DOWNLOAD_CONFIG"),
         source_path="gtfs_download_configs/dt={{ ds }}/ts={{ ts }}/configs.jsonl.gz",
+        trigger_rule=TriggerRule.NONE_FAILED,
     )
 
     downloads = DownloadConfigToGCSOperator.partial(
@@ -129,6 +130,7 @@ def download_parse_and_validate_gtfs():
         source_bucket=os.getenv("CALITP_BUCKET__GTFS_SCHEDULE_RAW"),
         map_index_template="{{ task.download_schedule_feed_results['config']['name'] }}",
         trigger_rule=TriggerRule.ALL_DONE,
+        pool="schedule_validate_pool",
     ).expand_kwargs(downloads.output.map(create_validate_kwargs))
 
     def unzip_files_kwargs(download):
@@ -151,6 +153,7 @@ def download_parse_and_validate_gtfs():
         results_path="unzipping_results/dt={{ ds }}/ts={{ ts }}/{{ task.base64_url }}.jsonl",
         map_index_template="{{ task.download_schedule_feed_results['config']['name'] }}",
         trigger_rule=TriggerRule.ALL_DONE,
+        pool="schedule_unzip_pool",
     ).expand_kwargs(downloads.output.map(unzip_files_kwargs))
 
     def list_unzipped_files(unzipped_file):
@@ -177,14 +180,12 @@ def download_parse_and_validate_gtfs():
         destination_bucket=os.getenv("CALITP_BUCKET__GTFS_SCHEDULE_PARSED_HOURLY"),
         map_index_template="{{ task.unzip_results['extract']['config']['name'] }}",
         trigger_rule=TriggerRule.ALL_DONE,
+        pool="schedule_parse_pool",
     ).expand_kwargs(unzip.output.map(list_unzipped_files))
 
-    (
-        latest_only
-        >> download_config_exists
-        >> (download_config, skip_download_config)
-        >> schedule_download_configs
-    )
+    (latest_only >> download_config_exists >> (download_config, skip_download_config))
+
+    ((download_config, skip_download_config) >> schedule_download_configs)
 
     (schedule_download_configs >> downloads >> (validate, (unzip >> convert)))
 
