c8e4e10711fba7c930b2023b72a4c60f63804042
Erika <erika@ministryofvelocity.com>
2026-01-15 17:32:29 -0800

    WIP: Replaced poetry by uv command
    Still need to test!

--- warehouse/scripts/dbt_artifacts/gen_artifacts.sh
+++ warehouse/scripts/dbt_artifacts/gen_artifacts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-poetry run datamodel-codegen --url https://schemas.getdbt.com/dbt/catalog/v1.json --output "$SCRIPT_DIR"/catalog.py --field-constraints --use-double-quotes --allow-extra-fields
-poetry run datamodel-codegen --url https://schemas.getdbt.com/dbt/manifest/v9.json --output "$SCRIPT_DIR"/manifest.py --field-constraints --use-double-quotes --allow-extra-fields
-poetry run datamodel-codegen --url https://schemas.getdbt.com/dbt/run-results/v4.json --output "$SCRIPT_DIR"/run_results.py --field-constraints --use-double-quotes --allow-extra-fields
-poetry run datamodel-codegen --url https://schemas.getdbt.com/dbt/sources/v3.json --output "$SCRIPT_DIR"/sources.py --field-constraints --use-double-quotes --allow-extra-fields
+uv run datamodel-codegen --url https://schemas.getdbt.com/dbt/catalog/v1.json --output "$SCRIPT_DIR"/catalog.py --field-constraints --use-double-quotes --allow-extra-fields
+uv run datamodel-codegen --url https://schemas.getdbt.com/dbt/manifest/v9.json --output "$SCRIPT_DIR"/manifest.py --field-constraints --use-double-quotes --allow-extra-fields
+uv run datamodel-codegen --url https://schemas.getdbt.com/dbt/run-results/v4.json --output "$SCRIPT_DIR"/run_results.py --field-constraints --use-double-quotes --allow-extra-fields
+uv run datamodel-codegen --url https://schemas.getdbt.com/dbt/sources/v3.json --output "$SCRIPT_DIR"/sources.py --field-constraints --use-double-quotes --allow-extra-fields
