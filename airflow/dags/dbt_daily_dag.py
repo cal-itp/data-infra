f4dcde025c18d28e4da9bd33dc4bec6448a82a0d
Erika <erika@ministryofvelocity.com>
2026-02-03 14:34:01 -0800

    Revert "Set dbt_all and dbt_daily to start earlier at 4am PDT/5am PST (12pm UTC) (#4631)"
    This reverts commit b24eb533f4c3590567d4bbbdd1b43e20f336f99a.

--- airflow/dags/dbt_daily_dag.py
+++ airflow/dags/dbt_daily_dag.py
@@ -30,8 +30,8 @@ default_args = {
 with DAG(
     dag_id="dbt_daily",
     tags=["dbt", "daily"],
-    # Sunday, Tuesday, Wednesday, Friday, Saturday at 4am PDT/5am PST (12pm UTC)
-    schedule="0 12 * * 0,2,3,5,6",
+    #  Sunday, Tuesday, Wednesday, Friday, Saturday at 7am PDT/8am PST (2pm UTC)
+    schedule="0 14 * * 0,2,3,5,6",
     start_date=datetime(2025, 8, 19),
     catchup=False,
     default_args={
