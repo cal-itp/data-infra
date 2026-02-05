62c681d66679c9e815170bee793a04e6756e33ba
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-02-05 22:10:05 +0000

    refactor downstream tables to use schedule+RT route summary

--- warehouse/models/mart/gtfs/fct_daily_rt_service_summary.sql
+++ warehouse/models/mart/gtfs/fct_daily_rt_service_summary.sql
@@ -5,53 +5,59 @@
     )
 }}
 
-WITH fct_observed_trips AS (
+WITH rt_route_summary AS (
     SELECT *
-    FROM {{ ref('fct_observed_trips' )}}
-),
-
-observed_trips AS (
-    SELECT
-        *,
-        -- num_distinct_message_keys = num_distinct_extract_ts and
-        -- num_distinct_message_keys / extract_duration_minutes = messages per minute this entity was present for, how continuously this trip was updated.
-        SAFE_DIVIDE(vp_num_distinct_extract_ts, vp_extract_duration_minutes) AS vp_messages_per_minute,
-        SAFE_DIVIDE(tu_num_distinct_extract_ts, tu_extract_duration_minutes) AS tu_messages_per_minute,
-    FROM fct_observed_trips
+    FROM {{ ref('fct_daily_schedule_rt_route_direction_summary') }}
+    WHERE n_vp_trips > 0 OR n_tu_trips > 0
+    -- filter to only where RT was present
+    -- this table aggregates across vp_base64_url, tu_base64_url
+    -- and handles the fact that trips may not have both tu and vp
+    -- but we want to see the quartet together
 ),
 
 summarize_service AS (
     SELECT
         service_date,
         schedule_base64_url,
-        -- found North County Trip Updates which had only schedule_base64_url and nulls for key and name
-        MAX(schedule_gtfs_dataset_key) AS schedule_gtfs_dataset_key,
-        MAX(schedule_name) AS schedule_name,
+        schedule_gtfs_dataset_name,
+        schedule_gtfs_dataset_key,
+        feed_key,
 
-        -- there can be trip_instance_keys where vp is present but not tu and vice versa
-        -- if they share the same schedule_name, fill it in, since we're aggregating
-        -- to operator.
-        MAX(vp_gtfs_dataset_key) AS vp_gtfs_dataset_key,
-        MAX(vp_name) AS vp_name,
-        MAX(vp_base64_url) AS vp_base64_url,
-        MAX(tu_gtfs_dataset_key) AS tu_gtfs_dataset_key,
-        MAX(tu_name) AS tu_name,
-        MAX(tu_base64_url) AS tu_base64_url,
+        tu_gtfs_dataset_key,
+        tu_name,
+        tu_base64_url,
+        vp_gtfs_dataset_key,
+        vp_name,
+        vp_base64_url,
 
-        -- vehicle positions
-        -- take average of vp per minute, every trip is equally weighted
-        ROUND(AVG(vp_messages_per_minute), 2) AS vp_messages_per_minute,
-        -- follow fct_daily_trip_updates_vehicle_positions_completeness
-        COALESCE(COUNTIF(vp_num_distinct_message_ids > 0), 0) AS n_vp_trips,
-        SUM(vp_extract_duration_minutes) AS vp_extract_duration_minutes,
+        SUM(n_trips) AS n_trips,
+        COUNT(DISTINCT route_id) AS n_routes,
+
+        LOGICAL_OR(appeared_in_tu) AS appeared_in_tu,
+        LOGICAL_OR(appeared_in_vp) AS appeared_in_vp,
 
         -- trip updates
-        ROUND(AVG(tu_messages_per_minute), 2) AS tu_messages_per_minute,
-        COALESCE(COUNTIF(tu_num_distinct_message_ids > 0), 0) AS n_tu_trips,
+        SUM(tu_num_distinct_updates) AS tu_num_distinct_updates,
+        SUM(n_tu_trips) AS n_tu_trips,
+        COUNT(DISTINCT IF(appeared_in_tu IS TRUE, route_id, NULL)) AS n_tu_routes,
         SUM(tu_extract_duration_minutes) AS tu_extract_duration_minutes,
+        ROUND(
+            SAFE_DIVIDE(SUM(tu_num_distinct_updates),
+            SUM(tu_extract_duration_minutes)
+        ), 2) AS tu_messages_per_minute,
 
-    FROM observed_trips
-    GROUP BY service_date, schedule_base64_url
+        -- vehicle positions
+        SUM(vp_num_distinct_updates) AS vp_num_distinct_updates,
+        SUM(n_vp_trips) AS n_vp_trips,
+        COUNT(DISTINCT IF(appeared_in_vp IS TRUE, route_id, NULL)) AS n_vp_routes,
+        SUM(vp_extract_duration_minutes) AS vp_extract_duration_minutes,
+        ROUND(
+            SAFE_DIVIDE(SUM(vp_num_distinct_updates),
+            SUM(vp_extract_duration_minutes)
+        ), 2) AS vp_messages_per_minute,
+
+    FROM rt_route_summary
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
 )
 
 SELECT * FROM summarize_service
--- warehouse/models/mart/gtfs_rollup/fct_monthly_operator_summary.sql
+++ warehouse/models/mart/gtfs_rollup/fct_monthly_operator_summary.sql
@@ -19,7 +19,10 @@ day_counts_by_availability AS (
         *
     FROM (
         SELECT
-            schedule_name,
+            COALESCE(gtfs_dataset_name, schedule_name) AS schedule_name,
+            schedule_base64_url,
+            vp_base64_url,
+            tu_base64_url,
             service_date,
             month_first_day,
             day_type,
@@ -38,6 +41,9 @@ day_counts_by_availability AS (
 pivoted AS (
     SELECT
         schedule_name,
+        schedule_base64_url,
+        vp_base64_url,
+        tu_base64_url,
         month_first_day,
         day_type,
         SUM(n_days_schedule_only) AS n_days_schedule_only,
@@ -46,9 +52,8 @@ pivoted AS (
         SUM(n_days_schedule_and_tu_only) AS n_days_schedule_and_tu_only,
 
     FROM day_counts_by_availability
-    GROUP BY 1, 2, 3
+    GROUP BY 1, 2, 3, 4, 5, 6
 ),
-
 
 monthly_summary AS (
     SELECT
@@ -56,39 +61,56 @@ monthly_summary AS (
         EXTRACT(month FROM daily_summary2.month_first_day) AS month,
         EXTRACT(year FROM daily_summary2.month_first_day) AS year,
         daily_summary2.month_first_day,
-        daily_summary2.schedule_name,
-        schedule_base64_url,
+
+        COALESCE(daily_summary2.gtfs_dataset_name, daily_summary2.schedule_name) AS schedule_name,
+        daily_summary2.schedule_base64_url,
         vp_name,
-        vp_base64_url,
+        daily_summary2.vp_base64_url,
         tu_name,
-        tu_base64_url,
+        daily_summary2.tu_base64_url,
 
         daily_summary2.day_type,
 
         SUM(n_trips) AS n_trips,
         ROUND(SUM(n_trips) / COUNT(DISTINCT service_date), 1) AS daily_trips,
         ROUND(SUM(ttl_service_hours), 1) AS ttl_service_hours,
-        AVG(n_routes) AS n_routes,
-        AVG(n_shapes) AS n_shapes,
-        AVG(n_stops) AS n_stops,
+        ROUND(SUM(ttl_service_hours) / COUNT(DISTINCT service_date), 1) AS daily_service_hours,
+
+        MAX(n_routes) AS n_routes,
+        MAX(n_shapes) AS n_shapes,
+        MAX(n_stops) AS n_stops,
+        SUM(num_stop_times) AS num_stop_times,
+        ROUND(SUM(num_stop_times) / COUNT(DISTINCT service_date), 1) AS daily_arrivals,
+
         COUNT(DISTINCT service_date) AS n_days,
+        COUNT(DISTINCT feed_key) AS n_feeds,
 
         ROUND(AVG(vp_messages_per_minute), 1) AS vp_messages_per_minute,
         SUM(n_vp_trips) AS n_vp_trips,
         ROUND(SUM(n_vp_trips) / COUNT(DISTINCT service_date), 1) AS daily_vp_trips,
         ROUND(AVG(pct_vp_trips), 3) AS pct_vp_trips,
-        ROUND(AVG(pct_vp_service_hours), 3) AS pct_vp_service_hours,
+        MAX(pct_vp_routes) AS pct_vp_routes, -- should the max be used for coverage?
+        SUM(vp_extract_duration_minutes) / COUNT(DISTINCT service_date) AS daily_vp_extract_duration_minutes,
 
         ROUND(AVG(tu_messages_per_minute), 1) AS tu_messages_per_minute,
         SUM(n_tu_trips) AS n_tu_trips,
         ROUND(SUM(n_tu_trips) / COUNT(DISTINCT service_date), 1) AS daily_tu_trips,
         ROUND(AVG(pct_tu_trips), 3) AS pct_tu_trips,
-        ROUND(AVG(pct_tu_service_hours), 3) AS pct_tu_service_hours,
+        MAX(pct_tu_routes) AS pct_tu_routes, -- should the max be used for coverage?
+        SUM(tu_extract_duration_minutes) / COUNT(DISTINCT service_date) AS daily_tu_extract_duration_minutes,
+
+        MAX(pivoted.n_days_schedule_only) AS n_days_schedule_only,
+        MAX(pivoted.n_days_schedule_and_rt) AS n_days_schedule_and_rt,
+        MAX(pivoted.n_days_schedule_and_vp_only) AS n_days_schedule_and_vp_only,
+        MAX(pivoted.n_days_schedule_and_tu_only) AS n_days_schedule_and_tu_only,
 
     FROM daily_summary2
     LEFT JOIN pivoted
         ON daily_summary2.month_first_day = pivoted.month_first_day
         AND daily_summary2.schedule_name = pivoted.schedule_name
+        AND daily_summary2.schedule_base64_url = pivoted.schedule_base64_url
+        AND daily_summary2.vp_base64_url = pivoted.vp_base64_url
+        AND daily_summary2.tu_base64_url = pivoted.tu_base64_url
         AND daily_summary2.day_type = pivoted.day_type
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 )
