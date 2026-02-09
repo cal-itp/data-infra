e1703946c81e3b673edbc2a8dd865218c347c74c
Rae Bonfanti <raebot@users.noreply.github.com>
2026-02-09 10:24:52 -0800

    Reduce analysis staging site caching (#4782)

--- iac/cal-itp-data-infra-staging/cdn/us/backend_bucket.tf
+++ iac/cal-itp-data-infra-staging/cdn/us/backend_bucket.tf
@@ -49,8 +49,8 @@ resource "google_compute_backend_bucket" "calitp-analysis-staging" {
   enable_cdn  = true
   cdn_policy {
     cache_mode        = "CACHE_ALL_STATIC"
-    client_ttl        = 3600
-    default_ttl       = 3600
+    client_ttl        = 300
+    default_ttl       = 300
     max_ttl           = 86400
     negative_caching  = true
     serve_while_stale = 86400
