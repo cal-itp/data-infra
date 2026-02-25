a406b780eb431171337c3b6278c5a8bd094a2635
Erika <erika@ministryofvelocity.com>
2026-02-24 19:05:45 -0800

    Fix build GTFS RT archiver image

--- services/gtfs-rt-archiver-v3/gtfs_rt_archiver_v3/__main__.py
+++ services/gtfs-rt-archiver-v3/gtfs_rt_archiver_v3/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
 print(
-    "This isn't very useful by itself; you probably want gtfs_rt_archiver_v2.ticker or gtfs_rt_archiver_v2.consumer"
+    "This isn't very useful by itself; you probably want gtfs_rt_archiver_v3.ticker or gtfs_rt_archiver_v3.consumer"
 )
 sys.exit(1)
