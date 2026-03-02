47c9caf93570d923904243b6e139b121140c3384
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-03-02 18:32:09 +0000

    add more to stop_order by route to better aggregate stops across trips

--- warehouse/models/intermediate/gtfs/int_gtfs_schedule__stop_order_by_route.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_schedule__stop_order_by_route.sql
@@ -10,16 +10,52 @@ WITH dim_stop_arrivals AS (
     FROM {{ ref('dim_stop_arrivals') }}
 ),
 
-ordered_stops AS (
+dim_routes AS (
     SELECT
         feed_key,
         route_id,
-        direction_id,
+        route_short_name,
+        route_long_name
+    FROM {{ ref('dim_routes') }}
+),
+
+daily_feeds AS (
+    SELECT
+        feed_key,
+        base64_url,
+        gtfs_dataset_key,
+        gtfs_dataset_name,
+        CAST(MIN(t1.date) AS DATE) AS _valid_from_service_date,
+        CAST(MAX(t1.date) AS DATE) AS _valid_to_service_date,
+
+    FROM {{ ref('fct_daily_schedule_feeds') }} AS t1
+    GROUP BY 1, 2, 3, 4
+),
+
+ordered_stops AS (
+    SELECT
+        dim_stop_arrivals.feed_key,
+        daily_feeds.base64_url,
+        daily_feeds.gtfs_dataset_key,
+        daily_feeds.gtfs_dataset_name,
+        dim_stop_arrivals.route_id,
+        dim_routes.route_short_name,
+        dim_routes.route_long_name,
+        dim_stop_arrivals.direction_id,
+        {{ get_combined_route_name('daily_feeds.gtfs_dataset_name', 'dim_stop_arrivals.route_id',
+            'dim_routes.route_short_name', 'dim_routes.route_long_name') }} AS route_name,
         stop_id,
-        ROUND(AVG(stop_sequence), 1) AS avg_stop_seq
+        ROUND(AVG(stop_sequence), 1) AS avg_stop_seq,
+        MIN(_valid_from_service_date) AS _valid_from_service_date,
+        MAX(_valid_to_service_date) AS _valid_to_service_date
 
     FROM dim_stop_arrivals
-    GROUP BY feed_key, route_id, direction_id, stop_id
+    INNER JOIN dim_routes
+        ON dim_stop_arrivals.feed_key = dim_routes.feed_key
+        AND dim_stop_arrivals.route_id = dim_routes.route_id
+    INNER JOIN daily_feeds
+        ON dim_stop_arrivals.feed_key = daily_feeds.feed_key
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 )
 
 SELECT * FROM ordered_stops
