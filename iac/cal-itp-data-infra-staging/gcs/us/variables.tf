31f2193de7471e924fed419bd0b971012cc7743c
Erika <erika@ministryofvelocity.com>
2026-03-02 17:02:33 -0800

    Create GTFS Manual bucket

--- iac/cal-itp-data-infra-staging/gcs/us/variables.tf
+++ iac/cal-itp-data-infra-staging/gcs/us/variables.tf
@@ -11,6 +11,7 @@ locals {
     "calitp-staging-gtfs-rt-parsed",
     "calitp-staging-gtfs-rt-raw-v2",
     "calitp-staging-gtfs-rt-validation",
+    "calitp-staging-gtfs-schedule-manual",
     "calitp-staging-gtfs-schedule-parsed",
     "calitp-staging-gtfs-schedule-parsed-hourly",
     "calitp-staging-gtfs-schedule-raw-v2",
