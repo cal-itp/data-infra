34b0d3f0946aaf59fa7eff7bc5084cdc18b1ea1a
Laurie Merrell <laurie.m@jarv.us>
2026-03-03 18:19:05 -0600

    feat: update dim_stop_arrivals microbatch strategy based on testing

--- warehouse/models/mart/gtfs/dim_stop_arrivals.sql
+++ warehouse/models/mart/gtfs/dim_stop_arrivals.sql
@@ -4,12 +4,14 @@
         incremental_strategy='microbatch',
         event_time = '_feed_valid_from',
         batch_size = 'day',
-        begin='2022-10-01',
+        begin=var('GTFS_SCHEDULE_START'),
+        lookback=5,
         partition_by={
             'field': '_feed_valid_from',
             'data_type': 'timestamp',
             'granularity': 'day',
         },
+        full_refresh=false,
         cluster_by='feed_key'
     )
 }}
