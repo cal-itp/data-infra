c7010cbcf865f65efb78d84b3432cf6543d3cb68
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2026-03-25 12:27:34 -0700

    Consolidate rollup route-direction summary and fix operator summaries when schedule has multiple RT feeds (#4982)
    * amtrak dropped off, keep if schedule had service, even if RT didn't
    
    * switch refs back
    
    * move frequency to daily table
    
    * switch to schedule_name
    
    * add route_type
    
    * get monthly table from daily averages
    
    * remove individual models, consolidated in schedule+RT monthly model
    
    * remove models from yml
    
    * add pos/neg percentiles, refactor to use daily freq averaged
    
    * just return percentile column once
    
    * rename tables in macro
    
    * need to reverse pos percentiles for chart
    
    * add dim_provider_gtfs_data to link quartet
    
    * use schedule_name earlier
    
    * remove rows with nulls if we find it elsewhere
    
    * check that multiple RT sharing schedule is aggregated correctly
    
    * rerun monthly operator summary
    
    * switch refs back
    
    * add route type into daily route-dir summary

--- warehouse/models/mart/gtfs/fct_daily_schedule_rt_route_direction_summary.sql
+++ warehouse/models/mart/gtfs/fct_daily_schedule_rt_route_direction_summary.sql
@@ -6,15 +6,22 @@
 }}
 
 WITH schedule_trips AS (
-    SELECT *
-    FROM {{ ref('fct_scheduled_trips') }}
+    SELECT * FROM {{ ref('fct_scheduled_trips') }}
 ),
 
 observed_trips AS (
-    SELECT *
-    FROM {{ ref('fct_observed_trips') }}
+    SELECT * FROM {{ ref('fct_observed_trips') }}
 ),
 
+-- add this to make sure we correctly link quartets
+dim_provider_gtfs_data AS (
+    SELECT
+        schedule_gtfs_dataset_key,
+        vehicle_positions_gtfs_dataset_key,
+        trip_updates_gtfs_dataset_key
+    FROM {{ ref('dim_provider_gtfs_data') }}
+    GROUP BY 1, 2, 3
+),
 
 gtfs_join AS (
     SELECT
@@ -25,7 +32,8 @@ gtfs_join AS (
         schedule.trip_instance_key,
 
         schedule.* EXCEPT(service_date, base64_url, name, gtfs_dataset_key, trip_instance_key),
-        rt.* EXCEPT(service_date, schedule_base64_url, schedule_name, schedule_gtfs_dataset_key, trip_instance_key)
+        rt.* EXCEPT(service_date, schedule_base64_url, schedule_name, schedule_gtfs_dataset_key, trip_instance_key),
+        {{ generate_time_of_day_hours('time_of_day') }} AS n_hours,
 
     FROM schedule_trips AS schedule
     LEFT JOIN observed_trips AS rt
@@ -34,8 +42,42 @@ gtfs_join AS (
         AND schedule.trip_instance_key = rt.trip_instance_key
 ),
 
--- group these separately so that we don't have values where it's these combos exist:
--- schedule/vp/no_tu, schedule/no_vp/tu, schedule/no_vp/no_tu, schedule/vp/tu
+time_of_day_counts AS (
+    SELECT
+        service_date,
+        schedule_gtfs_dataset_key,
+        time_of_day,
+        route_id,
+        direction_id,
+
+        COUNT(DISTINCT trip_instance_key) AS n_trips,
+        MAX(n_hours) AS n_hours,
+    FROM gtfs_join
+    GROUP BY 1, 2, 3, 4, 5
+),
+
+pivoted_timeofday AS (
+    SELECT *
+    FROM (
+        SELECT
+            schedule_gtfs_dataset_key,
+            service_date,
+            route_id,
+            direction_id,
+
+            time_of_day,
+            n_trips,
+            n_hours
+        FROM time_of_day_counts
+    )
+    PIVOT(
+        MIN(n_trips) AS trips,
+        MIN(n_trips / n_hours) AS frequency
+        FOR time_of_day IN
+        ("owl", "early_am", "am_peak", "midday", "pm_peak", "evening")
+    )
+),
+
 schedule_aggregation AS (
     SELECT
         service_date,
@@ -51,6 +93,7 @@ schedule_aggregation AS (
             'route_id', 'route_short_name', 'route_long_name'
         ) }} AS route_name,
         direction_id,
+        route_type,
 
         COUNT(DISTINCT trip_instance_key) AS n_trips,
         COUNT(DISTINCT route_id) AS n_routes,
@@ -59,9 +102,8 @@ schedule_aggregation AS (
         SUM(num_stop_times) AS num_stop_times,
         COALESCE(ROUND(SUM(service_hours), 2), 0) AS service_hours,
         COALESCE(ROUND(SUM(flex_service_hours), 2), 0) AS flex_service_hours,
-
     FROM gtfs_join
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ),
 
 tu_aggregation AS (
@@ -76,6 +118,7 @@ tu_aggregation AS (
             'schedule_gtfs_dataset_name',
             'route_id', 'route_short_name', 'route_long_name'
         ) }} AS route_name,
+        route_id,
         direction_id,
 
         -- trip updates
@@ -88,7 +131,7 @@ tu_aggregation AS (
         ), 2), 0) AS tu_messages_per_minute,
     FROM gtfs_join
     WHERE tu_base64_url IS NOT NULL
-    GROUP BY 1, 2, 3, 4, 5, 6, 7
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
 ),
 
 vp_aggregation AS (
@@ -103,6 +146,7 @@ vp_aggregation AS (
             'schedule_gtfs_dataset_name',
             'route_id', 'route_short_name', 'route_long_name'
         ) }} AS route_name,
+        route_id,
         direction_id,
 
         -- vehicle positions
@@ -115,15 +159,23 @@ vp_aggregation AS (
         ), 2), 0) AS vp_messages_per_minute,
     FROM gtfs_join
     WHERE vp_base64_url IS NOT NULL
-    GROUP BY 1, 2, 3, 4, 5, 6, 7
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
 ),
 
+schedule_with_quartet AS (
+    SELECT
+        schedule_aggregation.*,
+        dim_provider_gtfs_data.trip_updates_gtfs_dataset_key,
+        dim_provider_gtfs_data.vehicle_positions_gtfs_dataset_key,
+    FROM schedule_aggregation
+    INNER JOIN dim_provider_gtfs_data USING (schedule_gtfs_dataset_key)
+),
 
 route_direction_aggregation AS (
     SELECT
         schedule.service_date,
         schedule.schedule_base64_url,
-        schedule.schedule_gtfs_dataset_name,
+        schedule.schedule_gtfs_dataset_name AS schedule_name,
         schedule.schedule_gtfs_dataset_key,
         schedule.feed_key,
 
@@ -131,6 +183,7 @@ route_direction_aggregation AS (
         schedule.route_id_cleaned,
         schedule.route_name,
         schedule.direction_id,
+        schedule.route_type,
 
         tu.tu_gtfs_dataset_key,
         tu.tu_name,
@@ -147,9 +200,44 @@ route_direction_aggregation AS (
         schedule.service_hours,
         schedule.flex_service_hours,
 
+        -- from pivoted
+        COALESCE(trips_owl, 0) AS daily_trips_owl,
+		COALESCE(trips_early_am, 0) AS daily_trips_early_am,
+		COALESCE(trips_am_peak, 0) AS daily_trips_am_peak,
+		COALESCE(trips_midday, 0) AS daily_trips_midday,
+		COALESCE(trips_pm_peak, 0) AS daily_trips_pm_peak,
+		COALESCE(trips_evening, 0) AS daily_trips_evening,
+		COALESCE(trips_am_peak, 0) + COALESCE(trips_pm_peak, 0) AS daily_trips_peak,
+		n_trips - (COALESCE(trips_am_peak, 0) + COALESCE(trips_pm_peak, 0)) AS daily_trips_offpeak,
+
+		COALESCE(ROUND(frequency_owl, 2), 0) AS frequency_owl,
+		COALESCE(ROUND(frequency_early_am, 2), 0) AS frequency_early_am,
+		COALESCE(ROUND(frequency_am_peak, 2), 0) AS frequency_am_peak,
+		COALESCE(ROUND(frequency_midday, 2), 0) AS frequency_midday,
+		COALESCE(ROUND(frequency_pm_peak, 2), 0) AS frequency_pm_peak,
+		COALESCE(ROUND(frequency_evening, 2), 0) AS frequency_evening,
+
+        -- calculate frequency for peak/offpeak this way by weighting the hours present in each category, add frequency for all_day
+		ROUND(
+			(COALESCE(frequency_am_peak, 0) * 3 + COALESCE(frequency_pm_peak, 0) * 5) / 8,
+		2) AS frequency_peak,
+		ROUND(
+			(COALESCE(frequency_owl, 0) * 4 + COALESCE(frequency_early_am, 0) * 3
+			+ COALESCE(frequency_midday, 0) * 5 + COALESCE(frequency_evening, 0) * 4) / 16,
+		2) AS frequency_offpeak,
+        ROUND(n_trips / 24) AS frequency_all_day,
+
         -- follow pattern in fct_observed_trips
         tu_base64_url IS NOT NULL AS appeared_in_tu,
         vp_base64_url IS NOT NULL AS appeared_in_vp,
+
+        -- add this to detect rows that are nulls that are double counting schedule data
+        COUNT(tu_base64_url) OVER(
+            PARTITION BY schedule.service_date, schedule_gtfs_dataset_name, schedule.route_id, schedule.direction_id
+        ) AS has_tu,
+        COUNT(vp_base64_url) OVER(
+            PARTITION BY schedule.service_date, schedule_gtfs_dataset_name, schedule.route_id, schedule.direction_id
+        ) AS has_vp,
 
         -- vehicle positions
         vp.vp_num_distinct_updates,
@@ -163,18 +251,39 @@ route_direction_aggregation AS (
         tu.tu_extract_duration_minutes,
         tu.tu_messages_per_minute,
 
-    FROM schedule_aggregation AS schedule
+    FROM schedule_with_quartet AS schedule
+    INNER JOIN pivoted_timeofday AS pivoted
+        ON schedule.service_date = pivoted.service_date
+        AND schedule.schedule_gtfs_dataset_key = pivoted.schedule_gtfs_dataset_key
+        AND schedule.route_id = pivoted.route_id
+        AND COALESCE(schedule.direction_id, -1) = COALESCE(pivoted.direction_id, -1)
     LEFT JOIN tu_aggregation AS tu
         ON schedule.service_date = tu.service_date
         AND schedule.schedule_base64_url = tu.schedule_base64_url
+        AND schedule.trip_updates_gtfs_dataset_key = tu.tu_gtfs_dataset_key
         AND schedule.route_name = tu.route_name
         AND COALESCE(schedule.direction_id, -1) = COALESCE(tu.direction_id, -1)
     LEFT JOIN vp_aggregation AS vp
         ON schedule.service_date = vp.service_date
         AND schedule.schedule_base64_url = vp.schedule_base64_url
+		AND schedule.vehicle_positions_gtfs_dataset_key = vp.vp_gtfs_dataset_key
         AND schedule.route_name = vp.route_name
         AND COALESCE(schedule.direction_id, -1) = COALESCE(vp.direction_id, -1)
-    WHERE n_trips > 0 AND (n_vp_trips > 0 OR n_tu_trips > 0)
+    WHERE n_trips > 0
+),
+
+route_direction_aggregation2 AS (
+    SELECT
+        * EXCEPT(has_vp, has_tu)
+    FROM route_direction_aggregation
+    -- distinguish between rows that have no RT for that operator at all vs
+    -- particular row didn't have RT for that route-dir combo but had RT observed that day
+    -- for a day, there can be several combos from the left join:
+    -- day1  schedule1  no_tu_route_dir  yes_vp_route_dir
+    -- day1  schedule1  yes_tu_route_dir yes_vp_route_dir
+    -- day1  schedule1  no_tu_route_dir  no_vp_route_dir (drop row)
+    -- day1  schedule2  no_tu_route_dir  no_vp_route_dir (keep row)
+    WHERE (appeared_in_tu AND has_tu >= 1) OR (appeared_in_vp AND has_vp >= 1)
 )
 
-SELECT * FROM route_direction_aggregation
+SELECT * FROM route_direction_aggregation2
--- warehouse/models/mart/gtfs_rollup/fct_monthly_schedule_rt_route_direction_summary.sql
+++ warehouse/models/mart/gtfs_rollup/fct_monthly_schedule_rt_route_direction_summary.sql
@@ -1,30 +1,92 @@
 {{ config(materialized='table') }}
-WITH schedule_summary AS (
-    SELECT *
-    FROM {{ ref('fct_monthly_schedule_route_direction_summary') }}
-),
 
-rt_summary AS (
-    SELECT
-        * EXCEPT(month, year)
-    FROM {{ ref('fct_monthly_rt_route_direction_summary') }}
+WITH daily_summary AS (
+    SELECT *
+    FROM {{ ref('fct_daily_schedule_rt_route_direction_summary') }}
 ),
 
 route_direction_aggregation AS (
     SELECT
-        schedule_summary.*,
-        rt_summary.* EXCEPT(n_trips, n_days, month_first_day, day_type, route_name, direction_id),
-        rt_summary.n_trips AS n_rt_trips, -- can calculate percent of trips that had RT (tu and/or vp) vs schedule
-        rt_summary.n_days AS n_rt_days,
+        EXTRACT(month FROM service_date) AS month,
+        EXTRACT(year FROM service_date) AS year,
+        DATE_TRUNC(service_date, MONTH) AS month_first_day,
+        {{ generate_day_type('service_date') }} AS day_type,
+        schedule_base64_url,
+        schedule_name,
+        tu_base64_url,
+        tu_name, -- can there be different tu_base64_urls across a month? probably, and tu_name would change then
+        vp_base64_url,
+        vp_name,
+        route_name,
+        direction_id,
+        MAX(route_type) AS route_type,
 
-    FROM schedule_summary
-    INNER JOIN rt_summary
-      ON schedule_summary.month_first_day = rt_summary.month_first_day
-      AND schedule_summary.name = rt_summary.schedule_name
-      AND schedule_summary.day_type = rt_summary.day_type
-      AND schedule_summary.route_name = rt_summary.route_name
-      -- in fct_vehicle_locations, when there are nulls, coalesce and fill it in so join works
-      AND COALESCE(schedule_summary.direction_id, -1) = COALESCE(rt_summary.direction_id, -1)
+        -- schedule
+        SUM(n_trips) AS n_trips,
+		ROUND(AVG(n_trips), 2) AS daily_trips_all_day,
+        COUNT(DISTINCT route_id) AS n_route_ids,
+        ROUND(AVG(n_shapes), 1) AS n_shapes,
+        ROUND(AVG(avg_stops_served), 1) AS avg_stops_served,
+        COUNT(DISTINCT feed_key) AS n_feeds,
+        COUNT(DISTINCT service_date) AS n_days,
+
+        ROUND(AVG(daily_trips_owl), 2) AS daily_trips_owl,
+		ROUND(AVG(daily_trips_early_am), 2) AS daily_trips_early_am,
+		ROUND(AVG(daily_trips_am_peak), 2) AS daily_trips_am_peak,
+		ROUND(AVG(daily_trips_midday), 2) AS daily_trips_midday,
+		ROUND(AVG(daily_trips_pm_peak), 2) AS daily_trips_pm_peak,
+		ROUND(AVG(daily_trips_evening), 2) AS daily_trips_evening,
+		ROUND(AVG(daily_trips_peak), 2) AS daily_trips_peak,
+		ROUND(AVG(daily_trips_offpeak), 2) AS daily_trips_offpeak,
+
+		ROUND(AVG(frequency_owl), 2) AS frequency_owl,
+		ROUND(AVG(frequency_early_am), 2) AS frequency_early_am,
+		ROUND(AVG(frequency_am_peak), 2) AS frequency_am_peak,
+		ROUND(AVG(frequency_midday), 2) AS frequency_midday,
+		ROUND(AVG(frequency_pm_peak), 2) AS frequency_pm_peak,
+		ROUND(AVG(frequency_evening), 2) AS frequency_evening,
+		ROUND(AVG(frequency_peak), 2) AS frequency_peak,
+		ROUND(AVG(frequency_offpeak), 2) AS frequency_offpeak,
+		ROUND(AVG(frequency_all_day), 2) AS frequency_all_day,
+
+        LOGICAL_OR(appeared_in_tu) AS appeared_in_tu,
+        LOGICAL_OR(appeared_in_vp) AS appeared_in_vp,
+
+        -- vehicle positions
+        SUM(vp_num_distinct_updates) AS vp_num_distinct_updates,
+        SUM(n_vp_trips) AS n_vp_trips,
+        SUM(vp_extract_duration_minutes) AS vp_extract_duration_minutes,
+        ROUND(AVG(vp_messages_per_minute), 1) AS vp_messages_per_minute,
+
+        -- trip updates
+        SUM(tu_num_distinct_updates) AS tu_num_distinct_updates,
+        SUM(n_tu_trips) AS n_tu_trips,
+        SUM(tu_extract_duration_minutes) AS tu_extract_duration_minutes,
+        ROUND(AVG(tu_messages_per_minute), 1) AS tu_messages_per_minute,
+
+    FROM daily_summary
+	GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+),
+
+route_direction_aggregation_with_typology AS (
+    SELECT
+        *,
+        -- get what we can based on route name to separate our bus types
+        -- https://github.com/cal-itp/data-analyses/blob/dd3b5f963bfa480fb949c5949bea6485431361fe/gtfs_funnel/nacto_utils.py#L24-L58
+		CASE
+            WHEN route_type = "0" OR route_type = "1" OR route_type = "2" THEN "rail"
+            WHEN route_type = "4" THEN "ferry"
+            WHEN (
+              CONTAINS_SUBSTR(route_name, "Express")
+              OR CONTAINS_SUBSTR(route_name, "Limited")
+            ) AND route_type = "3" THEN "express"
+            WHEN CONTAINS_SUBSTR(route_name, "Rapid") AND route_type = "3" THEN "rapid"
+            WHEN route_type = "3" THEN "bus"
+            WHEN route_type = "5" THEN "cable_tram"
+            WHEN route_type = "11" THEN "trolleybus"
+            ELSE "unknown"
+		END AS route_typology,
+    FROM route_direction_aggregation
 )
 
-SELECT * FROM route_direction_aggregation
+SELECT * FROM route_direction_aggregation_with_typology
