b3de559814fba75161dc87471388b71bbec28ac5
Laurie Merrell <laurie.m@jarv.us>
2026-03-27 17:26:27 -0500

    wip: customer join

--- warehouse/models/intermediate/payments/int_payments__micropayments_to_aggregations.sql
+++ warehouse/models/intermediate/payments/int_payments__micropayments_to_aggregations.sql
@@ -9,6 +9,16 @@ WITH micropayments AS (
     FROM {{ ref('int_littlepay__unioned_micropayments') }}
 ),
 
+customer_mapping AS (
+    SELECT *
+    FROM {{ ref('int_payments__customers') }}
+),
+
+customer_funding_sources AS (
+    SELECT *
+    FROM {{ ref('int_payments__customer_funding_source_vaults') }}
+),
+
 -- flag micropayments that were adjusted
 micropayment_adj AS (
     SELECT DISTINCT micropayment_id
@@ -18,20 +28,33 @@ micropayment_adj AS (
 
 int_payments__micropayments_to_aggregations AS (
     SELECT
-        participant_id,
-        aggregation_id,
-        SUM(charge_amount) AS net_micropayment_amount_dollars,
-        SUM(COALESCE(nominal_amount,0)) AS total_nominal_amount_dollars,
-        MAX(transaction_time) AS latest_transaction_time,
-        COUNT(DISTINCT micropayment_id) AS num_micropayments,
-        LOGICAL_OR(charge_type = "refund") AS contains_pre_settlement_refund,
-        LOGICAL_OR(charge_type LIKE "%variable%") AS contains_variable_fare,
-        LOGICAL_OR(charge_type = "flat_fare") AS contains_flat_fare,
-        LOGICAL_OR(charge_type LIKE "%pending%") AS contains_pending_charge,
+        micropayments.participant_id,
+        micropayments.aggregation_id,
+        --customer_mapping.customer_id,
+        customer_mapping.principal_customer_id,
+        customer_funding_sources.card_scheme,
+        customer_funding_sources.bin,
+        SUM(micropayments.charge_amount) AS net_micropayment_amount_dollars,
+        SUM(COALESCE(micropayments.nominal_amount,0)) AS total_nominal_amount_dollars,
+        MAX(micropayments.transaction_time) AS latest_transaction_time,
+        COUNT(DISTINCT micropayments.micropayment_id) AS num_micropayments,
+        LOGICAL_OR(micropayments.charge_type = "refund") AS contains_pre_settlement_refund,
+        LOGICAL_OR(micropayments.charge_type LIKE "%variable%") AS contains_variable_fare,
+        LOGICAL_OR(micropayments.charge_type = "flat_fare") AS contains_flat_fare,
+        LOGICAL_OR(micropayments.charge_type LIKE "%pending%") AS contains_pending_charge,
         LOGICAL_OR(micropayment_adj.micropayment_id IS NOT NULL) AS contains_adjusted_micropayment
     FROM micropayments
-    LEFT JOIN micropayment_adj USING (micropayment_id)
-    GROUP BY 1, 2
+    LEFT JOIN micropayment_adj
+        ON micropayments.micropayment_id = micropayment_adj.micropayment_id
+    LEFT JOIN customer_mapping
+        ON micropayments.customer_id = customer_mapping.customer_id
+        AND micropayments.participant_id = customer_mapping.participant_id
+    LEFT JOIN customer_funding_sources
+        ON micropayments.funding_source_vault_id = customer_funding_sources.funding_source_vault_id
+        AND micropayments.participant_id = customer_funding_sources.participant_id
+        AND micropayments.transaction_time >= customer_funding_sources.calitp_valid_at
+        AND micropayments.transaction_time < customer_funding_sources.calitp_invalid_at
+    GROUP BY 1, 2, 3, 4, 5--, 6
 )
 
 SELECT * FROM int_payments__micropayments_to_aggregations
