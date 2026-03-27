35d336556642c726e51f43f0c7841359c4dcecf5
Laurie <55149902+lauriemerrell@users.noreply.github.com>
2026-03-27 08:52:25 -0500

    Migrate GTFS Schedule Dimension Tables To Microbatch Strategy (#4981)
    * chore: migrate microbatch notebook env to use uv instead of poetry
    
    * feat: update microbatch notebook to handle all gtfs dimensions at once
    
    * fix: use variable for lookback on dim_stop_arrivals
    
    * feat: microbatch incremental strategy for gtfs dims
    
    * chore: clean up _orig model for testing
    
    * feat: add event_time on gtfs staging models
    
    * feat: add labels to copied models

--- warehouse/models/mart/gtfs/dim_areas.sql
+++ warehouse/models/mart/gtfs/dim_areas.sql
@@ -1,9 +1,36 @@
-WITH make_dim AS (
-    {{ make_schedule_file_dimension_from_dim_schedule_feeds(
-        ref('dim_schedule_feeds'),
-        ref('stg_gtfs_schedule__areas'),
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='microbatch',
+        event_time = '_feed_valid_from',
+        batch_size = 'day',
+        begin=var('GTFS_SCHEDULE_START'),
+        lookback=var('DBT_ALL_MICROBATCH_LOOKBACK_DAYS'),
+        partition_by={
+            'field': '_feed_valid_from',
+            'data_type': 'timestamp',
+            'granularity': 'day',
+        },
+        full_refresh=false,
+        cluster_by='feed_key'
+    )
+}}
+
+WITH dim_schedule_feeds AS (
+    SELECT * FROM {{ ref('dim_schedule_feeds') }}
+),
+
+stg AS ( --noqa: ST03
+    SELECT * FROM {{ ref('stg_gtfs_schedule__areas') }}
+),
+
+make_dim AS (
+    {{ make_schedule_file_dimension_from_dim_schedule_feeds_microbatch(
+        'dim_schedule_feeds',
+        'stg',
     ) }}
 ),
+
 
 dim_areas AS (
     SELECT
