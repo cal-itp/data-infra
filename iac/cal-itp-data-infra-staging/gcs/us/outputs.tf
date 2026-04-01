5ef1e3a632f1dea95c21e73653d3a0e0ed018122
Jeff Parr <jeffparr100@gmail.com>
2026-04-01 16:14:43 -0700

    [staging] add bucket for elavon sftp

--- iac/cal-itp-data-infra-staging/gcs/us/outputs.tf
+++ iac/cal-itp-data-infra-staging/gcs/us/outputs.tf
@@ -266,6 +266,10 @@ output "google_storage_bucket_calitp-staging-enghouse-raw_name" {
   value = google_storage_bucket.calitp-staging-enghouse-raw.name
 }
 
+output "google_storage_bucket_calitp-staging-elavon-raw-v2_name" {
+  value = google_storage_bucket.calitp-staging-elavon-raw-v2.name
+}
+
 output "google_storage_bucket_calitp-reports-staging_name" {
   value = google_storage_bucket.calitp-reports-staging.name
 }
--- iac/cal-itp-data-infra-staging/gcs/us/storage_bucket_iam_member.tf
+++ iac/cal-itp-data-infra-staging/gcs/us/storage_bucket_iam_member.tf
@@ -52,6 +52,12 @@ resource "google_storage_bucket_iam_member" "enghouse-raw-sftp-service-account" 
   member = "serviceAccount:${data.terraform_remote_state.iam.outputs.google_service_account_sftp-pod-service-account_email}"
 }
 
+resource "google_storage_bucket_iam_member" "elavon-raw-v2-sftp-service-account" {
+  bucket = google_storage_bucket.calitp-staging-elavon-raw-v2.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.terraform_remote_state.iam.outputs.google_service_account_elavon-sftp-service-account_email}"
+}
+
 resource "google_storage_bucket_iam_member" "calitp-staging-pytest" {
   bucket = google_storage_bucket.calitp-staging-pytest.name
   member = "projectViewer:cal-itp-data-infra-staging"
