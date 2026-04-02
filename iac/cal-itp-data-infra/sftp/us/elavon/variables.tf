ec4b2b4ad14a7f59b6072801eaced3b97f70f6a9
Jeff Parr <jeffparr100@gmail.com>
2026-04-02 11:21:30 -0700

    Add k8 deployment for elavon sftp
    ref: #4420

---
+++ iac/cal-itp-data-infra/sftp/us/elavon/variables.tf
@@ -0,0 +1,51 @@
+locals {
+  sftp_user = "elavon"
+}
+
+data "terraform_remote_state" "gcs" {
+  backend = "gcs"
+
+  config = {
+    bucket = "calitp-prod-gcp-components-tfstate"
+    prefix = "cal-itp-data-infra/gcs"
+  }
+}
+
+data "terraform_remote_state" "gke" {
+  backend = "gcs"
+
+  config = {
+    bucket = "calitp-prod-gcp-components-tfstate"
+    prefix = "cal-itp-data-infra/gke"
+  }
+}
+
+data "terraform_remote_state" "iam" {
+  backend = "gcs"
+
+  config = {
+    bucket = "calitp-prod-gcp-components-tfstate"
+    prefix = "cal-itp-data-infra/iam"
+  }
+}
+
+data "terraform_remote_state" "networks" {
+  backend = "gcs"
+
+  config = {
+    bucket = "calitp-prod-gcp-components-tfstate"
+    prefix = "cal-itp-data-infra/networks"
+  }
+}
+
+data "google_secret_manager_secret_version" "elavon-sftp-public-key" {
+  secret = "elavon-sftp-public-key"
+}
+
+data "google_secret_manager_secret_version" "elavon-sftp-private-key" {
+  secret = "elavon-sftp-private-key"
+}
+
+data "google_secret_manager_secret_version" "elavon-sftp-authorizedkey" {
+  secret = "elavon-sftp-authorizedkey"
+}
