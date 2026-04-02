8011a7c1b36aa45b4d95362865cb117b350053af
Laurie Merrell <laurie.m@jarv.us>
2026-04-02 15:48:33 -0500

    feat: additional reconciliation columns

--- warehouse/models/mart/payments/fct_payments_aggregations_reconciliation.sql
+++ warehouse/models/mart/payments/fct_payments_aggregations_reconciliation.sql
@@ -35,6 +35,21 @@ customers AS (
     SELECT * FROM {{ ref('int_payments__customers_vaults_to_aggregations') }}
 ),
 
+elavon_info AS (
+  SELECT
+    purch_id AS elavon_purch_id,
+    MAX(settlement_date) AS elavon_settlement_date,
+    MAX(payment_date) AS elavon_payment_date,
+    SUM(amount) AS elavon_net_amount,
+    SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS elavon_sales,
+    SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS elavon_refunds,
+  FROM
+    {{ ref('fct_payments_deposit_transactions') }}
+  WHERE COALESCE(purch_id, '') != ''
+  GROUP BY
+    purch_id
+),
+
 -- TODO: add refunds
 
 join_payments AS (
@@ -84,6 +99,12 @@ fct_payments_aggregations AS (
         LAST_DAY(EXTRACT(DATE FROM aggregation_datetime), MONTH) AS end_of_month_date_utc,
         aggregation_id,
         principal_customer_id,
+        elavon_purch_id,
+        elavon_settlement_date,
+        elavon_payment_date,
+        elavon_net_amount,
+        elavon_sales,
+        elavon_refunds,
         bin,
         card_scheme,
         has_micropayment,
@@ -95,6 +116,7 @@ fct_payments_aggregations AS (
         net_micropayment_amount_dollars,
         total_nominal_amount_dollars AS total_micropayment_nominal_amount_dollars,
         latest_transaction_time AS latest_micropayment_transaction_datetime,
+        TIMESTAMP(DATETIME(latest_transaction_time, "America/Los_Angeles")) AS latest_micropayment_transaction_datetime_pacific,
         num_micropayments,
         contains_pre_settlement_refund,
         contains_variable_fare,
@@ -108,14 +130,27 @@ fct_payments_aggregations AS (
         authorisation_date_time_utc AS latest_authorisation_update_datetime,
         contains_imputed_type AS settlement_contains_imputed_type,
         latest_update_timestamp AS latest_settlement_update_datetime,
+        TIMESTAMP(DATETIME(latest_update_timestamp, "America/Los_Angeles")) AS latest_settlement_update_datetime_pacific,
         net_settled_amount_dollars,
         contains_refund AS settlement_contains_refund,
         debit_amount AS settlement_debit_amount,
         credit_amount AS settlement_credit_amount,
+        debit_amount > 0 AS contains_nonzero_sales,
         aggregation_is_settled,
         debit_is_settled,
-        credit_is_settled
+        credit_is_settled,
+        CASE
+            WHEN net_micropayment_amount_dollars = 0 THEN 'Zero-dollar value sales'
+            WHEN net_micropayment_amount_dollars > 0 AND aggregation_is_settled AND elavon_purch_id IS NOT NULL THEN 'Settled non-zero sales (with Elavon match)'
+            WHEN net_micropayment_amount_dollars > 0 AND aggregation_is_settled AND elavon_purch_id IS NULL THEN 'Settled non-zero sales (no Elavon match)'
+            WHEN net_micropayment_amount_dollars > 0 AND (status IS NULL OR status NOT IN ('LOST','DECLINED','UNAVAILABLE','UNRECOVERABLE','INVALID')) AND (NOT aggregation_is_settled) THEN 'Unsettled non-zero sales'
+            WHEN COALESCE(status,'') != '' AND status NOT IN ('AUTHORISED', 'VERIFIED') THEN 'Declined sales'
+            ELSE 'UNKNOWN'
+        END AS Reconciliation_Category
     FROM join_orgs
+    LEFT JOIN elavon_info
+        ON join_orgs.settlement_retrieval_reference_number = elavon_info.elavon_purch_id
+
 )
 
 SELECT * FROM fct_payments_aggregations
