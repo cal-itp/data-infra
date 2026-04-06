4db4b607f2332c165d4c7278c5b1866d49dcd73b
Jeff Parr <jeffparr100@gmail.com>
2026-04-06 09:48:29 -0700

    Adds new static ip for elavon sftp (#5033)
    ref #4420
    
    I wasnt able to re-use the ip from the old deployment so we will have to ask elavon to update their IP

--- iac/cal-itp-data-infra/networks/us/compute_address.tf
+++ iac/cal-itp-data-infra/networks/us/compute_address.tf
@@ -3,3 +3,9 @@ resource "google_compute_address" "enghouse-sftp-address" {
   region       = "us-west2"
   address_type = "EXTERNAL"
 }
+
+resource "google_compute_address" "elavon-sftp-address" {
+  name         = "elavon-sftp-address"
+  region       = "us-west2"
+  address_type = "EXTERNAL"
+}
