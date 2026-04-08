30e1d153e7a3c17e176bec93136dcc2ca703c95a
Laurie Merrell <laurie.m@jarv.us>
2026-04-08 09:38:21 -0500

    chore: remove ref to deleted column

--- warehouse/models/mart/gtfs/fct_hourly_rt_feed_files.sql
+++ warehouse/models/mart/gtfs/fct_hourly_rt_feed_files.sql
@@ -5,7 +5,6 @@ WITH
 int_gtfs_rt__daily_url_index AS (
     SELECT *
     FROM {{ ref('int_gtfs_rt__daily_url_index') }}
-    WHERE data_quality_pipeline
     AND {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 ),
 
