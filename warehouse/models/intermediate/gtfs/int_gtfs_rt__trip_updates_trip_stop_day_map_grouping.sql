31baf52c0d42df0a793c0c23e3663bd6e64ea728
Laurie Merrell <laurie.m@jarv.us>
2026-04-08 16:58:21 -0500

    fix: revert changes on trip updates trip stop day map grouping pending discussion

--- warehouse/models/intermediate/gtfs/int_gtfs_rt__trip_updates_trip_stop_day_map_grouping.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt__trip_updates_trip_stop_day_map_grouping.sql
@@ -1,24 +1,19 @@
 {{
     config(
         materialized='incremental',
-        incremental_strategy='microbatch',
-        event_time = 'dt',
-        batch_size = 'day',
-        begin=var('PROD_GTFS_RT_START'),
-        lookback=var('DBT_ALL_MICROBATCH_LOOKBACK_DAYS'),
+        incremental_strategy='insert_overwrite',
         partition_by={
             'field': 'dt',
             'data_type': 'date',
-            'granularity': 'day',
-        },
-        full_refresh=false,
-        cluster_by='base64_url',
+            'granularity': 'day'
+        }, cluster_by=['base64_url']
     )
 }}
 
 WITH stop_time_updates AS (
     SELECT *
     FROM {{ ref('fct_stop_time_updates') }}
+    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }} AND dt >= "2025-12-01"
 ),
 
 prediction_arrays AS (
