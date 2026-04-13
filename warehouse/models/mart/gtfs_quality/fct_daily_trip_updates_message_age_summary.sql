e6e3ce1d6f58d159441e89bb7d00a9bf9ee9ba74
Laurie Merrell <laurie.m@jarv.us>
2026-04-13 09:28:14 -0500

    fix: #3512 for trip updates message age

--- warehouse/models/mart/gtfs_quality/fct_daily_trip_updates_message_age_summary.sql
+++ warehouse/models/mart/gtfs_quality/fct_daily_trip_updates_message_age_summary.sql
@@ -9,12 +9,14 @@
 ) }}
 
 WITH trip_updates_ages AS (
-    SELECT DISTINCT
+    SELECT
         dt,
         base64_url,
+        _extract_ts,
+        gtfs_dataset_key,
+        gtfs_dataset_name,
         _header_message_age,
-        _trip_update_message_age,
-        _trip_update_message_age_vs_header,
+        _trip_update_message_age
     FROM {{ ref('fct_trip_updates_no_stop_times') }}
     WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 ),
@@ -25,6 +27,7 @@ distinct_headers AS (
     SELECT DISTINCT
         dt,
         base64_url,
+        _extract_ts,
         _header_message_age,
     FROM trip_updates_ages
 ),
@@ -66,11 +69,6 @@ trip_updates_age_percentiles AS (
         PERCENTILE_CONT(_trip_update_message_age, .75) OVER(PARTITION BY dt, base64_url) AS p75_trip_update_message_age,
         PERCENTILE_CONT(_trip_update_message_age, .90) OVER(PARTITION BY dt, base64_url) AS p90_trip_update_message_age,
         PERCENTILE_CONT(_trip_update_message_age, .99) OVER(PARTITION BY dt, base64_url) AS p99_trip_update_message_age,
-        PERCENTILE_CONT(_trip_update_message_age_vs_header, .5) OVER(PARTITION BY dt, base64_url) AS median_trip_update_message_age_vs_header,
-        PERCENTILE_CONT(_trip_update_message_age_vs_header, .25) OVER(PARTITION BY dt, base64_url) AS p25_trip_update_message_age_vs_header,
-        PERCENTILE_CONT(_trip_update_message_age_vs_header, .75) OVER(PARTITION BY dt, base64_url) AS p75_trip_update_message_age_vs_header,
-        PERCENTILE_CONT(_trip_update_message_age_vs_header, .90) OVER(PARTITION BY dt, base64_url) AS p90_trip_update_message_age_vs_header,
-        PERCENTILE_CONT(_trip_update_message_age_vs_header, .99) OVER(PARTITION BY dt, base64_url) AS p99_trip_update_message_age_vs_header
     FROM trip_updates_ages
 ),
 
@@ -78,24 +76,18 @@ summarize_trip_updates_ages AS (
     SELECT
         dt,
         base64_url,
+        gtfs_dataset_key,
+        gtfs_dataset_name,
         median_trip_update_message_age,
         p25_trip_update_message_age,
         p75_trip_update_message_age,
         p90_trip_update_message_age,
         p99_trip_update_message_age,
-        median_trip_update_message_age_vs_header,
-        p25_trip_update_message_age_vs_header,
-        p75_trip_update_message_age_vs_header,
-        p90_trip_update_message_age_vs_header,
-        p99_trip_update_message_age_vs_header,
         MAX(_trip_update_message_age) AS max_trip_update_message_age,
         MIN(_trip_update_message_age) AS min_trip_update_message_age,
         AVG(_trip_update_message_age) AS avg_trip_update_message_age,
-        MAX(_trip_update_message_age_vs_header) AS max_trip_update_message_age_vs_header,
-        MIN(_trip_update_message_age_vs_header) AS min_trip_update_message_age_vs_header,
-        AVG(_trip_update_message_age_vs_header) AS avg_trip_update_message_age_vs_header,
     FROM trip_updates_age_percentiles
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 ),
 
 fct_daily_trip_update_message_age_summary AS (
@@ -103,6 +95,8 @@ fct_daily_trip_update_message_age_summary AS (
         {{ dbt_utils.generate_surrogate_key(['dt', 'base64_url']) }} AS key,
         dt,
         base64_url,
+        gtfs_dataset_key,
+        gtfs_dataset_name,
         median_header_message_age,
         p25_header_message_age,
         p75_header_message_age,
@@ -119,14 +113,6 @@ fct_daily_trip_update_message_age_summary AS (
         max_trip_update_message_age,
         min_trip_update_message_age,
         avg_trip_update_message_age,
-        median_trip_update_message_age_vs_header,
-        p25_trip_update_message_age_vs_header,
-        p75_trip_update_message_age_vs_header,
-        p90_trip_update_message_age_vs_header,
-        p99_trip_update_message_age_vs_header,
-        max_trip_update_message_age_vs_header,
-        min_trip_update_message_age_vs_header,
-        avg_trip_update_message_age_vs_header
     FROM summarize_header_ages
     LEFT JOIN summarize_trip_updates_ages
         USING (dt, base64_url)
