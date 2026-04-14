9ca9826d7b6d78d07f0f409a2f40fb5d86622eac
Laurie Merrell <laurie.m@jarv.us>
2026-04-14 18:10:16 -0500

    feat: microbatch intermediate gtfs rt outcomes

--- warehouse/models/intermediate/gtfs/int_gtfs_rt__unioned_parse_outcomes.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt__unioned_parse_outcomes.sql
@@ -1,29 +1,31 @@
 {{ config(
     materialized='incremental',
-    incremental_strategy='insert_overwrite',
+    incremental_strategy='microbatch',
+    event_time = 'dt',
+    batch_size = 'day',
+    begin=var('PROD_GTFS_RT_START'),
+    lookback=var('DBT_ALL_MICROBATCH_LOOKBACK_DAYS'),
     partition_by = {
         'field': 'dt',
         'data_type': 'date',
         'granularity': 'day',
     },
+    full_refresh=false,
 ) }}
 
 WITH service_alerts AS (
     SELECT *
     FROM {{ ref('stg_gtfs_rt__service_alerts_outcomes') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 ),
 
 vehicle_positions AS (
     SELECT *
     FROM {{ ref('stg_gtfs_rt__vehicle_positions_outcomes') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 ),
 
 trip_updates AS (
     SELECT *
     FROM {{ ref('stg_gtfs_rt__trip_updates_outcomes') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 ),
 
 int_gtfs_rt__unioned_parse_outcomes AS (
