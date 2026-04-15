43e2c58d69e9be3ad0e7f3b63a7fc6acefab6ebe
Laurie Merrell <laurie.m@jarv.us>
2026-04-15 10:04:10 -0500

    feat: migrate gtfs rt validation notices & outcomes to microbatch

--- warehouse/models/intermediate/gtfs_quality/int_gtfs_quality__rt_validation_notices.sql
+++ warehouse/models/intermediate/gtfs_quality/int_gtfs_quality__rt_validation_notices.sql
@@ -1,29 +1,32 @@
 {{ config(
     materialized='incremental',
-    incremental_strategy='insert_overwrite',
+    incremental_strategy='microbatch',
+    event_time = 'dt',
+    batch_size = 'day',
+    begin=var('PROD_GTFS_RT_START'),
+    lookback=var('DBT_ALL_MICROBATCH_LOOKBACK_DAYS'),
     partition_by = {
         'field': 'dt',
         'data_type': 'date',
         'granularity': 'day',
     },
-) }}
+    full_refresh=false,
+)
+}}
 
 WITH
 
 int_gtfs_quality__rt_validation_notices AS (
-    -- predicate pushdown does not seem to work through UNIONs so list these all out
+
     SELECT * FROM {{ ref('stg_gtfs_rt__service_alerts_validation_notices') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 
     UNION ALL
 
     SELECT * FROM {{ ref('stg_gtfs_rt__trip_updates_validation_notices') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 
     UNION ALL
 
     SELECT * FROM {{ ref('stg_gtfs_rt__vehicle_positions_validation_notices') }}
-    WHERE {{ incremental_where(default_start_var='PROD_GTFS_RT_START') }}
 )
 
 SELECT * FROM int_gtfs_quality__rt_validation_notices
--- warehouse/models/staging/gtfs_quality/stg_gtfs_rt__vehicle_positions_validation_notices.sql
+++ warehouse/models/staging/gtfs_quality/stg_gtfs_rt__vehicle_positions_validation_notices.sql
@@ -1,3 +1,5 @@
+{{ config(event_time='dt') }}
+
 WITH stg_gtfs_rt__vehicle_positions_validation_notices AS (
     {{ gtfs_rt_stg_validation_notices(source('external_gtfs_rt', 'vehicle_positions_validation_notices')) }}
 )
