0178eae388994a84aef6e37e5480506d259f9a47
Laurie Merrell <laurie.m@jarv.us>
2026-04-15 18:00:38 -0500

    wip: draft new macro

---
+++ warehouse/macros/incremental_filter_ranged.sql
@@ -0,0 +1,43 @@
+{%- macro ranged_incremental_min_date(default_lookback, data_earliest_start, lookback_unit = 'day') -%}
+
+    {# first, set min and max dates for the range #}
+    {%- if is_incremental() -%}
+
+        {%- if env_var('DBT_EVENT_TIME_START') -%}
+            {%- set min_dt = env_var('DBT_EVENT_TIME_START') %}
+            '{{ min_dt }}'
+        {%- else -%}
+            {%- set min_dt = 'date_sub(current_date(), interval' + default_lookback + ' ' + lookback_unit + ')' %}
+            {{ min_dt }}
+        {%- endif -%}
+
+    {%- else  -%}
+        {# if not incremental, just set min to dataset start #}
+        {%- set min_dt = data_earliest_start %}
+        '{{ min_dt }}'
+
+    {%- endif -%}
+
+
+
+{%- endmacro %}
+
+{%- macro ranged_incremental_max_date() -%}
+
+    {%- if is_incremental() -%}
+
+        {%- if env_var('DBT_EVENT_TIME_END') -%}
+            {%- set max_dt = env_var('DBT_EVENT_TIME_END') %}
+            '{{ max_dt }}'
+        {%- else -%}
+            {%- set max_dt = 'current_date()' %}
+            {{ max_dt }}
+        {%- endif -%}
+
+    {%- else  -%}
+        {# if not incremental, just set max to today #}
+        {%- set max_dt = 'current_date()' %}
+        {{ max_dt }}
+    {%- endif -%}
+
+{%- endmacro %}
