569ad2350dfa36e4470a533263a93e113aa95bd9
Laurie Merrell <laurie.m@jarv.us>
2026-04-15 10:04:10 -0500

    feat: migrate gtfs rt validation notices & outcomes to microbatch

--- warehouse/models/staging/gtfs_quality/stg_gtfs_rt__trip_updates_validation_outcomes.sql
+++ warehouse/models/staging/gtfs_quality/stg_gtfs_rt__trip_updates_validation_outcomes.sql
@@ -1,3 +1,5 @@
+{{ config(event_time='dt') }}
+
 WITH stg_gtfs_rt__trip_updates_validation_outcomes AS (
     {{ gtfs_rt_stg_outcomes('validation', source('external_gtfs_rt', 'trip_updates_validation_outcomes')) }}
 )
