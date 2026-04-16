8b9bace83bd5cad961fabde0cfc6b439160f6239
Doc Ritezel <pair+doc@ministryofvelocity.com>
2026-04-15 18:58:02 -0700

    Deploy latest GTFS-RT archiver via SHA

--- services/gtfs-rt-archiver/main.py
+++ services/gtfs-rt-archiver/main.py
@@ -1,22 +1,20 @@
 import json
-import logging
 import os
+import traceback
 from concurrent.futures import TimeoutError
 from datetime import datetime
 
 from google.cloud import pubsub_v1
-import google.cloud.logging as google_logging
-
 from gtfs_rt_archiver.archiver import Archiver
 from gtfs_rt_archiver.configuration import Configuration
 from gtfs_rt_archiver.downloader import Downloader
+from gtfs_rt_archiver.logger import logger
 
-logger = logging.getLogger(__name__)
 
-class StreamingPullHandler:
+class GtfsRtArchiver:
     @staticmethod
-    def run(message):
-        StreamingPullHandler(message=message, current_time=datetime.now()).callback()
+    def callback(message):
+        GtfsRtArchiver(message=message, current_time=datetime.now()).run()
 
     def __init__(self, message: str, current_time: datetime):
         self.message: datetime = message
@@ -37,29 +35,29 @@ class StreamingPullHandler:
     def archiver(self):
         return Archiver(configuration=self.configuration())
 
-    def callback(self):
+    def run(self):
         try:
             logger.info(f"{self.configuration().url} - processing {self.message.data}")
             self.archiver().save(result=self.downloader().get())
         except Exception as e:
             logger.error(f"{self.configuration().url} - {e}")
+            logger.error(traceback.format_exc())
         self.message.ack()
 
 
 if __name__ == "__main__":
-    google_logging.Client().setup_logging()
-
     subscriber = pubsub_v1.SubscriberClient()
 
     streaming_pull_future = subscriber.subscribe(
-        os.environ["GTFS_RT_ARCHIVER__SUBSCRIPTION"], callback=StreamingPullHandler.run
+        os.environ["GTFS_RT_ARCHIVER__SUBSCRIPTION"], callback=GtfsRtArchiver.callback
     )
 
     with subscriber:
         try:
             streaming_pull_future.result()
-        except TimeoutError:
+        except TimeoutError as e:
+            logger.error(f"Streaming pull timed out: {e}")
             streaming_pull_future.cancel()
             streaming_pull_future.result()
         except Exception as e:
-            logging.error(f"Streaming pull failed: {e}")
+            logger.error(f"Streaming pull failed: {e}")
