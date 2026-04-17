14d1a8c1fa1d12f0538a93bd32af636b5977d46d
Doc Ritezel <pair+doc@ministryofvelocity.com>
2026-04-17 12:05:23 -0700

    Disable self-signed verfication

--- services/gtfs-rt-archiver/main.py
+++ services/gtfs-rt-archiver/main.py
@@ -2,7 +2,6 @@ import json
 import os
 import traceback
 from concurrent.futures import TimeoutError
-from datetime import datetime
 
 from google.cloud import pubsub_v1
 from gtfs_rt_archiver.archiver import Archiver
@@ -14,17 +13,16 @@ from gtfs_rt_archiver.logger import logger
 class GtfsRtArchiver:
     @staticmethod
     def callback(message):
-        GtfsRtArchiver(message=message, current_time=datetime.now()).run()
+        GtfsRtArchiver(message=message).run()
 
-    def __init__(self, message: str, current_time: datetime):
-        self.message: datetime = message
-        self.current_time: datetime = current_time
+    def __init__(self, message: pubsub_v1.subscriber.message.Message):
+        self.message: pubsub_v1.subscriber.message.Message = message
         self._configuration = None
 
     def configuration(self):
         if not self._configuration:
             self._configuration = Configuration.resolve(
-                published_time=self.current_time,
+                published_time=self.message.publish_time,
                 **json.loads(self.message.data.decode("utf-8")),
             )
         return self._configuration
@@ -36,14 +34,13 @@ class GtfsRtArchiver:
         return Archiver(configuration=self.configuration())
 
     def run(self):
+        logger.info(f"{self.message.message_id} - {self.configuration().url}")
         try:
-            logger.debug(dir(self.message))
-            logger.debug(self.message)
-            logger.info(f"{self.configuration().url} - processing {self.message.data}")
             self.archiver().save(result=self.downloader().get())
+            logger.info(f"{self.message.message_id} - Processed")
         except Exception as e:
             logger.error(
-                f"{self.configuration().url} - Caught error {e}\n{traceback.format_exc()}"
+                f"{self.message.message_id} - Caught error {e}\n{traceback.format_exc()}"
             )
         self.message.ack()
 
