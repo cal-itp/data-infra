1e706808a32fed311551c852fc9607f60e2df9c1
V <vevetron@gmail.com>
2026-04-22 10:52:12 -0700

    Added calitp team slack to the notificaiton channels

--- iac/cal-itp-data-infra-staging/alerting/us/channel.tf
+++ iac/cal-itp-data-infra-staging/alerting/us/channel.tf
@@ -6,3 +6,12 @@ resource "google_monitoring_notification_channel" "calitp-meta" {
     team         = "minifast"
   }
 }
+
+resource "google_monitoring_notification_channel" "gcp-monitoring" {
+  display_name = "gcp-monitoring"
+  type         = "slack"
+  labels = {
+    channel_name = "#gcp-monitoring"
+    team         = "cal-itp"
+  }
+}
