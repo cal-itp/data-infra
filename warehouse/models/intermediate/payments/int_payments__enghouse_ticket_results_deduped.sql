1c2fa3de072645ae6875d167bf61c10813d75830
Steven Schrayer <sschrayer@foursquareitp.com>
2026-04-27 10:59:29 -0400

    chore: update dedupe process

--- warehouse/models/intermediate/payments/int_payments__enghouse_ticket_results_deduped.sql
+++ warehouse/models/intermediate/payments/int_payments__enghouse_ticket_results_deduped.sql
@@ -4,22 +4,6 @@ WITH ticket_results AS (
     SELECT * FROM {{ ref('stg_enghouse__ticket_results') }}
 ),
 
-deduplicated AS (
-    SELECT * FROM (
-        SELECT
-            *,
-            ROW_NUMBER() OVER (
-                PARTITION BY _content_hash
-                ORDER BY
-                    CASE WHEN start_dttm IS NOT NULL THEN 0 ELSE 1 END ASC,
-                    CASE WHEN end_dttm IS NOT NULL THEN 0 ELSE 1 END ASC,
-                    CASE WHEN created_dttm IS NOT NULL THEN 0 ELSE 1 END ASC
-            ) AS row_num
-        FROM ticket_results
-    )
-    WHERE row_num = 1
-),
-
 int_payments__enghouse_ticket_results_deduped AS (
     SELECT
         operator_id,
@@ -27,20 +11,21 @@ int_payments__enghouse_ticket_results_deduped AS (
         ticket_id,
         station_name,
         amount,
-        clearing_id,
         reason,
         tap_id,
         ticket_type,
-        created_dttm,
         line,
         start_station,
         end_station,
-        start_dttm,
-        end_dttm,
         ticket_code,
         additional_infos,
-        _content_hash
-    FROM deduplicated
+        _content_hash,
+        MIN(clearing_id) AS clearing_id,
+        MIN(created_dttm) AS created_dttm,
+        MIN(start_dttm) AS start_dttm,
+        MIN(end_dttm) AS end_dttm
+    FROM ticket_results
+    {{ dbt_utils.group_by(n=14) }}
 )
 
 SELECT * FROM int_payments__enghouse_ticket_results_deduped
