d28038ebb3b4f76147541631eff5f5d48eda00f7
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2026-04-28 14:00:39 -0700

    model_vp_trajectory calls the operator and saves to bucket

--- airflow/dags/model_vp_trajectory.py
+++ airflow/dags/model_vp_trajectory.py
@@ -1,5 +1,4 @@
 import os
-from base64 import urlsafe_b64encode
 from datetime import datetime, timedelta
 
 from dags import log_failure_to_slack
@@ -7,9 +6,7 @@ from operators.bigquery_vehicle_locations_to_gcs_vehicle_trajectories import (
     BigQueryVehicleLocationsToTrajectory,
 )
 
-from airflow import XComArg
-from airflow.decorators import dag, task
-from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.decorators import dag
 from airflow.utils.trigger_rule import TriggerRule
 
 # Set new bucket by Terrform 
@@ -22,7 +19,6 @@ CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS = os.environ["CALITP_BUCKET__GTFS_VEHICLE_
     start_date=datetime(2026, 5, 1),
     catchup=False,
     tags=["gtfs"],
-    user_defined_macros={"urlsafe_b64encode": urlsafe_b64encode},
     default_args={
         "email": os.getenv("CALITP_NOTIFY_EMAIL"),
         "email_on_failure": True,
@@ -34,19 +30,19 @@ CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS = os.environ["CALITP_BUCKET__GTFS_VEHICLE_
 def model_vp_trajectory():
   
     model_trajectory = BigQueryVehicleLocationsToTrajectory(
-        task_id="bigquery_vehicle_locations_to_trajectory",
+        task_id="model_trajectory",
         retries=1,
         retry_delay=timedelta(seconds=10),
         dataset_name="mart_gtfs",
         table_name="fct_vehicle_locations",
-        ts="{{ data_interval_end | ts }}",
         destination_bucket=os.getenv("CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS"),
         # can this be partitioned on service_date?
-        destination_path="vehicle_locations_trajectory/service_date={{ data_interval_end | ds }}.jsonl.gz",
+        destination_path="vehicle_locations_trajectory/service_date={{ dt }}trajectories.jsonl.gz",
     )
 
     model_trajectory >> create_external_table 
-    #TODO how to add the task to create the external table from the hive-partitioned bucket?
+    #TODO how to add the task to create the external table from the hive-partitioned bucket? how does the yaml get called? 
+    # set trigger rule to success of model_trajectory
 
 
 model_vp_trajectory_instance = model_trajectory()
