be215eb308c1be1273cd372c295899c655b49397
Doc Ritezel <doc@ministryofvelocity.com>
2026-04-29 11:01:05 -0700

    GTFS-RT archiver requests application/x-protobuf when requesting feeds (#5183)

--- services/gtfs-rt-archiver/gtfs_rt_archiver/configuration.py
+++ services/gtfs-rt-archiver/gtfs_rt_archiver/configuration.py
@@ -10,7 +10,7 @@ from google.cloud.secretmanager import SecretManagerServiceClient
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept": "application/x-protobuf,text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Language": "en-US,en;q=0.9",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
