587e758e5da093d6bd54b511ccfc00b1b6ebd1c7
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2026-04-29 10:10:25 -0700

    have dag run through a list of ranged service_dates

--- airflow/dags/model_vp_trajectory.py
+++ airflow/dags/model_vp_trajectory.py
@@ -19,6 +19,7 @@ CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS = os.environ["CALITP_BUCKET__GTFS_VEHICLE_
     start_date=datetime(2026, 5, 1),
     catchup=False,
     tags=["gtfs"],
+    user_defined_macros={"service_date_min": "", "service_date_max": ""}, # how to define the range of dates?
     default_args={
         "email": os.getenv("CALITP_NOTIFY_EMAIL"),
         "email_on_failure": True,
@@ -28,21 +29,25 @@ CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS = os.environ["CALITP_BUCKET__GTFS_VEHICLE_
 )
 
 def model_vp_trajectory():
-  
-    model_trajectory = BigQueryVehicleLocationsToTrajectory(
-        task_id="model_trajectory",
-        retries=1,
-        retry_delay=timedelta(seconds=10),
-        dataset_name="mart_gtfs",
-        table_name="fct_vehicle_locations",
-        destination_bucket=os.getenv("CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS"),
-        # can this be partitioned on service_date?
-        destination_path="vehicle_locations_trajectory/service_date={{ dt }}trajectories.jsonl.gz",
-    )
+    service_date_range = pd.date_range(service_date_min, service_date_max)
+    
+    for one_date in service_date_range:
+        model_trajectory = BigQueryVehicleLocationsToTrajectory(
+            task_id="model_trajectory",
+            retries=1,
+            retry_delay=timedelta(seconds=10),
+            dataset_name="mart_gtfs",
+            table_name="fct_vehicle_locations",
+            one_service_date=one_date,
+            destination_bucket=os.getenv("CALITP_BUCKET__GTFS_VEHICLE_LOCATIONS"),
+            destination_path="vehicle_locations_trajectory/service_date={{ one_date }}/trajectories.jsonl.gz", 
+            # https://docs.cloud.google.com/bigquery/docs/hive-partitioned-queries
+            # can be partitioned on service_date
+            # filename is trajectories.jsonl.gz
+        )
 
     model_trajectory >> create_external_table 
-    #TODO how to add the task to create the external table from the hive-partitioned bucket? how does the yaml get called? 
-    # set trigger rule to success of model_trajectory
+    #TODO how to add the task to create the external table from the hive-partitioned bucket? how does the yaml get called?
 
 
 model_vp_trajectory_instance = model_trajectory()
