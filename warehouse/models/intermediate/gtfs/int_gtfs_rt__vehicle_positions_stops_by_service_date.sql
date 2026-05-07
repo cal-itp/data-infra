e465b9652a5dcd058b2dc998bfcce3b5e1d89eb6
Laurie Merrell <laurie.m@jarv.us>
2026-05-07 11:59:26 -0500

    refactor: remove unnecessary dependencies, add comments

--- warehouse/models/intermediate/gtfs/int_gtfs_rt__vehicle_positions_stops_by_service_date.sql
+++ warehouse/models/intermediate/gtfs/int_gtfs_rt__vehicle_positions_stops_by_service_date.sql
@@ -11,14 +11,14 @@
 }}
 
 -- fetch a list of feed keys
--- the where clause here needs to align exactly with the where clause used on fct_scheduled_trips below
+-- **the where clause here needs to align exactly with the where clause used on fct_scheduled_trips below**
 -- this allows us to filter stop times grouped for significant efficiency gains
 {%- call statement('get_keys', fetch_result=True) -%}
     SELECT ARRAY_TO_STRING(ARRAY_AGG(DISTINCT '"' || feed_key || '"' ), ',')
     FROM {{ ref('fct_scheduled_trips') }}
     WHERE service_date
         -- subtract 1 at the end to account for 1-day offset between UTC and Pacific dates (always want one service day earlier than the UTC lookback day would be)
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_START")) }} - 1
+        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_STOP_ANALYSIS_START")) }} - 1
             AND {{ ranged_incremental_max_date() }}
 {%- endcall -%}
 
@@ -35,7 +35,7 @@ WITH trips AS (
     FROM {{ ref('fct_scheduled_trips') }}
     WHERE service_date
         -- subtract 1 at the end to account for 1-day offset between UTC and Pacific dates (always want one service day earlier than the UTC lookback day would be)
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_START")) }} - 1
+        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_STOP_ANALYSIS_START")) }} - 1
             AND {{ ranged_incremental_max_date() }}
 ),
 
@@ -62,8 +62,7 @@ stops AS (
 
     FROM {{ ref('fct_daily_scheduled_stops') }}
     WHERE service_date
-        -- subtract 1 at the end to account for 1-day offset between UTC and Pacific dates (always want one service day earlier than the UTC lookback day would be)
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_START")) }} - 1
+        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_STOP_ANALYSIS_START")) }}
             AND {{ ranged_incremental_max_date() }}
 ),
 
@@ -73,115 +72,94 @@ daily_rt_feeds AS (
         base64_url AS vp_base64_url,
     FROM {{ ref('fct_daily_rt_feed_files') }}
     WHERE `date`
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_START")) }}
+        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_STOP_ANALYSIS_START")) }}
             AND {{ ranged_incremental_max_date() }}
         AND feed_type = 'vehicle_positions'
 ),
 
-int_vp_trips AS (
-    SELECT DISTINCT
-        dt,
-        service_date,
-        base64_url,
-        trip_id,
-    FROM {{ ref('int_gtfs_rt__vehicle_positions_trip_day_map_grouping') }}
-    WHERE dt
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_START")) }}
-            AND {{ ranged_incremental_max_date() }}
-),
-
-vp_trips AS (
-    SELECT DISTINCT
-        service_date,
-        base64_url,
-        trip_id,
-        trip_instance_key,
-    FROM {{ ref('fct_vehicle_positions_trip_summaries') }}
-),
-
-
 vehicle_locations AS (
     SELECT
         key,
-        dt,
         service_date,
         base64_url,
         trip_instance_key,
         location
-
     FROM {{ ref('fct_vehicle_locations') }}
     WHERE dt
-        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_SCHEDULE_START")) }}
+        BETWEEN {{ ranged_incremental_min_date(default_lookback=var("DBT_ALL_INCREMENTAL_LOOKBACK_DAYS"), data_earliest_start=var("GTFS_RT_STOP_ANALYSIS_START")) }}
             AND {{ ranged_incremental_max_date() }}
 ),
 
 schedule_joins AS (
     SELECT
-        int_vp_trips.dt,
         trips.feed_key,
         trips.service_date,
         trips.trip_instance_key,
         stop_times_grouped.key AS st_trip_key,
         stop_id,
         stops.pt_geom,
-        daily_rt_feeds.vp_base64_url,
-
-
+    -- what we want is the batch of all scheduled trips from the last X service dates
     FROM trips
-    INNER JOIN stop_times_grouped
+    LEFT JOIN stop_times_grouped
         ON trips.feed_key = stop_times_grouped.feed_key
         AND trips.trip_id = stop_times_grouped.trip_id
         AND trips.trip_first_departure_sec = stop_times_grouped.trip_first_departure_sec
     -- use trip_first_departure_sec instead of iteration_num because scheduled_trips does more coalescing on iteration_num, so we might not match it
     LEFT JOIN UNNEST(stop_times_grouped.stop_id_array) AS stop_id
-    INNER JOIN stops
+    LEFT JOIN stops
         ON trips.feed_key = stops.feed_key
         AND trips.service_date = stops.service_date
         AND stop_id = stops.stop_id
+),
+
+-- we only want scheduled trips that have a VP url associated
+-- this could potentially be split out into a dedicated upstream model
+-- so that the filtering on grouped stop times could be even tighter
+scheduled_with_vp_url AS (
+    SELECT
+        schedule_joins.feed_key,
+        schedule_joins.service_date,
+        schedule_joins.trip_instance_key,
+        schedule_joins.st_trip_key,
+        schedule_joins.stop_id,
+        schedule_joins.pt_geom,
+        daily_rt_feeds.vp_base64_url
+    FROM schedule_joins
     INNER JOIN daily_rt_feeds
         ON trips.feed_key = daily_rt_feeds.schedule_feed_key
-    INNER JOIN vp_trips
-        ON trips.service_date = vp_trips.service_date
-        AND daily_rt_feeds.vp_base64_url = vp_trips.base64_url
-        AND trips.trip_id = vp_trips.trip_id
-        AND trips.trip_instance_key = vp_trips.trip_instance_key
-    INNER JOIN int_vp_trips
-        ON trips.service_date = int_vp_trips.service_date
-        AND vp_trips.base64_url = int_vp_trips.base64_url
-        AND vp_trips.trip_id = int_vp_trips.trip_id
 ),
+
 
 vp_near_stops AS (
    SELECT
-        schedule_joins.dt,
-        schedule_joins.service_date,
-        schedule_joins.vp_base64_url,
+        scheduled_with_vp_url.dt,
+        scheduled_with_vp_url.service_date,
+        scheduled_with_vp_url.vp_base64_url,
         vehicle_locations.key AS vp_key,
         --vehicle_locations.location,
 
-        schedule_joins.feed_key,
-        schedule_joins.trip_instance_key,
-        schedule_joins.stop_id,
-        schedule_joins.st_trip_key, -- use to key back into schedule trip info found in stop times (int_gtfs_schedule__stop_times_grouped)
+        scheduled_with_vp_url.feed_key,
+        scheduled_with_vp_url.trip_instance_key,
+        scheduled_with_vp_url.stop_id,
+        scheduled_with_vp_url.st_trip_key, -- use to key back into schedule trip info found in stop times (int_gtfs_schedule__stop_times_grouped)
         --schedule_joins.pt_geom,
 
-        MIN(ROUND(ST_DISTANCE(vehicle_locations.location, schedule_joins.pt_geom), 2)) AS distance_meters,
+        MIN(ROUND(ST_DISTANCE(vehicle_locations.location, scheduled_with_vp_url.pt_geom), 2)) AS distance_meters,
         --ST_CLOSESTPOINT(vehicle_locations.location, schedule_joins.pt_geom) is returning a point useful? It'll be difficult to match against, use vp_keys instead and distances
 
-    FROM schedule_joins
+    FROM scheduled_with_vp_url
     INNER JOIN vehicle_locations
-        ON schedule_joins.vp_base64_url = vehicle_locations.base64_url
-        AND schedule_joins.service_date = vehicle_locations.service_date
-        AND schedule_joins.trip_instance_key = vehicle_locations.trip_instance_key
-        AND ST_DWITHIN(vehicle_locations.location, schedule_joins.pt_geom, 100)
+        ON scheduled_with_vp_url.vp_base64_url = vehicle_locations.base64_url
+        AND scheduled_with_vp_url.service_date = vehicle_locations.service_date
+        AND scheduled_with_vp_url.trip_instance_key = vehicle_locations.trip_instance_key
+        AND ST_DWITHIN(vehicle_locations.location, scheduled_with_vp_url.pt_geom, 100)
         -- 100 meters ~ 0.1 miles, 328 ft
-    GROUP BY dt, service_date, vp_base64_url, feed_key, trip_instance_key, stop_id, st_trip_key, vp_key
+    GROUP BY service_date, vp_base64_url, feed_key, trip_instance_key, stop_id, st_trip_key, vp_key
 ),
 
 
-vp_counts_near_stops AS (
+int_gtfs_rt__vehicle_positions_stops_by_service_date AS (
     SELECT
-        dt,
         service_date,
         vp_base64_url,
         feed_key,
@@ -197,7 +175,7 @@ vp_counts_near_stops AS (
         ARRAY_AGG(vp_key ORDER BY distance_meters, vp_key) AS vp_key_array,
 
     FROM vp_near_stops
-    GROUP BY dt, service_date, vp_base64_url, feed_key, trip_instance_key, stop_id, st_trip_key
+    GROUP BY service_date, vp_base64_url, feed_key, trip_instance_key, stop_id, st_trip_key
 )
 
-SELECT * FROM vp_counts_near_stops
+SELECT * FROM int_gtfs_rt__vehicle_positions_stops_by_service_date
