63014982b7c98b66dddd92cecd85297b272e22a6
Doc Ritezel <doc@ministryofvelocity.com>
2026-05-22 08:41:12 -0700

    Owners are added to the owner role (#4898)
    * Owners are added to the owner role [#4885]
    
    * Add IAM permissions for DDS_Payments_Admins and DDS_Warehouse_Users_Flex
    
    - Add ms-entra-id-DDS_Payments_Admins with viewer access
    - Add ms-entra-id-DDS_Warehouse_Users_Flex with viewer access
    
    ---------
    
    Co-authored-by: Daniel Huang <tihuang@gmail.com>

--- iac/cal-itp-data-infra/iam/us/project_iam_member.tf
+++ iac/cal-itp-data-infra/iam/us/project_iam_member.tf
@@ -615,6 +615,34 @@ resource "google_project_iam_member" "ms-entra-id-DDS_Cloud_Admins" {
   project = "cal-itp-data-infra"
 }
 
+resource "google_project_iam_member" "ms-entra-id-DDS_Payments_Admins" {
+  for_each = toset([
+    "roles/viewer",
+  ])
+  role    = each.key
+  member  = "principalSet://iam.googleapis.com/locations/global/workforcePools/dot-ca-gov/group/DDS_Payments_Admins"
+  project = "cal-itp-data-infra"
+}
+
+resource "google_project_iam_member" "ms-entra-id-DDS_Warehouse_Users_Flex" {
+  for_each = toset([
+    "roles/viewer",
+  ])
+  role    = each.key
+  member  = "principalSet://iam.googleapis.com/locations/global/workforcePools/dot-ca-gov/group/DDS_Warehouse_Users_Flex"
+  project = "cal-itp-data-infra"
+}
+
+# DDS_Payments_Admins and DDS_Warehouse_Users_Flex have been added with viewer access enabled.
+resource "google_project_iam_member" "ms-entra-id-DDS_Cloud_Owners" {
+  for_each = toset([
+    "roles/owner"
+  ])
+  role    = each.key
+  member  = "principalSet://iam.googleapis.com/locations/global/workforcePools/dot-ca-gov/group/DDS_Cloud_Owners"
+  project = "cal-itp-data-infra"
+}
+
 # FIXME: if this exists after 8/1/2025, delete it and see if GitHub Actions fail
 resource "google_project_iam_member" "staging-github-actions-service-account" {
   role    = google_project_iam_custom_role.tfer--projects-002F-cal-itp-data-infra-002F-roles-002F-DataAnalyst.id
