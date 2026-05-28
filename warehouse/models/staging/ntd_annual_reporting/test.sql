c3aac4c47a02c4d9508c012296aee64b08309432
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-05-28 17:22:09 +0000

    cast as big numeric

--- warehouse/models/staging/ntd_annual_reporting/test.sql
+++ warehouse/models/staging/ntd_annual_reporting/test.sql
@@ -14,10 +14,8 @@ stg_ntd__service_by_mode_and_time_period AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['_5_digit_ntd_id', 'report_year', 'mode', 'type_of_service', 'time_period']) }} AS key,
         {{ trim_make_empty_string_null('_5_digit_ntd_id') }} AS ntd_id,
-        PARSE_BIGNUMERIC(CAST actual_vehicles_passenger_car_deadhead_hours AS STRING) AS actual_vehicles_passenger_car_deadhead_hours,
-        PARSE_BIGNUMERIC(CAST actual_vehicles_passenger_car_hours AS STRING) AS actual_vehicles_passenger_car_hours,
-        PARSE_BIGNUMERIC(CAST actual_vehicles_passenger_car_miles AS STRING) AS actual_vehicles_passenger_car_miles,
-        PARSE_BIGNUMERIC(CAST actual_vehicles_passenger_car_revenue_hours AS STRING) AS actual_vehicles_passenger_car_revenue_hours,
+        CAST actual_vehicles_passenger_car_deadhead_hours AS BIGNUMERIC) AS actual_vehicles_passenger_car_deadhead_hours,
+
 
     FROM get_latest_extract
 )
