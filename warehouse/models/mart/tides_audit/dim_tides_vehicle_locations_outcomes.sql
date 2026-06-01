c2024a7a9e2a8ea11fafd3b966956c47d57ff0b8
Erika <erika@ministryofvelocity.com>
2026-06-01 15:00:57 -0400

    Rename dim_tides_vehicle_locations_outcomes

--- warehouse/models/mart/tides_audit/dim_tides_vehicle_locations_outcomes.sql
+++ warehouse/models/mart/tides_audit/dim_tides_vehicle_locations_outcomes.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
-WITH dim_tides_vehicle_location_outcomes AS (
+WITH dim_tides_vehicle_locations_outcomes AS (
     SELECT DISTINCT
            display_name,
            organization_source_record_id,
@@ -8,9 +8,10 @@ WITH dim_tides_vehicle_location_outcomes AS (
            dataset_name,
            table_name,
            destination_path,
+           service_date,
            dt,
            ts
-      FROM {{ source('external_tides', 'vehicle_location_outcomes') }}
+      FROM {{ source('external_tides', 'vehicle_locations_outcomes') }}
 )
 
-SELECT * FROM dim_tides_vehicle_location_outcomes
+SELECT * FROM dim_tides_vehicle_locations_outcomes
