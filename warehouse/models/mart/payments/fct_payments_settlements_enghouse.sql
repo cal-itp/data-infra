2098ccaba4cee371ee89b455f64736405ea9b34d
Anna Paten <anna.paten@dot.ca.gov>
2026-06-09 12:50:47 -0700

    fix(payments)!: remove taps join in fct_payments_settlements_enghouse (#5399)

--- warehouse/models/mart/payments/fct_payments_settlements_enghouse.sql
+++ warehouse/models/mart/payments/fct_payments_settlements_enghouse.sql
@@ -5,10 +5,6 @@ WITH transactions AS (
     SELECT * FROM {{ ref('stg_enghouse__transactions') }}
 ),
 
-taps AS (
-    SELECT * FROM {{ ref('stg_enghouse__taps') }}
-),
-
 payments_entity_mapping AS (
     SELECT
         * EXCEPT(enghouse_operator_id),
@@ -16,48 +12,8 @@ payments_entity_mapping AS (
     FROM {{ ref('payments_entity_mapping_enghouse') }}
 ),
 
-fct_daily_schedule_feeds AS (
-    SELECT * FROM {{ ref('fct_daily_schedule_feeds') }}
-),
-
 dim_gtfs_datasets AS (
     SELECT * FROM {{ ref('dim_gtfs_datasets') }}
-),
-
-dim_orgs AS (
-    SELECT * FROM {{ ref('dim_organizations') }}
-),
-
-dim_agency AS (
-    SELECT * FROM {{ ref('dim_agency') }}
-),
-
-dim_routes AS (
-    SELECT * FROM {{ ref('dim_routes') }}
-),
-
-participants_to_routes_and_agency AS (
-    SELECT
-        map.operator_id,
-        map.organization_source_record_id,
-        map._in_use_from,
-        map._in_use_until,
-        feeds.date,
-        routes.route_id,
-        routes.route_short_name,
-        routes.route_long_name,
-        agency.agency_id,
-        agency.agency_name,
-    FROM payments_entity_mapping AS map
-    LEFT JOIN dim_gtfs_datasets AS gtfs
-        ON map.gtfs_dataset_source_record_id = gtfs.source_record_id
-    LEFT JOIN fct_daily_schedule_feeds AS feeds
-        ON gtfs.key = feeds.gtfs_dataset_key
-    LEFT JOIN dim_routes AS routes
-        ON feeds.feed_key = routes.feed_key
-    LEFT JOIN dim_agency AS agency
-        ON routes.agency_id = agency.agency_id
-            AND routes.feed_key = agency.feed_key
 ),
 
 join_orgs AS (
@@ -85,22 +41,7 @@ join_orgs AS (
 
         dim_orgs.name AS organization_name,
         direct_map.organization_source_record_id,
-
-        -- Common transaction info
-        routes.route_long_name,
-        routes.route_short_name,
-        routes.agency_id,
-        routes.agency_name
     FROM transactions
-    LEFT JOIN taps
-        ON transactions.payment_reference = taps.payment_reference
-    LEFT JOIN participants_to_routes_and_agency AS routes
-        ON transactions.operator_id = routes.operator_id
-            AND EXTRACT(DATE FROM TIMESTAMP(transactions.timestamp)) = routes.date
-            AND routes.route_id = taps.line_public_number
-            AND CAST(transactions.timestamp AS TIMESTAMP)
-                BETWEEN CAST(routes._in_use_from AS TIMESTAMP)
-                AND CAST(routes._in_use_until AS TIMESTAMP)
     LEFT JOIN payments_entity_mapping AS direct_map
         ON transactions.operator_id = direct_map.operator_id
             AND CAST(transactions.timestamp AS TIMESTAMP)
@@ -134,10 +75,6 @@ fct_payments_settlements_enghouse AS (
         brand,
         organization_name,
         organization_source_record_id,
-        route_long_name,
-        route_short_name,
-        agency_id,
-        agency_name,
         LAST_DAY(EXTRACT(DATE FROM timestamp AT TIME ZONE "America/Los_Angeles"), MONTH) AS end_of_month_date_pacific,
         LAST_DAY(EXTRACT(DATE FROM timestamp), MONTH) AS end_of_month_date_utc,
         _content_hash
