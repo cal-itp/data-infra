6d1a8d7bf75d305caccf756e18b539536242df86
Daniel Huang <1581615+tihuang02@users.noreply.github.com>
2026-07-01 19:32:48 +0000

    switch to nix to for package management

--- .devcontainer/postCreate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# uv sync
-uv sync --locked --all-groups --directory warehouse
-uv sync --locked --all-groups --directory airflow
-
-echo "✅ Dev‑container setup complete."
