382c32b2cf2dedb3dfc469159778ce5461516840
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2026-07-09 08:56:38 -0700

    Refactor ntd monthly models (#5523)
    * fix macro, add space
    
    * remove annual models, apply macros to monthly model
    
    * add capital expenditures and operating/cap funding models
    
    * move order of join
    
    * fix typos

--- warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates.sql
+++ warehouse/models/mart/ntd_ridership/fct_complete_monthly_ridership_with_adjustments_and_estimates.sql
@@ -5,44 +5,23 @@ WITH staging_complete_monthly_ridership_with_adjustments_and_estimates AS (
     FROM {{ ref('stg_ntd__complete_monthly_ridership_with_adjustments_and_estimates') }}
 ),
 
-current_dim_organizations AS (
-    SELECT
-        ntd_id,
-        caltrans_district AS caltrans_district_current,
-        caltrans_district_name AS caltrans_district_name_current
-    FROM {{ ref('dim_organizations_latest_with_caltrans_district') }}
-),
-
-ntd_modes as (
-    SELECT * FROM {{ ref('ntd_modes_to_full_names') }}
-),
-
 fct_complete_monthly_ridership_with_adjustments_and_estimates AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['stg.ntd_id', 'stg.mode', 'stg.tos', 'period_month', 'period_year']) }} as key,
+        {{ dbt_utils.generate_surrogate_key(['stg.ntd_id', 'stg.mode', 'stg.type_of_service', 'month_first_day']) }} as key,
 
         stg.ntd_id,
         stg.agency,
 
-        orgs.caltrans_district_current,
-        orgs.caltrans_district_name_current,
-
         stg.date,
-        stg.period_month,
-        stg.period_year,
-        stg.period_year_month,
-        stg.tos,
+        stg.month,
+        stg.year,
+        stg.month_first_day,
+        stg.type_of_service,
         stg.mode,
 
-        ntd_modes.ntd_mode_full_name AS mode_name,
-
-        CASE
-            WHEN stg.mode IN ('AG', 'AR', 'CB', 'CC', 'CR', 'FB', 'HR', 'IP', 'IP', 'LR', 'MB', 'MG', 'MO', 'RB', 'SR', 'TB', 'TR', 'YR')
-            THEN 'Fixed Route'
-            WHEN stg.mode IN ('DR', 'DT', 'VP', 'JT', 'PB')
-            THEN 'Demand Response'
-            ELSE 'Unknown' -- mode is null sometimes
-        END AS service_type,
+        {{ generate_ntd_mode_full_name('mode') }} AS mode_full_name,
+        {{ generate_ntd_type_of_service_full_name('type_of_service') }} AS type_of_service_full_name,
+        {{ generate_ntd_mode_service_type('stg.mode') }} AS service_type,
 
         stg.agency_mode_tos_date,
         stg.voms,
@@ -56,35 +35,18 @@ fct_complete_monthly_ridership_with_adjustments_and_estimates AS (
         stg.reporter_type,
         stg.mode_type_of_service_status,
         stg.vrh,
+
+        LAG(upt) OVER (PARTITION BY ntd_id, mode, type_of_service ORDER BY month_first_day) AS upt_prior_year_month,
+            upt - LAG(upt) OVER (PARTITION BY ntd_id, mode, type_of_service ORDER BY month_first_day) AS upt_change_1yr,
+        ROUND(SAFE_DIVIDE(
+            (upt - LAG(upt) OVER (PARTITION BY ntd_id, mode, type_of_service ORDER BY month_first_day)),
+            LAG(upt) OVER (PARTITION BY ntd_id, mode, type_of_service ORDER BY month_first_day)
+        ), 4) AS upt_pct_change_1yr,
+
         stg.legacy_ntd_id,
         stg.dt,
         stg.execution_ts
     FROM staging_complete_monthly_ridership_with_adjustments_and_estimates AS stg
-    LEFT JOIN current_dim_organizations AS orgs
-        USING (ntd_id)
-    LEFT JOIN ntd_modes
-        ON stg.mode = ntd_modes.ntd_mode_abbreviation
-    WHERE mode IN ("AG",
-            "AR",
-            "CB",
-            "CC",
-            "CR",
-            "DR",
-            "FB",
-            "HR",
-            "IP",
-            "LR",
-            "MB",
-            "MG",
-            "MO",
-            "OR",
-            "PB",
-            "RB",
-            "SR",
-            "TB",
-            "TR",
-            "VP",
-            "YR")
 )
 
 SELECT * FROM fct_complete_monthly_ridership_with_adjustments_and_estimates
