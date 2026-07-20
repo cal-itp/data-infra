03dbf5b2081b4c55fa5ae7faf644b2943f35ea25
V <156962295+vevetron@users.noreply.github.com>
2026-07-20 14:55:21 -0700

    Delete unused gtfsrt-v4 and jobs-v1 node pools
    Both have run zero nodes for 60+ days. Works toward #5332.

--- iac/cal-itp-data-infra/gke/us/outputs.tf
+++ iac/cal-itp-data-infra/gke/us/outputs.tf
@@ -6,14 +6,6 @@ output "google_container_node_pool_tfer--data-infra-apps_apps-v2_id" {
   value = google_container_node_pool.tfer--data-infra-apps_apps-v2.id
 }
 
-output "google_container_node_pool_tfer--data-infra-apps_gtfsrt-v4_id" {
-  value = google_container_node_pool.tfer--data-infra-apps_gtfsrt-v4.id
-}
-
-output "google_container_node_pool_tfer--data-infra-apps_jobs-v1_id" {
-  value = google_container_node_pool.tfer--data-infra-apps_jobs-v1.id
-}
-
 output "google_container_node_pool_tfer--data-infra-apps_jupyterhub-users_id" {
   value = google_container_node_pool.tfer--data-infra-apps_jupyterhub-users.id
 }
