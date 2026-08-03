d840538ba3faeb214eb5b38e6dde143c9f2bd7d5
FarhadS <54083813+fsalemi@users.noreply.github.com>
2026-08-03 09:50:38 -0700

    chore(gtfs-rt-archiver): force staging redeploy (#5604)
    Co-authored-by: farhad <farhad.salemi@calitp.org>

--- services/gtfs-rt-archiver/gtfs_rt_archiver/heartbeat.py
+++ services/gtfs-rt-archiver/gtfs_rt_archiver/heartbeat.py
@@ -10,6 +10,7 @@ from google.auth import default
 from google.cloud import pubsub_v1, storage
 
 PUBLISH_TIMEOUT = int(os.environ.get("PUBLISH_TIMEOUT", "10"))
+# Supports all currently archived GTFS-Realtime feed types.
 GTFS_RT_FEED_TYPES = [
     "service_alerts",
     "trip_updates",
