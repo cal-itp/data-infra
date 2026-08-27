cc82e148834dd4f65adec520ba0d789d5feba660
farhad <farhad.salemi@calitp.org>
2026-08-27 22:36:54 +0000

    chore: set GTFS validator 8.0.1 effective date

--- airflow/plugins/hooks/gtfs_validator_hook.py
+++ airflow/plugins/hooks/gtfs_validator_hook.py
@@ -114,7 +114,7 @@ class GTSFValidatorVersion:
             "filename": "gtfs-validator-7.1.0-cli.jar",
         },
         {
-            "version_date": pendulum.datetime(2026, 8, 23),
+            "version_date": pendulum.datetime(2026, 8, 28),
             "number": "8.0.1",
             "filename": "gtfs-validator-8.0.1-cli.jar",
         },
