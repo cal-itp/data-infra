18738d3bef0fa5716ad68e37224ef5128e2fa5c5
Erika <erika@ministryofvelocity.com>
2026-08-31 23:38:01 -0700

    remove empty space

--- warehouse/macros/define_day_of_week.sql
+++ warehouse/macros/define_day_of_week.sql
@@ -1,15 +1,14 @@
 -- Transit is typically Weekday / Saturday / Sunday service
 -- Big Query has Sunday as 1 (https://cloud.google.com/bigquery/docs/reference/standard-sql/date_functions#extract)
-{% macro generate_day_type(date_column) %}
+{%- macro generate_day_type(date_column) %}
     CASE
         WHEN EXTRACT(DAYOFWEEK FROM {{ date_column }}) = 1 THEN "Sunday"
         WHEN EXTRACT(DAYOFWEEK FROM {{ date_column }}) = 7 THEN "Saturday"
         ELSE 'Weekday'
     END
+{%- endmacro %}
 
-{% endmacro %}
-
-{% macro generate_time_of_day_column(hour_column) %}
+{%- macro generate_time_of_day_column(hour_column) -%}
     CASE
         WHEN {{ hour_column }} < 4 OR ( {{ hour_column }} >= 24 AND {{ hour_column }} < 28 ) THEN "owl"
         WHEN {{ hour_column }} < 7 OR ( {{ hour_column }} >= 28 AND {{ hour_column }} < 31 ) THEN "early_am"
@@ -19,10 +18,9 @@
         WHEN {{ hour_column }} < 24 OR ( {{ hour_column }} >= 44 AND {{ hour_column }} < 48 ) THEN "evening"
         ELSE NULL
     END
+{%- endmacro %}
 
-{% endmacro %}
-
-{% macro generate_time_of_day_hours(time_of_day_column) %}
+{%- macro generate_time_of_day_hours(time_of_day_column) -%}
     CASE
         -- hours 0-3; 20-23
         WHEN {{ time_of_day_column }} = "owl" OR {{ time_of_day_column }} = "evening" THEN 4
@@ -32,5 +30,4 @@
         WHEN {{ time_of_day_column }}  = "midday" OR {{ time_of_day_column }}  = "pm_peak" THEN 5
         ELSE NULL
     END
-
-{% endmacro %}
+{%- endmacro %}
