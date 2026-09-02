be8ceca2f45e198baa11910cc7ed30bcc6886233
Erika <erika@ministryofvelocity.com>
2026-09-02 13:27:44 -0700

    Daily and Monthly Scheduled Stop review (#5684)
    * Rewrite fct_monthly_schedule_stops to fix wrong results
    * Add parse_route_id and transit_mode to fct_daily_scheduled_stops to match fct_monthly_scheduled_stops
    * Use IF statement instead of pivot table
    * Add service_date to pivot tables to fix time of day and route type totals not grouping by service_date
    * Add warning columns and tests to validate totals
    * Fix test missing nested argument
    * Update documentation
    
       To run the tests, uncomment these two lines for gtfs_mart on dbt_project: 
        gtfs:
            +enabled: true
    ---------
    
    Co-authored-by: tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>

--- warehouse/models/docs/gtfs schedule/gtfs_rollup_metrics.md
+++ warehouse/models/docs/gtfs schedule/gtfs_rollup_metrics.md
@@ -6,12 +6,12 @@
 Parsed and standardized combined form of `route_id`, `route_short_name`, and `route_long_name`.
 This column is used to describe a route entity with time-series data.
 
-Ex 1 same route, different route_ids, due to feed versioning by transit operator: 
+Ex 1 same route, different route_ids, due to feed versioning by transit operator:
 - LA Metro Route 150
 - route_id: 150-13188 (on 2025-12-01) and 150-13196 (on 2026-02-01)
 - route_short_name: 150 (both dates)
-- route_long_name: Metro Local Line (both dates) 
-- route_name for time-series labeling: `150__150 Metro Local Line` with the 
+- route_long_name: Metro Local Line (both dates)
+- route_name for time-series labeling: `150__150 Metro Local Line` with the
 pattern `[parsed_route_id]__[route_short_name] [route_long_name]`
 
 Ex 2 route_short_name and route_long_name provide duplicate labeling:
@@ -19,7 +19,7 @@ Ex 2 route_short_name and route_long_name provide duplicate labeling:
 - route_id: 5
 - route_short_name: 5
 - route_long_name: Route 5
-- route_name for time-series labeling: `5__Route 5` with the 
+- route_name for time-series labeling: `5__Route 5` with the
 pattern `[parsed_route_id]__[route_long_name]`
 
 See the macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.get_combined_route_name
@@ -34,9 +34,42 @@ For longer periods, it is the total stop arrivals / n_service_dates for the peri
 
 {% enddocs %}
 
+
+{% docs column_avg_daily_stop_arrivals %}
+
+Scheduled stop arrivals are scheduled stop times.
+The total stop arrivals / n_service_dates for the period.
+
+{% enddocs %}
+
+
+{% docs column_monthly_stop_arrivals %}
+
+Scheduled stop arrivals are scheduled stop times.
+The count of stop arrivals, the count of scheduled stop arrivals, per month.
+
+{% enddocs %}
+
+
+{% docs column_hours_in_service %}
+
+Number of hours in service per day.
+It is the distinct number of arrival hours based on a scheduled stop_time's arrival_time column.
+
+{% enddocs %}
+
+
+{% docs column_monthly_hours_in_service %}
+
+Number of hours in service per month.
+It is the total number of arrival hours based on a scheduled stop_time's arrival_time column.
+
+{% enddocs %}
+
+
 {% docs column_stop_arrivals_per_hour %}
 
-Number of arrivals per hour (total stop arrivals / n_hours) for each time-of-day in 
+Number of arrivals per hour (total stop arrivals / n_hours) for each time-of-day in
 `owl`, `early_am`, `am_peak`, `midday`, `pm_peak`, `evening`, `peak`, `offpeak`
 
 This normalized metric provides a useful comparison across time-of-day values.
@@ -46,7 +79,7 @@ This normalized metric provides a useful comparison across time-of-day values.
 * peak = 8 hours
 * offpeak = 16 hours
 
-See macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.generate_time_of_day_hours 
+See macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.generate_time_of_day_hours
 
 {% enddocs %}
 
@@ -62,17 +95,36 @@ In a 24 hour period:
 * hours 10-14: midday
 * hours 15-19: pm_peak
 * hours 20-23: evening
-* peak = am_peak + pm_peak 
+* peak = am_peak + pm_peak
 * offpeak = owl + early_am + midday + evening
 * all_day: 24 hours
 
-See macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.generate_time_of_day_column     
+See macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.generate_time_of_day_column
+{% enddocs %}
+
+
+{% docs column_monthly_stop_arrivals_by_time_of_day %}
+
+Total of arrivals during the time-of-day per month.
+
+In a 24 hour period:
+* hours 0-3: owl
+* hours 4-6: early_am
+* hours 7-9: am_peak
+* hours 10-14: midday
+* hours 15-19: pm_peak
+* hours 20-23: evening
+* peak = am_peak + pm_peak
+* offpeak = owl + early_am + midday + evening
+* all_day: 24 hours
+
+See macro: https://dbt-docs.dds.dot.ca.gov/#!/macro/macro.calitp_warehouse.generate_time_of_day_column
 {% enddocs %}
 
 
 {% docs column_daily_trips_by_time_of_day %}
 
-Daily trips by for each time-of-day in 
+Daily trips by for each time-of-day in
 `owl`, `early_am`, `am_peak`, `midday`, `pm_peak`, `evening`, `peak`, `offpeak`
 
 For daily tables, it is number of trips on that service_date.
@@ -83,7 +135,7 @@ For monthly tables, it is the average daily number of trips calculated over the 
 
 {% docs column_frequency_trips_per_hour %}
 
-Number of trips per hour (n_trips / n_hours) by for each time-of-day in 
+Number of trips per hour (n_trips / n_hours) by for each time-of-day in
 `owl`, `early_am`, `am_peak`, `midday`, `pm_peak`, `evening`, `peak`, `offpeak`
 
 This normalized metric provides a useful comparison across time-of-day values.
@@ -103,7 +155,7 @@ Count of distinct trip_instance_keys from the trip updates dataset.
 pct_tu_trips is the percent of scheduled trips with trip updates.
 pct_tu_trips = n_tu_trips / n_scheduled_trips
 
-daily_tu_trips is the average daily number of trips with trip updates over this 
+daily_tu_trips is the average daily number of trips with trip updates over this
 period (up to a month).
 daily_tu_trips = n_tu_trips / n_service_dates.
 
@@ -117,7 +169,7 @@ Count of distinct trip_instance_keys from the vehicle positions dataset.
 pct_vp_trips is the percent of scheduled trips with vehicle positions.
 pct_vp_trips = n_vp_trips / n_scheduled_trips
 
-daily_vp_trips is the average daily number of trips with vehicle positions over this 
+daily_vp_trips is the average daily number of trips with vehicle positions over this
 period (up to a month).
 daily_vp_trips = n_vp_trips / n_service_dates.
 
@@ -126,13 +178,13 @@ daily_vp_trips = n_vp_trips / n_service_dates.
 
 {% docs column_rt_messages_per_minute %}
 
-The `num_distinct_updates` number of distinct updates is the 
+The `num_distinct_updates` number of distinct updates is the
 count of how many messages per minute this entity was present for,
 which can help give a sense for how continuously this entity was updated.
 This number should be equal to `num_distinct_message_keys`.
 
-If you divide this value by `extract_duration_minutes`, you would get a count of 
-how many messages per minute this entity was present for, which can help give a 
+If you divide this value by `extract_duration_minutes`, you would get a count of
+how many messages per minute this entity was present for, which can help give a
 sense for how continuously this entity was updated.
 
 See `num_distinct_extract_ts` and `num_distinct_message_keys`.
--- warehouse/models/mart/gtfs_rollup/fct_monthly_scheduled_stops.sql
+++ warehouse/models/mart/gtfs_rollup/fct_monthly_scheduled_stops.sql
@@ -1,135 +1,84 @@
 {{ config(materialized='table') }}
 
-WITH stops AS (
+WITH monthly_stops AS (
     SELECT
-        -- get these from dim_stops, since pt_geom can't be grouped or select distinct on
-        * EXCEPT(tts_stop_name, pt_geom, parent_station, stop_code,
-        stop_name, stop_desc, location_type, wheelchair_boarding)
-    FROM {{ ref('fct_daily_scheduled_stops') }}
-    WHERE service_date <= LAST_DAY(
-        DATE_SUB(CURRENT_DATE("America/Los_Angeles"), INTERVAL 1 MONTH)
-    )
-),
-
-dim_stops AS (
-    SELECT *
-    FROM {{ ref('dim_stops') }}
-    -- clustered on feed_key, can we make use to optimize joins?
-),
-
-
-feeds AS (
-    SELECT
-        feed_key,
-        gtfs_dataset_name AS name,
-    FROM {{ ref('fct_daily_schedule_feeds') }}
-    GROUP BY feed_key, name
-),
-
-stops2 AS (
-    SELECT
-        stops.*,
-        feeds.name,
+        feeds.gtfs_dataset_name AS name,
 
         DATE_TRUNC(service_date, MONTH) AS month_first_day,
-
+        EXTRACT(YEAR FROM service_date) AS year,
+        EXTRACT(MONTH FROM service_date) AS month,
         {{ generate_day_type('service_date') }} AS day_type,
 
-    FROM stops
-    INNER JOIN feeds
-      ON feeds.feed_key = stops.feed_key
-),
+        stops.stop_id,
+        stops.stop_key,
 
-monthly_stop_counts AS (
-    SELECT
-        name,
-        month_first_day,
-        day_type,
-        stop_id,
+        COUNT(DISTINCT service_date) AS n_days,
+        COUNT(DISTINCT stops.feed_key) AS n_feeds,
 
         SUM(daily_arrivals) AS total_stop_arrivals,
         ROUND(SUM(daily_arrivals) / COUNT(DISTINCT service_date), 1) AS daily_stop_arrivals,
-        ROUND(AVG(n_hours_in_service), 0) AS n_hours_in_service,
 
-        ROUND(AVG(arrivals_per_hour_owl), 1) AS arrivals_per_hour_owl,
-        ROUND(AVG(arrivals_per_hour_early_am), 1) AS arrivals_per_hour_early_am,
-        ROUND(AVG(arrivals_per_hour_am_peak), 1) AS arrivals_per_hour_am_peak,
-        ROUND(AVG(arrivals_per_hour_midday), 1) AS arrivals_per_hour_midday,
-        ROUND(AVG(arrivals_per_hour_pm_peak), 1) AS arrivals_per_hour_pm_peak,
-        ROUND(AVG(arrivals_per_hour_evening), 1) AS arrivals_per_hour_evening,
+        SUM(n_hours_in_service) AS total_n_hours_in_service,
+        ROUND(AVG(n_hours_in_service), 0) AS daily_n_hours_in_service,
 
-        -- would sum or averages make more sense?
-        -- we can always use sum() / n_dates to get average
-        -- start with averages, because we want typical metrics over the month
-        ROUND(AVG(arrivals_owl), 1) AS arrivals_owl,
-        ROUND(AVG(arrivals_early_am), 1) AS arrivals_early_am,
-        ROUND(AVG(arrivals_am_peak), 1) AS arrivals_am_peak,
-        ROUND(AVG(arrivals_midday), 1) AS arrivals_midday,
-        ROUND(AVG(arrivals_pm_peak), 1) AS arrivals_pm_peak,
-        ROUND(AVG(arrivals_evening), 1) AS arrivals_evening,
+        ROUND(AVG(arrivals_per_hour_owl), 1) AS daily_arrivals_per_hour_owl,
+        ROUND(AVG(arrivals_per_hour_early_am), 1) AS daily_arrivals_per_hour_early_am,
+        ROUND(AVG(arrivals_per_hour_am_peak), 1) AS daily_arrivals_per_hour_am_peak,
+        ROUND(AVG(arrivals_per_hour_midday), 1) AS daily_arrivals_per_hour_midday,
+        ROUND(AVG(arrivals_per_hour_pm_peak), 1) AS daily_arrivals_per_hour_pm_peak,
+        ROUND(AVG(arrivals_per_hour_evening), 1) AS daily_arrivals_per_hour_evening,
 
-        ROUND(AVG(route_type_0), 1) AS route_type_0,
-        ROUND(AVG(route_type_1), 1) AS route_type_1,
-        ROUND(AVG(route_type_2), 1) AS route_type_2,
-        ROUND(AVG(route_type_3), 1) AS route_type_3,
-        ROUND(AVG(route_type_4), 1) AS route_type_4,
-        ROUND(AVG(route_type_5), 1) AS route_type_5,
-        ROUND(AVG(route_type_6), 1) AS route_type_6,
-        ROUND(AVG(route_type_7), 1) AS route_type_7,
-        ROUND(AVG(route_type_11), 1) AS route_type_11,
-        ROUND(AVG(route_type_12), 1) AS route_type_12,
-        ROUND(AVG(missing_route_type), 1) AS missing_route_type,
+        SUM(arrivals_owl) AS total_arrivals_owl,
+        SUM(arrivals_early_am) AS total_arrivals_early_am,
+        SUM(arrivals_am_peak) AS total_arrivals_am_peak,
+        SUM(arrivals_midday) AS total_arrivals_midday,
+        SUM(arrivals_pm_peak) AS total_arrivals_pm_peak,
+        SUM(arrivals_evening) AS total_arrivals_evening,
 
-        -- Ex: a stop for 30 days, with route_type_array = [0, 3] for rail and bus. Output here should get the same, not [0, 3, 0, 3, repeated]
-        -- unnest the arrays first then get distinct.
-        ARRAY_AGG(DISTINCT route_type ) AS route_type_array,
-        ARRAY_AGG(DISTINCT {{ parse_route_id('name', 'route_id') }}) AS route_id_array,
-        ARRAY_AGG(DISTINCT
-          CASE
-            WHEN route_type IN (0, 1, 2) THEN "rail"
-            WHEN route_type = 3 THEN "bus"
-            WHEN route_type = 4 THEN "ferry"
-            WHEN route_type IN (5, 6, 7, 12) THEN "other_rail"
-            WHEN route_type = 11 THEN "trolleybus"
-          END
-        ) AS transit_mode_array,
-        COUNT(DISTINCT service_date) AS n_days,
-        COUNT(DISTINCT feed_key) AS n_feeds,
+        SUM(route_type_0) AS total_route_type_0,
+        SUM(route_type_1) AS total_route_type_1,
+        SUM(route_type_2) AS total_route_type_2,
+        SUM(route_type_3) AS total_route_type_3,
+        SUM(route_type_4) AS total_route_type_4,
+        SUM(route_type_5) AS total_route_type_5,
+        SUM(route_type_6) AS total_route_type_6,
+        SUM(route_type_7) AS total_route_type_7,
+        SUM(route_type_11) AS total_route_type_11,
+        SUM(route_type_12) AS total_route_type_12,
+        SUM(missing_route_type) AS total_missing_route_type,
 
-    FROM stops2
-    LEFT JOIN UNNEST(stops2.route_type_array) AS route_type
-    LEFT JOIN UNNEST(stops2.route_id_array) AS route_id
-    GROUP BY name, month_first_day, day_type, stop_id
+        ROUND(AVG(route_type_0), 1) AS daily_route_type_0,
+        ROUND(AVG(route_type_1), 1) AS daily_route_type_1,
+        ROUND(AVG(route_type_2), 1) AS daily_route_type_2,
+        ROUND(AVG(route_type_3), 1) AS daily_route_type_3,
+        ROUND(AVG(route_type_4), 1) AS daily_route_type_4,
+        ROUND(AVG(route_type_5), 1) AS daily_route_type_5,
+        ROUND(AVG(route_type_6), 1) AS daily_route_type_6,
+        ROUND(AVG(route_type_7), 1) AS daily_route_type_7,
+        ROUND(AVG(route_type_11), 1) AS daily_route_type_11,
+        ROUND(AVG(route_type_12), 1) AS daily_route_type_12,
+        ROUND(AVG(missing_route_type), 1) AS daily_missing_route_type,
+
+        -- Ex: a stop for 30 days, with route_type_array = [0, 3] for rail and bus.
+        -- Output here should get the same, not [0, 3, 0, 3, repeated] - DISTINCT applied on final query
+        ARRAY_CONCAT_AGG(route_id_array) AS route_id_array,
+        ARRAY_CONCAT_AGG(route_type_array) AS route_type_array,
+        ARRAY_CONCAT_AGG(transit_mode_array) AS transit_mode_array
+
+    FROM {{ ref('fct_daily_scheduled_stops') }} AS stops
+    INNER JOIN {{ ref('fct_daily_schedule_feeds') }} AS feeds
+       ON feeds.feed_key = stops.feed_key
+      AND feeds.date = DATE_TRUNC(service_date, MONTH)
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
 ),
 
-most_common_stop_key AS (
+fct_monthly_scheduled_stops AS (
     SELECT
-        name,
-        month_first_day,
-        day_type,
-        stop_key,
-        SUM(daily_arrivals) AS max_stop_arrivals
+        monthly_stops.* EXCEPT(route_type_array, route_id_array, transit_mode_array),
 
-    FROM stops2
-    GROUP BY name, month_first_day, day_type, stop_id, stop_key
-    -- dedupe and keep the stop_key with the most arrivals
-    -- if multiple stop_keys are found for a gtfs_dataset_name-month_first_day_stop_id
-    -- combination (different feed_keys or gtfs_dataset_keys)
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY
-                               name, month_first_day, day_type, stop_id
-                               ORDER BY max_stop_arrivals DESC) = 1
-),
-
-fct_monthly_stops AS (
-    SELECT
-        monthly_stop_counts.name,
-        monthly_stop_counts.month_first_day,
-        EXTRACT(YEAR FROM monthly_stop_counts.month_first_day) AS year,
-        EXTRACT(MONTH FROM monthly_stop_counts.month_first_day) AS month,
-        monthly_stop_counts.* EXCEPT(name, month_first_day),
-        ARRAY_LENGTH(monthly_stop_counts.route_type_array) AS n_route_types,
-
-        most_common_stop_key.stop_key,
+        ARRAY(SELECT DISTINCT route_type FROM UNNEST(monthly_stops.route_type_array) AS route_type ORDER BY 1) AS route_type_array,
+        ARRAY(SELECT DISTINCT route_id FROM UNNEST(monthly_stops.route_id_array) AS route_id ORDER BY 1) AS route_id_array,
+        ARRAY(SELECT DISTINCT transit_mode FROM UNNEST(monthly_stops.transit_mode_array) AS transit_mode ORDER BY 1) AS transit_mode_array,
 
         dim_stops.tts_stop_name,
         dim_stops.pt_geom,
@@ -138,16 +87,84 @@ fct_monthly_stops AS (
         dim_stops.stop_name,
         dim_stops.stop_desc,
         dim_stops.location_type,
-        dim_stops.wheelchair_boarding,
+        dim_stops.wheelchair_boarding
 
-    FROM most_common_stop_key
-    INNER JOIN dim_stops
-        ON most_common_stop_key.stop_key = dim_stops.key
-    INNER JOIN monthly_stop_counts
-        ON monthly_stop_counts.name = most_common_stop_key.name
-        AND monthly_stop_counts.month_first_day = most_common_stop_key.month_first_day
-        AND monthly_stop_counts.day_type = most_common_stop_key.day_type
-        AND monthly_stop_counts.stop_id = dim_stops.stop_id
+    FROM monthly_stops
+    INNER JOIN {{ ref('dim_stops') }} AS dim_stops
+       ON monthly_stops.stop_key = dim_stops.key
 )
 
-SELECT * FROM fct_monthly_stops
+SELECT
+    name,
+
+    month_first_day,
+    `year`,
+    `month`,
+    day_type,
+
+    stop_id,
+
+    total_stop_arrivals,
+    daily_stop_arrivals,
+
+    total_n_hours_in_service,
+    daily_n_hours_in_service,
+
+    daily_arrivals_per_hour_owl,
+    daily_arrivals_per_hour_early_am,
+    daily_arrivals_per_hour_am_peak,
+    daily_arrivals_per_hour_midday,
+    daily_arrivals_per_hour_pm_peak,
+    daily_arrivals_per_hour_evening,
+
+    total_arrivals_owl,
+    total_arrivals_early_am,
+    total_arrivals_am_peak,
+    total_arrivals_midday,
+    total_arrivals_pm_peak,
+    total_arrivals_evening,
+
+    total_route_type_0,
+    total_route_type_1,
+    total_route_type_2,
+    total_route_type_3,
+    total_route_type_4,
+    total_route_type_5,
+    total_route_type_6,
+    total_route_type_7,
+    total_route_type_11,
+    total_route_type_12,
+    total_missing_route_type,
+
+    daily_route_type_0,
+    daily_route_type_1,
+    daily_route_type_2,
+    daily_route_type_3,
+    daily_route_type_4,
+    daily_route_type_5,
+    daily_route_type_6,
+    daily_route_type_7,
+    daily_route_type_11,
+    daily_route_type_12,
+    daily_missing_route_type,
+
+    route_type_array,
+    route_id_array,
+    transit_mode_array,
+
+    ARRAY_LENGTH(route_type_array) AS n_route_types,
+
+    n_days,
+    n_feeds,
+
+    stop_key,
+    tts_stop_name,
+    pt_geom,
+    parent_station,
+    stop_code,
+    stop_name,
+    stop_desc,
+    location_type,
+    wheelchair_boarding
+
+FROM fct_monthly_scheduled_stops
